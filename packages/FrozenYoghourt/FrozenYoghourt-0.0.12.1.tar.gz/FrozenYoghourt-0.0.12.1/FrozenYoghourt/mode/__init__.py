4293ebd38959286414701154577cde95f14026fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package FrozenYoghourt/0.0.12.1/FrozenYoghourt-0.0.12.1.tar.gz

---
+++ packages/FrozenYoghourt/FrozenYoghourt-0.0.12.1.tar.gz/FrozenYoghourt-0.0.12.1/FrozenYoghourt/mode/__init__.py
@@ -0,0 +1,24 @@
+from FrozenYoghourt import *
+    
+
+class Mode:
+    
+    representation = 'numpy'
+    
+    @classmethod
+    def toggle(cls, specific_mode = None):
+
+        if specific_mode is None:
+            cls.representation = {'numpy': 'sympy',
+                                  'sympy': 'numpy'}[cls.representation]
+            print(f'\033[1;34m{cls.representation.upper()} \033[1;32mactivated')
+        else:
+            cls.representation = {'n': 'numpy', 
+                                  'numpy': 'numpy', 
+                                  's': 'sympy', 
+                                  'sympy':'sympy'}[specific_mode]
+            print(f'\033[1;34m{cls.representation.upper()} \033[1;32mactivated')
+            
+    @classmethod
+    def now(cls):
+        print(f'\033[1;34m{cls.representation.upper()} \033[1;30mis\033[1;30m \033[1;32mon')
