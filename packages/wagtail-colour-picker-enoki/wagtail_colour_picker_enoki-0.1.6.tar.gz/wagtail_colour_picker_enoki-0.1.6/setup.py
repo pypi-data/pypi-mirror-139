e9671389777f0792befa2e5a5206413c5ca49bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:55 +0000

    Add package wagtail-colour-picker-enoki/0.1.6/wagtail_colour_picker_enoki-0.1.6.tar.gz

---
+++ packages/wagtail-colour-picker-enoki/wagtail_colour_picker_enoki-0.1.6.tar.gz/wagtail_colour_picker_enoki-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name='wagtail_colour_picker_enoki',
+    version='0.1.6',
+    author='Enoki-Studio',
+    author_email='theo@enoki-studio.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/wagtail_colour_picker_enoki/',
+    license='LICENSE.txt',
+    description='WagtailColorPickerByEnoki',
+    long_description='fix bugs',
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=[
+        "Wagtail >= 2.12.2",
+        "Django >= 2.2.13",
+        "pytest",
+    ],
+    keywords=['wagtail', 'draftjs', 'colour', 'picker', 'accent', 'design'],
+)
