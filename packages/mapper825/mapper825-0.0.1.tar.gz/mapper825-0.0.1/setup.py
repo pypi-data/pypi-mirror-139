eb6493644a02f889c782825a66e5c913be41145b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:59 +0000

    Add package mapper825/0.0.1/mapper825-0.0.1.tar.gz

---
+++ packages/mapper825/mapper825-0.0.1.tar.gz/mapper825-0.0.1/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(
+  name = 'mapper825',         # How you named your package folder (MyLib)
+  packages = ['mapper825'],   # Chose the same as "name"
+  version = '0.0.1'   # Start with a small number and increase it with every change you make
+)
