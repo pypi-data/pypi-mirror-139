9827e8e6b8ea25262616efe83e85d097398c46f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package galeodes/0.7/galeodes-0.7.tar.gz

---
+++ packages/galeodes/galeodes-0.7.tar.gz/galeodes-0.7/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='galeodes',
+    author='QeeqBox',
+    author_email='gigaqeeq@gmail.com',
+    description="Browsers options",
+    long_description=long_description,
+    version='0.7',
+    license='AGPL-3.0',
+    url='https://github.com/qeeqbox/galeodes',
+    packages=['galeodes'],
+    install_requires=[
+        'selenium',
+        'requests',
+        'Pillow'
+    ],
+    python_requires='>=3.5',
+    entry_points={
+        "console_scripts": [
+            'galeodes=galeodes.__main__:main_logic'
+        ]
+    }
+)
+
