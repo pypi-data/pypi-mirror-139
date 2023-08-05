fbbd78c6e0a7c7e500f5213d75a888929fb765d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:07 +0000

    Add package egttools/0.1.9.dev6/egttools-0.1.9.dev6-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/egttools/egttools-0.1.9.dev6-cp39-cp39-macosx_10_15_x86_64.whl/egttools/games/__init__.py
@@ -0,0 +1,8 @@
+"""
+API reference documentation for the `games` submodule.
+"""
+
+from egttools.numerical.games import AbstractGame, NormalFormGame, CRDGame, CRDGameTU, OneShotCRD
+
+from .pgg import PGG
+from .informal_risk import InformalRiskGame
