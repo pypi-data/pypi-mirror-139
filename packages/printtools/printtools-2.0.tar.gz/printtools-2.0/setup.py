99f426b7bc13977fcc3f5be19e5a89505c624274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:22 +0000

    Add package printtools/2.0/printtools-2.0.tar.gz

---
+++ packages/printtools/printtools-2.0.tar.gz/printtools-2.0/setup.py
@@ -0,0 +1,28 @@
+"""printtools setup script."""
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='printtools',
+    version='2.0',
+    packages=['printtools'],
+    license='MIT',
+    install_requires='pyfiglet',
+    author='Tianshu Huang',
+    author_email='tianshu@cmu.edu',
+    description=(
+        'Collection of print utilities making heavy use of ANSI '
+        'escape sequences with PyFiglet integration'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thetianshuhuang/print',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
