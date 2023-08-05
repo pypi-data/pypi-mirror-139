85408f3b4a7b6e13ce9493b7bcd9b842912690de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:34 +0000

    Add package OpenFisca-France-Fiscalite-Miniere/2.0.2/OpenFisca_France_Fiscalite_Miniere-2.0.2-py3-none-any.whl

---
+++ packages/OpenFisca-France-Fiscalite-Miniere/OpenFisca_France_Fiscalite_Miniere-2.0.2-py3-none-any.whl/openfisca_france_fiscalite_miniere/entities.py
@@ -0,0 +1,32 @@
+from openfisca_core.entities import build_entity
+
+
+Societe = build_entity(
+    key = "societe",
+    plural = "societes",
+    label = 'Société',
+    is_person = True,  # = est entité pivot
+    doc = '''
+        Société = titre minier à ce stade.
+        Un titre peut aussi être sur plusieurs communes.
+        L'une des communes du titre est le lieu principal d'exploitation.
+        Il y a une substance extraite par titre.
+    '''
+    )
+
+
+Commune = build_entity(
+    # suppose qu'il n'y a qu'une commune par société/titre ?
+    key = "commune",
+    plural = "communes",
+    label = 'Commune',
+    roles = [
+        {
+            'key': 'societe',
+            'plural': 'societes',
+            'label': 'Sociétés'
+            }
+        ]
+    )
+
+entities = [Societe, Commune]
