6c09fa8f6db6e932b7a86d33fa4bb5774c627535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package bsb/4.0.0a4/bsb-4.0.0a4-py3-none-any.whl

---
+++ packages/bsb/bsb-4.0.0a4-py3-none-any.whl/bsb/cli/commands/_projects.py
@@ -0,0 +1,63 @@
+from . import BaseCommand
+from ...option import BsbOption
+from ...reporting import report
+from ... import config
+import pathlib
+import toml
+
+
+class ProjectNewCommand(BaseCommand, name="new"):
+    def get_options(self):
+        return {}
+
+    def add_parser_arguments(self, parser):
+        parser.add_argument("project_name", nargs="?", help="Project name", default="")
+        parser.add_argument(
+            "path", nargs="?", default=".", help="Location of the project"
+        )
+
+    def handler(self, context):
+        name = (
+            context.arguments.project_name
+            or input("Project name [my_model]: ")
+            or "my_model"
+        )
+        root = pathlib.Path(context.arguments.path) / name
+        try:
+            root.mkdir(exist_ok=False)
+        except FileExistsError:
+            return report(
+                f"Could not create '{root.absolute()}', directory exists.", level=0
+            )
+
+        (root / name).mkdir()
+        template = input("Config template [template.json]: ") or "template.json"
+        config.copy_template(template, output=root / "network_configuration.json")
+        with open(root / "pyproject.toml", "w") as f:
+            toml.dump(
+                {
+                    "tools": {
+                        "bsb": {
+                            "config": "network_configuration.json",
+                            "morpho": "morphologies.h5",
+                            "networks": {
+                                "config_link": [
+                                    "sys",
+                                    "network_configuration.json",
+                                    "always",
+                                ],
+                                "morpho_link": ["sys", "morphologies.h5", "changes"],
+                            },
+                        }
+                    }
+                },
+                f,
+            )
+        with open(root / name / "__init__.py", "w") as f:
+            f.write("\n")
+        with open(root / name / "placement.py", "w") as f:
+            f.write("from bsb.placement import PlacementStrategy\n")
+        with open(root / name / "connectome.py", "w") as f:
+            f.write("from bsb.connectivity import ConnectionStrategy\n")
+
+        report(f"Created '{name}' project structure.")
