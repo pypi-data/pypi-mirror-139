b13be5ecedb4620007d105a7df1beb31bfa174a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:47 +0000

    Add package tendrils/0.0.4/tendrils-0.0.4.tar.gz

---
+++ packages/tendrils/tendrils-0.0.4.tar.gz/tendrils-0.0.4/README.md
@@ -0,0 +1,2 @@
+# tendrils
+Cosmic tendrils is the API for both FLOWS pipeline and project. 
---
+++ packages/tendrils/tendrils-0.0.4.tar.gz/tendrils-0.0.4/tendrils/version.py
@@ -0,0 +1,9 @@
+"""
+Version
+"""
+
+major = 0
+minor = 0
+bugfix = 4
+version_info = (major, minor, bugfix)
+version = f"{major}.{minor}.{bugfix}"
