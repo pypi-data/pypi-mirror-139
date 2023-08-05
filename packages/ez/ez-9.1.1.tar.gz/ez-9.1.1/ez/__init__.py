e1a7bb7fad60752cb3e34be5096cd2d88d1b624c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:17 +0000

    Add package ez/9.1.1/ez-9.1.1.tar.gz

---
+++ packages/ez/ez-9.1.1.tar.gz/ez-9.1.1/ez/__init__.py
@@ -0,0 +1,8 @@
+from . ez import *
+from . firefox import *
+
+__all__=[
+'Firefox', 'BY', 'KEYS',
+'JDict', 'TimeStamp', 'Moment', 
+'fire', 'requests', 'sqlite3', 'json', 'io', 'os', 'sys', 'platform', 'string', 'random', 're', 'datetime', 'tzlocal', 'pd', 'np', 'urlparse', 'BeautifulSoup', 
+]
