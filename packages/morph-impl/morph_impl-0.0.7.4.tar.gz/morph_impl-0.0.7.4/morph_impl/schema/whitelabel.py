af7cd5867e5a0314e8c78337a47a6dc31baf41e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:38 +0000

    Add package morph-impl/0.0.7.4/morph_impl-0.0.7.4.tar.gz

---
+++ packages/morph-impl/morph_impl-0.0.7.4.tar.gz/morph_impl-0.0.7.4/morph_impl/schema/whitelabel.py
@@ -0,0 +1,30 @@
+{
+    'morpheusComponent': {
+        'type': 'string',
+        'required': True
+    },
+    'enabled': {
+        'type': 'string',
+        'required': True
+    },
+    'applianceName': {
+        'type': 'string',
+        'required': True
+    },
+    'headerBgColor': {
+        'type': 'string',
+        'required': True
+    },
+    'footerBgColor': {
+        'type': 'string',
+        'required': True
+    },
+    'loginBgColor': {
+        'type': 'string',
+        'required': True
+    },
+    'disableSupportMenu': {
+        'type': 'string',
+        'required': True
+    }
+}
