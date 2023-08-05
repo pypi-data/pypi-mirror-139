c8b18631d16b6f14d837e8256f28c322b71eed38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:07 +0000

    Add package tretikoffhw1/0.0.1/tretikoffhw1-0.0.1.tar.gz

---
+++ packages/tretikoffhw1/tretikoffhw1-0.0.1.tar.gz/tretikoffhw1-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tretikoffhw1',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Konstantin.Tretiakov',
+    author_email='pukoh@mail.ru',
+    description=''
+)
