f9c74c5edd9a36eba32a7b605d2dfcbed7d58a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:18 +0000

    Add package shakhs/0.1.5/shakhs-0.1.5.tar.gz

---
+++ packages/shakhs/shakhs-0.1.5.tar.gz/shakhs-0.1.5/shakhs/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup , find_packages
+
+classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python'
+]
+
+
+
+
+
+
+
+setup(name='shakhs',
+      version='0.1.5',
+      description='this package generate personal details of fake iranian person',
+      long_description_content_type='text/markdown',
+      long_description=open('README.txt').read()+'\n\n'+open('CHANGELOG.txt').read(),
+      author='samrand majnooni',
+      author_email='sammajnoni@gamil.com',
+      url='https://pypi.org/project/shakhs/',
+      data_files=['data/code-city.json','lastname.txt','nameboy.txt','namegirl.txt'],
+      include_package_data=True,
+      license='MIT',
+      keywords=['fake', 'persian','name','person','id','lastname'],
+      packages=find_packages(),
+      classifiers=classifiers,
+      install_requiers=[''], 
+      project_urls = {
+          'HomePage': 'https://github.com/samrand-majnooni/IR-fake-person-generator'}
+     )
