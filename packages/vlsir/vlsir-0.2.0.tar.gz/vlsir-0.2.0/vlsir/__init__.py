a2da3ea3bc75311c8902e1557a2e83316245a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:15 +0000

    Add package vlsir/0.2.0/vlsir-0.2.0.tar.gz

---
+++ packages/vlsir/vlsir-0.2.0.tar.gz/vlsir-0.2.0/vlsir/__init__.py
@@ -0,0 +1,15 @@
+"""
+VLSIR Python Bindings 
+"""
+
+__version__ = "0.2.0"
+
+# Schema 
+from . import utils_pb2 as utils
+from . import spice_pb2 as spice
+from . import circuit_pb2 as circuit
+from . import raw_pb2 as raw
+from . import tetris_pb2 as tetris
+
+# Primitives
+from . import primitives 
