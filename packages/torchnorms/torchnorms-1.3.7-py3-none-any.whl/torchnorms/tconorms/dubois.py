e274ec191fc4bb3459d6a38ed24d626194ab6ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:05 +0000

    Add package torchnorms/1.3.7/torchnorms-1.3.7-py3-none-any.whl

---
+++ packages/torchnorms/torchnorms-1.3.7-py3-none-any.whl/torchnorms/tconorms/dubois.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn, Tensor
+
+from torchnorms.tconorms.base import BaseTCoNorm
+
+from typing import Optional
+
+
+class DuboisTCoNorm(BaseTCoNorm):
+    def __init__(self,
+                 p: Optional[Tensor] = None,
+                 default_p: float = 0.1) -> None:
+        super().__init__()
+        self.p = p
+        if self.p is None:
+            assert default_p >= -1.0
+
+            self.p = nn.Parameter(torch.tensor(default_p))
+        else:
+            assert self.p >= -1.0
+
+        assert len(self.p.shape) == 0
+
+    def __call__(self,
+                 a: Tensor,
+                 b: Tensor) -> Tensor:
+            self.p.data = torch.clamp(self.p.data, min=0, max=1)
+
+            p_1 = a + b
+            p_2 = p_1 + (self.p * a * b)
+            res = torch.minimum(p_2, torch.tensor(1))
+
+            return res
