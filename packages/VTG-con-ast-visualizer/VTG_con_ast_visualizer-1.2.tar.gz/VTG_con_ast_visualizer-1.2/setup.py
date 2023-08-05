3efeeb5e91e503318b51bdefd723b75a53ff4459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:18 +0000

    Add package VTG-con-ast-visualizer/1.2/VTG_con_ast_visualizer-1.2.tar.gz

---
+++ packages/VTG-con-ast-visualizer/VTG_con_ast_visualizer-1.2.tar.gz/VTG_con_ast_visualizer-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name= 'VTG_con_ast_visualizer',
+    version='1.2',
+    packages=["."],
+    long_description="Library to build ast for fibonacci function",
+)
