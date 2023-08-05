f4573cfb8ff6d0396e4a84da49e6fe9e5bc8bce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:07 +0000

    Add package edu-py-logger/0.7/edu-py-logger-0.7.tar.gz

---
+++ packages/edu-py-logger/edu-py-logger-0.7.tar.gz/edu-py-logger-0.7/edu_py_logger/utils.py
@@ -0,0 +1,63 @@
+from typing import Dict, Union
+
+from fastapi import Body, Request
+from pydantic import BaseModel
+
+
+def get_request_extra_data(
+    request: Request, action_context: Dict = Body(None, alias="actionContext")
+):
+    return {
+        **(action_context or {}),
+        "trace_id": request.state.trace_id,
+    }
+
+
+class PropertyBaseModel(BaseModel):
+    """
+    Workaround for serializing properties with pydantic until
+    https://github.com/samuelcolvin/pydantic/issues/935
+    is solved
+    """
+
+    @classmethod
+    def get_properties(cls):
+        return [
+            prop
+            for prop in dir(cls)
+            if isinstance(getattr(cls, prop), property)
+            and prop not in ("__values__", "fields")
+        ]
+
+    def dict(
+        self,
+        *,
+        include: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        exclude: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        by_alias: bool = False,
+        skip_defaults: bool = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> "DictStrAny":
+        attribs = super().dict(
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            skip_defaults=skip_defaults,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+        )
+        props = self.get_properties()
+        # Include and exclude properties
+        if include:
+            props = [prop for prop in props if prop in include]
+        if exclude:
+            props = [prop for prop in props if prop not in exclude]
+
+        # Update the attribute dict with the properties
+        if props:
+            attribs.update({prop: getattr(self, prop) for prop in props})
+
+        return attribs
