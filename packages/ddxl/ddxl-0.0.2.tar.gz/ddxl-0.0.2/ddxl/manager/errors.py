b5baa4d5f044fbc09e76f621ee88511fbdc2c8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:09 +0000

    Add package ddxl/0.0.2/ddxl-0.0.2.tar.gz

---
+++ packages/ddxl/ddxl-0.0.2.tar.gz/ddxl-0.0.2/ddxl/manager/errors.py
@@ -0,0 +1,12 @@
+class StatusError(Exception):
+    """Bot Status Error"""
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class ContainerNameError(Exception):
+    """Bot Status Error"""
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
---
+++ packages/ddxl/ddxl-0.0.2.tar.gz/ddxl-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='ddxl',
+      version='0.0.2',
+      description='DDXL',
+      author='LcsH0s',
+      author_email='decastro@et.esiea.fr',
+      url='https://github.com/LcsH0s/ddxl',
+      long_description=long_description,
+      classifiers=["Development Status :: 2 - Pre-Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: Unix",
+                   "Programming Language :: Python :: 3.9",
+                   ],
+      packages=find_packages(exclude=["tests.*", "tests"]),
+      install_requires=[
+          'docker',
+      ],
+      )
