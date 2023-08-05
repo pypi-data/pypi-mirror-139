c965381fcf878d7119893d11303c3f49c0b46b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:40 +0000

    Add package bitcoin-value/1.5.1/bitcoin_value-1.5.1.tar.gz

---
+++ packages/bitcoin-value/bitcoin_value-1.5.1.tar.gz/bitcoin_value-1.5.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+    name = "bitcoin_value",
+    version = "1.5.1",
+    author = "Thomas Dewitte",
+    author_email = "thomasdewittecontact@gmail.com",
+    license = "MIT",
+    url = "https://github.com/dewittethomas/bitcoin-value",
+    
+    description = "A tracker that gets the latest value of Bitcoin in any currency",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+
+    package_dir = {"bitcoin_value": "bitcoin_value"},
+    install_requires = [
+        "requests>=2.22.0"
+    ],
+
+    packages = find_packages(),
+
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+
+    keywords = "bitcoin currency value worth btc usd eur gbp crypto fetch"
+)
