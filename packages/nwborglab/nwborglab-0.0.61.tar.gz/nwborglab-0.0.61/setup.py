0b264c752dc895ef27e95cb7e6064fc3eaac7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:55 +0000

    Add package nwborglab/0.0.61/nwborglab-0.0.61.tar.gz

---
+++ packages/nwborglab/nwborglab-0.0.61.tar.gz/nwborglab-0.0.61/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nwborglab",
+    version="0.0.61",
+    scripts=['scripts/nwborglab', 'scripts/nwborglabcommand'],
+    author="Shane Allcroft",
+    author_email="shaneallcroft@fedoraproject.org",
+    description="Parse, compose, evaluate and execute org files and data in org-down notation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaneallcroft/nwborglab",
+    package_data={'': ['src/nwborglab/.nwborglabsrc.org']},
+    include_package_data=True,
+    project_urls={
+        "Bug Tracker": "https://github.com/shaneallcroft/nwborglab/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/nwborglab/nwborglab-0.0.61.tar.gz/nwborglab-0.0.61/src/nwborglab/subjectHandler.py
@@ -0,0 +1,82 @@
+import pynwb
+import os
+from orgutils import orgutils
+from datetime import datetime as dt
+import SAPOFTO
+
+
+def createSubject(args, subject_id=None):
+    # survey user about subject information
+    #dotnwborglab = SAPOFTO.SAPOFTO(key='dotnwborglab', filename='.nwborglab.org')
+
+    #survey_node = dotnwborglab['SURVEYS']['SUBJECT CREATE']
+
+    #for question_key in survey_node.key():
+        
+
+    
+    if not os.path.isfile('.nwborglab.org'):
+        print('NWBORG ERROR: not in the uppermost folder of an nwborg project.')
+        return
+
+
+    
+    print('Create Subject!')
+    if subject_id == None:
+        subject_id = input('Please input the subject\'s ID, or press enter to autogenerate a new ID')
+    if subject_id == '':
+        subject_files = os.listdir('subjects')
+        largest_id = 0
+        for subject_file in subject_files:
+            subject_num = int(subject_file)
+            if subject_num > largest_id:
+                largest_id = subject_num
+        new_id = largest_id + 1
+        preceding_zeroes = ''
+        if new_id < 1000:
+            preceding_zeroes += '0'
+        if new_id < 100:
+            preceding_zeroes += '0'
+        if new_id < 10:
+            preceding_zeroes += '0'
+        subject_id = preceding_zeroes + str(new_id)
+    
+    print('Proceeding with id ' + subject_id + ' for subject creation...')
+    
+    subject_dict = dict()
+    subject_dict['Subject ID'] = subject_id
+    subject_dict['Species'] = input('Please enter subject species...')
+    subject_dict['Age'] = input('Please enter subject age in any desired units (ex. "23 years")...')
+    subject_dict['Sex'] = input('Please enter subject sex M for male F for female O for other...')
+    subject_dict['Date of Birth'] = input('Please input the subject\'s date of birth (ex. 08/11/1998)...')
+    subject_dict['Description'] = input('Please provide additional description for the subject...')
+    os.mkdir('subjects/' + subject_id)
+    orgutils.dictToOrg(org_data=subject_dict,output_filename='subjects/' + subject_id + '/' + subject_id + '.org')
+    print('subject info saved to subjects/' + subject_id + '.org')
+    
+
+
+
+def subjectIdToNwbSubjectObject(subject_id):
+    subject_node = SAPOFTO.SAPOFTO(os.path.join('Subjects',str(subject_id),str(subject_id),'.org'))
+
+    subject_nwb = pynwb.file.Subject()
+    if 'AGE' in subject_node.keys():
+        subject_nwb.age = subject_node['AGE'].getValue()
+    if 'DESCRIPTION' in subject_node.keys():
+        subject_nwb.description = subject_node['DESCRIPTION'].getValue()
+    if 'GENOTYPE' in subject_node.keys():
+        subject_nwb.genotype = subject_node['GENOTYPE'].getValue()
+    if 'SEX' in subject_node.keys():
+        subject_nwb.sex = subject_node['SEX'].getValue()
+    if 'SPECIES' in subject_node.keys():
+        subject_nwb.species = subject_node['SPECIES'].getValue()
+    if 'SUBJECT_ID' in subject_node.keys():
+        subject_nwb.subject_id = subject_node['SUBJECT_ID'].getValue()
+    if 'WEIGHT' in subject_node.keys():
+        subject_nwb.weight = subject_node['WEIGHT'].getValue()
+    if 'STRAIN' in subject_node.keys():
+        subject_nwb.strain = subject_node['STRAIN'].getValue()
+    if 'DATE_OF_BIRTH' in subject_node.keys():
+        subject_nwb.date_of_birth = subject_node['DATE_OF_BIRTH'].getValue()
+    return subject_nwb
