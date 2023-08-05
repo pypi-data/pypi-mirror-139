69dc2bcdf1732efbe5448145b707f85497718f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:46 +0000

    Add package SBMLDiagrams/0.1.9/SBMLDiagrams-0.1.9-py3-none-any.whl

---
+++ packages/SBMLDiagrams/SBMLDiagrams-0.1.9-py3-none-any.whl/SBMLDiagrams/styleSBML.py
@@ -0,0 +1,92 @@
+"""
+@author: Jessie Jiang
+"""
+class Style:
+    def __init__(self, styleName):
+        if styleName == 'default':
+            # for compartment's color
+            self.comp_fill_color = (255, 255, 255, 255)
+            self.comp_border_color = (255, 255, 255, 255)
+
+            # for node's color
+            self.spec_fill_color = (255, 204, 153, 200)
+            self.spec_border_color = (255, 108, 9, 255)
+            self.reaction_line_color = (91, 176, 253, 50)
+            self.text_line_color = (0, 0, 0, 255)
+
+            # for node's dimension
+            self.node_dimension = [40,60]
+            self.image_size = [1000,1000]
+
+            # for progress bar's color
+            self.process_fill_color = (255, 108, 9, 200)
+            self.full_fill_color = (91, 176, 253, 200)
+            self.process_border_color = (255, 204, 153, 200)
+
+        elif styleName == "simplicity":
+            self.comp_fill_color = (0, 0, 0, 0)
+            self.comp_border_color = (0, 0, 0, 255)
+            self.spec_fill_color = (0, 0, 0, 0)
+            self.spec_border_color = (0, 0, 0, 255)
+            self.reaction_line_color = (0, 0, 0, 200)
+            self.text_line_color = (0, 0, 0, 255)
+
+    def getImageSize(self):
+        return self.image_size
+
+    def setImageSize(self, imageSize):
+        self.image_size = imageSize
+
+    def getNodeDimension(self):
+        return self.node_dimension
+
+    def setNodeDimension(self, dimension):
+        self.node_dimension = dimension
+
+    def getProcessFillColor(self):
+        return self.process_fill_color
+
+    def getProcessBorderColor(self):
+        return self.process_border_color
+
+    def getFullFillColor(self):
+        return self.full_fill_color
+
+    def getCompFillColor(self):
+        return self.comp_fill_color
+        
+    def getCompBorderColor(self):
+        return self.comp_border_color
+        
+    def getSpecFillColor(self):
+        return self.spec_fill_color
+        
+    def getSpecBorderColor(self):
+        return self.spec_border_color
+        
+    def getReactionLineColor(self):
+        return self.reaction_line_color
+        
+    def getTextLineColor(self):
+        return self.text_line_color
+
+    def setCompFillColor(self, color):
+        self.comp_fill_color = color
+
+    def setCompBorderColor(self, color):
+        self.comp_border_color = color
+
+    def setSpecFillColor(self, color):
+        self.spec_fill_color = color
+
+    def setSpecBorderColor(self, color):
+        self.spec_border_color = color
+
+    def setReactionLineColor(self, color):
+        self.reaction_line_color = color
+
+    def setTextLineColor(self, color):
+        self.text_line_color = color
+
+
+
