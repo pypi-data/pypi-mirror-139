745d869e25d36527c6ea388a00954ebb5f60dbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:26 +0000

    Add package jetpack-io/0.5.1.dev202202161645069062/jetpack-io-0.5.1.dev202202161645069062.tar.gz

---
+++ packages/jetpack-io/jetpack-io-0.5.1.dev202202161645069062.tar.gz/jetpack-io-0.5.1.dev202202161645069062/src/jetpack/_task/task.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Optional
+import uuid
+
+# Prevent circular dependency
+if TYPE_CHECKING:
+    from jetpack._task.jetpack_function_with_client import JetpackFunctionWithClient
+
+
+class Task:
+    def __init__(
+        self,
+        jetpack_function: JetpackFunctionWithClient[Any],
+        target_time: int,
+        is_scheduled: bool,
+    ):
+        self.jetpack_function = jetpack_function
+        self.target_time = target_time
+        self.id: Optional[uuid.UUID] = None
+        self.is_scheduled = is_scheduled
+
+    def symbol_name(self) -> str:
+        return self.jetpack_function.name()
+
+    async def cancel(self) -> bool:
+        return await self.jetpack_function.client.cancel_task(str(self.id))
