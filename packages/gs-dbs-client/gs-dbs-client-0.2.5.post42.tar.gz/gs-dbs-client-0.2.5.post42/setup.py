fd0e047626c82eb7ba42c71a55a1cabdbfd927e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:59 +0000

    Add package gs-dbs-client/0.2.5.post42/gs-dbs-client-0.2.5.post42.tar.gz

---
+++ packages/gs-dbs-client/gs-dbs-client-0.2.5.post42.tar.gz/gs-dbs-client-0.2.5.post42/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="gs-dbs-client",
+    version="0.2.5-42",
+    description="python wrapper for the Glass Sphere DBS",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://glass-sphere-ai.de",
+    author="Glass Sphere Software",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["gsdbs"],
+    include_package_data=True,
+    install_requires=["gql==3.0.0a5",
+                      "pandas",
+                      "PyYAML",
+                      "requests",
+                      "aiohttp",
+                      "stomper==0.4.3",
+                      "twine==3.4.1",
+                      "graphql-core==3.1.5",
+                      "Pillow==8.2.0",
+                      "websocket-client"
+                      ],
+    entry_points={
+        "console_scripts": [
+            "realpython=gsdbs.__main__:main",
+        ]
+    },
+)
