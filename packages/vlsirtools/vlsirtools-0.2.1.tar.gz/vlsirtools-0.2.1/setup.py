a4c0a5de7817cff3d28adcce1952d6b681f48ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:41 +0000

    Add package vlsirtools/0.2.1/vlsirtools-0.2.1.tar.gz

---
+++ packages/vlsirtools/vlsirtools-0.2.1.tar.gz/vlsirtools-0.2.1/setup.py
@@ -0,0 +1,32 @@
+"""
+# VlsirTools Setup Script
+
+Derived from the setuptools sample project at
+https://github.com/pypa/sampleproject/blob/main/setup.py
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "readme.md").read_text(encoding="utf-8")
+
+setup(
+    name="vlsirtools",
+    version="0.2.1",
+    description="Tools for the Vlsir IC Design Schema",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dan-fritchman/Vlsir",
+    author="Dan Fritchman",
+    packages=find_packages(),
+    python_requires=">=3.7, <4",
+    install_requires=["vlsir==0.2.1", "numpy==1.21.5"],
+    extras_require={
+        "dev": ["pytest==5.2", "coverage", "pytest-cov", "black==19.10b0", "twine"]
+    },
+)
---
+++ packages/vlsirtools/vlsirtools-0.2.1.tar.gz/vlsirtools-0.2.1/vlsirtools/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Vlsir Tools
+"""
+
+__version__ = "0.2.1"
+
+from .netlist import netlist
+from . import spice
+from . import xyce
+from . import spectre
+
