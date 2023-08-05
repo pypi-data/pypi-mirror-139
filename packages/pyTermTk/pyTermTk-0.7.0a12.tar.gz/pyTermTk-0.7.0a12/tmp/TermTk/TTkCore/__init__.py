672e438a6f2c03690e7eab01cd4888f46d2795c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:05 +0000

    Add package pyTermTk/0.7.0a12/pyTermTk-0.7.0a12.tar.gz

---
+++ packages/pyTermTk/pyTermTk-0.7.0a12.tar.gz/pyTermTk-0.7.0a12/tmp/TermTk/TTkCore/__init__.py
@@ -0,0 +1,8 @@
+from .log import *
+from .cfg import *
+from .signal import pyTTkSlot, pyTTkSignal
+from .ttk import *
+from .color import *
+from .string import *
+from .timer import *
+from .filebuffer import *
