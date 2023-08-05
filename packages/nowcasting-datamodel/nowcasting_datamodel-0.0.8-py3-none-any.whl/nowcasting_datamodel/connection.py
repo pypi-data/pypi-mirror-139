e7de195746c8a41d62adfdc80415b4312cfa88db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:06 +0000

    Add package nowcasting-datamodel/0.0.8/nowcasting_datamodel-0.0.8-py3-none-any.whl

---
+++ packages/nowcasting-datamodel/nowcasting_datamodel-0.0.8-py3-none-any.whl/nowcasting_datamodel/connection.py
@@ -0,0 +1,40 @@
+""" Database Connection class"""
+import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm.session import Session
+
+Base_Forecast = declarative_base()
+Base_PV = declarative_base()
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseConnection:
+    """Database connection class"""
+
+    def __init__(self, url, base=Base_Forecast, echo: bool = True):
+        """
+        Set up database connection
+
+        url: the database url, used for connecting
+        """
+        self.url = url
+
+        self.engine = create_engine(self.url, echo=echo)
+
+        # quick and easy way to make sure the database table names are made.
+        # This should be moved, or done separate from here.
+        try:
+            base.metadata.create_all(self.engine)
+        except Exception as e:
+            logger.debug(
+                f'Try to run "create all" on database, but failed. ' f"Will pass this anyway {e}"
+            )
+
+        self.Session = sessionmaker(bind=self.engine)
+
+    def get_session(self) -> Session:
+        """Get sqlalamcy session"""
+        return self.Session()
