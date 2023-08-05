13522e8ee39159ce1af959c57d49ccd1196f274a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:47 +0000

    Add package autocrosswalk/0.0.1/autocrosswalk-0.0.1-py3-none-any.whl

---
+++ packages/autocrosswalk/autocrosswalk-0.0.1-py3-none-any.whl/autocrosswalk/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__init__
+"""
+from autocrosswalk import (data,matching,main,utils)
+__all__ = [data,matching,main,utils]
---
+++ packages/autocrosswalk/autocrosswalk-0.0.1-py3-none-any.whl/autocrosswalk/matching/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__init__
+"""
+from matching import (nlp,numeric_matching,text_matching)
+__all__ = [nlp,numeric_matching,text_matching]
