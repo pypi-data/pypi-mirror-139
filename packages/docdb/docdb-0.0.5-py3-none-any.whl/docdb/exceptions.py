255b5d9f68b5f7e8082857abfb6bd5d8d25e9334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:32 +0000

    Add package docdb/0.0.5/docdb-0.0.5-py3-none-any.whl

---
+++ packages/docdb/docdb-0.0.5-py3-none-any.whl/docdb/exceptions.py
@@ -0,0 +1,23 @@
+class PKNotInDocument(Exception):
+    pass
+
+class UUIDNotInDocument(Exception):
+    pass
+
+class InvalidDocumentID(Exception):
+    pass
+
+class InvalidCollectionMethod(Exception):
+    pass
+
+class InvalidOperator(Exception):
+    pass
+
+class DocumentNotFound(Exception):
+    pass
+
+class NoDocumentsFound(Exception):
+    pass
+
+class NonIterable(Exception):
+    pass
