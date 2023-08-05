e9606cfadeb40bd9de108f3e983fbeed9b574de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package pytorch-common/0.0.7/pytorch_common-0.0.7-py3-none-any.whl

---
+++ packages/pytorch-common/pytorch_common-0.0.7-py3-none-any.whl/pytorch_common/callbacks/save_best_model_checkpoint.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import datetime
+
+import torch
+
+from pytorch_common.callbacks import Callback
+from pytorch_common.callbacks.metric_improve_mixin import MetricImproveMixin
+from pytorch_common.util import create_path
+
+
+class SaveBestModel(MetricImproveMixin, Callback):
+    """
+    Save model weights to file while model validation metric improve.
+    """
+
+    def __init__(self, metric, path='./weights', mode='min', experiment_name='model', time_pattern='%Y-%m-%d_%H-%M-%S'):
+        self._init('checkpoint', metric, mode)
+        self.__path = create_path(path)
+        self.__time_pattern = time_pattern
+        self.__experiment_name = experiment_name
+
+    def _on_improve(self, ctx, mode):
+        file_path = self.__get_file_path(ctx)
+        logging.info("Save best model: {}".format(file_path))
+        torch.save(ctx.model.state_dict(), file_path)
+        ctx['best_model_path'] = file_path
+
+    def __get_file_path(self, ctx):
+        return '{}/{}--{}--epoch_{}--{}_{}.pt'.format(
+            self.__path,
+            datetime.now().strftime(self.__time_pattern),
+            self.__experiment_name,
+            ctx.epoch,
+            self._metric_name,
+            self._metric(ctx)
+        )
---
+++ packages/pytorch-common/pytorch_common-0.0.7-py3-none-any.whl/pytorch_common/util/__init__.py
@@ -0,0 +1,6 @@
+from .data_utils import train_val_test_split, train_val_split
+from .device_utils import set_device_name, set_device_memory, get_device, get_device_name
+from .module_utils import trainable_params_count
+from .os_utils import create_path
+from .stopwatch import Stopwatch
+from .tensor_utils import tensor_eq
