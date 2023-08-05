fe28305cf8f7f207a6d822645e22ef049f00222e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:46 +0000

    Add package cv-basic-utils/0.0.4/cv_basic_utils-0.0.4-py3-none-any.whl

---
+++ packages/cv-basic-utils/cv_basic_utils-0.0.4-py3-none-any.whl/cv_basic_utils/subpackage1/moduleA.py
@@ -0,0 +1,10 @@
+print('subpackage 1 loaded')
+
+def fun_a1():
+    print('this is subpackage1, module A')
+    return True
+
+def fun_a2():
+    print('this is subpackage1, module A')
+    return True
+
