c18ec3d00ae108869d29f399fca291a805c13432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package joungwoo-test/0.1/joungwoo_test-0.1-py3-none-any.whl

---
+++ packages/joungwoo-test/joungwoo_test-0.1-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,5 @@
+from test import joungwoo_test
+
+__version__ = '0.1'
+__author__ = 'joungwoo'
+
---
+++ packages/joungwoo-test/joungwoo_test-0.1-py3-none-any.whl/test/joungwoo_test.py
@@ -0,0 +1,8 @@
+
+
+def birthday():
+    print("지훈아 생일축하해")
+
+
+if __name__ == "__main__":
+    birthday()
