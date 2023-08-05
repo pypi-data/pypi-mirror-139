fc9e3a44d51a42d788e8ed073cc2ff8d7571fa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:35 +0000

    Add package kids4kars/1.0.0/kids4kars-1.0.0.tar.gz

---
+++ packages/kids4kars/kids4kars-1.0.0.tar.gz/kids4kars-1.0.0/kids4kars/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.0.0'
+__author__ = 'detpik'
+
+s = """
+1-8-5-5 kids for kars
+just about to go to mars
+1-8-5-5 kids for kars
+donate your kids today! (yay!)
+"""
+
+print(s)
---
+++ packages/kids4kars/kids4kars-1.0.0.tar.gz/kids4kars-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kids4kars',
+      version='1.0.0',
+      description='the most useless python module.',
+      long_description="`import kids4kars` and start donating your kids today. remember, kids are worth much more than you think.",
+      long_description_content_type="text/markdown",
+      author='detpik',
+      platforms='any',
+      packages=['kids4kars'],
+      zip_safe=False)
