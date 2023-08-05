8bb97bc5c196a1a381bdc925141622e359429257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:57 +0000

    Add package rbmotd/2.1/rbmotd-2.1-py2.py3-none-any.whl

---
+++ packages/rbmotd/rbmotd-2.1-py2.py3-none-any.whl/rbmotd/forms.py
@@ -0,0 +1,43 @@
+"""Forms for the rbmotd extension."""
+
+from __future__ import unicode_literals
+
+import hashlib
+
+from django import forms
+from django.utils.translation import ugettext as _
+from djblets.extensions.forms import SettingsForm
+
+
+class MotdSettingsForm(SettingsForm):
+    """The settings form for the motd extension."""
+
+    enabled = forms.BooleanField(initial=False, required=False)
+    message = forms.CharField(
+        max_length=512,
+        required=False,
+        help_text=_('This field expects valid HTML. Entities must be '
+                    'properly escaped.'),
+        widget=forms.TextInput(attrs={
+            'size': 100,
+        }))
+
+    def set_key_value(self, key, value):
+        """Set the given value in the extension settings.
+
+        This overrides the base settings form method so that when the
+        ``message`` key is set, it will additionally compute and store a
+        ``message_id`` key.
+
+        Args:
+            key (unicode):
+                The key to set.
+
+            value (object):
+                The value to set.
+        """
+        if key == 'message':
+            message_id = hashlib.sha256(value.encode('utf-8')).hexdigest()
+            self.set_key_value('message_id', message_id)
+
+        super(MotdSettingsForm, self).set_key_value(key, value)
