ba21cef1e939ad5e76c6da3285cf03fc8f2e2723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:20 +0000

    Add package viadot/0.3.2/viadot-0.3.2-py3-none-any.whl

---
+++ packages/viadot/viadot-0.3.2-py3-none-any.whl/tests/unit/test_task_utils.py
@@ -0,0 +1,146 @@
+import pytest
+import numpy as np
+import os
+import pandas as pd
+from typing import List
+
+from viadot.task_utils import (
+    chunk_df,
+    df_get_data_types_task,
+    df_map_mixed_dtypes_for_parquet,
+    df_to_csv,
+    df_to_parquet,
+    union_dfs_task,
+    dtypes_to_json,
+    write_to_json,
+)
+
+
+def count_dtypes(dtypes_dict: dict = None, dtypes_to_count: List[str] = None) -> int:
+    dtypes_counter = 0
+    for v in dtypes_dict.values():
+        if v in dtypes_to_count:
+            dtypes_counter += 1
+    return dtypes_counter
+
+
+def test_map_dtypes_for_parquet():
+    df = pd.DataFrame(
+        {
+            "a": {0: 55.7, 1: "Hello", 2: "Hello"},
+            "b": {0: "Start", 1: "Hello", 2: "Hello"},
+            "w": {0: 679, 1: "Hello", 2: "Hello"},
+            "x": {0: 1, 1: 2, 2: 444},
+            "y": {0: 1.5, 1: 11.97, 2: 56.999},
+            "z": {0: "Start", 1: 1, 2: "2021-01-01"},
+        }
+    )
+    dtyps_dict = df_get_data_types_task.run(df)
+    sum_of_dtypes = count_dtypes(dtyps_dict, ["Object", "String"])
+
+    df_map = df_map_mixed_dtypes_for_parquet.run(df, dtyps_dict)
+    dtyps_dict_mapped = df_get_data_types_task.run(df_map)
+    sum_of_mapped_dtypes = count_dtypes(dtyps_dict_mapped, ["String"])
+
+    assert sum_of_dtypes == sum_of_mapped_dtypes
+
+
+def test_chunk_df():
+    df = pd.DataFrame(
+        {
+            "AA": [1, 2, 3, 4, 5],
+            "BB": [11, 22, 33, 4, 5],
+            "CC": [4, 5, 6, 4, 5],
+            "DD": [44, 55, 66, 1, 2],
+        }
+    )
+    res = chunk_df.run(df=df, size=2)
+    assert len(res) == 3
+
+
+def test_df_get_data_types_task():
+    df = pd.DataFrame(
+        {
+            "a": {0: "ann", 1: "test", 2: "Hello"},
+            "b": {0: 9, 1: "2021-01-01", 2: "Hello"},
+            "w": {0: 679, 1: "Hello", 2: "Hello"},
+            "x": {0: -1, 1: 2, 2: 444},
+            "y": {0: 1.5, 1: 11.97, 2: 56.999},
+            "z": {0: "2022-01-01", 1: "2021-11-01", 2: "2021-01-01"},
+        }
+    )
+    res = df_get_data_types_task.run(df)
+    assert res == {
+        "a": "String",
+        "b": "Object",
+        "w": "Object",
+        "x": "Integer",
+        "y": "Float",
+        "z": "Date",
+    }
+
+
+def test_df_to_csv():
+    df = pd.DataFrame(
+        {
+            "a": {0: "a", 1: "b", 2: "c"},
+            "b": {0: "a", 1: "b", 2: "c"},
+            "w": {0: "a", 1: "b", 2: "c"},
+        }
+    )
+
+    df_to_csv.run(df, "test.csv")
+    result = pd.read_csv("test.csv", sep="\t")
+    assert df.equals(result)
+    os.remove("test.csv")
+
+
+def test_df_to_parquet():
+    df = pd.DataFrame(
+        {
+            "a": {0: "a", 1: "b", 2: "c"},
+            "b": {0: "a", 1: "b", 2: "c"},
+            "w": {0: "a", 1: "b", 2: "c"},
+        }
+    )
+
+    df_to_parquet.run(df, "test.parquet")
+    result = pd.read_parquet("test.parquet")
+    assert df.equals(result)
+    os.remove("test.parquet")
+
+
+def test_union_dfs_task():
+    df1 = pd.DataFrame(
+        {
+            "a": {0: "a", 1: "b", 2: "c"},
+            "b": {0: "a", 1: "b", 2: "c"},
+            "w": {0: "a", 1: "b", 2: "c"},
+        }
+    )
+    df2 = pd.DataFrame(
+        {
+            "a": {0: "d", 1: "e"},
+            "b": {0: "d", 1: "e"},
+        }
+    )
+    list_dfs = []
+    list_dfs.append(df1)
+    list_dfs.append(df2)
+    res = union_dfs_task.run(list_dfs)
+    assert isinstance(res, pd.DataFrame)
+    assert len(res) == 5
+
+
+def test_dtypes_to_json():
+    dtypes = {"country": "VARCHAR(100)", "sales": "FLOAT(24)"}
+    dtypes_to_json.run(dtypes_dict=dtypes, local_json_path="dtypes.json")
+    assert os.path.exists("dtypes.json")
+    os.remove("dtypes.json")
+
+
+def test_write_to_json():
+    dict = {"name": "John", 1: [2, 4, 3]}
+    write_to_json.run(dict, "dict.json")
+    assert os.path.exists("dict.json")
+    os.remove("dict.json")
---
+++ packages/viadot/viadot-0.3.2-py3-none-any.whl/viadot/sources/azure_sql.py
@@ -0,0 +1,146 @@
+from typing import List, Literal
+
+from prefect.utilities import logging
+
+from .base import SQL
+
+logger = logging.get_logger(__name__)
+
+
+class AzureSQL(SQL):
+    DEFAULT_SCHEMA = "dbo"
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.credentials["driver"] = "ODBC Driver 17 for SQL Server"
+
+    @property
+    def schemas(self) -> List[str]:
+        """Returns list of schemas"""
+        schemas_tuples = self.run("SELECT s.name as schema_name from sys.schemas s")
+        return [schema_tuple[0] for schema_tuple in schemas_tuples]
+
+    @property
+    def tables(self) -> List[str]:
+        """Returns list of tables"""
+        tables_tuples = self.run("SELECT * FROM information_schema.tables")
+        return [table for row in tables_tuples for table in row]
+
+    def bulk_insert(
+        self,
+        table: str,
+        schema: str = None,
+        source_path: str = None,
+        sep="\t",
+        if_exists: Literal = "append",
+    ):
+        """Fuction to bulk insert.
+        Args:
+            table (str): Table name.
+            schema (str, optional): Schema name. Defaults to None.
+            source_path (str, optional): Full path to a data file. Defaults to one.
+            sep (str, optional):  field terminator to be used for char and widechar data files. Defaults to "\t".
+            if_exists (Literal, optional): What to do if the table already exists. Defaults to "append".
+        """
+        if schema is None:
+            schema = self.DEFAULT_SCHEMA
+        fqn = f"{schema}.{table}"
+        insert_sql = f"""
+            BULK INSERT {fqn} FROM '{source_path}'
+            WITH (
+                CHECK_CONSTRAINTS,
+                DATA_SOURCE='{self.credentials['data_source']}',
+                DATAFILETYPE='char',
+                FIELDTERMINATOR='{sep}',
+                ROWTERMINATOR='0x0a',
+                FIRSTROW=2,
+                KEEPIDENTITY,
+                TABLOCK,
+                CODEPAGE='65001'
+            );
+        """
+        if if_exists == "replace":
+            self.run(f"DELETE FROM {schema}.{table}")
+        self.run(insert_sql)
+        return True
+
+    def create_external_database(
+        self,
+        external_database_name: str,
+        storage_account_name: str,
+        container_name: str,
+        sas_token: str,
+        master_key_password: str,
+        credential_name: str = None,
+    ):
+        """Create an external database. Used to eg. execute BULK INSERT or OPENROWSET
+        queries.
+
+        Args:
+            external_database_name (str): The name of the extrnal source (db) to be created.
+            storage_account_name (str): The name of the Azure storage account.
+            container_name (str): The name of the container which should become the "database".
+            sas_token (str): The SAS token to be used as the credential. Note that the auth
+            system in Azure is pretty broken and you might need to paste here your storage
+            account's account key instead.
+            master_key_password (str): The password for the database master key of your
+            Azure SQL Database.
+            credential_name (str): How to name the SAS credential. This is really an Azure
+            internal thing and can be anything. By default '{external_database_name}_credential`.
+        """
+
+        # stupid Microsoft thing
+        if sas_token.startswith("?"):
+            sas_token = sas_token[1:]
+
+        if credential_name is None:
+            credential_name = f"{external_database_name}_credential"
+
+        create_master_key_sql = (
+            f"CREATE MASTER KEY ENCRYPTION BY PASSWORD = {master_key_password}"
+        )
+
+        create_external_db_credential_sql = f"""
+        CREATE DATABASE SCOPED CREDENTIAL {credential_name}
+        WITH IDENTITY = 'SHARED ACCESS SIGNATURE'
+        SECRET = '{sas_token}';
+        """
+
+        create_external_db_sql = f"""
+        CREATE EXTERNAL DATA SOURCE {external_database_name} WITH (
+        LOCATION = f'https://{storage_account_name}.blob.core.windows.net/{container_name}',
+        CREDENTIAL = {credential_name}
+        );
+        """
+
+        self.run(create_master_key_sql)
+        self.run(create_external_db_credential_sql)
+        self.run(create_external_db_sql)
+
+    def exists(self, table: str, schema: str = None) -> bool:
+        """Check whether a table exists.
+
+        Args:
+            table (str): The table to be checked.
+            schema (str, optional): The schema whethe the table is located. Defaults to 'dbo'.
+
+        Returns:
+            bool: Whether the table exists.
+        """
+
+        if not schema:
+            schema = self.DEFAULT_SCHEMA
+
+        list_table_info_query = f"""
+            SELECT *
+            FROM sys.tables t
+            JOIN sys.schemas s
+                ON t.schema_id = s.schema_id
+            WHERE s.name = '{schema}' AND t.name = '{table}'
+        """
+        exists = bool(self.run(list_table_info_query))
+        return exists
