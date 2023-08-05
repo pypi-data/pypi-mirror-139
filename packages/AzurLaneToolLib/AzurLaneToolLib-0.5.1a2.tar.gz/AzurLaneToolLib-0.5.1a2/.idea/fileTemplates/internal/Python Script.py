150e835d607767f23d5e26e4a74d8edc9d5e90bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:32 +0000

    Add package AzurLaneToolLib/0.5.1a2/AzurLaneToolLib-0.5.1a2.tar.gz

---
+++ packages/AzurLaneToolLib/AzurLaneToolLib-0.5.1a2.tar.gz/AzurLaneToolLib-0.5.1a2/.idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+THIS FILE IS PART OF 
+${NAME}.py - The core part of the Azur Lane Tool.
+
+Author: Matt Belfast Brown
+Create Date: 2021-07-10
+Version Date: ${DATE}
+Version: 
+
+THIS PROGRAM IS FREE FOR EVERYONE,IS LICENSED UNDER GPL-3.0
+YOU SHOULD HAVE RECEIVED A COPY OF GPL-3.0 LICENSE.
+
+Copyright (C) 2021-2022 Matt Belfast Brown
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# import lib list
