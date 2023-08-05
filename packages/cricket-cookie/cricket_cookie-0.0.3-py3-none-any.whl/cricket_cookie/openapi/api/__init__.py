71c5325d1640cc599dfc9ec89a615c1f1b943bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:12 +0000

    Add package cricket-cookie/0.0.3/cricket_cookie-0.0.3-py3-none-any.whl

---
+++ packages/cricket-cookie/cricket_cookie-0.0.3-py3-none-any.whl/cricket_cookie/openapi/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cricket_cookie.openapi.api.auth_service_api import AuthServiceApi
+from cricket_cookie.openapi.api.cluster_service_api import ClusterServiceApi
+from cricket_cookie.openapi.api.lightningapp_service_api import LightningappServiceApi
+from cricket_cookie.openapi.api.projects_service_api import ProjectsServiceApi
+from cricket_cookie.openapi.api.user_service_api import UserServiceApi
