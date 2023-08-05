9b44b2be96985301cc1844b05e9fe221a70fd13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package conan-server/2.0.0a4/conan-server-2.0.0a4.tar.gz

---
+++ packages/conan-server/conan-server-2.0.0a4.tar.gz/conan-server-2.0.0a4/conans/client/cmd/export.py
@@ -0,0 +1,307 @@
+import os
+import shutil
+
+import yaml
+
+from conan.tools.files import copy
+from conan.tools.files.copy_pattern import report_copied_files
+from conans.cli.output import ScopedOutput
+from conans.client.tools import chdir
+from conans.errors import ConanException, conanfile_exception_formatter
+from conans.model.manifest import FileTreeManifest
+from conans.model.recipe_ref import RecipeReference
+from conans.model.scm import SCM, get_scm_data
+from conans.paths import CONANFILE, DATA_YML
+from conans.util.files import is_dirty, load, rmdir, save, set_dirty, mkdir, \
+    merge_directories, clean_dirty
+from conans.util.log import logger
+
+
+def cmd_export(app, conanfile_path, name, version, user, channel, graph_lock=None,
+               ignore_dirty=False):
+    """ Export the recipe
+    param conanfile_path: the original source directory of the user containing a
+                       conanfile.py
+    """
+    loader, cache, hook_manager = app.loader, app.cache, app.hook_manager
+    conanfile = loader.load_export(conanfile_path, name, version, user, channel, graph_lock)
+
+    ref = RecipeReference(conanfile.name, conanfile.version,  conanfile.user, conanfile.channel)
+    if str(ref) != str(ref).lower():
+        raise ConanException(f"Conan packages names '{ref}' must be all lowercase")
+
+    conanfile.display_name = str(ref)
+    conanfile.output.scope = conanfile.display_name
+    scoped_output = conanfile.output
+
+    recipe_layout = cache.create_export_recipe_layout(ref)
+
+    hook_manager.execute("pre_export", conanfile=conanfile, conanfile_path=conanfile_path,
+                         reference=ref)
+    logger.debug("EXPORT: %s" % conanfile_path)
+
+    scoped_output.highlight("Exporting package recipe")
+
+    export_folder = recipe_layout.export()
+    export_src_folder = recipe_layout.export_sources()
+    # TODO: cache2.0 move this creation to other place
+    mkdir(export_folder)
+    mkdir(export_src_folder)
+    export_recipe(conanfile, export_folder)
+    export_source(conanfile, export_src_folder)
+    shutil.copy2(conanfile_path, recipe_layout.conanfile())
+
+    # Calculate the "auto" values and replace in conanfile.py
+    scm_data, local_src_folder = _capture_scm_auto_fields(conanfile,
+                                                          os.path.dirname(conanfile_path),
+                                                          recipe_layout,
+                                                          ignore_dirty)
+
+    scm_sources_folder = recipe_layout.scm_sources()
+    if local_src_folder:
+        # Copy the local scm folder to scm_sources in the cache
+        mkdir(scm_sources_folder)
+        _export_scm(conanfile.output, scm_data, local_src_folder, scm_sources_folder)
+
+    # Execute post-export hook before computing the digest
+    hook_manager.execute("post_export", conanfile=conanfile, reference=ref,
+                         conanfile_path=recipe_layout.conanfile())
+
+    # Compute the new digest
+    manifest = FileTreeManifest.create(export_folder, export_src_folder)
+    manifest.save(export_folder)
+
+    # Compute the revision for the recipe
+    revision = calc_revision(scoped_output=conanfile.output,
+                             path=os.path.dirname(conanfile_path),
+                             manifest=manifest,
+                             revision_mode=conanfile.revision_mode)
+
+    ref.revision = revision
+    recipe_layout.reference = ref
+    cache.assign_rrev(recipe_layout)
+    # TODO: cache2.0 check if this is the message we want to output
+    scoped_output.success('A new %s version was exported' % CONANFILE)
+    scoped_output.info('Folder: %s' % recipe_layout.export())
+
+    # FIXME: Conan 2.0 Clear the registry entry if the recipe has changed
+    # TODO: cache2.0: check this part
+    source_folder = recipe_layout.source()
+    if os.path.exists(source_folder):
+        try:
+            if is_dirty(source_folder):
+                scoped_output.info("Source folder is corrupted, forcing removal")
+                rmdir(source_folder)
+                clean_dirty(source_folder)
+        except BaseException as e:
+            scoped_output.error("Unable to delete source folder. Will be marked as corrupted "
+                                "for deletion")
+            scoped_output.warning(str(e))
+            set_dirty(source_folder)
+
+    scoped_output.info("Exported revision: %s" % revision)
+    if graph_lock is not None:
+        graph_lock.update_lock_export_ref(ref)
+
+    return ref
+
+
+def _capture_scm_auto_fields(conanfile, conanfile_dir, recipe_layout, ignore_dirty):
+    """Deduce the values for the scm auto fields or functions assigned to 'url' or 'revision'
+       and replace the conanfile.py contents.
+       Returns a tuple with (scm_data, path_to_scm_local_directory)"""
+    scm_data = get_scm_data(conanfile)
+    if not scm_data:
+        return None, None
+
+    # Resolve SCMData in the user workspace (someone may want to access CVS or import some py)
+    scm = SCM(scm_data, conanfile_dir, conanfile.output)
+    captured = scm_data.capture_origin or scm_data.capture_revision
+
+    if not captured:
+        
+        _add_scmdata_to_conandata_yml(recipe_layout, scm_data)
+        return scm_data, None
+
+    if not scm.is_pristine() and not ignore_dirty:
+        conanfile.output.warning("There are uncommitted changes, skipping the replacement of 'scm.url' and "
+                                "'scm.revision' auto fields. Use --ignore-dirty to force it. The 'conan "
+                       "upload' command will prevent uploading recipes with 'auto' values in these "
+                       "fields.")
+        origin = scm.get_qualified_remote_url(remove_credentials=True)
+        local_src_path = scm.get_local_path_to_url(origin)
+        return scm_data, local_src_path
+
+    if scm_data.url == "auto":
+        origin = scm.get_qualified_remote_url(remove_credentials=True)
+        if not origin:
+            conanfile.output.warning(
+                "Repo origin cannot be deduced, 'auto' fields won't be replaced."
+                " 'conan upload' command will prevent uploading recipes with 'auto'"
+                " values in these fields.")
+            local_src_path = scm.get_local_path_to_url(origin)
+            return scm_data, local_src_path
+        if scm.is_local_repository():
+            conanfile.output.warning("Repo origin looks like a local path: %s" % origin)
+        conanfile.output.success("Repo origin deduced by 'auto': %s" % origin)
+        scm_data.url = origin
+
+    if scm_data.revision == "auto":
+        # If it is pristine by default we don't replace the "auto" unless forcing
+        # This prevents the recipe to get uploaded pointing to an invalid commit
+        scm_data.revision = scm.get_revision()
+        conanfile.output.success("Revision deduced by 'auto': %s" % scm_data.revision)
+
+    local_src_path = scm.get_local_path_to_url(scm_data.url)
+    _add_scmdata_to_conandata_yml(recipe_layout, scm_data)
+
+    return scm_data, local_src_path
+
+
+def _add_scmdata_to_conandata_yml(recipe_layout, scm_data):
+    conandata_path = os.path.join(recipe_layout.export(), DATA_YML)
+    conandata_yml = {}
+    if os.path.exists(conandata_path):
+
+        conandata_yml = yaml.safe_load(load(conandata_path))
+        conandata_yml = conandata_yml or {}  # In case the conandata is a blank file
+        if '.conan' in conandata_yml:
+            raise ConanException("Field '.conan' inside '{}' file is reserved to "
+                                 "Conan usage.".format(DATA_YML))
+    scm_data_copied = scm_data.as_dict()
+    scm_data_copied.pop('username', None)
+    scm_data_copied.pop('password', None)
+    conandata_yml['.conan'] = {'scm': scm_data_copied}
+    save(conandata_path, yaml.safe_dump(conandata_yml, default_flow_style=False))
+
+
+def _detect_scm_revision(path):
+    if not path:
+        raise ConanException("Not path supplied")
+
+    repo_type = SCM.detect_scm(path)
+    if not repo_type:
+        raise ConanException("'{}' repository not detected".format(repo_type))
+
+    repo_obj = SCM.availables.get(repo_type)(path)
+    return repo_obj.get_revision(), repo_type, repo_obj.is_pristine()
+
+
+def calc_revision(scoped_output, path, manifest, revision_mode):
+    if revision_mode not in ["scm", "hash"]:
+        raise ConanException("Revision mode should be one of 'hash' (default) or 'scm'")
+
+    # Use the proper approach depending on 'revision_mode'
+    if revision_mode == "hash":
+        revision = manifest.summary_hash
+        scoped_output.info("Using the exported files summary hash as the recipe"
+                           " revision: {} ".format(revision))
+    else:
+        try:
+            rev_detected, repo_type, is_pristine = _detect_scm_revision(path)
+        except Exception as exc:
+            error_msg = "Cannot detect revision using '{}' mode from repository at " \
+                        "'{}'".format(revision_mode, path)
+            raise ConanException("{}: {}".format(error_msg, exc))
+
+        revision = rev_detected
+
+        scoped_output.info("Using %s commit as the recipe revision: %s" % (repo_type, revision))
+        if not is_pristine:
+            scoped_output.warning("Repo status is not pristine: there might be modified files")
+
+    return revision
+
+
+def _classify_patterns(patterns):
+    patterns = patterns or []
+    included, excluded = [], []
+    for p in patterns:
+        if p.startswith("!"):
+            excluded.append(p[1:])
+        else:
+            included.append(p)
+
+    return included, excluded
+
+
+def _export_scm(scoped_output, scm_data, origin_folder, scm_sources_folder):
+    """ Copy the local folder to the scm_sources folder in the cache, this enables to work
+        with local sources without committing and pushing changes to the scm remote.
+        https://github.com/conan-io/conan/issues/5195"""
+    excluded = SCM(scm_data, origin_folder, scoped_output).excluded_files
+    excluded.append("conanfile.py")
+    scoped_output.info("SCM: Getting sources from folder: %s" % origin_folder)
+    merge_directories(origin_folder, scm_sources_folder, excluded=excluded)
+
+
+def export_source(conanfile, destination_source_folder):
+    if callable(conanfile.exports_sources):
+        raise ConanException("conanfile 'exports_sources' shouldn't be a method, "
+                             "use 'export_sources()' instead")
+
+    if isinstance(conanfile.exports_sources, str):
+        conanfile.exports_sources = (conanfile.exports_sources,)
+
+    included_sources, excluded_sources = _classify_patterns(conanfile.exports_sources)
+    copied = []
+    for pattern in included_sources:
+        _tmp = copy(conanfile, pattern, src=conanfile.recipe_folder,
+                    dst=destination_source_folder, excludes=excluded_sources)
+        copied.extend(_tmp)
+
+    output = conanfile.output
+    package_output = ScopedOutput("%s exports_sources" % output.scope, output)
+    report_copied_files(copied, package_output)
+    _run_method(conanfile, "export_sources", destination_source_folder)
+
+
+def export_recipe(conanfile, destination_folder):
+    if callable(conanfile.exports):
+        raise ConanException("conanfile 'exports' shouldn't be a method, use 'export()' instead")
+    if isinstance(conanfile.exports, str):
+        conanfile.exports = (conanfile.exports,)
+
+    scoped_output = conanfile.output
+    package_output = ScopedOutput("%s exports" % scoped_output.scope, scoped_output)
+
+    if os.path.exists(os.path.join(conanfile.recipe_folder, DATA_YML)):
+        package_output.info("File '{}' found. Exporting it...".format(DATA_YML))
+        tmp = [DATA_YML]
+        if conanfile.exports:
+            tmp.extend(conanfile.exports)  # conanfile.exports could be a tuple (immutable)
+        conanfile.exports = tmp
+
+    included_exports, excluded_exports = _classify_patterns(conanfile.exports)
+
+    copied = []
+    for pattern in included_exports:
+        tmp = copy(conanfile, pattern, conanfile.recipe_folder, destination_folder,
+                   excludes=excluded_exports)
+        copied.extend(tmp)
+    report_copied_files(copied, package_output)
+
+    _run_method(conanfile, "export", destination_folder)
+
+
+def _run_method(conanfile, method, destination_folder):
+    export_method = getattr(conanfile, method, None)
+    if export_method:
+        if not callable(export_method):
+            raise ConanException("conanfile '%s' must be a method" % method)
+        conanfile.output.highlight("Calling %s()" % method)
+        folder_name = "%s_folder" % method
+        setattr(conanfile, folder_name, destination_folder)
+        default_options = conanfile.default_options
+        options = conanfile.options
+        try:
+            # TODO: Poor man attribute control access. Convert to nice decorator
+            conanfile.default_options = None
+            conanfile.options = None
+            with chdir(conanfile.recipe_folder):
+                with conanfile_exception_formatter(conanfile, method):
+                    export_method()
+        finally:
+            conanfile.default_options = default_options
+            conanfile.options = options
+            delattr(conanfile, folder_name)
---
+++ packages/conan-server/conan-server-2.0.0a4.tar.gz/conan-server-2.0.0a4/conans/model/graph_lock.py
@@ -0,0 +1,162 @@
+import json
+import os
+
+from conans.client.graph.graph import RECIPE_VIRTUAL, RECIPE_CONSUMER, CONTEXT_BUILD
+from conans.errors import ConanException
+from conans.model.recipe_ref import RecipeReference
+from conans.util.files import load, save
+
+LOCKFILE = "conan.lock"
+LOCKFILE_VERSION = "0.5"
+
+
+class Lockfile(object):
+
+    def __init__(self, deps_graph=None):
+        self.requires = []
+        self.python_requires = []
+        self.build_requires = []
+        self.alias = {}
+        self.strict = False
+
+        if deps_graph is None:
+            return
+
+        requires = set()
+        python_requires = set()
+        build_requires = set()
+        for graph_node in deps_graph.nodes:
+            try:
+                python_requires.update(graph_node.conanfile.python_requires.all_refs())
+            except AttributeError:
+                pass
+            if graph_node.recipe in (RECIPE_VIRTUAL, RECIPE_CONSUMER) or graph_node.ref is None:
+                continue
+            assert graph_node.conanfile is not None
+
+            if graph_node.context == CONTEXT_BUILD:
+                build_requires.add(graph_node.ref)
+            else:
+                requires.add(graph_node.ref)
+
+        # Sorted, newer versions first, so first found is valid for version ranges
+        # TODO: Need to impmlement same ordering for revisions, based on revision time
+        self.requires = list(reversed(sorted(requires)))
+        self.python_requires = list(reversed(sorted(python_requires)))
+        self.build_requires = list(reversed(sorted(build_requires)))
+        self.alias = deps_graph.aliased
+
+    @staticmethod
+    def load(path):
+        if not path:
+            raise IOError("Invalid path")
+        if not os.path.isfile(path):
+            raise ConanException("Missing lockfile in: %s" % path)
+        content = load(path)
+        try:
+            return Lockfile.deserialize(json.loads(content))
+        except Exception as e:
+            raise ConanException("Error parsing lockfile '{}': {}".format(path, e))
+
+    def save(self, path):
+        save(path, json.dumps(self.serialize(), indent=True))
+
+    def update_lock(self, deps_graph):
+        """ add new things at the beginning, to give more priority
+        """
+        requires = set()
+        build_requires = set()
+        for graph_node in deps_graph.nodes:
+            if graph_node.recipe in (RECIPE_VIRTUAL, RECIPE_CONSUMER) or graph_node.ref is None:
+                continue
+            assert graph_node.conanfile is not None
+
+            if graph_node.context == CONTEXT_BUILD:
+                build_requires.add(graph_node.ref)
+            else:
+                requires.add(graph_node.ref)
+
+        requires.update(self.requires)
+        self.requires = list(reversed(sorted(requires)))
+        build_requires.update(self.build_requires)
+        self.build_requires = list(reversed(sorted(build_requires)))
+        # TODO other members
+
+    def merge(self, other):
+        self.requires = list(reversed(sorted(set(other.requires + self.requires))))
+        self.build_requires = list(reversed(sorted(set(other.build_requires + self.build_requires))))
+
+    @staticmethod
+    def deserialize(data):
+        """ constructs a GraphLock from a json like dict
+        """
+        graph_lock = Lockfile(deps_graph=None)
+        version = data.get("version")
+        if version and version != LOCKFILE_VERSION:
+            raise ConanException("This lockfile was created with an incompatible "
+                                 "version. Please regenerate the lockfile")
+        for r in data["requires"]:
+            graph_lock.requires.append(RecipeReference.loads(r))
+        for r in data["python_requires"]:
+            graph_lock.python_requires.append(RecipeReference.loads(r))
+        for r in data["build_requires"]:
+            graph_lock.build_requires.append(RecipeReference.loads(r))
+        return graph_lock
+
+    def serialize(self):
+        """ returns the object serialized as a dict of plain python types
+        that can be converted to json
+        """
+        return {"version": LOCKFILE_VERSION,
+                "requires": [repr(r) for r in self.requires],
+                "python_requires": [repr(r) for r in self.python_requires],
+                "build_requires": [repr(r) for r in self.build_requires]}
+
+    def resolve_locked(self, node, require):
+        if require.build or node.context == CONTEXT_BUILD:
+            locked_refs = self.build_requires
+        else:
+            locked_refs = self.requires
+        self._resolve(require, locked_refs)
+
+    def _resolve(self, require, locked_refs):
+        ref = require.ref
+        version_range = require.version_range
+
+        if version_range:
+            matches = [r for r in locked_refs if r.name == ref.name and r.user == ref.user and
+                       r.channel == ref.channel]
+            for m in matches:
+                if m.version in version_range:
+                    require.ref = m
+                    break
+            else:
+                if self.strict:
+                    raise ConanException(f"Requirement '{ref}' not in lockfile")
+        else:
+            alias = require.alias
+            if alias:
+                require.ref = self.alias.get(require.ref, require.ref)
+            elif require.ref.revision is None:
+                for r in locked_refs:
+                    if r.name == ref.name and r.version == ref.version and r.user == ref.user and \
+                            r.channel == ref.channel:
+                        require.ref = r
+                        break
+            else:
+                if ref not in locked_refs and self.strict:
+                    raise ConanException(f"Requirement '{repr(ref)}' not in lockfile")
+
+    def resolve_locked_pyrequires(self, require):
+        locked_refs = self.python_requires  # CHANGE
+        self._resolve(require, locked_refs)
+
+    def update_lock_export_ref(self, ref):
+        """ when the recipe is exported, it will complete the missing RREV, otherwise it should
+        match the existing RREV
+        """
+        # Filter existing matching
+        if ref not in self.requires:
+            # It is inserted in the first position, because that will result in prioritization
+            # That includes testing previous versions in a version range
+            self.requires.insert(0, ref)
