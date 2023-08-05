8182d19e47d8c11de63d600146b082379abec179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:28 +0000

    Add package funkify/0.4.4/funkify-0.4.4.tar.gz

---
+++ packages/funkify/funkify-0.4.4.tar.gz/funkify-0.4.4/funkify/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "funkify"
+__description__ = "Funkify modules so that they are callable"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.4.4"
