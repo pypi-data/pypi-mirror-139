63d3dcbe390035c03142e855e818bab93b4d6ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:34 +0000

    Add package vegafusion-python-embed/0.0.4rc1/vegafusion_python_embed-0.0.4_rc.1.tar.gz

---
+++ packages/vegafusion-python-embed/vegafusion_python_embed-0.0.4_rc.1.tar.gz/vegafusion_python_embed-0.0.4_rc.1/local_dependencies/vegafusion-rt-datafusion/src/task_graph/cache.rs
@@ -0,0 +1,192 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 Jon Mease
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+use async_lock::{Mutex, RwLock};
+use futures::FutureExt;
+use lru::LruCache;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::future::Future;
+use std::panic::{resume_unwind, AssertUnwindSafe};
+use std::sync::Arc;
+use vegafusion_core::error::{DuplicateResult, Result, ToExternalError};
+use vegafusion_core::task_graph::task_value::TaskValue;
+
+#[derive(Debug, Clone)]
+struct CachedValue {
+    value: NodeValue, // Maybe add metrics like compute time, or a cache weight
+}
+
+type NodeValue = (TaskValue, Vec<TaskValue>);
+type Initializer = Arc<RwLock<Option<Result<NodeValue>>>>;
+
+#[derive(Debug, Clone)]
+pub struct VegaFusionCache {
+    values: Arc<Mutex<LruCache<u64, CachedValue>>>,
+    initializers: Arc<RwLock<HashMap<u64, Initializer>>>,
+}
+
+impl VegaFusionCache {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            values: Arc::new(Mutex::new(LruCache::new(capacity))),
+            initializers: Default::default(),
+        }
+    }
+
+    pub async fn clear(&self) {
+        // Clear the values cache. There may still be initializers representing in progress
+        // futures which will not be cleared.
+        self.values.lock().await.clear();
+    }
+
+    async fn get_from_values(&self, state_fingerprint: u64) -> Option<CachedValue> {
+        // This is a write lock because the LruCache.get function mutates the Cache to update
+        // the LRU status
+        self.values.lock().await.get(&state_fingerprint).cloned()
+    }
+
+    async fn set_value(&self, state_fingerprint: u64, value: NodeValue) -> Option<CachedValue> {
+        self.values
+            .lock()
+            .await
+            .put(state_fingerprint, CachedValue { value })
+    }
+
+    async fn remove_initializer(&self, state_fingerprint: u64) -> Option<Initializer> {
+        self.initializers.write().await.remove(&state_fingerprint)
+    }
+
+    pub async fn get_or_try_insert_with<F>(
+        &self,
+        state_fingerprint: u64,
+        init: F,
+    ) -> Result<NodeValue>
+    where
+        F: Future<Output = Result<NodeValue>> + Send + 'static,
+    {
+        // Check if present in the values cache
+        if let Some(value) = self.get_from_values(state_fingerprint).await {
+            return Ok(value.value);
+        }
+
+        // Check if present in initializers
+        let mut initializers_lock = self.initializers.write().await;
+
+        match initializers_lock.entry(state_fingerprint) {
+            Entry::Occupied(entry) => {
+                // Calculation is in progress, await on Arc clone of it's initializer
+                let initializer = entry.get().clone();
+                // Drop lock on initializers collection
+                drop(initializers_lock);
+                let result = initializer.read().await;
+                let result = result.as_ref().unwrap();
+                result.duplicate()
+            }
+            Entry::Vacant(entry) => {
+                // Create new initializer
+                let initializer: Initializer = Arc::new(RwLock::new(None));
+
+                // Get and hold write lock for initializer
+                let mut initializer_lock = initializer.write().await;
+
+                // Store Arc clone of initializer in initializers map
+                entry.insert(initializer.clone());
+
+                // Drop write lock
+                drop(initializers_lock);
+
+                // Invoke future to initialize
+                match AssertUnwindSafe(tokio::spawn(init)).catch_unwind().await {
+                    // Resolved.
+                    Ok(Ok(value)) => {
+                        // If result Ok, clone to values
+                        match value {
+                            Ok(value) => {
+                                *initializer_lock = Some(Ok(value.clone()));
+                                self.set_value(state_fingerprint, value.clone()).await;
+
+                                // Stored initializer no longer required. Initializers are Arc
+                                // pointers, so it's fine to drop initializer from here even if
+                                // other tasks are still awaiting on it.
+                                self.remove_initializer(state_fingerprint).await;
+                                Ok(value)
+                            }
+                            Err(e) => {
+                                // Remove initializer so that another future can try again
+                                *initializer_lock = Some(Err(e.duplicate()));
+                                self.remove_initializer(state_fingerprint).await;
+                                Err(e)
+                            }
+                        }
+                    }
+                    Ok(Err(err)) => Err(err).external("tokio error"),
+                    // Panicked.
+                    Err(payload) => {
+                        // Remove the waiter so that others can retry.
+                        self.remove_initializer(state_fingerprint).await;
+                        // triggers panic, so no return value in this branch
+                        resume_unwind(payload);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_cache {
+    use crate::task_graph::cache::{NodeValue, VegaFusionCache};
+    use tokio::time::Duration;
+    use vegafusion_core::data::scalar::ScalarValue;
+    use vegafusion_core::error::Result;
+    use vegafusion_core::task_graph::task_value::TaskValue;
+
+    async fn make_value(value: ScalarValue) -> Result<NodeValue> {
+        tokio::time::sleep(Duration::from_millis(1000)).await;
+        Ok((TaskValue::Scalar(value), Vec::new()))
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn try_cache() {
+        let cache = VegaFusionCache::new(4);
+
+        let value_future1 = cache.get_or_try_insert_with(1, make_value(ScalarValue::from(23.5)));
+        let value_future2 = cache.get_or_try_insert_with(1, make_value(ScalarValue::from(23.5)));
+        let value_future3 = cache.get_or_try_insert_with(1, make_value(ScalarValue::from(23.5)));
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+        println!("{:?}", cache.initializers);
+
+        // assert_eq!(cache.num_values().await, 0);
+        // assert_eq!(cache.num_initializers().await, 1);
+
+        let futures = vec![value_future1, value_future2];
+        let values = futures::future::join_all(futures).await;
+
+        let next_value = value_future3.await;
+
+        // tokio::time::sleep(Duration::from_millis(300));
+        println!("{:?}", cache.initializers);
+        // assert_eq!(cache.num_values().await, 1);
+        // assert_eq!(cache.num_initializers().await, 0);
+
+        println!("values: {:?}", values);
+        println!("next_value: {:?}", next_value);
+    }
+}
---
+++ packages/vegafusion-python-embed/vegafusion_python_embed-0.0.4_rc.1.tar.gz/vegafusion_python_embed-0.0.4_rc.1/src/lib.rs
@@ -0,0 +1,82 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 Jon Mease
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+use pyo3::prelude::*;
+use pyo3::types::PyBytes;
+use tokio::runtime::Runtime;
+use vegafusion_core::error::ToExternalError;
+
+use vegafusion_rt_datafusion::task_graph::runtime::TaskGraphRuntime;
+
+#[pyclass]
+struct PyTaskGraphRuntime {
+    runtime: TaskGraphRuntime,
+    tokio_runtime: Runtime,
+}
+
+#[pymethods]
+impl PyTaskGraphRuntime {
+    #[new]
+    pub fn new(max_capacity: i32, worker_threads: Option<i32>) -> PyResult<Self> {
+        let mut tokio_runtime_builder = tokio::runtime::Builder::new_multi_thread();
+        tokio_runtime_builder.enable_all();
+
+        if let Some(worker_threads) = worker_threads {
+            tokio_runtime_builder.worker_threads(worker_threads.max(1) as usize);
+        }
+
+        // Build tokio runtime
+        let tokio_runtime = tokio_runtime_builder
+            .build()
+            .external("Failed to create Tokio thread pool")?;
+
+        Ok(Self {
+            runtime: TaskGraphRuntime::new(max_capacity as usize),
+            tokio_runtime,
+        })
+    }
+
+    pub fn process_request_bytes(&self, request_bytes: &PyBytes) -> PyResult<PyObject> {
+        let response_bytes = self
+            .tokio_runtime
+            .block_on(self.runtime.process_request_bytes(request_bytes.as_bytes()))?;
+        Python::with_gil(|py| Ok(PyBytes::new(py, &response_bytes).into()))
+    }
+
+    pub fn clear_cache(&self) {
+        self.tokio_runtime.block_on(self.runtime.clear_cache());
+    }
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn vegafusion_embed(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyTaskGraphRuntime>()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
