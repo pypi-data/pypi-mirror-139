8c81beced446987442ad894e596370ee0c8951e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:36 +0000

    Add package IntuneCD/1.0.7/IntuneCD-1.0.7-py3-none-any.whl

---
+++ packages/IntuneCD/IntuneCD-1.0.7-py3-none-any.whl/IntuneCD/backup_managementIntents.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+"""
+This module backs up all Intents in Intune.
+
+Parameters
+----------
+path : str
+    The path to save the backup to
+output : str
+    The format the backup will be saved as
+token : str
+    The token to use for authenticating the request
+"""
+
+import json
+import os
+import yaml
+
+from .clean_filename import clean_filename
+from .graph_request import makeapirequest
+from .get_add_assignments import get_assignments
+
+## Set MS Graph base endpoint
+baseEndpoint = "https://graph.microsoft.com/beta/deviceManagement"
+
+## Get all Intents and save them in specified path
+
+
+def savebackup(path, output, token):
+    configpath = path+"/"+"Management Intents/"
+    intents = makeapirequest(baseEndpoint + "/intents", token)
+
+    for intent in intents['value']:
+        if intent['templateId'] == None:
+            continue
+        else:
+            print("Backing up Intent: " + intent['displayName'])
+            ## Get Intent template details
+            intent_template = makeapirequest(
+                baseEndpoint + "/templates" + "/" + intent['templateId'], token)
+            ## Get Intent categories
+            intent_template_categories = makeapirequest(
+                baseEndpoint + "/templates" + "/" + intent['templateId'] + "/categories", token)
+
+            configpath = path+"/"+"Management Intents/" + \
+                intent_template['displayName'] + "/"
+            if os.path.exists(configpath) == False:
+                os.makedirs(configpath)
+
+            ## Save all setings for the Intent
+            settings_delta = {}
+            for intent_category in intent_template_categories['value']:
+                intent_settings = makeapirequest(
+                    baseEndpoint + "/intents" + "/" + intent['id'] + "/categories" + "/" + intent_category['id'] + "/settings", token)
+                for setting in intent_settings['value']:
+                    setting.pop('id', None)
+                settings_delta = intent_settings['value']
+
+            intent_value = {
+                "displayName": intent['displayName'],
+                "description": intent['description'],
+                "templateId": intent['templateId'],
+                "settingsDelta": settings_delta,
+                "roleScopeTagIds": intent['roleScopeTagIds']
+            }
+
+            ## Get assignments of Intent
+            get_assignments(baseEndpoint + "/intents",
+                            intent_value, intent['id'], token)
+
+            ## Get filename without illegal characters
+            fname = clean_filename(intent['displayName'])
+            ## Save Intent as JSON or YAML depending on configured value in "-o"
+            if output != "json":
+                with open(configpath+fname+".yaml", 'w') as yamlFile:
+                    yaml.dump(intent_value, yamlFile,
+                              sort_keys=False, default_flow_style=False)
+            else:
+                with open(configpath+fname+".json", 'w') as jsonFile:
+                    json.dump(intent_value, jsonFile, indent=10)
---
+++ packages/IntuneCD/IntuneCD-1.0.7-py3-none-any.whl/IntuneCD/update_powershellScripts.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""
+This module updates all Powershell scripts in Intune if the configuration in Intune differs from the JSON/YAML file.
+
+Parameters
+----------
+path : str
+    The path to where the backup is saved
+token : str
+    The token to use for authenticating the request
+"""
+
+import json
+import os
+import base64
+import yaml
+import re
+
+from .graph_request import makeapirequest, makeapirequestPatch, makeapirequestPost
+from .get_add_assignments import add_assignment
+
+from deepdiff import DeepDiff
+
+## Set MS Graph endpoint
+endpoint = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts"
+
+
+def update(path, token, assignment=False):
+
+    ## Set Powershell script path
+    configpath = path+"/"+"Scripts/Powershell"
+    ## If Powershell script path exists, continue
+    if os.path.exists(configpath) == True:
+        for filename in os.listdir(configpath):
+            file = os.path.join(configpath, filename)
+            # If path is Directory, skip
+            if os.path.isdir(file):
+                continue
+            # If file is .DS_Store, skip
+            if filename == ".DS_Store":
+                continue
+
+            ## Check which format the file is saved as then open file, load data and set query parameter
+            with open(file) as f:
+                    if filename.endswith(".yaml"):
+                        data = json.dumps(yaml.safe_load(f))
+                        repo_data = json.loads(data)
+                        q_param = {"$filter": "displayName eq " +
+                            "'" + repo_data['displayName'] + "'"}
+                    elif filename.endswith(".json"):
+                        f = open(file)
+                        repo_data = json.load(f)
+                        q_param = {"$filter": "displayName eq " + "'" + repo_data['displayName'] + "'"}
+
+                    ## Create object to pass in to assignment function
+                    assign_obj = {}
+                    if "assignments" in repo_data:
+                        assign_obj['assignments'] = repo_data['assignments']
+                    repo_data.pop('assignments', None)
+
+                    ## Get Powershell script with query parameter
+                    mem_powershellScript = makeapirequest(endpoint, token,q_param)
+
+                    ## If Powershell script exists, continue
+                    if mem_powershellScript['value']:
+                        print("-" * 90)
+                        q_param = None
+                        ## Get Powershell script details
+                        mem_data = makeapirequest(endpoint + "/" + mem_powershellScript['value'][0]['id'], token,q_param)
+                        pid = mem_data['id']
+                        ## Remove keys before using DeepDiff
+                        remove_keys = {'id', 'createdDateTime','version','lastModifiedDateTime'}
+                        for k in remove_keys:
+                            mem_data.pop(k, None)
+
+                        ## Check if assignment needs updating and apply chanages
+                        if assignment == True:
+                            add_assignment(endpoint, assign_obj,pid,token,script=True)
+
+                        ## Check if script data is saved and read the file
+                        if os.path.exists(configpath + "/Script Data/" + repo_data['fileName']):
+                            with open(configpath + "/Script Data/" + repo_data['fileName'], 'r') as f:
+                                repo_payload_config = f.read()
+
+                            mem_payload_config = base64.b64decode(
+                                mem_data['scriptContent']).decode('utf-8')
+
+                            pdiff = DeepDiff(mem_payload_config, repo_payload_config, ignore_order=True).get('values_changed', {})
+                            cdiff = DeepDiff(mem_data, repo_data, ignore_order=True, exclude_paths="root['scriptContent']").get('values_changed', {})
+
+                            ## If any changed values are found, push them to Intune
+                            if pdiff or cdiff:
+                                if cdiff:
+                                    print("Updating Powershell script: " + \
+                                          repo_data['displayName'] + ", values changed:")
+                                    for key, value in cdiff.items():
+                                        setting = re.search(
+                                            "\[(.*)\]", key).group(1)
+                                        new_val = value['new_value']
+                                        old_val = value['old_value']
+                                        print(
+                                            f"Setting: {setting}, New Value: {new_val}, Old Value: {old_val}")
+                                if pdiff:
+                                    print(
+                                        "Script changed, check commit history for change details")
+                                powershell_bytes = repo_payload_config.encode(
+                                    'utf-8')
+                                repo_data['scriptContent'] = base64.b64encode(
+                                    powershell_bytes).decode('utf-8')
+                                request_data = json.dumps(repo_data)
+                                makeapirequestPatch(endpoint + "/" + pid, token,q_param,request_data)
+                            else:
+                                print(
+                                    'No difference found for Powershell script: ' + repo_data['displayName'])
+
+                    ## If Powershell script does not exist, create it and assign
+                    else:
+                        print("-" * 90)
+                        print(
+                            "Powershell script not found, creating script: " + repo_data['displayName'])
+                        request_json = json.dumps(repo_data)
+                        post_request = makeapirequestPost(endpoint, token,q_param=None,jdata=request_json,status_code=201)
+                        add_assignment(endpoint, assign_obj,post_request['id'],token,script=True)
+                        print("Powershell script created with id: " + post_request['id'])
