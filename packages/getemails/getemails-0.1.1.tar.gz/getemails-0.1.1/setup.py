534f5b4e47bd52ebed98b91ab3e30b323d53b03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package getemails/0.1.1/getemails-0.1.1.tar.gz

---
+++ packages/getemails/getemails-0.1.1.tar.gz/getemails-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='getemails',
+    version='0.1.1',    
+    description='Python module for generating random emails',
+    long_description='Python module for generating random emails and names and surnames',
+    long_description_content_type='text/x-rst',
+    
+    url='https://github.com/hendalf332/genemails',
+    author='Hendalf332',
+    author_email='ludvih177xe@tutanota.com',
+    license='GPL',
+    packages=['genemails'],
+    install_requires=['colorama>=0.4.4',                   
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',        
+    ],
+)
