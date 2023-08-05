15a83c53e21ec07563dd60ecfdaa0da0a3e23c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:55 +0000

    Add package pdtypes/0.0.3/pdtypes-0.0.3-py3-none-any.whl

---
+++ packages/pdtypes/pdtypes-0.0.3-py3-none-any.whl/pdtypes/groupby.py
@@ -0,0 +1,39 @@
+from pandas import DataFrame
+from pandas.core.groupby import DataFrameGroupBy
+
+orig_dataframegroupby_repr_html_ = getattr(
+    DataFrameGroupBy,
+    "_repr_html_",
+    None,
+)
+
+orig_dataframegroupby__repr__ = DataFrameGroupBy.__repr__
+
+
+def dataframegroupby_repr_html_(self, *args, **kwargs) -> str:
+    self.obj.attrs["_html_footer"] = getattr(
+        self,
+        "_html_footer",
+        (
+            "<p>Groups: "
+            f"{', '.join((str(name) for name in self.grouper.names))} "
+            f"(n={self.grouper.ngroups})</p>"
+        ),
+    )
+    out = DataFrame._repr_html_(self.obj, *args, **kwargs)
+    del self.obj.attrs["_html_footer"]
+    return out
+
+
+def dataframegroupby__repr__(self, *args, **kwargs) -> str:
+    self.obj.attrs["_str_footer"] = getattr(
+        self,
+        "_str_footer",
+        (
+            f"[Groups: {', '.join((str(name) for name in self.grouper.names))} "
+            f"(n={self.grouper.ngroups})]"
+        ),
+    )
+    out = DataFrame.__repr__(self.obj, *args, **kwargs)
+    del self.obj.attrs["_str_footer"]
+    return out
