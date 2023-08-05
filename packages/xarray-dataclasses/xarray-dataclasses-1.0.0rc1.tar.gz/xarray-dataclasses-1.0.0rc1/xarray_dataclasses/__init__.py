3b361101ea2cbb4f2bfbd579b78de6cf9d81d31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:31 +0000

    Add package xarray-dataclasses/1.0.0rc1/xarray-dataclasses-1.0.0rc1.tar.gz

---
+++ packages/xarray-dataclasses/xarray-dataclasses-1.0.0rc1.tar.gz/xarray-dataclasses-1.0.0rc1/xarray_dataclasses/__init__.py
@@ -0,0 +1,35 @@
+# type: ignore
+__author__ = "Akio Taniguchi"
+__version__ = "1.0.0-rc.1"
+
+
+# for Python 3.7 and 3.8
+def _make_field_generic():
+    from dataclasses import Field
+    from typing import Sequence
+
+    GenericAlias = type(Sequence[int])
+    Field.__class_getitem__ = classmethod(GenericAlias)
+
+
+_make_field_generic()
+
+
+# submodules
+from . import dataarray
+from . import dataset
+from . import datamodel
+from . import dataoptions
+from . import typing
+
+
+# aliases
+from .dataarray import *
+from .dataset import *
+from .datamodel import *
+from .dataoptions import *
+from .typing import *
+
+
+# for Sphinx docs
+__all__ = dir()
