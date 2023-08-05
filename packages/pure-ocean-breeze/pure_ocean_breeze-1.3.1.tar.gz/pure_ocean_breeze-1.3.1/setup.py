5959ef1c5e5d69b1c00f599747d79ebce2a64a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:24 +0000

    Add package pure-ocean-breeze/1.3.1/pure_ocean_breeze-1.3.1.tar.gz

---
+++ packages/pure-ocean-breeze/pure_ocean_breeze-1.3.1.tar.gz/pure_ocean_breeze-1.3.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import setuptools
+setup(
+    name='pure_ocean_breeze',
+    version='1.3.1',
+    description='芷琦哥的回测框架',
+    author='chenzongwei',
+    author_email='17695480342@163.com',
+    py_modules=['stars','initialize'],
+    url='https://github.com/chen-001/pure_ocean_breeze.git',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    requires=[]
+)
