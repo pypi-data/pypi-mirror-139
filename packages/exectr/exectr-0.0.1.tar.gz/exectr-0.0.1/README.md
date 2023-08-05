22bb2966515d78f9cc2a96a23f77d89eaa1d3765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:46 +0000

    Add package exectr/0.0.1/exectr-0.0.1.tar.gz

---
+++ packages/exectr/exectr-0.0.1.tar.gz/exectr-0.0.1/README.md
@@ -0,0 +1,21 @@
+
+examples shell script
+
+```
+# executor set-dependent
+touch ./this
+touch ./that
+
+# executor if 7
+cat ./that > ./those
+```
+
+script.sh.executor
+
+```
+original script
+
+log output
+
+per-line status (untreated, executed, skipped), return code, output
+```
---
+++ packages/exectr/exectr-0.0.1.tar.gz/exectr-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="exectr",
+    description="The shell-script executor you didn't know you needed",
+    author='Daniel Dugas',
+    version='0.0.1',
+    packages=[],
+    scripts=['scripts/executor'],
+    python_requires='>=3.6, <3.7',
+    install_requires=[
+        "pexpect",
+        "strictfire",
+        "Pygments",
+    ],
+)
