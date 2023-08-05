51593fc5b48ea8647592edb4791ebeac8a0fad69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package repository-orm/0.11.0/repository_orm-0.11.0-py3-none-any.whl

---
+++ packages/repository-orm/repository_orm-0.11.0-py3-none-any.whl/repository_orm/model.py
@@ -0,0 +1,102 @@
+"""Store the common business model of all entities."""
+
+import os
+from datetime import datetime
+from typing import Any, AnyStr, Generic, Optional, Union
+
+from pydantic import BaseModel, PrivateAttr
+
+from .exceptions import FileContentNotLoadedError
+
+EntityID = Union[int, str]
+
+
+class Entity(BaseModel):
+    """Model of any object no defined by it's attributes whom instead has an identity.
+
+    Unlike value objects, they have *identity equality*. We can change their values, and
+    they are still recognizably the same thing.
+
+    An entity with a negative id means that the id needs to be set by the repository.
+    """
+
+    id_: EntityID = -1
+    _model_name: str = PrivateAttr()
+
+    def __init__(self, **data: Any) -> None:
+        """Set the _model_name attribute."""
+        super().__init__(**data)
+        self._model_name = self.__class__.__name__
+
+    def __lt__(self, other: "Entity") -> bool:
+        """Assert if an object is smaller than us.
+
+        Args:
+            other: Entity to compare.
+        """
+        return str(self.id_) < str(other.id_)
+
+    def __gt__(self, other: "Entity") -> bool:
+        """Assert if an object is greater than us.
+
+        Args:
+            other: Entity to compare.
+        """
+        return str(self.id_) > str(other.id_)
+
+    def __hash__(self) -> int:
+        """Create an unique hash of the class object."""
+        return hash(f"{self._model_name}-{self.id_}")
+
+
+class File(Entity, Generic[AnyStr]):
+    """Model a computer file."""
+
+    path: str
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    owner: Optional[str] = None
+    group: Optional[str] = None
+    permissions: Optional[str] = None
+
+    # The use of a private attribute and the impossibility of loading the content
+    # at object creation will be fixed on Pydantic 1.9.
+    # We will be able to define the excluded attribute content in the Config of the
+    # model.
+    #
+    # For more information on how to improve this code, read this:
+    # https://lyz-code.github.io/blue-book/coding/python/pydantic/#define-fields-to-exclude-from-exporting-at-config-level # noqa:E501
+    _content: Optional[AnyStr] = PrivateAttr(None)
+    # If the content is of type bytes
+    is_bytes: bool = False
+
+    @property
+    def basename(self) -> str:
+        """Return the name of the file."""
+        return os.path.basename(self.path)
+
+    @property
+    def dirname(self) -> str:
+        """Return the name of the file."""
+        return os.path.dirname(self.path)
+
+    @property
+    def extension(self) -> str:
+        """Return the name of the file."""
+        return self.basename.split(".")[-1]
+
+    @property
+    def content(self) -> AnyStr:
+        """Return the content of the file.
+
+        Returns:
+            The content of the file.
+
+        Raises:
+            FileContentNotLoadedError: if the content is not yet loaded.
+        """
+        if self._content is None:
+            raise FileContentNotLoadedError(
+                "The content of the file has not been loaded yet."
+            )
+        return self._content
