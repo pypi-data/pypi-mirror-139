025dfb0e16bb781b81db8d864e20bb6b949e4c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:06 +0000

    Add package nationality-predictor/1.3.1/nationality_predictor-1.3.1.tar.gz

---
+++ packages/nationality-predictor/nationality_predictor-1.3.1.tar.gz/nationality_predictor-1.3.1/nationality_predictor/nationality_predictor.py
@@ -0,0 +1,32 @@
+import requests, pycountry, json
+
+def predict(name=""):
+    if name != "":
+        url = f"https://api.nationalize.io?name={name}"
+
+        req = requests.get(url)
+        result = req.json()
+
+        countries = result["country"]
+        
+        if countries != []:
+            for country in countries:
+                country["country_id"] = pycountry.countries.get(alpha_2=country["country_id"]).name
+                country["country_name"] = country.pop("country_id") 
+
+            data = {
+                'name': result['name'],
+                'countries': result['country']
+            }
+
+        status_code = req.status_code
+
+        if status_code == 422:
+            raise ValueError("Invalid name.")
+        elif status_code == 200:
+            return json.dumps(data, indent=4, ensure_ascii=False)
+                
+    else:
+        raise ValueError("Name is not defined.")
+
+print(predict("Thomas"))
