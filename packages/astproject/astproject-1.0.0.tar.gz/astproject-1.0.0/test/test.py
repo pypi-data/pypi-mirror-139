469525e5b84f54e244d95ecdc33c0befddcf6033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package astproject/1.0.0/astproject-1.0.0.tar.gz

---
+++ packages/astproject/astproject-1.0.0.tar.gz/astproject-1.0.0/test/test.py
@@ -0,0 +1,19 @@
+import pytest
+from hw1.src.fibonacci import get_n_fibonacci_number
+
+
+def test_fibonacci(sequence):
+    """
+    Test checks that first 10 fibonacci numbers are computed correctly.
+    """
+    key, value = sequence
+    assert value == get_n_fibonacci_number(key)
+
+
+@pytest.fixture(params=[(1, 0), (2, 1), (3, 1), (4, 2), (5, 3), (6, 5), (7, 8), (8, 13), (9, 21), (10, 34)])
+def sequence(request):
+    """
+    Consistently yields 10 tuples of numbers (fibonacci sequence number and fibonacci number).
+    """
+    yield request.param
+
