ca0f1c0f6dcb0a772da0a67dea2475defa751cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:13 +0000

    Add package python-rj-app/0.1.0/python-rj-app-0.1.0.tar.gz

---
+++ packages/python-rj-app/python-rj-app-0.1.0.tar.gz/python-rj-app-0.1.0/src/rj_api/media.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+from base_request import RjBaseRequest
+
+
+class MP3(RjBaseRequest):
+    """
+    Class to operate on mp3 files
+    """
+
+    def get_playlist(self) -> Dict:
+        """
+        Get user active play lost
+
+        :return:
+        :rtype: Dict
+        """
+
+        return self.get("https://r-j-app-desk.com/api2/playlists_dash", True)
+
+    def info(self, mp3_id: int):
+        """
+
+        :param mp3_id:
+        :return:
+        """
+
+        return self.get(f"https://r-j-app-desk.com/api2/mp3?id={mp3_id}", True)
+
+    def vote(self, mp3_id: int):
+        res = self.get(f"https://r-j-app-desk.com/api2/mp3_vote?id={mp3_id}&type=mp3&vote=5", True)
+        return res
