9c1d8bc4b073d586f2891e7ca757f1b804c97590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:43 +0000

    Add package python-hw/0.0.0/python_hw-0.0.0.tar.gz

---
+++ packages/python-hw/python_hw-0.0.0.tar.gz/python_hw-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_hw',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Konstantin.Tretiakov',
+    author_email='pukoh@mail.ru',
+    description=''
+)
