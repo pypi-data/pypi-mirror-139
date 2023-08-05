1f417566311d9167805a8cec04a37aa55290bbda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:02 +0000

    Add package quaterion-models/0.1.5/quaterion-models-0.1.5.tar.gz

---
+++ packages/quaterion-models/quaterion-models-0.1.5.tar.gz/quaterion-models-0.1.5/quaterion_models/encoders/encoder.py
@@ -0,0 +1,73 @@
+from torch import Tensor
+from torch import nn
+from torch.utils.data.dataloader import default_collate
+
+from quaterion_models.types import TensorInterchange, CollateFnType
+
+
+class Encoder(nn.Module):
+    """
+    Base class for encoder abstraction
+    """
+
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def disable_gradients_if_required(self):
+        """
+        Disables gradients of the model if it is declared as not trainable
+        :return:
+        """
+        if not self.trainable():
+            for key, weights in self.named_parameters():
+                weights.requires_grad = False
+
+    def trainable(self) -> bool:
+        """
+        Defines if encoder is trainable. This flag affects caching and checkpoint saving of the encoder.
+        :return: bool
+        """
+        raise NotImplementedError()
+
+    def embedding_size(self) -> int:
+        """
+        :return: Size of resulting embedding
+        """
+        raise NotImplementedError()
+
+    def get_collate_fn(self) -> CollateFnType:
+        """
+        Provides function that converts raw data batch into suitable model input
+
+        :return: Model input
+        """
+        return default_collate
+
+    def forward(self, batch: TensorInterchange) -> Tensor:
+        """
+        Infer encoder - convert input batch to embeddings
+
+        :param batch: processed batch
+        :return: embeddings, shape: [batch_size x embedding_size]
+        """
+        raise NotImplementedError()
+
+    def save(self, output_path: str):
+        """
+        Persist current state to the provided directory
+
+        :param output_path:
+        :return:
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def load(cls, input_path: str) -> "Encoder":
+        """
+        Instantiate encoder from saved state.
+        If no state required - just call `create` instead
+
+        :param input_path:
+        :return:
+        """
+        raise NotImplementedError()
