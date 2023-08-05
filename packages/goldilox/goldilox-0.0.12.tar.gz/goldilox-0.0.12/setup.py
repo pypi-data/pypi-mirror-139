7dfce060646ac52e2ca40959e66ddd05afef3798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:35 +0000

    Add package goldilox/0.0.12/goldilox-0.0.12.tar.gz

---
+++ packages/goldilox/goldilox-0.0.12.tar.gz/goldilox-0.0.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from goldilox.config import GOLDILOX_VERSION
+
+packages = find_packages()
+setup(
+    name="goldilox",
+    packages=packages,
+    include_package_data=True,
+    install_requires=["scikit-learn", "pandas", "numpy", "cloudpickle", "traitlets",
+                      "gunicorn", "Click", "fastapi", "uvicorn", "pickle5"],
+    version=GOLDILOX_VERSION,
+    url="https://github.com/xdssio/goldilox",
+    description="A tool for deploying machine learning",
+    author="Yonatan Alexander",
+    author_email="jonathan@xdss.io",
+    entry_points="""
+        [console_scripts]
+        glx=goldilox.app.cli:main
+    """,
+    python_requires='>=3',
+)
