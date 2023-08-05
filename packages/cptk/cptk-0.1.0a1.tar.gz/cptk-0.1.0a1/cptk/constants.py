853d885a90ef27013c4c3971c41cd972cccbbbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:33 +0000

    Add package cptk/0.1.0a1/cptk-0.1.0a1.tar.gz

---
+++ packages/cptk/cptk-0.1.0a1.tar.gz/cptk-0.1.0a1/cptk/constants.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+RECIPE_FILE = 'recipes.cptk.yaml'
+PROJECT_FILE = '.cptk/project.cptk.yaml'
+LAST_FILE = '.cptk/stayaway/last.cptk.txt'
+
+MOVE_FILE = '.cptk/moves.cptk.txt'
+MOVE_FILE_SEPERATOR = '::'
+MOVE_SAFES = ['./', '**/.cptk/**']
+
+LAST_FILE_SEPERATOR = '::'
