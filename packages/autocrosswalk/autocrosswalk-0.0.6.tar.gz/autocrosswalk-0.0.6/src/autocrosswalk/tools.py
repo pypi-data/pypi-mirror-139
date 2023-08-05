2f2aa3ba0257f44d8a3a69e61cf9934dfcf0a67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:06 +0000

    Add package autocrosswalk/0.0.6/autocrosswalk-0.0.6.tar.gz

---
+++ packages/autocrosswalk/autocrosswalk-0.0.6.tar.gz/autocrosswalk-0.0.6/src/autocrosswalk/tools.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+# Libraries
+#------------------------------------------------------------------------------
+# Standard
+import os
+import pandas as pd
+
+#------------------------------------------------------------------------------
+# Main
+#------------------------------------------------------------------------------
+def load_example_data():
+    
+    # PATHS
+    THIS_DIR = __file__.rsplit("/", maxsplit=1)[0]+"/"
+    DATA_DIR = os.path.join(THIS_DIR, "../data/")
+        
+    df = pd.read_parquet(path=os.path.join(DATA_DIR,"example_data.parquet"))
+    
+    return df
