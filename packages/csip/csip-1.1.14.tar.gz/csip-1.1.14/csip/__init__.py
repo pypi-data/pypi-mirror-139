0eeb297e485986d07ac4cae17944063ea88e7432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package csip/1.1.14/csip-1.1.14.tar.gz

---
+++ packages/csip/csip-1.1.14.tar.gz/csip-1.1.14/csip/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '1.1.14' 
+__built__ = '1.1.14 f788f8e61243a390db368ca5464cc70f5048f4ff 2022-02-16 17:12 -0700 od'
+
+from .model_data_service import ModelDataService
+from .utils import Client
