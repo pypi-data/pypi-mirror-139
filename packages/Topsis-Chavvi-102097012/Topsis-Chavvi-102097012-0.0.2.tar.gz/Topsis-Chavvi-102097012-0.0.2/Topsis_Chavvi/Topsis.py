0fe5d90970aa2e26d930918104cace08f8d2ff15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:44 +0000

    Add package Topsis-Chavvi-102097012/0.0.2/Topsis-Chavvi-102097012-0.0.2.tar.gz

---
+++ packages/Topsis-Chavvi-102097012/Topsis-Chavvi-102097012-0.0.2.tar.gz/Topsis-Chavvi-102097012-0.0.2/Topsis_Chavvi/Topsis.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 12:10:28 2022
+
+@author: chavvi
+"""
+
+import sys
+import os.path
+import pandas as pd
+import topsispy as tp
+import numpy as np
+from pandas.api.types import is_numeric_dtype
+
+def topsis(data,weight,impact,output):
+        i=pd.read_csv(data)
+        table=i.drop(columns=['Fund Name'])
+        arr = table.to_numpy()
+        topsis = tp.topsis(arr, weight, impact)
+        table = pd.DataFrame(arr)
+        table.insert(0,"Fund Name",i["Fund Name"])
+        table = table.rename(columns={0:"P1",1:'P2',2:'P3',3:'P4',4:'P5'})
+        table['Topsis Score'] = topsis[1]
+        j = 1
+        t = topsis[1]
+        t = list(t)
+        rank={}
+        while len(t)!=0:
+            rank[max(t)] = j
+            t.remove(max(t))
+            j = j + 1
+            
+        table['Rank'] = table["Topsis Score"].map(rank)
+        table.to_csv(output)
+def main():
+    data = sys.argv[1]
+    impact = sys.argv[3]
+    output = sys.argv[4]
+    
+    ##########
+    if(os.path.exists(data)==False):
+        print("INPUT FILE DOES NOT EXIST")
+        exit(1)
+    ##########
+    
+    
+    ##########
+    if len(sys.argv)!=5:
+        print("5 ARGUMENTS NEEDED!")
+        exit(1)
+    ##########
+    i=pd.read_csv(data)
+    
+    ##########
+    if len(i.columns)<3:
+        print("INPUT FILE MUST CONATIN 3 OR MORE COLUMNS")
+        exit(1)
+    ##########
+    for j in range(1,len(i.columns)):
+        if(is_numeric_dtype(i.iloc[:,j])==False):
+            print(i+"th COLUMN MUST CONATIN NUMERIC VALUES ONLY")
+    ##########
+    try:
+        weight = sys.argv[2]
+        weight = list(map(int, weight.split(',')))
+    except:
+        print("The weights should be sepearted by commas")
+        exit(0)
+        
+    try:
+        impact = impact.split(',')
+    except:
+        print("The impacts should be sepearted by commas")
+        exit(0)
+        
+    ##########
+    for j in range(len(impact)):
+        if impact[j]=="+" or impact[j]=="-":
+            continue
+        else:
+            print("IMPACTS MUST BE EITHER +VE OR -VE")
+            exit(1)
+    ##########
+    impact = list(map(lambda x:x.replace('+','1'),impact))
+    impact = list(map(lambda x:x.replace('-','-1'),impact))
+    impact = [int(i) for i in impact]
+    
+    ##########
+    if((len(weight)==len(impact) and len(weight)==len(i.columns)-1)==False):
+        print("Number of weights, number of impacts and number of columns (from 2nd to last columns) must be same")
+        exit(1)
+    ##########
+    topsis(data,weight,impact,output)
+    
+if __name__=="__main__":
+	main()
