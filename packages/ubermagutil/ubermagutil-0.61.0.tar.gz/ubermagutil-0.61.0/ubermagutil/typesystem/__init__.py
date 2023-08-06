102b32a4f6da2a98f46144abdbe85fb71261a74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:11 +0000

    Add package ubermagutil/0.61.0/ubermagutil-0.61.0.tar.gz

---
+++ packages/ubermagutil/ubermagutil-0.61.0.tar.gz/ubermagutil-0.61.0/ubermagutil/typesystem/__init__.py
@@ -0,0 +1,4 @@
+"""Typesystem utility."""
+from .descriptors import (Descriptor, Dictionary, Name, Parameter, Scalar,
+                          Subset, Typed, Vector)
+from .typesystem import typesystem
