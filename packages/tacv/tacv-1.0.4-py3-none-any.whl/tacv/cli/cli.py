8a0632a8d9ae8a6007f23bd3cd5ebfec5429aaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:33 +0000

    Add package tacv/1.0.4/tacv-1.0.4-py3-none-any.whl

---
+++ packages/tacv/tacv-1.0.4-py3-none-any.whl/tacv/cli/cli.py
@@ -0,0 +1,12 @@
+import sys
+from ..video import video2images, images2video
+
+
+def cli_images2video():
+    args = sys.argv
+    images2video(*args)
+
+
+def cli_video2images():
+    args = sys.argv
+    video2images(*args)
