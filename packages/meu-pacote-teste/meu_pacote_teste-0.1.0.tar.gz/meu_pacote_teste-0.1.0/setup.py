97e358a66332398ae6af504847603fabbfefee49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:08 +0000

    Add package meu-pacote-teste/0.1.0/meu_pacote_teste-0.1.0.tar.gz

---
+++ packages/meu-pacote-teste/meu_pacote_teste-0.1.0.tar.gz/meu_pacote_teste-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='meu_pacote_teste', 
+    version = "0.1.0",
+    author = "Anthony Jatobá",
+    author_email = "anthonyjatoba@gmail.com",
+    description = ("Teste de setuptools"),    
+    packages=['meu_pacote']
+)
