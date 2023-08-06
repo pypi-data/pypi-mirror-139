7415b71aedff4142dc98c72d4e0e3f20c0514fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:11 +0000

    Add package baselooper/0.0.2/baselooper-0.0.2.tar.gz

---
+++ packages/baselooper/baselooper-0.0.2.tar.gz/baselooper-0.0.2/README.md
@@ -0,0 +1,3 @@
+# baselooper
+
+The documentation will be [here](https://baselooper.readthedocs.io).
---
+++ packages/baselooper/baselooper-0.0.2.tar.gz/baselooper-0.0.2/src/baselooper/__init__.py
@@ -0,0 +1,7 @@
+VERSION = "0.0.2"
+
+from baselooper.state import State
+from baselooper.module import Module, ModuleConfig, SeededModule, SeededModuleConfig, NOP, NOPConfig, ModuleList, ModuleListConfig
+from baselooper.looper import Looper, LooperConfig, LooperState, LooperIterationStop, LooperIterationStopConfig
+
+import baselooper.logging
