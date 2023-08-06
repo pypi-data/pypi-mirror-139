b04bc3d6b8076a7e0a58bd29bbbcaf5412440871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package warez/0.0.1/warez-0.0.1.tar.gz

---
+++ packages/warez/warez-0.0.1.tar.gz/warez-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['warez', 'warez.pkg', 'warez.pkg.licensing', 'warez.src']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'warez',
+    'version': '0.0.1',
+    'description': 'Decentralized software development',
+    'long_description': None,
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@ragt.ag',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
