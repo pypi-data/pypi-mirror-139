8579284f9cd209f9af1d7637a32c61f73702fa74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:07 +0000

    Add package vcoml/1.0.0/vcoml-1.0.0.tar.gz

---
+++ packages/vcoml/vcoml-1.0.0.tar.gz/vcoml-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vcoml']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vcoml',
+    'version': '1.0.0',
+    'description': 'Imagine a markup language, but worse',
+    'long_description': '# vcoml\n\nImagine a markup language, but worse\n\n## Syntax Example\n\nVcoML:\n\n```vcoml\n:"members"\n  :297045071457681409\n    :"username"\n      "vcokltfre"\n    :"discriminator"\n      6868\n    :"mfa"\n      ya\n    :"verified"\n      na\n    :"email"\n      idk\n    :"roles"\n      >\n        :"id"\n          1234\n        :"name"\n          "Admin"\n      >\n        :"id"\n          5678\n        :"name"\n          "Moderator"\n```\n\nYAML:\n\n```yml\nmembers:\n  297045071457681409:\n    username: vcokltfre\n    discriminator: 6868\n    mfa: true\n    verified: false\n    email: null\n    roles:\n      - id: 1234\n        name: Admin\n      - id: 5678\n        name: Moderator\n```\n\n## Usage\n\n```py\nfrom vcoml import unpack\n\n\ndata = unpack("""\n:"abc"\n  123\n:"def"\n  >\n    "list"\n  >\n    "more list"\n""")\n\nprint(data)\n```\n\n## Todo\n\n- Implement packing\n- CLI\n',
+    'author': 'vcokltfre',
+    'author_email': 'vcokltfre@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/vcokltfre/vcoml',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
