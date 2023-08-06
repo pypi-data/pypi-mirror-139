0ea4294156a0f637833b3ad985a447ca3c97ad27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:30 +0000

    Add package consensual/0.1.0/consensual-0.1.0-py3.7.egg

---
+++ packages/consensual/consensual-0.1.0-py3.7.egg/consensual/core/raft/__init__.py
@@ -0,0 +1,6 @@
+from .hints import Processor
+from .messages import MessageKind
+from .node import Node
+from .receiver import Receiver
+from .sender import (ReceiverUnavailable,
+                     Sender)
---
+++ packages/consensual/consensual-0.1.0-py3.7.egg/consensual/raft.py
@@ -0,0 +1,13 @@
+from .core.raft import (MessageKind,
+                        Node,
+                        Processor,
+                        Receiver,
+                        ReceiverUnavailable,
+                        Sender)
+
+MessageKind = MessageKind
+Node = Node
+Processor = Processor
+Receiver = Receiver
+ReceiverUnavailable = ReceiverUnavailable
+Sender = Sender
