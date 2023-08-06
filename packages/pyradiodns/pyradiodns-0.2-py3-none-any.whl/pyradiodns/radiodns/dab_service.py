060370572fb7d07b83cd1d0e47e493308a6d9449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:16 +0000

    Add package pyradiodns/0.2/pyradiodns-0.2-py3-none-any.whl

---
+++ packages/pyradiodns/pyradiodns-0.2-py3-none-any.whl/pyradiodns/radiodns/dab_service.py
@@ -0,0 +1,2 @@
+# maps the legacy module/class name to the new one
+from pyradiodns.bearer import DABBearer as RadioDNS_DABService
---
+++ packages/pyradiodns/pyradiodns-0.2-py3-none-any.whl/pyradiodns/radiodns/hd_service.py
@@ -0,0 +1,2 @@
+# maps the legacy module/class name to the new one
+from pyradiodns.bearer import HDBearer as RadioDNS_HDService
