709e7b358ace9d00b2bd735533226497f770054b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:14 +0000

    Add package zm-au/1.2.0/zm_au-1.2.0-py3-none-any.whl

---
+++ packages/zm-au/zm_au-1.2.0-py3-none-any.whl/zm_au/__init__.py
@@ -0,0 +1,14 @@
+from .base_au import BaseAU
+from .pip_github_au import PipGitHubAU
+from .pip_au import PipAU
+
+from zetuptools import PipPackage
+
+package_name = "au"
+
+__version__ = '1.2.0'
+if __version__ == "0.0.0":
+    try:
+        __version__ = PipPackage(package_name).version
+    except FileNotFoundError:
+        pass
