9796879f10fac086a82627a0616c194df16b8fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:37 +0000

    Add package mopidy-ytmusic/0.3.4/Mopidy-YTMusic-0.3.4.tar.gz

---
+++ packages/mopidy-ytmusic/Mopidy-YTMusic-0.3.4.tar.gz/Mopidy-YTMusic-0.3.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mopidy_ytmusic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Mopidy>=3,<4', 'pytube>=12.0.0,<13.0.0', 'ytmusicapi>=0.20.0,<0.21.0']
+
+entry_points = \
+{'mopidy.ext': ['ytmusic = mopidy_ytmusic:Extension']}
+
+setup_kwargs = {
+    'name': 'mopidy-ytmusic',
+    'version': '0.3.4',
+    'description': 'Mopidy extension for playling music/managing playlists in Youtube Music',
+    'long_description': None,
+    'author': 'Ozymandias (Tomas Ravinskas)',
+    'author_email': 'tomas.rav@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
