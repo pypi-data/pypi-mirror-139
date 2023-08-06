d11c3804059e1a28cbde2d75610fcf3513ddbe65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:54 +0000

    Add package FireSVM/0.0.3/FireSVM-0.0.3.tar.gz

---
+++ packages/FireSVM/FireSVM-0.0.3.tar.gz/FireSVM-0.0.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='FireSVM',
+      description='Library containing different variants of the SVM algorithm',
+      version = "0.0.3",
+      author='Ayman Lafaz',
+      author_email='ayman.lafaz@um5r.ac.ma',
+      packages=find_packages(where='.'),
+     )
