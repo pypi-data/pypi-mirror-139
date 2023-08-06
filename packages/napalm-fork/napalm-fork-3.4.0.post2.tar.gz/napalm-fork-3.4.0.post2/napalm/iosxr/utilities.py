9e331709fc2c68ca5749bec05432c3f6f2b63ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:30 +0000

    Add package napalm-fork/3.4.0.post2/napalm-fork-3.4.0.post2.tar.gz

---
+++ packages/napalm-fork/napalm-fork-3.4.0.post2.tar.gz/napalm-fork-3.4.0.post2/napalm/iosxr/utilities.py
@@ -0,0 +1,13 @@
+try:
+    import regex as re
+except ModuleNotFoundError:
+    import re
+
+
+def strip_config_header(config):
+    """Normalize items that should not show up in IOS-XR compare_config."""
+    config = re.sub(r"^Building config.*\n!! IOS.*", "", config, flags=re.M)
+    config = config.strip()
+    config = re.sub(r"^!!.*", "", config)
+    config = re.sub(r"end$", "", config)
+    return config.strip()
