ecbd11c3053c28d50fa45e2f6a98bf8ff0fea9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:31 +0000

    Add package baas-web/1.0.3/baas_web-1.0.3.tar.gz

---
+++ packages/baas-web/baas_web-1.0.3.tar.gz/baas_web-1.0.3/config/setting.py
@@ -0,0 +1,14 @@
+#-*- coding:utf8 -*-
+'''
+Created on 2021��12��20��
+
+@author: mrray
+'''
+
+import os
+
+
+# 加载配置文件
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEST_CONFIG = os.path.join(BASE_DIR, 'config', 'config.ini')
