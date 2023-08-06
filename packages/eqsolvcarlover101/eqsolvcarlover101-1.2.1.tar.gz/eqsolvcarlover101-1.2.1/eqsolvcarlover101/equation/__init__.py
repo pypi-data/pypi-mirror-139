3bba4888b30b1a7466336f343aca31d211b83fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:51 +0000

    Add package eqsolvcarlover101/1.2.1/eqsolvcarlover101-1.2.1.tar.gz

---
+++ packages/eqsolvcarlover101/eqsolvcarlover101-1.2.1.tar.gz/eqsolvcarlover101-1.2.1/eqsolvcarlover101/equation/__init__.py
@@ -0,0 +1,5 @@
+from .quadsolve import quadsolve
+from .slopeint import slopeint
+from .commands import commands
+from .help import help
+from .midpoint import midpoint
---
+++ packages/eqsolvcarlover101/eqsolvcarlover101-1.2.1.tar.gz/eqsolvcarlover101-1.2.1/eqsolvcarlover101/equation/commands.py
@@ -0,0 +1,9 @@
+class commands():
+  def __init__(self):
+    import os
+    if os.name == "nt":
+      os.system("cls")
+    else:
+      os.system("clear")
+    print("The commands are: \neqsolvcarlover101.quadsolve() - For quadratic equations. \neqsolvcarlover101.slopeint() - For slope-intercept equations. \neqsolvcarlover101.midpoint() - For finding the midpoint of two points. 
+\neqsolvcarlover101.help() - Brings up the help page.")
