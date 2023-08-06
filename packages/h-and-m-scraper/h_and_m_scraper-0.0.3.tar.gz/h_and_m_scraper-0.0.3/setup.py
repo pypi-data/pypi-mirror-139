096aff3981fd8ddf851dc24c1bfa9104a5af1fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package h-and-m-scraper/0.0.3/h_and_m_scraper-0.0.3.tar.gz

---
+++ packages/h-and-m-scraper/h_and_m_scraper-0.0.3.tar.gz/h_and_m_scraper-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'h_and_m_scraper',
+    version = '0.0.3',
+    description = 'A scraper of the H&M website for both mens and womens clothes...',
+    long_description = 'A scraper of the H&M website for both mens and womens clothes...',
+    url = 'https://github.com/IvanYingX/Urbanoutfitters-Scraper-Project',
+    author = 'James Overend, Quratulaen Ikram, Dan Bouchard',
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = ['boto3', 'selenium', 'sklearn', 'tqdm', 'sqlalchemy', 'pandas', 'psycopg2-binary'],
+)
