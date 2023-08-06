761f6ec2d0612b729e3f8457d999b30d6591b3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:38 +0000

    Add package pyams-skin/1.6.2/pyams_skin-1.6.2-py3.8.egg

---
+++ packages/pyams-skin/pyams_skin-1.6.2-py3.8.egg/pyams_skin/interfaces/__init__.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2015-2019 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""PyAMS_skin.interfaces module
+
+This module provides general skin-related interfaces.
+"""
+
+__docformat__ = 'restructuredtext'
+
+
+BOOTSTRAP_STATUS = ('primary', 'secondary', 'success', 'danger',
+                    'warning', 'info', 'light', 'dark')
+"""Bootstrap status list"""
