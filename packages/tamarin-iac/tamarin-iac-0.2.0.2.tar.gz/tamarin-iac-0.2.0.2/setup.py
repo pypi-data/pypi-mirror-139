eb3485821a0b9f7e4a3144f81b8ce3f0580c04de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:11 +0000

    Add package tamarin-iac/0.2.0.2/tamarin-iac-0.2.0.2.tar.gz

---
+++ packages/tamarin-iac/tamarin-iac-0.2.0.2.tar.gz/tamarin-iac-0.2.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='tamarin-iac',  
+     version='0.2.0.2',
+     author="Alisson Machado",
+     author_email="alisson.itops@gmail.com",
+     description="Infrastructure as code",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/AlissonMMenezes/Tamarin",
+     classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    entry_points = {
+        'console_scripts': ['tamarin=tamarin_iac.tamarin:main'],
+    },
+    install_requires=[
+    "paramiko==2.9.2",
+    "PyYAML==6.0",
+    "setuptools==60.5.0",
+    "terminaltables==3.1.10",
+    ]
+ )
