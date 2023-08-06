b96657c69b23b1d0b1a2c49f8740ff5f4f1b6d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:53 +0000

    Add package favorites-crawler/0.0.6/favorites_crawler-0.0.6-py3-none-any.whl

---
+++ packages/favorites-crawler/favorites_crawler-0.0.6-py3-none-any.whl/favorites_crawler/processors.py
@@ -0,0 +1,31 @@
+import re
+
+from itemloaders.processors import TakeFirst, Identity
+
+from favorites_crawler.constants.blacklists import PIXIV_TAG_KEYWORD_BLACKLIST
+
+
+take_first = TakeFirst()
+identity = Identity()
+
+
+def filter_pixiv_tags(tags):
+    def filter_tag(tag):
+        for v in tag.values():
+            for k in PIXIV_TAG_KEYWORD_BLACKLIST:
+                if v and k in v:
+                    return False
+        return True
+    return [tag for tag in filter(filter_tag, tags)]
+
+
+def get_nhentai_id(url):
+    """Get comic ID from comic url"""
+    match = re.match(r'https.+g/(\d+)/', url)
+    if not match:
+        return ''
+    return match.group(1)
+
+
+def replace_space_with_under_scope(text):
+    return '_'.join(text.split())
