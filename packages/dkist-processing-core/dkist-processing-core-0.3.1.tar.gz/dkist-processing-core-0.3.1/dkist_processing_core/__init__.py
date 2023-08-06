93a164f64a37b8a60751d9757b4562b8aeb33212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:58 +0000

    Add package dkist-processing-core/0.3.1/dkist-processing-core-0.3.1.tar.gz

---
+++ packages/dkist-processing-core/dkist-processing-core-0.3.1.tar.gz/dkist-processing-core-0.3.1/dkist_processing_core/__init__.py
@@ -0,0 +1,11 @@
+from pkg_resources import DistributionNotFound
+from pkg_resources import get_distribution
+
+from dkist_processing_core.task import TaskBase
+from dkist_processing_core.workflow import Workflow
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
---
+++ packages/dkist-processing-core/dkist-processing-core-0.3.1.tar.gz/dkist-processing-core-0.3.1/dkist_processing_core/tests/zero_node_workflow_package/workflow.py
@@ -0,0 +1,5 @@
+from dkist_processing_core import Workflow
+
+zero_node_workflow = Workflow(
+    process_category="invalid", process_name="zero_node_workflow", workflow_package=__package__
+)
