8d38bd243b83d44132e674145674533ccda8cac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:59 +0000

    Add package cdk-toolkit/0.0.68/cdk_toolkit-0.0.68-py3-none-any.whl

---
+++ packages/cdk-toolkit/cdk_toolkit-0.0.68-py3-none-any.whl/cdk_toolkit/stacks/notification_stacks.py
@@ -0,0 +1,50 @@
+from aws_cdk import (
+    Stack,
+    aws_pinpoint as pinpoint
+) 
+from constructs import Construct 
+
+from OpenSSL import crypto
+import base64
+
+
+
+
+
+def get_apns_certificate(cert_path, cert_pw):
+    p12 = crypto.load_pkcs12(open(cert_path, 'rb').read(), cert_pw)
+    pem = crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate())
+    print(pem)
+    print(pem.decode("ascii"))
+    print(pem.decode("utf-8"))
+    print(pem.decode("utf-8").replace("\n","\\n"))
+    message_bytes = pem.decode("utf-8").encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+
+class NotificationStack(Stack): 
+    '''
+    This Stack ...
+    '''
+    def __init__(self, scope: Construct, construct_id: str, pinpoint_app_name: str, apns_bundle_id: str, apns_certificate_path: str, apns_certificate_pw: str,  **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        pinpoint_app = pinpoint.CfnApp(self, pinpoint_app_name, name=pinpoint_app_name)
+
+        apns_certificate = get_apns_certificate(apns_certificate_path, apns_certificate_pw) 
+
+        cfn_aPNSSandbox_channel = pinpoint.CfnAPNSSandboxChannel(self, "{}-apns-sandbox".format(pinpoint_app_name),
+            application_id=pinpoint_app.ref,
+
+            # the properties below are optional
+            bundle_id=apns_bundle_id,
+            certificate=apns_certificate,
+            default_authentication_method="certificate",
+            enabled=True,
+            # private_key=apns_certificate,
+            # team_id="teamId",
+            # token_key="tokenKey",
+            # token_key_id="tokenKeyId"
+        )
