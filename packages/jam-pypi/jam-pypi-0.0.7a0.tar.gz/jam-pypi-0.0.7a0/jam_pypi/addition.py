3db4197f933e13daa19dd143cd86927305a93633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:42 +0000

    Add package jam-pypi/0.0.7a0/jam-pypi-0.0.7a0.tar.gz

---
+++ packages/jam-pypi/jam-pypi-0.0.7a0.tar.gz/jam-pypi-0.0.7a0/jam_pypi/addition.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+Array = np.array
+
+
+def add(x: Array, y: Array) -> Array:
+    """add two Array
+
+    :param x: comments for x
+    :type x: Array
+    :param y: comments for y
+    :type y: Array
+    :return: summation results
+    :rtype: Array
+    """
+    return x + y
---
+++ packages/jam-pypi/jam-pypi-0.0.7a0.tar.gz/jam-pypi-0.0.7a0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jam_pypi', 'jam_pypi.helper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.21.2,<2.0.0']
+
+extras_require = \
+{'exp': ['jamexp>=0.0.10,<0.0.11']}
+
+setup_kwargs = {
+    'name': 'jam-pypi',
+    'version': '0.0.7a0',
+    'description': 'Tesing repo for publish via ci',
+    'long_description': '# A testing pypi repo',
+    'author': 'qsh.27',
+    'author_email': 'qsh.zh27@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
