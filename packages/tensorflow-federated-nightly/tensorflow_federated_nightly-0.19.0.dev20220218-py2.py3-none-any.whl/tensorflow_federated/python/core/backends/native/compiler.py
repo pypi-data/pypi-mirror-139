23b6bda9e059c582f3752249fd37fde7b4baf2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package tensorflow-federated-nightly/0.19.0.dev20220218/tensorflow_federated_nightly-0.19.0.dev20220218-py2.py3-none-any.whl

---
+++ packages/tensorflow-federated-nightly/tensorflow_federated_nightly-0.19.0.dev20220218-py2.py3-none-any.whl/tensorflow_federated/python/core/backends/native/compiler.py
@@ -0,0 +1,89 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Library of compiler functions for usage in the native execution context."""
+
+from typing import Optional
+
+from absl import logging
+import tensorflow as tf
+
+from tensorflow_federated.python.common_libs import tracing
+from tensorflow_federated.python.core.impl.compiler import building_blocks
+from tensorflow_federated.python.core.impl.compiler import transformations
+from tensorflow_federated.python.core.impl.compiler import tree_transformations
+from tensorflow_federated.python.core.impl.computation import computation_impl
+from tensorflow_federated.python.core.impl.wrappers import computation_wrapper_instances
+
+
+def transform_to_native_form(
+    comp: computation_impl.ConcreteComputation,
+    transform_math_to_tf: bool = False,
+    grappler_config: Optional[tf.compat.v1.ConfigProto] = None
+) -> computation_impl.ConcreteComputation:
+  """Compiles a computation for execution in the TFF native runtime.
+
+  This function transforms the proto underlying `comp` by transforming it
+  to call-dominant form (see `tff.framework.to_deduped_call_dominant` for
+  definition).
+
+  Args:
+    comp: Instance of `computation_impl.ConcreteComputation` to compile.
+    transform_math_to_tf: Whether to additional transform math to TensorFlow
+      graphs. Necessary if running on a execution state without
+      ReferenceResolvingExecutors underneath FederatingExecutors.
+    grappler_config: Configuration for Grappler optimizations to perform on the
+      TensorFlow computations. If `None`, Grappler will not be run and no
+      optimizations wil be applied.
+
+  Returns:
+    A new `computation_impl.ConcreteComputation` representing the compiled
+      version of `comp`.
+  """
+  proto = computation_impl.ConcreteComputation.get_proto(comp)
+  computation_building_block = building_blocks.ComputationBuildingBlock.from_proto(
+      proto)
+  try:
+    logging.debug('Compiling TFF computation to CDF.')
+    with tracing.span(
+        'transform_to_native_form', 'to_deduped_call_dominant', span=True):
+      call_dominant_form = transformations.to_deduped_call_dominant(
+          computation_building_block)
+    logging.debug('Computation compiled to:')
+    logging.debug(call_dominant_form.formatted_representation())
+    if transform_math_to_tf:
+      logging.debug('Compiling local computations to TensorFlow.')
+      with tracing.span(
+          'transform_to_native_form',
+          'compile_local_subcomputations_to_tensorflow',
+          span=True):
+        call_dominant_form = transformations.compile_local_subcomputations_to_tensorflow(
+            call_dominant_form)
+      logging.debug('Computation compiled to:')
+      logging.debug(call_dominant_form.formatted_representation())
+    if grappler_config is not None:
+      with tracing.span(
+          'transform_to_native_form', 'optimize_tf_graphs', span=True):
+        call_dominant_form, _ = transformations.optimize_tensorflow_graphs(
+            call_dominant_form, grappler_config)
+    with tracing.span(
+        'transform_to_native_form', 'transform_tf_add_ids', span=True):
+      form_with_ids, _ = tree_transformations.transform_tf_add_ids(
+          call_dominant_form)
+    return computation_wrapper_instances.building_block_to_computation(
+        form_with_ids)
+  except ValueError as e:
+    logging.debug('Compilation for native runtime failed with error %s', e)
+    logging.debug('computation: %s',
+                  computation_building_block.compact_representation())
+    return comp
---
+++ packages/tensorflow-federated-nightly/tensorflow_federated_nightly-0.19.0.dev20220218-py2.py3-none-any.whl/tensorflow_federated/tools/python_package/setup.py
@@ -0,0 +1,138 @@
+# Copyright 2018, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""TensorFlow Federated is an open-source federated learning framework.
+
+TensorFlow Federated (TFF) is an open-source framework for machine learning and
+other computations on decentralized data. TFF has been developed to facilitate
+open research and experimentation with Federated Learning (FL), an approach to
+machine learning where a shared global model is trained across many
+participating clients that keep their training data locally. For example, FL has
+been used to train prediction models for mobile keyboards without uploading
+sensitive typing data to servers.
+
+TFF enables developers to use the included federated learning algorithms with
+their models and data, as well as to experiment with novel algorithms. The
+building blocks provided by TFF can also be used to implement non-learning
+computations, such as aggregated analytics over decentralized data.
+
+TFF's interfaces are organized in two layers:
+
+* Federated Learning (FL) API
+
+  The `tff.learning` layer offers a set of high-level interfaces that allow
+  developers to apply the included implementations of federated training and
+  evaluation to their existing TensorFlow models.
+
+* Federated Core (FC) API
+
+  At the core of the system is a set of lower-level interfaces for concisely
+  expressing novel federated algorithms by combining TensorFlow with distributed
+  communication operators within a strongly-typed functional programming
+  environment. This layer also serves as the foundation upon which we've built
+  `tff.learning`.
+
+TFF enables developers to declaratively express federated computations, so they
+could be deployed to diverse runtime environments. Included with TFF is a
+single-machine simulation runtime for experiments. Please visit the
+tutorials and try it out yourself!
+"""
+# TODO(b/124800187): Keep in sync with the contents of README.
+
+import datetime
+import sys
+import setuptools
+
+DOCLINES = __doc__.split('\n')
+
+REQUIRED_PACKAGES = [
+    'absl-py~=1.0.0',
+    'attrs~=21.2.0',
+    'cachetools~=3.1.1',
+    'dm-tree~=0.1.1',
+    'farmhashpy~=0.4.0',
+    'grpcio~=1.34.0',
+    'jax~=0.2.27',
+    'jaxlib~=0.1.76',
+    'numpy~=1.21.4',
+    'portpicker~=1.3.1',
+    'semantic-version~=2.8.5',
+    'tensorflow-model-optimization~=0.7.1',
+    'tensorflow-privacy~=0.7.3',
+    'tensorflow~=2.8.0',
+    'tqdm~=4.28.1',
+]
+
+with open('tensorflow_federated/version.py') as fp:
+  globals_dict = {}
+  exec(fp.read(), globals_dict)  # pylint: disable=exec-used
+  VERSION = globals_dict['__version__']
+
+
+def get_package_name(requirement: str) -> str:
+  allowed_operators = ['~=', '<', '>', '==', '<=', '>=', '!=']
+  separator = allowed_operators[0]
+  for operator in allowed_operators[1:]:
+    requirement = requirement.replace(operator, separator)
+  name, _ = requirement.split(separator, maxsplit=1)
+  return name
+
+
+if '--nightly' in sys.argv:
+  sys.argv.remove('--nightly')
+  PROJECT_NAME = 'tensorflow_federated_nightly'
+  date = datetime.date.today().strftime('%Y%m%d')
+  VERSION = '{}.dev{}'.format(VERSION, date)
+  for index, required_package in enumerate(REQUIRED_PACKAGES):
+    package_name = get_package_name(required_package)
+    if package_name == 'grpcio':
+      REQUIRED_PACKAGES[index] = 'grpcio~=1.37.0'
+    elif package_name == 'tensorflow':
+      REQUIRED_PACKAGES[index] = 'tf-nightly'
+else:
+  PROJECT_NAME = 'tensorflow_federated'
+
+setuptools.setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    packages=setuptools.find_packages(exclude=('tools')),
+    description=DOCLINES[0],
+    long_description='\n'.join(DOCLINES[2:]),
+    long_description_content_type='text/plain',
+    author='Google Inc.',
+    author_email='packages@tensorflow.org',
+    url='http://tensorflow.org/federated',
+    download_url='https://github.com/tensorflow/federated/tags',
+    install_requires=REQUIRED_PACKAGES,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    keywords='tensorflow federated machine learning',
+)
