d68374ac3e947666e92a823522377273f7c528f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:59 +0000

    Add package nautobot-device-lifecycle-mgmt/0.4.1/nautobot-device-lifecycle-mgmt-0.4.1.tar.gz

---
+++ packages/nautobot-device-lifecycle-mgmt/nautobot-device-lifecycle-mgmt-0.4.1.tar.gz/nautobot-device-lifecycle-mgmt-0.4.1/nautobot_device_lifecycle_mgmt/tables.py
@@ -0,0 +1,372 @@
+"""Tables implementation for the Lifecycle Management plugin."""
+
+import django_tables2 as tables
+from django_tables2.utils import A
+from nautobot.utilities.tables import BaseTable, ButtonsColumn, BooleanColumn, ToggleColumn, TagColumn
+from nautobot.extras.tables import StatusTableMixin
+from nautobot_device_lifecycle_mgmt.models import (
+    HardwareLCM,
+    SoftwareLCM,
+    ValidatedSoftwareLCM,
+    ContractLCM,
+    ProviderLCM,
+    ContactLCM,
+    CVELCM,
+    VulnerabilityLCM,
+    DeviceSoftwareValidationResult,
+    InventoryItemSoftwareValidationResult,
+)
+
+
+class PercentageColumn(tables.Column):
+    """Column used to display percentage."""
+
+    def render(self, value):
+        """Render percentage value."""
+        return f"{value} %"
+
+
+class HardwareLCMTable(BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:hardwarelcm", text=lambda record: record, args=[A("pk")]
+    )
+    reference_item = tables.TemplateColumn(
+        template_code="""{% if record.device_type %}
+                    <a href="{% url 'dcim:devicetype' pk=record.device_type.pk %}">{{ record.device_type }}</a>
+                    {% elif record.inventory_item %}
+                    {{ record.inventory_item }}
+                    {% endif %}""",
+        verbose_name="Reference",
+    )
+    documentation_url = tables.TemplateColumn(
+        template_code="""{% if record.documentation_url %}
+                    <a href="{{ record.documentation_url }}" target="_blank" data-toggle="tooltip" data-placement="left" title="{{ record.documentation_url }}">
+                        <span class="mdi mdi-open-in-new"></span>
+                    </a>
+                    {% else %}
+                    —
+                    {% endif %}""",
+        verbose_name="Documentation",
+    )
+    actions = ButtonsColumn(HardwareLCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = HardwareLCM
+        fields = (
+            "pk",
+            "name",
+            "reference_item",
+            "release_date",
+            "end_of_sale",
+            "end_of_support",
+            "end_of_sw_releases",
+            "end_of_security_patches",
+            "documentation_url",
+            "actions",
+        )
+
+
+class SoftwareLCMTable(BaseTable):
+    """Table for SoftwareLCMListView."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:softwarelcm",
+        text=lambda record: record,
+        args=[A("pk")],
+        orderable=False,
+    )
+    device_platform = tables.TemplateColumn("{{ record.device_platform }}")
+    long_term_support = BooleanColumn()
+    pre_release = BooleanColumn()
+    actions = ButtonsColumn(SoftwareLCM, buttons=("edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = SoftwareLCM
+        fields = (
+            "pk",
+            "name",
+            "version",
+            "alias",
+            "device_platform",
+            "release_date",
+            "end_of_support",
+            "long_term_support",
+            "pre_release",
+            "actions",
+        )
+
+
+class ValidatedSoftwareLCMTable(BaseTable):
+    """Table for ValidatedSoftwareLCMListView."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:validatedsoftwarelcm",
+        text=lambda record: record,
+        args=[
+            A("pk"),
+        ],
+        orderable=False,
+    )
+    valid = BooleanColumn(verbose_name="Valid Now", orderable=False)
+    software = tables.LinkColumn(verbose_name="Software")
+    actions = ButtonsColumn(ValidatedSoftwareLCM, buttons=("edit", "delete"))
+    preferred = BooleanColumn()
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = ValidatedSoftwareLCM
+        fields = (
+            "pk",
+            "name",
+            "software",
+            "start",
+            "end",
+            "valid",
+            "preferred",
+            "actions",
+        )
+
+
+class DeviceSoftwareValidationResultTable(BaseTable):
+    """Table for device software validation report."""
+
+    name = tables.Column(accessor="device__device_type__model", verbose_name="Device Type")
+    total = tables.Column(accessor="total", verbose_name="Total")
+    valid = tables.Column(accessor="valid", verbose_name="Valid")
+    invalid = tables.Column(accessor="invalid", verbose_name="Invalid")
+    no_software = tables.Column(accessor="no_software", verbose_name="No Software")
+    valid_percent = PercentageColumn(accessor="valid_percent", verbose_name="Compliance (%)")
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Metaclass attributes of DeviceSoftwareValidationResultTable."""
+
+        model = DeviceSoftwareValidationResult
+        fields = ["name", "total", "valid", "invalid", "no_software", "valid_percent"]
+        default_columns = [
+            "name",
+            "total",
+            "valid",
+            "invalid",
+            "no_software",
+            "valid_percent",
+        ]
+
+
+class InventoryItemSoftwareValidationResultTable(BaseTable):
+    """Table for inventory item software validation report."""
+
+    part_id = tables.Column(accessor="inventory_item__part_id", verbose_name="Part ID")
+    total = tables.Column(accessor="total", verbose_name="Total")
+    valid = tables.Column(accessor="valid", verbose_name="Valid")
+    invalid = tables.Column(accessor="invalid", verbose_name="Invalid")
+    no_software = tables.Column(accessor="no_software", verbose_name="No Software")
+    valid_percent = PercentageColumn(accessor="valid_percent", verbose_name="Compliance (%)")
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Metaclass attributes of InventoryItemSoftwareValidationResultTable."""
+
+        model = InventoryItemSoftwareValidationResult
+        fields = ["part_id", "total", "valid", "invalid", "no_software", "valid_percent"]
+        default_columns = [
+            "part_id",
+            "total",
+            "valid",
+            "invalid",
+            "no_software",
+            "valid_percent",
+        ]
+
+
+class ContractLCMTable(BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:contractlcm", text=lambda record: record, args=[A("pk")]
+    )
+    provider = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:providerlcm",
+        text=lambda record: record.provider,
+        args=[A("provider.pk")],
+    )
+    cost = tables.TemplateColumn(
+        template_code="""{{ record.cost }}{% if record.currency %} {{ record.currency }}{% endif %}"""
+    )
+    actions = ButtonsColumn(ContractLCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = ContractLCM
+        fields = (
+            "pk",
+            "name",
+            "start",
+            "end",
+            "cost",
+            "support_level",
+            "contract_type",
+            "provider",
+            "actions",
+        )
+
+
+class ProviderLCMTable(BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:providerlcm", text=lambda record: record, args=[A("pk")]
+    )
+    portal_url = tables.TemplateColumn(
+        template_code="""{% if record.portal_url %}
+                    <a href="{{ record.portal_url }}" target="_blank" data-toggle="tooltip" data-placement="left" title="{{ record.portal_url }}">
+                        <span class="mdi mdi-open-in-new"></span>
+                    </a>{% else %} — {% endif %}""",
+        verbose_name="URL",
+    )
+    actions = ButtonsColumn(ProviderLCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = ProviderLCM
+        fields = (
+            "pk",
+            "name",
+            "physical_address",
+            "phone",
+            "email",
+            "portal_url",
+            "actions",
+        )
+
+
+class ContactLCMTable(BaseTable):
+    """Table for list view."""
+
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:contactlcm", text=lambda record: record, args=[A("pk")]
+    )
+    actions = ButtonsColumn(ContactLCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = ContactLCM
+        fields = (
+            "pk",
+            "name",
+            "address",
+            "phone",
+            "email",
+            "comments",
+            "priority",
+            "contract",
+            "actions",
+        )
+
+
+class CVELCMTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    model = CVELCM
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:cvelcm", text=lambda record: record, args=[A("pk")]
+    )
+    link = tables.TemplateColumn(
+        template_code="""{% if record.link %}
+                    <a href="{{ record.link }}" target="_blank" data-toggle="tooltip" data-placement="left" title="{{ record.link }}">
+                        <span class="mdi mdi-open-in-new"></span>
+                    </a>
+                    {% else %}
+                    —
+                    {% endif %}""",
+        verbose_name="Link",
+    )
+    tags = TagColumn()
+    actions = ButtonsColumn(CVELCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = CVELCM
+        fields = (
+            "pk",
+            "name",
+            "published_date",
+            "link",
+            "severity",
+            "cvss",
+            "cvss_v2",
+            "cvss_v3",
+            "status",
+            "tags",
+            "actions",
+        )
+        default_columns = (
+            "pk",
+            "name",
+            "published_date",
+            "link",
+            "severity",
+            "cvss",
+            "cvss_v2",
+            "cvss_v3",
+            "status",
+            "actions",
+        )
+
+
+class VulnerabilityLCMTable(StatusTableMixin, BaseTable):
+    """Table for list view."""
+
+    model = VulnerabilityLCM
+    pk = ToggleColumn()
+    name = tables.LinkColumn(
+        "plugins:nautobot_device_lifecycle_mgmt:vulnerabilitylcm", text=lambda record: record, args=[A("pk")]
+    )
+    cve = tables.LinkColumn(verbose_name="CVE")
+    software = tables.LinkColumn()
+    device = tables.LinkColumn()
+    inventory_item = tables.LinkColumn(verbose_name="Inventory Item")
+    tags = TagColumn()
+    actions = ButtonsColumn(VulnerabilityLCM, buttons=("changelog", "edit", "delete"))
+
+    class Meta(BaseTable.Meta):  # pylint: disable=too-few-public-methods
+        """Meta attributes."""
+
+        model = VulnerabilityLCM
+        fields = (
+            "pk",
+            "name",
+            "cve",
+            "software",
+            "device",
+            "inventory_item",
+            "status",
+            "tags",
+            "actions",
+        )
+        default_columns = (
+            "pk",
+            "name",
+            "cve",
+            "software",
+            "device",
+            "inventory_item",
+            "status",
+            "actions",
+        )
---
+++ packages/nautobot-device-lifecycle-mgmt/nautobot-device-lifecycle-mgmt-0.4.1.tar.gz/nautobot-device-lifecycle-mgmt-0.4.1/nautobot_device_lifecycle_mgmt/tests/test_forms.py
@@ -0,0 +1,378 @@
+"""Test forms."""
+from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+
+from nautobot.dcim.models import DeviceType, Manufacturer, Device, DeviceRole, Site, InventoryItem, Platform
+from nautobot.extras.models import Status
+
+from nautobot_device_lifecycle_mgmt.forms import HardwareLCMForm, SoftwareLCMForm, ValidatedSoftwareLCMForm, CVELCMForm
+from nautobot_device_lifecycle_mgmt.models import SoftwareLCM, CVELCM
+
+
+class HardwareLCMFormTest(TestCase):
+    """Test class for Device Lifecycle forms."""
+
+    def setUp(self):
+        """Create necessary objects."""
+        self.manufacturer = Manufacturer.objects.create(name="Cisco", slug="cisco")
+        self.device_type = DeviceType.objects.create(model="c9300-24", slug="c9300-24", manufacturer=self.manufacturer)
+        self.device_role = DeviceRole.objects.create(name="Backbone Switch", slug="backbone-switch")
+        self.site = Site.objects.create(name="Test 1", slug="test-1")
+        self.device = Device.objects.create(
+            name="Test-9300-Switch",
+            device_type=self.device_type,
+            device_role=self.device_role,
+            site=self.site,
+        )
+        self.inventory_item = InventoryItem.objects.create(
+            device=self.device,
+            manufacturer=self.manufacturer,
+            name="SUP2T Card",
+            part_id="VS-S2T-10G",
+        )
+
+    def test_specifying_all_fields(self):
+        form = HardwareLCMForm(
+            data={
+                "device_type": self.device_type,
+                "end_of_sale": "2021-04-01",
+                "end_of_support": "2022-04-01",
+                "end_of_sw_releases": "2023-04-01",
+                "end_of_security_patches": "2024-04-01",
+                "documentation_url": "https://cisco.com",
+            }
+        )
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_one_of_eo_sale(self):
+        form = HardwareLCMForm(data={"device_type": self.device_type, "end_of_sale": "2021-04-01"})
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_one_of_eo_support(self):
+        form = HardwareLCMForm(data={"device_type": self.device_type, "end_of_support": "2021-04-01"})
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_required_fields_missing(self):
+        form = HardwareLCMForm(
+            data={
+                "end_of_sale": "2021-04-01",
+                "end_of_support": "2022-04-01",
+                "end_of_sw_releases": "2023-04-01",
+                "end_of_security_patches": "2024-04-01",
+                "documentation_url": "https://cisco.com",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertDictEqual(
+            {
+                "inventory_item": ["One and only one of `Inventory Item` OR `Device Type` must be specified."],
+                "device_type": ["One and only one of `Inventory Item` OR `Device Type` must be specified."],
+            },
+            form.errors,
+        )
+
+    def test_eo_sale_support_fields_missing(self):
+        form = HardwareLCMForm(data={"device_type": self.device_type})
+        self.assertFalse(form.is_valid())
+        self.assertIn("End of Sale or End of Support must be specified.", form.errors["end_of_sale"][0])
+
+    def test_device_type_and_inventory_item_error(self):
+        form = HardwareLCMForm(data={"device_type": self.device_type, "inventory_item": "VS-S2T-10G"})
+        self.assertFalse(form.is_valid())
+        self.assertIn(
+            "One and only one of `Inventory Item` OR `Device Type` must be specified.", form.errors["inventory_item"][0]
+        )
+
+    def test_validation_error_end_of_sale(self):
+        form = HardwareLCMForm(data={"device_type": self.device_type, "end_of_sale": "April 1st, 2021"})
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_of_sale", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end_of_sale"])
+
+    def test_validation_error_end_of_support(self):
+        form = HardwareLCMForm(
+            data={"device_type": self.device_type, "end_of_sale": "2021-04-01", "end_of_support": "April 1st, 2022"}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_of_support", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end_of_support"])
+
+    def test_validation_error_end_of_sw_releases(self):
+        form = HardwareLCMForm(
+            data={
+                "device_type": self.device_type,
+                "end_of_sale": "2021-04-01",
+                "end_of_support": "2021-04-01",
+                "end_of_sw_releases": "April 1st, 2022",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_of_sw_releases", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end_of_sw_releases"])
+
+    def test_validation_error_end_of_security_patches(self):
+        form = HardwareLCMForm(
+            data={
+                "device_type": self.device_type,
+                "end_of_sale": "2021-04-01",
+                "end_of_support": "2022-04-01",
+                "end_of_sw_releases": "2023-04-01",
+                "end_of_security_patches": "April 1st, 2022",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_of_security_patches", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end_of_security_patches"])
+
+    def test_validation_error_documentation_url(self):
+        form = HardwareLCMForm(
+            data={
+                "device_type": self.device_type,
+                "end_of_sale": "2021-04-01",
+                "end_of_support": "2022-04-01",
+                "end_of_sw_releases": "2023-04-01",
+                "end_of_security_patches": "2024-04-01",
+                "documentation_url": "htttps://cisco.com",
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("documentation_url", form.errors)
+        self.assertIn("Enter a valid URL.", form.errors["documentation_url"])
+
+
+class SoftwareLCMFormTest(TestCase):  # pylint: disable=no-member
+    """Test class for SoftwareLCM forms."""
+
+    form_class = SoftwareLCMForm
+
+    def setUp(self):
+        """Create necessary objects."""
+        self.device_platform = Platform.objects.create(name="Cisco IOS", slug="cisco_ios")
+
+    def test_specifying_all_fields(self):
+        data = {
+            "device_platform": self.device_platform,
+            "version": "17.3.3 MD",
+            "alias": "Amsterdam-17.3.3 MD",
+            "end_of_support": "2022-05-15",
+            "documentation_url": "https://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-15-4m-t/series.html",
+            "download_url": "ftp://device-images.local.com/cisco/asr1001x-universalk9.17.03.03.SPA.bin",
+            "image_file_name": "asr1001x-universalk9.17.03.03.SPA.bin",
+            "image_file_checksum": "9cf2e09b59207a4d8ea40886fbbe5b4b68e19e58a8f96b34240e4cea9971f6ae6facab9a1855a34e1ed8755f3ffe4c969cf6e6ef1df95d42a91540a44d4b9e14",
+            "long_term_support": True,
+            "pre_release": False,
+        }
+        form = self.form_class(data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_one_of_eo_support(self):
+        data = {"device_platform": self.device_platform, "version": "17.3.3 MD", "end_of_support": "2022-05-15"}
+        form = self.form_class(data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_required_fields_missing(self):
+        data = {
+            "end_of_support": "2022-05-15",
+            "documentation_url": "https://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-15-4m-t/series.html",
+            "download_url": "ftp://device-images.local.com/cisco/asr1001x-universalk9.17.03.03.SPA.bin",
+            "image_file_name": "asr1001x-universalk9.17.03.03.SPA.bin",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertDictEqual(
+            {"device_platform": ["This field is required."], "version": ["This field is required."]},
+            form.errors,
+        )
+
+    def test_validation_error_end_of_support(self):
+        data = {"device_platform": self.device_platform, "version": "17.3.3 MD", "end_of_support": "2022 May 5th"}
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("end_of_support", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end_of_support"])
+
+    def test_validation_error_documentation_url(self):
+        data = {
+            "device_platform": self.device_platform,
+            "version": "17.3.3 MD",
+            "end_of_support": "2022-05-15",
+            "documentation_url": "httpss://www.cisco.com/",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("documentation_url", form.errors)
+        self.assertIn("Enter a valid URL.", form.errors["documentation_url"])
+
+    def test_validation_error_download_url(self):
+        data = {
+            "device_platform": self.device_platform,
+            "version": "17.3.3 MD",
+            "end_of_support": "2022-05-15",
+            "download_url": "ftppp://images.local.my.org/",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("download_url", form.errors)
+        self.assertIn("Enter a valid URL.", form.errors["download_url"])
+
+
+class ValidatedSoftwareLCMFormTest(TestCase):  # pylint: disable=no-member
+    """Test class for ValidatedSoftwareLCMForm forms."""
+
+    form_class = ValidatedSoftwareLCMForm
+
+    def setUp(self):
+        """Create necessary objects."""
+        device_platform = Platform.objects.create(name="Cisco IOS", slug="cisco_ios")
+        self.software = SoftwareLCM.objects.create(
+            **{
+                "device_platform": device_platform,
+                "version": "17.3.3 MD",
+                "alias": "Amsterdam-17.3.3 MD",
+                "end_of_support": "2022-05-15",
+                "documentation_url": "https://www.cisco.com/c/en/us/support/ios-nx-os-software/ios-15-4m-t/series.html",
+                "download_url": "ftp://device-images.local.com/cisco/asr1001x-universalk9.17.03.03.SPA.bin",
+                "image_file_name": "asr1001x-universalk9.17.03.03.SPA.bin",
+                "image_file_checksum": "9cf2e09b59207a4d8ea40886fbbe5b4b68e19e58a8f96b34240e4cea9971f6ae6facab9a1855a34e1ed8755f3ffe4c969cf6e6ef1df95d42a91540a44d4b9e14",
+                "long_term_support": True,
+                "pre_release": False,
+            }
+        )
+
+        status_active = Status.objects.get(slug="active")
+        manufacturer = Manufacturer.objects.create(name="Cisco", slug="cisco")
+        site = Site.objects.create(name="Site 1", slug="site-1")
+        devicerole = DeviceRole.objects.create(name="Router", slug="router", color="ff0000")
+        self.devicetype_1 = DeviceType.objects.create(manufacturer=manufacturer, model="ASR-1000", slug="asr-1000")
+        self.device_1 = Device.objects.create(
+            device_type=self.devicetype_1, device_role=devicerole, name="Device 1", site=site, status=status_active
+        )
+        self.inventoryitem_1 = InventoryItem.objects.create(device=self.device_1, name="SwitchModule1")
+
+    def test_specifying_all_fields_w_devices(self):
+        data = {
+            "software": self.software,
+            "devices": [self.device_1],
+            "start": "2021-06-06",
+            "end": "2023-08-31",
+            "preferred": False,
+        }
+        form = self.form_class(data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_specifying_all_fields_w_device_type(self):
+        data = {
+            "software": self.software,
+            "device_types": [self.devicetype_1],
+            "start": "2021-06-06",
+            "end": "2023-08-31",
+            "preferred": False,
+        }
+        form = self.form_class(data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_specifying_all_fields_w_inventory_item_type(self):
+        data = {
+            "software": self.software,
+            "inventory_items": [self.inventoryitem_1],
+            "start": "2021-06-06",
+            "end": "2023-08-31",
+            "preferred": False,
+        }
+        form = self.form_class(data)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_software_missing(self):
+        data = {
+            "end": "2023-08-31",
+            "preferred": False,
+        }
+        form = self.form_class(data)
+        with self.assertRaises(SoftwareLCM.DoesNotExist):
+            form.is_valid()
+
+    def test_validation_error_start(self):
+        data = {
+            "software": self.software,
+            "devices": [self.device_1],
+            "start": "2020 May 15th",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("start", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["start"])
+
+    def test_validation_error_end(self):
+        data = {
+            "software": self.software,
+            "devices": [self.device_1],
+            "start": "2021-06-06",
+            "end": "2024 June 2nd",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("end", form.errors)
+        self.assertIn("Enter a valid date.", form.errors["end"])
+
+    def test_assigned_to_must_specify_at_least_one_object(self):
+        """ValidatedSoftwareLCM must be assigned to at least one object."""
+        data = {
+            "software": self.software,
+            "start": "2021-06-06",
+        }
+        form = self.form_class(data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            "You need to assign to at least one object.",
+            form.errors["__all__"][0],
+        )
+
+
+class CVELCMFormTest(TestCase):
+    """Test class for Device Lifecycle forms."""
+
+    def setUp(self):
+        """Create necessary objects."""
+        self.cve_ct = ContentType.objects.get_for_model(CVELCM)
+        self.status = Status.objects.create(
+            name="Fixed", slug="fixed", color="4caf50", description="Unit has been fixed"
+        )
+        self.status.content_types.set([self.cve_ct])
+
+    def test_specifying_all_fields(self):
+        form = CVELCMForm(
+            data={
+                "name": "CVE-2021-34699",
+                "published_date": "2021-09-23",
+                "link": "https://www.cvedetails.com/cve/CVE-2021-34699/",
+                "status": self.status,
+                "description": "Thanos",
+                "severity": "High",
+                "cvss": 6.8,
+                "cvss_v2": 6.9,
+                "cvss_v3": 6.7,
+                "fix": "Avengers",
+                "comments": "This is very bad juju.",
+            }
+        )
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.save())
+
+    def test_required_fields_missing(self):
+        form = CVELCMForm(data={"name": "CVE-2022-0002"})
+        self.assertFalse(form.is_valid())
+        self.assertDictEqual(
+            {
+                "published_date": ["This field is required."],
+                "link": ["This field is required."],
+            },
+            form.errors,
+        )
