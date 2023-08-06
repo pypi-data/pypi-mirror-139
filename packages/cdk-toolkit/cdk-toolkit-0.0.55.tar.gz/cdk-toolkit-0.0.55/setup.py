ee5eca241cd3ab683be33142c0728083b5af7d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:43 +0000

    Add package cdk-toolkit/0.0.55/cdk-toolkit-0.0.55.tar.gz

---
+++ packages/cdk-toolkit/cdk-toolkit-0.0.55.tar.gz/cdk-toolkit-0.0.55/setup.py
@@ -0,0 +1,22 @@
+# https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cdk-toolkit',
+    version='0.0.55',
+    author='Ryan Moos',
+    author_email='ryan@moos.engineering',
+    packages=find_packages(),
+    # scripts=['bin/script1','bin/script2'],
+    url='http://pypi.python.org/pypi/cdk-toolkit/',
+    license='LICENSE',
+    description='AWS Cloud Development Toolkit',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "aws-cdk-lib",
+        "boto3",
+    ],
+)
+
