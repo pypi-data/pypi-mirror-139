4148e6545ed94529ee175c0a09a02b301bba4fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package qpcr/3.1.1/qpcr-3.1.1.tar.gz

---
+++ packages/qpcr/qpcr-3.1.1.tar.gz/qpcr-3.1.1/qpcr/_auxiliary/defaults.py
@@ -0,0 +1,64 @@
+"""
+Stores basic default settings
+"""
+
+#  =================================================================
+#                       Default FileIO settings
+#  =================================================================
+
+
+supported_filetypes = ["csv", "xlsx"]
+
+
+
+#  =================================================================
+#                       Default Inference settings
+#  =================================================================
+
+# default column names for raw Ct data files (don't change this!)
+raw_col_names = ["id", "Ct"]
+
+default_dataset_header = "assay"
+default_group_name = "group{}"
+
+# default assumed column names for Id and Ct values
+default_id_header = "Name"
+default_ct_header = "Ct"
+
+#  =================================================================
+#                       Default Figure settings
+#  =================================================================
+
+# PreviewResults
+# --------------------------------
+static_PreviewResults = dict(
+                                color = "white",
+                                edgecolor = "xkcd:pure blue",
+                                ecolor = "black",
+                                edgewidth = 1,
+                                rot = 0,
+                                legend = False, 
+                                frame = False,
+                                title = "Preview of Results",
+                                ylabel  = "norm. $\Delta\Delta$Ct",
+                            )
+
+interactive_PreviewResults = dict(
+                                    template = "plotly_white",
+                                    title = "Preview of Results",
+                                    ylabel = "norm. ΔΔCT"
+                                )
+
+# ReplicateBoxPlot
+# --------------------------------
+static_ReplicateBoxPlot = dict(
+                                    title = "Summary of Replicates",
+                                    linewidth = 0.8,
+                                    frame = False,
+                                    palette = "Blues"
+                                )
+
+interactive_ReplicateBoxPlot = dict(
+                                        template = "plotly_white",
+                                        title = "Summary of Replicates"
+                                    )
