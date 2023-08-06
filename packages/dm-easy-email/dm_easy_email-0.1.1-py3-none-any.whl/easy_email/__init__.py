f1c3237a38e53d651b6c62b8298f3311c0289156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:12 +0000

    Add package dm-easy-email/0.1.1/dm_easy_email-0.1.1-py3-none-any.whl

---
+++ packages/dm-easy-email/dm_easy_email-0.1.1-py3-none-any.whl/easy_email/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2022/02/22 14:31:27
+@Author  :   Shenxian Shi 
+@Version :   
+@Contact :   shishenxian@bluemoon.com.cn
+@Desc    :   None
+'''
+
+# here put the import lib
+from easy_email import Email
