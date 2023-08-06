70d5a2d6d8d975b118a11e166c27c3cea24c3f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:31 +0000

    Add package cyberchase/1.0.0/cyberchase-1.0.0.tar.gz

---
+++ packages/cyberchase/cyberchase-1.0.0.tar.gz/cyberchase-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='cyberchase',
+    version='1.0.0',
+    description='Game code for TAMU Spring 2022 Turing Games',
+    url='https://github.com/smh3005/cyberchase',
+    author='Seth Hamilton and Trevor Bolton',
+    author_email='logistics@theturinggames.com',
+    license='BSD 2-clause',
+    packages=['cyberchase'],
+    install_requires=['perlin-noise',
+                      'numpy',
+                      'perlin-noise',
+                      'pygame',        
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
