1ab1aa905428bbdb71d64587b733087ce0d9bc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:26 +0000

    Add package pierpy/0.3.0.5/pierpy-0.3.0.5.tar.gz

---
+++ packages/pierpy/pierpy-0.3.0.5.tar.gz/pierpy-0.3.0.5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+
+packages = \
+['pierpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sqlalchemy'],
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='pierpy',  
+    version='0.3.0.5',
+    author="Surasak Choedpasuporn",
+    author_email="surasakc@bot.or.th",
+    description="A python package for internally use in PIER",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/surasakcho/pierpy",
+    packages=packages,
+    package_data=package_data,
+    install_requires=install_requires,
+    python_requires='>=3.6.1,<4.0.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+     ],    
+ )
