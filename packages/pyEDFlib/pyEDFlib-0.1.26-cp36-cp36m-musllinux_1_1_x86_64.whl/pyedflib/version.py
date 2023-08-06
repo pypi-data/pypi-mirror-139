c88024706f703ba960f2370ba829acecf5ad2b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package pyEDFlib/0.1.26/pyEDFlib-0.1.26-cp36-cp36m-musllinux_1_1_x86_64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.26-cp36-cp36m-musllinux_1_1_x86_64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.26'
+version = '0.1.26'
+full_version = '0.1.26'
+git_revision = 'c79797fc72bec5ae64f535b0775239a6d411fbc6'
+release = True
+
+if not release:
+    version = full_version
