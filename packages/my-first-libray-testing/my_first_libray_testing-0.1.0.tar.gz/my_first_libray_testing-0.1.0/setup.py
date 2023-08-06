b7a3ce6295449cc3383aee4756dad629f3196874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:16 +0000

    Add package my-first-libray-testing/0.1.0/my_first_libray_testing-0.1.0.tar.gz

---
+++ packages/my-first-libray-testing/my_first_libray_testing-0.1.0.tar.gz/my_first_libray_testing-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='my_first_libray_testing',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+)
