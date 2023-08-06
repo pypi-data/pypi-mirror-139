52fee356410c4bf8f4f91f8429cc498e33a9d206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:55 +0000

    Add package better-cryptography/0.3.1/better%20cryptography-0.3.1.tar.gz

---
+++ packages/better-cryptography/better%20cryptography-0.3.1.tar.gz/better cryptography-0.3.1/Revenant_App/main.py
@@ -0,0 +1,6 @@
+import PySimpleGUI as sg
+from better_cryptography import Ciphers, Log, hash_
+
+
+class Main:
+    pass
---
+++ packages/better-cryptography/better%20cryptography-0.3.1.tar.gz/better cryptography-0.3.1/better_cryptography/__init__.py
@@ -0,0 +1,2 @@
+from .ciphers import Ciphers, hash_, random_choice, compare_hashes, token_generation, gen_random_password, sec_delete, delete
+from .log import Log
