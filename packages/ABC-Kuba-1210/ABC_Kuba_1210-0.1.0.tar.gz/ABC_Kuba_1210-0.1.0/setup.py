3c3455fb1fa04c3ecd76cebf5822f105db038478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:43 +0000

    Add package ABC-Kuba-1210/0.1.0/ABC_Kuba_1210-0.1.0.tar.gz

---
+++ packages/ABC-Kuba-1210/ABC_Kuba_1210-0.1.0.tar.gz/ABC_Kuba_1210-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ABC_Kuba_1210',
+    version='0.1.0',
+    author='Kuba',
+    author_email='kuba.polakiewicz2002@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Kuba projekt testowy'
+)
