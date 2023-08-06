7bc562c814adaeff048bd56e426009b59a15d4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:06 +0000

    Add package screenpy-selenium/4.0.0b0/screenpy_selenium-4.0.0b0-py3-none-any.whl

---
+++ packages/screenpy-selenium/screenpy_selenium-4.0.0b0-py3-none-any.whl/screenpy_selenium/protocols.py
@@ -0,0 +1,18 @@
+from typing_extensions import Protocol
+
+from screenpy import Actor
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class Chainable(Protocol):
+    """Actions that can be added to a chain are Chainable."""
+
+    def add_to_chain(self, the_actor: Actor, the_chain: ActionChains) -> None:
+        """
+        Add this chainable Action to a chain of Actions.
+
+        Args:
+            the_actor: the Actor who will be performing the Action chain.
+            the_chain: the |ActionChains| instance that is being built.
+        """
+        ...
---
+++ packages/screenpy-selenium/screenpy_selenium-4.0.0b0-py3-none-any.whl/screenpy_selenium/resolutions/__init__.py
@@ -0,0 +1,18 @@
+from .is_clickable import IsClickable
+from .is_visible import IsVisible
+
+# Natural-language-enabling syntactic sugar
+IsEnabled = Enabled = Clickable = IsClickable
+IsDisplayed = Displayed = Visible = IsVisible
+
+
+__all__ = [
+    "Clickable",
+    "Displayed",
+    "Enabled",
+    "IsClickable",
+    "IsDisplayed",
+    "IsEnabled",
+    "IsVisible",
+    "Visible",
+]
