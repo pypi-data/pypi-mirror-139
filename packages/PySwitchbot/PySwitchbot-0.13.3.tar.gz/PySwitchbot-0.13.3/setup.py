d9222a020a9d5f0e62ff7816791a4c3f71427491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:44 +0000

    Add package PySwitchbot/0.13.3/PySwitchbot-0.13.3.tar.gz

---
+++ packages/PySwitchbot/PySwitchbot-0.13.3.tar.gz/PySwitchbot-0.13.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'PySwitchbot',
+    packages = ['switchbot'],
+    install_requires=['bluepy'],
+    version = '0.13.3',
+    description = 'A library to communicate with Switchbot',
+    author='Daniel Hjelseth Hoyer',
+    url='https://github.com/Danielhiversen/pySwitchbot/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
