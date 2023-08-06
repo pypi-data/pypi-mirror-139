3bc038ec5f13406429b2ddc3f8acbc76763e1c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:06 +0000

    Add package ml-callbacks/0.19.7/ml_callbacks-0.19.7-py3-none-any.whl

---
+++ packages/ml-callbacks/ml_callbacks-0.19.7-py3-none-any.whl/ml_callbacks/tensorflow_callback.py
@@ -0,0 +1,5 @@
+from ml_callbacks.enviroment_callback import EnviromentInterface
+
+class TensorflowCallback(EnviromentInterface):
+    def model_saving(self, model, path):
+        print("Not Implemented")
