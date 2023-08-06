f3fa6a90873d86fdb7a1fa7a90d6d8c078aaba43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:59 +0000

    Add package lookie/0.1.7/lookie-0.1.7-py3-none-any.whl

---
+++ packages/lookie/lookie-0.1.7-py3-none-any.whl/src/version.py
@@ -0,0 +1,17 @@
+from src.utilities import get_path
+
+
+def get_version():
+    to_return = None
+    try:
+        with open("pyproject.toml", "r") as pp:
+            for i in pp.readlines():
+                if "version" in i:
+                    to_return = i.split(" ")[-1].replace('"', '')
+    except Exception as ex:
+        pyf = get_path().replace("/src", "/") + "/pyproject.toml"
+        with open(pyf, "r") as pp:
+            for i in pp.readlines():
+                if "version" in i:
+                    to_return = i.split(" ")[-1].replace('"', '')
+    return to_return
