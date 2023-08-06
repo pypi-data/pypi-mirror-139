e9b5a6def01c418eb5943b14225386583d8992a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package blockchain-users-generator/0.2.1/blockchain_users_generator-0.2.1.tar.gz

---
+++ packages/blockchain-users-generator/blockchain_users_generator-0.2.1.tar.gz/blockchain_users_generator-0.2.1/src/blockchain_users_generator/models/user.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from . import blockchain, country
+import random
+from dataclasses import asdict
+
+genders = ["female", "male"]
+
+@dataclass
+class User:
+  uuid:             str
+  first_name:       str
+  last_name:        str
+  full_name:        str
+  age:              int
+  username:         str = None
+  password:         str = None
+  gender:           str = None
+  country_info:     country.CountryInfo = None
+  blockchain_info:  blockchain.BlockchainInfo = None
+  def to_dict(self):
+    return asdict(self)
+def get_random_user_gender():
+  return random.choice(genders)
