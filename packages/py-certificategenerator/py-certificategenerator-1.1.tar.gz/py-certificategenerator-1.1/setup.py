9a8adbdbf5a8766bd86889a1d67e624468c283b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:01 +0000

    Add package py-certificategenerator/1.1/py-certificategenerator-1.1.tar.gz

---
+++ packages/py-certificategenerator/py-certificategenerator-1.1.tar.gz/py-certificategenerator-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-certificategenerator',
+    version='1.1',
+    packages=['py-certificategenerator'],
+    url='https://github.com/codinlikewilly/py-certgenerator',
+    license='MIT ',
+    author='will',
+    author_email='will@theapiguys.com',
+    description='Generates SSL x509 certificates'
+)
