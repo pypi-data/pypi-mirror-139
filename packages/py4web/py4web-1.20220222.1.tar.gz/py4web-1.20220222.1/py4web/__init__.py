065653126dbe80aa1a566ae52a29bd77a8101c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:51 +0000

    Add package py4web/1.20220222.1/py4web-1.20220222.1.tar.gz

---
+++ packages/py4web/py4web-1.20220222.1.tar.gz/py4web-1.20220222.1/py4web/__init__.py
@@ -0,0 +1,35 @@
+import sys
+
+__author__ = "Massimo Di Pierro <massimo.dipierro@gmail.com>"
+__license__ = "BSDv3"
+__version__ = "1.20220222.1"
+
+
+def _maybe_gevent():
+    for arg in sys.argv[1:]:
+        if 'gevent' in arg.lower():
+            from gevent import monkey
+            monkey.patch_all()
+            break
+_maybe_gevent()
+
+
+from .core import (
+    action,  # main py4web decorator
+    request,
+    response,
+    redirect,
+    abort,
+    HTTP,  # bottle
+    DAL,
+    Field,  # pydal
+    render,  # yatl
+    Translator,  # from pluralize
+    Session,
+    Cache,
+    Flash,
+    user_in,  # additional fixtures
+    URL,  # custom helper
+    check_compatible,
+)  # checks for version compatibility
+
