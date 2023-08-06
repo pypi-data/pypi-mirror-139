7d5adaa6c7d29d81e7fed4057321b3cf763fd05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:25 +0000

    Add package hestia-earth-orchestrator/0.2.13/hestia_earth_orchestrator-0.2.13-py3-none-any.whl

---
+++ packages/hestia-earth-orchestrator/hestia_earth_orchestrator-0.2.13-py3-none-any.whl/hestia_earth/orchestrator/log.py
@@ -0,0 +1,42 @@
+import os
+import logging
+
+
+def _add_handler(logger: logging.Logger, formatter: logging.Formatter, handler: logging.Handler):
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+logger = logging.getLogger('hestia_earth.orchestrator')
+logger.setLevel(logging.getLevelName('DEBUG'))
+
+# stream logging
+_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.getLevelName(LOG_LEVEL))
+_add_handler(logger, _formatter, stream_handler)
+
+
+def log_to_file(filepath: str):
+    """
+    By default, all logs are saved into a file with path stored in the env variable `LOG_FILENAME`.
+    If you do not set the environment variable `LOG_FILENAME`, you can use this function with the file path.
+
+    Parameters
+    ----------
+    filepath : str
+        Path of the file.
+    """
+    formatter = logging.Formatter(
+        '{"timestamp": "%(asctime)s", "level": "%(levelname)s", "logger": "%(name)s", '
+        '"filename": "%(filename)s", "message": "%(message)s"}',
+        '%Y-%m-%dT%H:%M:%S%z')
+    handler = logging.FileHandler(filepath, encoding='utf-8')
+    handler.setLevel(logging.getLevelName('DEBUG'))
+    _add_handler(logger, formatter, handler)
+
+
+LOG_FILENAME = os.getenv('LOG_FILENAME')
+if LOG_FILENAME is not None:
+    log_to_file(LOG_FILENAME)
