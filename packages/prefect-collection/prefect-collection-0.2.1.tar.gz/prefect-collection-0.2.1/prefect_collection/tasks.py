a434d5d5af82e6aff6734ecc15b53458efe9f1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:34 +0000

    Add package prefect-collection/0.2.1/prefect-collection-0.2.1.tar.gz

---
+++ packages/prefect-collection/prefect-collection-0.2.1.tar.gz/prefect-collection-0.2.1/prefect_collection/tasks.py
@@ -0,0 +1,31 @@
+from prefect import task
+
+
+@task
+def hello_prefect_collection() -> str:
+    """
+    Sample task that says hello!
+
+    Returns:
+        A greeting for your collection
+    """
+    return "Hello, prefect-collection!"
+
+
+@task
+def goodbye_prefect_collection() -> str:
+    """
+    Sample task that says goodbye!
+
+    Returns:
+        A farewell for your collection
+    """
+    return "Goodbye, prefect-collection!"
+
+
+@task
+def really_swell_task() -> str:
+    """
+    A really swell task
+    """
+    return "This is a really swell task!"
