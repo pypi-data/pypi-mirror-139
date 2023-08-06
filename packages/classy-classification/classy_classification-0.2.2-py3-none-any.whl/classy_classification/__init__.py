d71b6bc8eb11a08c43596c5b7f62296f8a639340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:55 +0000

    Add package classy-classification/0.2.2/classy_classification-0.2.2-py3-none-any.whl

---
+++ packages/classy-classification/classy_classification-0.2.2-py3-none-any.whl/classy_classification/__init__.py
@@ -0,0 +1,52 @@
+import os
+
+from spacy import util
+from spacy.language import Language
+from spacy.tokens import Doc
+
+from .classifiers.sentence_transformer import \
+    classySentenceTransformer as classyClassifier
+from .classifiers.spacy_external import classySpacyExternal
+from .classifiers.spacy_internal import classySpacyInternal
+
+__all__ = [
+    'classyClassifier',
+    'classySpacyExternal',
+    'classySpacyInternal'
+]
+
+@Language.factory(
+    "text_categorizer",
+    default_config={
+        "data": None,
+        "model": 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2',
+        "config": {                      
+            "C": [1, 2, 5, 10, 20, 100],
+            "kernels": ["linear"],                              
+            "max_cross_validation_folds": 5
+        },
+    },
+)
+def make_text_categorizer(
+    nlp: Language,
+    name: str,
+    data: dict,
+    model: str,
+    config: dict,
+):  
+    if model == 'spacy':
+        return classySpacyInternal(
+            nlp=nlp,
+            name=name,
+            data=data,
+            config=config
+        )
+    else:
+        return classySpacyExternal(
+            name=name,
+            data=data,
+            model=model,
+            config=config
+        )
+    
+
