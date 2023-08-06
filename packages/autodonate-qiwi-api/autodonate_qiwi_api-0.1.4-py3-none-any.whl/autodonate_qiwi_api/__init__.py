a81da205274e41b102a5252dfaf27061b35569dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:38 +0000

    Add package autodonate-qiwi-api/0.1.4/autodonate_qiwi_api-0.1.4-py3-none-any.whl

---
+++ packages/autodonate-qiwi-api/autodonate_qiwi_api-0.1.4-py3-none-any.whl/autodonate_qiwi_api/__init__.py
@@ -0,0 +1,21 @@
+import autodonate_qiwi_api.types
+from autodonate.lib.context_processors import register_variable
+
+__version__ = "0.1.4"
+__all__ = ["QIWI", "initialize", "types"]
+
+# Don't create millions of separated objects (and threads). Use only one.
+QIWI = None
+
+
+def initialize(*args, **kwargs) -> None:
+    """Create Qiwi object and start it."""
+    from autodonate_qiwi_api.main import Qiwi
+
+    global QIWI
+    if QIWI:
+        log.warning("QIWI API already initialized.")
+        return None
+    register_variable("QIWI_API_INSTALLED", True)
+    QIWI = Qiwi(*args, **kwargs)
+    QIWI.start_thread()
