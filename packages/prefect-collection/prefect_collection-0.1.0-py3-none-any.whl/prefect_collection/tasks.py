2d2273c31299810b9c780496f9a5255dfa178097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:30 +0000

    Add package prefect-collection/0.1.0/prefect_collection-0.1.0-py3-none-any.whl

---
+++ packages/prefect-collection/prefect_collection-0.1.0-py3-none-any.whl/prefect_collection/tasks.py
@@ -0,0 +1,24 @@
+from prefect import task
+
+
+@task
+def hello_prefect_collection() -> str:
+    """
+    Sample task that says hello!
+
+    Returns:
+        A greeting for your collection
+    """
+    return "Hello, prefect-collection!"
+
+
+@task
+def goodbye_prefect_collection() -> str:
+    """
+    Sample task that says goodbye!
+
+    Returns:
+        A farewell for your collection
+    """
+    return "Goodbye, prefect-collection!"
+
