65b3ca864bd4dd8e73893ceb0c3074f4059f4961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:34 +0000

    Add package traq-bot/1.0.2/traq_bot-1.0.2.tar.gz

---
+++ packages/traq-bot/traq_bot-1.0.2.tar.gz/traq_bot-1.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['traq_bot']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'traq-bot',
+    'version': '1.0.2',
+    'description': 'Bot library for traQ',
+    'long_description': None,
+    'author': 'd_etteiu8383',
+    'author_email': 'ysdr83@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9.5,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
