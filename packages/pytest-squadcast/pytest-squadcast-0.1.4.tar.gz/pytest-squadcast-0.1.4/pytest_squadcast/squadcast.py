a15b1a10a5970ea4b6f2e92005e0c9b3e884ec5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:17 +0000

    Add package pytest-squadcast/0.1.4/pytest-squadcast-0.1.4.tar.gz

---
+++ packages/pytest-squadcast/pytest-squadcast-0.1.4.tar.gz/pytest-squadcast-0.1.4/pytest_squadcast/squadcast.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+import pytest
+import requests
+from _pytest.config import ExitCode, Config
+from _pytest.main import Session
+from _pytest.terminal import TerminalReporter
+
+
+class Squadcast:
+    def __init__(self, config: Config):
+        self.config = config
+
+    @pytest.hookimpl(trylast=True)
+    def pytest_sessionfinish(self, session: Session, exitstatus: Union[int, ExitCode]):
+        if session.config.hook.pytest_squadcast_send_alert(exitstatus=exitstatus)[0]:
+            self.send_incident(session, exitstatus)
+
+    @pytest.hookimpl(trylast=True)
+    def pytest_terminal_summary(
+        self,
+        terminalreporter: TerminalReporter,
+        exitstatus: Union[int, ExitCode],
+        config: Config,
+    ):
+        if config.hook.pytest_squadcast_send_alert(exitstatus=exitstatus)[0]:
+            terminalreporter.write_sep("-", "incident reported on Squadcast")
+
+    def send_incident(self, session: Session, exitstatus: Union[int, ExitCode]):
+        # https://support.squadcast.com/docs/apiv2
+        payload = session.config.hook.pytest_squadcast_create_payload(
+            session=session
+        )
+        if not payload:
+            raise Exception("you must implement the hook to create payload")
+        requests.post(
+            url=f"https://api.squadcast.com/v2/incidents/api/{payload[0]['service']}",
+            json=payload[0]["data"],
+        )
