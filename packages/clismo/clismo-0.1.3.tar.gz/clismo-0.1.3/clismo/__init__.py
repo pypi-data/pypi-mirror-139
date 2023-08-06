8f6b6b15bb7e1c428004a17e90ba82fccab74f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:40 +0000

    Add package clismo/0.1.3/clismo-0.1.3.tar.gz

---
+++ packages/clismo/clismo-0.1.3.tar.gz/clismo-0.1.3/clismo/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+import clismo.csre
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+__all__ = ["csre"]
+
+__version__ = "0.1.3"
