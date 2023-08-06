fb9323f6f029775faf3dec151a1b1484bcc465be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:45 +0000

    Add package celery-typed-tasks/0.1.23/celery-typed-tasks-0.1.23.tar.gz

---
+++ packages/celery-typed-tasks/celery-typed-tasks-0.1.23.tar.gz/celery-typed-tasks-0.1.23/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['celery_typed_tasks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['celery>=5.0.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'celery-typed-tasks',
+    'version': '0.1.23',
+    'description': '',
+    'long_description': None,
+    'author': 'massover',
+    'author_email': 'joshm@simplebet.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4',
+}
+
+
+setup(**setup_kwargs)
