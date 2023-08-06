6be767419ca4b01c6ba022795b8092b94c8672e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package smart-range/1.1.0/smart_range-1.1.0.tar.gz

---
+++ packages/smart-range/smart_range-1.1.0.tar.gz/smart_range-1.1.0/README.md
@@ -0,0 +1,5 @@
+# SmartRange
+
+[![Documentation Status](https://readthedocs.org/projects/smartrange/badge/?version=latest)](https://smartrange.readthedocs.io/en/latest/?badge=latest)
+
+View docs at https://smartrange.readthedocs.io/en/latest/.
---
+++ packages/smart-range/smart_range-1.1.0.tar.gz/smart_range-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='smart_range',
+    version='1.1.0',
+    packages=['smart_range'],
+    url='https://github.com/PythonCoderAS/SmartRange',
+    license='MIT',
+    author='PythonCoderAS',
+    author_email='sarkaraoyan@gmail.com',
+    description='A class to easily deal with ranges.',
+    extras_require={"docs": ["sphinx"]},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6'
+)
