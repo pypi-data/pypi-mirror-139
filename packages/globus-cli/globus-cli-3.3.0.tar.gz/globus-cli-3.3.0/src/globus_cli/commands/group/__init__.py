c34496e12ffb31d46062d280e4a801d5608df702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:01 +0000

    Add package globus-cli/3.3.0/globus-cli-3.3.0.tar.gz

---
+++ packages/globus-cli/globus-cli-3.3.0.tar.gz/globus-cli-3.3.0/src/globus_cli/commands/group/__init__.py
@@ -0,0 +1,20 @@
+from globus_cli.commands.group.create import group_create
+from globus_cli.commands.group.delete import group_delete
+from globus_cli.commands.group.list import group_list
+from globus_cli.commands.group.member import group_member
+from globus_cli.commands.group.show import group_show
+from globus_cli.commands.group.update import group_update
+from globus_cli.parsing import group
+
+
+@group("group")
+def group_command() -> None:
+    """Manage Globus Groups"""
+
+
+group_command.add_command(group_list)
+group_command.add_command(group_show)
+group_command.add_command(group_create)
+group_command.add_command(group_update)
+group_command.add_command(group_delete)
+group_command.add_command(group_member)
---
+++ packages/globus-cli/globus-cli-3.3.0.tar.gz/globus-cli-3.3.0/src/globus_cli/commands/search/__init__.py
@@ -0,0 +1,17 @@
+from globus_cli.parsing import group
+
+from .index import index_command
+from .ingest import ingest_command
+from .query import query_command
+from .task import task_command
+
+
+@group("search")
+def search_command():
+    """Use Globus Search to store and query for data"""
+
+
+search_command.add_command(ingest_command)
+search_command.add_command(index_command)
+search_command.add_command(query_command)
+search_command.add_command(task_command)
