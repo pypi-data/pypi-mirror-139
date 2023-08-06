af053b516bd99559f9e81090af93fbb224440f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:40 +0000

    Add package mlchen/1.0.0/mlchen-1.0.0.tar.gz

---
+++ packages/mlchen/mlchen-1.0.0.tar.gz/mlchen-1.0.0/setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup
+setup(
+    name='mlchen', 
+    version='1.0.0',
+    install_requires=['pillow'],
+    packages=['mlchen'],
+    include_package_data=True,
+)
