b151635060db9f087a596ddb2540e9e4713e4536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:26 +0000

    Add package meerschaum/0.5.11/meerschaum-0.5.11.tar.gz

---
+++ packages/meerschaum/meerschaum-0.5.11.tar.gz/meerschaum-0.5.11/meerschaum/connectors/sql/_tools.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Import everything from `meerschaum.connectors.sql.tools` for backwards compatability.
+"""
+
+from meerschaum.connectors.sql.tools import *
