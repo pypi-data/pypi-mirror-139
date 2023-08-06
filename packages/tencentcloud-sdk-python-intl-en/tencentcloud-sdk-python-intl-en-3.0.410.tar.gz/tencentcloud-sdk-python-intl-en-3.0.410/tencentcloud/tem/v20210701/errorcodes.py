410ced4e848257047df12eff5e2452594153dd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:30 +0000

    Add package tencentcloud-sdk-python-intl-en/3.0.410/tencentcloud-sdk-python-intl-en-3.0.410.tar.gz

---
+++ packages/tencentcloud-sdk-python-intl-en/tencentcloud-sdk-python-intl-en-3.0.410.tar.gz/tencentcloud-sdk-python-intl-en-3.0.410/tencentcloud/tem/v20210701/errorcodes.py
@@ -0,0 +1,84 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2017-2021 THL A29 Limited, a Tencent company. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# 
+INTERNALERROR_ACTIONREADTIMEOUT = 'InternalError.ActionReadTimeout'
+
+# 
+INTERNALERROR_CREATEAPMRESOURCEERROR = 'InternalError.CreateApmResourceError'
+
+# 
+INTERNALERROR_CREATEEKSCLUSTERERROR = 'InternalError.CreateEksClusterError'
+
+# Failed to create the service.
+INTERNALERROR_CREATESERVICEERROR = 'InternalError.CreateServiceError'
+
+# 
+INTERNALERROR_DEFAULTINTERNALERROR = 'InternalError.DefaultInternalError'
+
+# 
+INTERNALERROR_DEPLOYVERSIONERROR = 'InternalError.DeployVersionError'
+
+# 
+INTERNALERROR_DESCRIBERUNPODLISTERROR = 'InternalError.DescribeRunPodListError'
+
+# 
+INTERNALERROR_RESTARTAPPLICATIONERROR = 'InternalError.RestartApplicationError'
+
+# 
+INVALIDPARAMETERVALUE_INVALIDDEPLOYVERSION = 'InvalidParameterValue.InvalidDeployVersion'
+
+# 
+INVALIDPARAMETERVALUE_NAMESPACEDUPLICATEERROR = 'InvalidParameterValue.NamespaceDuplicateError'
+
+# 
+INVALIDPARAMETERVALUE_NAMESPACEREACHMAXIMUM = 'InvalidParameterValue.NamespaceReachMaximum'
+
+# 
+INVALIDPARAMETERVALUE_SERVICEFOUNDRUNNINGVERSION = 'InvalidParameterValue.ServiceFoundRunningVersion'
+
+# This application name already exists.
+INVALIDPARAMETERVALUE_SERVICENAMEDUPLICATEERROR = 'InvalidParameterValue.ServiceNameDuplicateError'
+
+# 
+INVALIDPARAMETERVALUE_TRAITSTRACINGNOTSUPPORTED = 'InvalidParameterValue.TraitsTracingNotSupported'
+
+# 
+INVALIDPARAMETERVALUE_VERSIONLOWERCASE = 'InvalidParameterValue.VersionLowerCase'
+
+# There is still traffic routed to this version.
+INVALIDPARAMETERVALUE_VERSIONROUTERATENOTZERO = 'InvalidParameterValue.VersionRouteRateNotZero'
+
+# 
+MISSINGPARAMETER_NAMESPACEIDNULL = 'MissingParameter.NamespaceIdNull'
+
+# 
+RESOURCENOTFOUND_MICROSERVICEOFFLINE = 'ResourceNotFound.MicroserviceOffline'
+
+# 
+RESOURCENOTFOUND_SERVICENOTFOUND = 'ResourceNotFound.ServiceNotFound'
+
+# 
+RESOURCENOTFOUND_SERVICERUNNINGVERSIONNOTFOUND = 'ResourceNotFound.ServiceRunningVersionNotFound'
+
+# 
+RESOURCENOTFOUND_VERSIONNAMESPACENOTFOUND = 'ResourceNotFound.VersionNamespaceNotFound'
+
+# 
+RESOURCEUNAVAILABLE_WAITFORKRUISE = 'ResourceUnavailable.WaitForKruise'
+
+# 
+UNAUTHORIZEDOPERATION_UNAUTHORIZEDOPERATION = 'UnauthorizedOperation.UnauthorizedOperation'
