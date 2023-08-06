b9a3a5f509c77e7613e6c648215559b6da32658d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:02 +0000

    Add package ultralytics-dev/0.0.49/ultralytics_dev-0.0.49-py3-none-any.whl

---
+++ packages/ultralytics-dev/ultralytics_dev-0.0.49-py3-none-any.whl/ultralytics/config.py
@@ -0,0 +1,17 @@
+# Global variables
+
+# ENVIRONMENT = 'development'
+ENVIRONMENT = 'production'
+
+if ENVIRONMENT == 'production':
+    HUB_API_ROOT = "https://api.ultralytics.com/"
+    REPO_URL = "https://github.com/ultralytics/yolov5.git"
+    REPO_BRANCH = "v6.1"  # master
+    BY_PASS_LOGIN = False
+else:
+    HUB_API_ROOT = "http://127.0.0.1:5000/"
+    # REPO_URL = "https://github.com/KalenMike/yolov5.git"
+    # REPO_BRANCH = "feature/pip-package-clone"
+    REPO_URL = "https://github.com/ultralytics/yolov5.git"
+    REPO_BRANCH = "v6.1"  # master
+    BY_PASS_LOGIN = False
