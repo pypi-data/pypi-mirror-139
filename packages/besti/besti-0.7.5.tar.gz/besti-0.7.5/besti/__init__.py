e1ba2f32295a7944674dd54a4242a7580a1cf27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:31 +0000

    Add package besti/0.7.5/besti-0.7.5.tar.gz

---
+++ packages/besti/besti-0.7.5.tar.gz/besti-0.7.5/besti/__init__.py
@@ -0,0 +1,5 @@
+from .common import *
+
+import os, sys
+sys.path.append(os.getcwd())
+#取得当前的工作目录
---
+++ packages/besti/besti-0.7.5.tar.gz/besti-0.7.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="besti",
+    version="0.7.5",
+    atuhor="Hungloek",
+    author_email="2213935054@qq.com",
+    description="大创项目",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent"
+    ],
+)
