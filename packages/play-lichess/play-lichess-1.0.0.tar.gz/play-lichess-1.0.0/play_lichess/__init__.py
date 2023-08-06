469c92805e021818ebc0bf01254d64d1300c7fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:36 +0000

    Add package play-lichess/1.0.0/play-lichess-1.0.0.tar.gz

---
+++ packages/play-lichess/play-lichess-1.0.0.tar.gz/play-lichess-1.0.0/play_lichess/__init__.py
@@ -0,0 +1,11 @@
+from .match import Match, RealTimeMatch, CorrespondenceMatch, UnlimitedMatch
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "__version__",
+    "Match",
+    "RealTimeMatch",
+    "CorrespondenceMatch",
+    "UnlimitedMatch",
+]
