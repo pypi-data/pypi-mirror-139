e05ba504250cd505cbeb20f3f3affc4cb65c2943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:56 +0000

    Add package nonebot-plugin-pixivrank-search/0.5/nonebot_plugin_pixivrank_search-0.5.tar.gz

---
+++ packages/nonebot-plugin-pixivrank-search/nonebot_plugin_pixivrank_search-0.5.tar.gz/nonebot_plugin_pixivrank_search-0.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nonebot_plugin_pixivrank_search']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofiles>=0.8.0,<0.9.0',
+ 'aiohttp>=3.8.1,<4.0.0',
+ 'beautifulsoup4>=4.10.0,<5.0.0',
+ 'feedparser>=6.0.8,<7.0.0',
+ 'lxml>=4.7.1,<5.0.0',
+ 'nonebot-adapter-onebot>=2.0.0-beta.1,<3.0.0',
+ 'nonebot2>=2.0.0-beta.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'nonebot-plugin-pixivrank-search',
+    'version': '0.5',
+    'description': '基于RSSHUB阅读器实现的获取P站排行和P站搜图',
+    'long_description': None,
+    'author': 'HibiKier',
+    'author_email': '775757368@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
