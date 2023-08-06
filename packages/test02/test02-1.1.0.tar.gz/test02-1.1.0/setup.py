6b5c800546d5fcc877230b39497e5260541d4437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:18 +0000

    Add package test02/1.1.0/test02-1.1.0.tar.gz

---
+++ packages/test02/test02-1.1.0.tar.gz/test02-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'test02',
+        version = '1.1.0',
+        py_modules = ['test02'],
+        author = 'gaoyinghe',
+        author_email = 'gaoyinghe@neusoft.com',
+        url = 'http://www.headfirstlabs.com',
+        description = '这是第一个python打包'
+    
+    )
---
+++ packages/test02/test02-1.1.0.tar.gz/test02-1.1.0/test02.py
@@ -0,0 +1,38 @@
+fa_movies = ["Test1","Test2",2022,["Test_Arr01","Test_Arr02","Test_Arr03"],"Test3"]
+
+fa_movies.insert(3,"TestAdd1");
+fa_movies.insert(4,"TestAdd2");
+fa_movies.append("Test_tail");
+
+
+for var_each in fa_movies:
+    if isinstance(var_each,list):
+        for child_each in var_each:
+             print(child_each);
+    else:
+        print(var_each);
+
+"""定义一个方法，递归查找展示列表数据"""
+"""def print_cycle(var_list):
+    if isinstance(var_list,list):
+        for var in var_list:
+            print_cycle(var);
+    else:
+        print(var_list);
+
+
+print_cycle(fa_movies);"""
+
+
+def print_cycle(var_list,level=0):
+    for item in var_list:
+        if isinstance(item,list):
+            print_cycle(item,level+1);
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(item);
+                      
+print_cycle(fa_movies,0);                      
+"""import sys;
+print(sys.path);"""
