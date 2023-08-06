9ffbbd603ef2b86be5a45c1c28391c6cd21c5587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:26 +0000

    Add package asdf/2.10.0/asdf-2.10.0.tar.gz

---
+++ packages/asdf/asdf-2.10.0.tar.gz/asdf-2.10.0/asdf/resource.py
@@ -0,0 +1,273 @@
+"""
+Support for plugins that provide access to resources such
+as schemas.
+"""
+import fnmatch
+import os
+import pkgutil
+from collections.abc import Mapping
+from pathlib import Path
+
+from .util import get_class_name
+
+__all__ = [
+    "ResourceMappingProxy",
+    "DirectoryResourceMapping",
+    "ResourceManager",
+    "JsonschemaResourceMapping",
+    "get_json_schema_resource_mappings",
+]
+
+
+class ResourceMappingProxy(Mapping):
+    """
+    Wrapper around a resource mapping that carries
+    additional information on the package that provided
+    the mapping.
+    """
+
+    @classmethod
+    def maybe_wrap(self, delegate):
+        if isinstance(delegate, ResourceMappingProxy):
+            return delegate
+        else:
+            return ResourceMappingProxy(delegate)
+
+    def __init__(self, delegate, package_name=None, package_version=None):
+        if not isinstance(delegate, Mapping):
+            raise TypeError("Resource mapping must implement the Mapping interface")
+
+        self._delegate = delegate
+        self._package_name = package_name
+        self._package_version = package_version
+        self._class_name = get_class_name(delegate)
+
+    def __getitem__(self, uri):
+        return self._delegate.__getitem__(uri)
+
+    def __len__(self):
+        return self._delegate.__len__()
+
+    def __iter__(self):
+        return self._delegate.__iter__()
+
+    @property
+    def delegate(self):
+        """
+        Get the wrapped mapping instance.
+
+        Returns
+        -------
+        collections.abc.Mapping
+        """
+        return self._delegate
+
+    @property
+    def package_name(self):
+        """
+        Get the name of the Python package that provided this mapping.
+
+        Returns
+        -------
+        str or None
+            `None` if the mapping was added at runtime.
+        """
+        return self._package_name
+
+    @property
+    def package_version(self):
+        """
+        Get the version of the Python package that provided the mapping.
+
+        Returns
+        -------
+        str or None
+            `None` if the mapping was added at runtime.
+        """
+        return self._package_version
+
+    @property
+    def class_name(self):
+        """ "
+        Get the fully qualified class name of the mapping.
+
+        Returns
+        -------
+        str
+        """
+        return self._class_name
+
+    def __eq__(self, other):
+        if isinstance(other, ResourceMappingProxy):
+            return other.delegate is self.delegate
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(id(self.delegate))
+
+    def __repr__(self):
+        if self.package_name is not None:
+            package_description = "{}=={}".format(self.package_name, self.package_version)
+        else:
+            package_description = "(none)"
+
+        return "<ResourceMappingProxy class: {} package: {} len: {}>".format(
+            self.class_name,
+            package_description,
+            len(self),
+        )
+
+
+class DirectoryResourceMapping(Mapping):
+    """
+    Resource mapping that reads resource content
+    from a directory or directory tree.
+
+    Parameters
+    ----------
+    root : str or importlib.abc.Traversable
+        Root directory (or directory-like Traversable) of the resource
+        files.  `str` will be interpreted as a filesystem path.
+    uri_prefix : str
+        Prefix used to construct URIs from file paths.  The
+        prefix will be prepended to paths relative to the root
+        directory.
+    recursive : bool, optional
+        If `True`, recurse into subdirectories.  Defaults to `False`.
+    filename_pattern : str, optional
+        Glob pattern that identifies relevant filenames.
+        Defaults to `"*.yaml"`.
+    stem_filename : bool, optional
+        If `True`, remove the filename's extension when
+        constructing its URI.
+    """
+
+    def __init__(self, root, uri_prefix, recursive=False, filename_pattern="*.yaml", stem_filename=True):
+        self._uri_to_file = {}
+        self._recursive = recursive
+        self._filename_pattern = filename_pattern
+        self._stem_filename = stem_filename
+
+        if isinstance(root, str):
+            self._root = Path(root)
+        else:
+            self._root = root
+
+        if uri_prefix.endswith("/"):
+            self._uri_prefix = uri_prefix[:-1]
+        else:
+            self._uri_prefix = uri_prefix
+
+        for file, path_components in self._iterate_files(self._root, []):
+            self._uri_to_file[self._make_uri(file, path_components)] = file
+
+    def _iterate_files(self, directory, path_components):
+        for obj in directory.iterdir():
+            if obj.is_file() and fnmatch.fnmatch(obj.name, self._filename_pattern):
+                yield obj, path_components
+            elif obj.is_dir() and self._recursive:
+                yield from self._iterate_files(obj, path_components + [obj.name])
+
+    def _make_uri(self, file, path_components):
+        if self._stem_filename:
+            filename = os.path.splitext(file.name)[0]
+        else:
+            filename = file.name
+
+        return "/".join([self._uri_prefix] + path_components + [filename])
+
+    def __getitem__(self, uri):
+        return self._uri_to_file[uri].read_bytes()
+
+    def __len__(self):
+        return len(self._uri_to_file)
+
+    def __iter__(self):
+        yield from self._uri_to_file
+
+    def __repr__(self):
+        return "{}({!r}, {!r}, recursive={!r}, filename_pattern={!r}, stem_filename={!r})".format(
+            self.__class__.__name__,
+            self._root,
+            self._uri_prefix,
+            self._recursive,
+            self._filename_pattern,
+            self._stem_filename,
+        )
+
+
+class ResourceManager(Mapping):
+    """
+    Wraps multiple resource mappings into a single interface
+    with some friendlier error handling.
+
+    Parameters
+    ----------
+    resource_mappings : iterable of collections.abc.Mapping
+        Underlying resource mappings.  In the case of a duplicate URI,
+        the first mapping takes precedence.
+    """
+
+    def __init__(self, resource_mappings):
+        self._resource_mappings = resource_mappings
+
+        self._mappings_by_uri = {}
+        for mapping in resource_mappings:
+            for uri in mapping:
+                if uri not in self._mappings_by_uri:
+                    self._mappings_by_uri[uri] = mapping
+
+    def __getitem__(self, uri):
+        if uri not in self._mappings_by_uri:
+            raise KeyError("Resource unavailable for URI: {}".format(uri))
+
+        content = self._mappings_by_uri[uri][uri]
+        if isinstance(content, str):
+            content = content.encode("utf-8")
+
+        return content
+
+    def __len__(self):
+        return len(self._mappings_by_uri)
+
+    def __iter__(self):
+        yield from self._mappings_by_uri
+
+    def __contains__(self, uri):
+        # Implement __contains__ only for efficiency.
+        return uri in self._mappings_by_uri
+
+    def __repr__(self):
+        return "<ResourceManager len: {}>".format(self.__len__())
+
+
+_JSONSCHEMA_URI_TO_FILENAME = {
+    "http://json-schema.org/draft-04/schema": "draft4.json",
+}
+
+
+class JsonschemaResourceMapping(Mapping):
+    """
+    Resource mapping that fetches metaschemas from
+    the jsonschema package.
+    """
+
+    def __getitem__(self, uri):
+        filename = _JSONSCHEMA_URI_TO_FILENAME[uri]
+        return pkgutil.get_data("jsonschema", "schemas/{}".format(filename))
+
+    def __len__(self):
+        return len(_JSONSCHEMA_URI_TO_FILENAME)
+
+    def __iter__(self):
+        yield from _JSONSCHEMA_URI_TO_FILENAME
+
+    def __repr__(self):
+        return "JsonschemaResourceMapping()"
+
+
+def get_json_schema_resource_mappings():
+    return [
+        JsonschemaResourceMapping(),
+    ]
---
+++ packages/asdf/asdf-2.10.0.tar.gz/asdf-2.10.0/asdf/tests/test_resource.py
@@ -0,0 +1,313 @@
+import io
+import sys
+from collections.abc import Mapping
+from pathlib import Path
+
+if sys.version_info < (3, 9):
+    import importlib_resources as importlib
+else:
+    import importlib
+
+import pytest
+
+from asdf.resource import (
+    DirectoryResourceMapping,
+    JsonschemaResourceMapping,
+    ResourceManager,
+    ResourceMappingProxy,
+    get_json_schema_resource_mappings,
+)
+
+
+def test_directory_resource_mapping(tmpdir):
+    tmpdir.mkdir("schemas")
+    (tmpdir / "schemas").mkdir("nested")
+    with (tmpdir / "schemas" / "foo-1.2.3.yaml").open("w") as f:
+        f.write("id: http://somewhere.org/schemas/foo-1.2.3\n")
+    with (tmpdir / "schemas" / "nested" / "bar-4.5.6.yaml").open("w") as f:
+        f.write("id: http://somewhere.org/schemas/nested/bar-4.5.6\n")
+    with (tmpdir / "schemas" / "baz-7.8.9").open("w") as f:
+        f.write("id: http://somewhere.org/schemas/baz-7.8.9\n")
+
+    mapping = DirectoryResourceMapping(str(tmpdir / "schemas"), "http://somewhere.org/schemas")
+    assert isinstance(mapping, Mapping)
+    assert len(mapping) == 1
+    assert set(mapping) == {"http://somewhere.org/schemas/foo-1.2.3"}
+    assert "http://somewhere.org/schemas/foo-1.2.3" in mapping
+    assert b"http://somewhere.org/schemas/foo-1.2.3" in mapping["http://somewhere.org/schemas/foo-1.2.3"]
+    assert "http://somewhere.org/schemas/baz-7.8.9" not in mapping
+    assert "http://somewhere.org/schemas/baz-7.8" not in mapping
+    assert "http://somewhere.org/schemas/foo-1.2.3.yaml" not in mapping
+    assert "http://somewhere.org/schemas/nested/bar-4.5.6" not in mapping
+
+    mapping = DirectoryResourceMapping(str(tmpdir / "schemas"), "http://somewhere.org/schemas", recursive=True)
+    assert len(mapping) == 2
+    assert set(mapping) == {"http://somewhere.org/schemas/foo-1.2.3", "http://somewhere.org/schemas/nested/bar-4.5.6"}
+    assert "http://somewhere.org/schemas/foo-1.2.3" in mapping
+    assert b"http://somewhere.org/schemas/foo-1.2.3" in mapping["http://somewhere.org/schemas/foo-1.2.3"]
+    assert "http://somewhere.org/schemas/baz-7.8.9" not in mapping
+    assert "http://somewhere.org/schemas/baz-7.8" not in mapping
+    assert "http://somewhere.org/schemas/nested/bar-4.5.6" in mapping
+    assert b"http://somewhere.org/schemas/nested/bar-4.5.6" in mapping["http://somewhere.org/schemas/nested/bar-4.5.6"]
+
+    mapping = DirectoryResourceMapping(
+        str(tmpdir / "schemas"),
+        "http://somewhere.org/schemas",
+        recursive=True,
+        filename_pattern="baz-*",
+        stem_filename=False,
+    )
+
+    assert len(mapping) == 1
+    assert set(mapping) == {"http://somewhere.org/schemas/baz-7.8.9"}
+    assert "http://somewhere.org/schemas/foo-1.2.3" not in mapping
+    assert "http://somewhere.org/schemas/baz-7.8.9" in mapping
+    assert b"http://somewhere.org/schemas/baz-7.8.9" in mapping["http://somewhere.org/schemas/baz-7.8.9"]
+    assert "http://somewhere.org/schemas/nested/bar-4.5.6" not in mapping
+
+    # Check that the repr is reasonable
+    # Need to be careful checking the path string because
+    # pathlib normalizes Windows paths.
+    assert repr(Path(str(tmpdir / "schemas"))) in repr(mapping)
+    assert "http://somewhere.org/schemas" in repr(mapping)
+    assert "recursive=True" in repr(mapping)
+    assert "filename_pattern='baz-*'" in repr(mapping)
+    assert "stem_filename=False" in repr(mapping)
+
+    # Make sure trailing slash is handled correctly
+    mapping = DirectoryResourceMapping(str(tmpdir / "schemas"), "http://somewhere.org/schemas/")
+    assert len(mapping) == 1
+    assert set(mapping) == {"http://somewhere.org/schemas/foo-1.2.3"}
+    assert "http://somewhere.org/schemas/foo-1.2.3" in mapping
+    assert b"http://somewhere.org/schemas/foo-1.2.3" in mapping["http://somewhere.org/schemas/foo-1.2.3"]
+
+
+def test_directory_resource_mapping_with_traversable():
+    """
+    Confirm that DirectoryResourceMapping doesn't use pathlib.Path
+    methods outside of the Traversable interface.
+    """
+
+    class MockTraversable(importlib.abc.Traversable):
+        def __init__(self, name, value):
+            self._name = name
+            self._value = value
+
+        def iterdir(self):
+            if isinstance(self._value, dict):
+                for key, child in self._value.items():
+                    yield MockTraversable(key, child)
+
+        def read_bytes(self):
+            if not isinstance(self._value, bytes):
+                raise RuntimeError("Not a file")
+            return self._value
+
+        def read_text(self, encoding="utf-8"):
+            return self.read_bytes().decode(encoding)
+
+        def is_dir(self):
+            return isinstance(self._value, dict)
+
+        def is_file(self):
+            return self._value is not None and not isinstance(self._value, dict)
+
+        def joinpath(self, child):
+            if isinstance(self._value, dict):
+                child_value = self._value.get(child)
+            else:
+                child_value = None
+
+            return MockTraversable(child, child_value)
+
+        def __truediv__(self, child):
+            return self.joinpath(child)
+
+        def open(self, mode="r", *args, **kwargs):
+            if not self.is_file():
+                raise RuntimeError("Not a file")
+
+            if mode == "r":
+                return io.TextIOWrapper(io.BytesIO(self._value), *args, **kwargs)
+            elif mode == "rb":
+                return io.BytesIO(self._value)
+            else:
+                raise "Not a valid mode"
+
+        @property
+        def name(self):
+            return self._name
+
+    root = MockTraversable(
+        "/path/to/some/root",
+        {"foo-1.0.0.yaml": b"foo", "bar-1.0.0.yaml": b"bar", "baz-1.0.0": b"baz", "nested": {"foz-1.0.0.yaml": b"foz"}},
+    )
+
+    mapping = DirectoryResourceMapping(root, "http://somewhere.org/schemas")
+    assert len(mapping) == 2
+    assert set(mapping) == {"http://somewhere.org/schemas/foo-1.0.0", "http://somewhere.org/schemas/bar-1.0.0"}
+    assert "http://somewhere.org/schemas/foo-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/foo-1.0.0"] == b"foo"
+    assert "http://somewhere.org/schemas/bar-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/bar-1.0.0"] == b"bar"
+    assert "http://somewhere.org/schemas/baz-1.0.0" not in mapping
+    assert "http://somewhere.org/schemas/nested/foz-1.0.0" not in mapping
+
+    mapping = DirectoryResourceMapping(root, "http://somewhere.org/schemas", recursive=True)
+    assert len(mapping) == 3
+    assert set(mapping) == {
+        "http://somewhere.org/schemas/foo-1.0.0",
+        "http://somewhere.org/schemas/bar-1.0.0",
+        "http://somewhere.org/schemas/nested/foz-1.0.0",
+    }
+    assert "http://somewhere.org/schemas/foo-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/foo-1.0.0"] == b"foo"
+    assert "http://somewhere.org/schemas/bar-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/bar-1.0.0"] == b"bar"
+    assert "http://somewhere.org/schemas/baz-1.0.0" not in mapping
+    assert "http://somewhere.org/schemas/nested/foz-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/nested/foz-1.0.0"] == b"foz"
+
+    mapping = DirectoryResourceMapping(
+        root, "http://somewhere.org/schemas", filename_pattern="baz-*", stem_filename=False
+    )
+    assert len(mapping) == 1
+    assert set(mapping) == {"http://somewhere.org/schemas/baz-1.0.0"}
+    assert "http://somewhere.org/schemas/foo-1.0.0" not in mapping
+    assert "http://somewhere.org/schemas/bar-1.0.0" not in mapping
+    assert "http://somewhere.org/schemas/baz-1.0.0" in mapping
+    assert mapping["http://somewhere.org/schemas/baz-1.0.0"] == b"baz"
+    assert "http://somewhere.org/schemas/nested/foz-1.0.0" not in mapping
+
+
+def test_resource_manager():
+    mapping1 = {
+        "http://somewhere.org/schemas/foo-1.0.0": b"foo",
+        "http://somewhere.org/schemas/bar-1.0.0": b"bar",
+    }
+    mapping2 = {
+        "http://somewhere.org/schemas/foo-1.0.0": b"duplicate foo",
+        "http://somewhere.org/schemas/baz-1.0.0": b"baz",
+        "http://somewhere.org/schemas/foz-1.0.0": "foz",
+    }
+    manager = ResourceManager([mapping1, mapping2])
+
+    assert isinstance(manager, Mapping)
+
+    assert len(manager) == 4
+    assert set(manager) == {
+        "http://somewhere.org/schemas/foo-1.0.0",
+        "http://somewhere.org/schemas/bar-1.0.0",
+        "http://somewhere.org/schemas/baz-1.0.0",
+        "http://somewhere.org/schemas/foz-1.0.0",
+    }
+    assert "http://somewhere.org/schemas/foo-1.0.0" in manager
+    assert manager["http://somewhere.org/schemas/foo-1.0.0"] == b"foo"
+    assert "http://somewhere.org/schemas/bar-1.0.0" in manager
+    assert manager["http://somewhere.org/schemas/bar-1.0.0"] == b"bar"
+    assert "http://somewhere.org/schemas/baz-1.0.0" in manager
+    assert manager["http://somewhere.org/schemas/baz-1.0.0"] == b"baz"
+    assert "http://somewhere.org/schemas/foz-1.0.0" in manager
+    assert manager["http://somewhere.org/schemas/foz-1.0.0"] == b"foz"
+
+    with pytest.raises(KeyError, match="http://somewhere.org/schemas/missing-1.0.0"):
+        manager["http://somewhere.org/schemas/missing-1.0.0"]
+
+    # Confirm that the repr string is reasonable:
+    assert "len: 4" in repr(manager)
+
+
+def test_jsonschema_resource_mapping():
+    mapping = JsonschemaResourceMapping()
+    assert isinstance(mapping, Mapping)
+
+    assert len(mapping) == 1
+    assert set(mapping) == {"http://json-schema.org/draft-04/schema"}
+    assert "http://json-schema.org/draft-04/schema" in mapping
+    assert b"http://json-schema.org/draft-04/schema" in mapping["http://json-schema.org/draft-04/schema"]
+
+    assert repr(mapping) == "JsonschemaResourceMapping()"
+
+
+@pytest.mark.parametrize(
+    "uri",
+    [
+        "http://json-schema.org/draft-04/schema",
+    ],
+)
+def test_get_json_schema_resource_mappings(uri):
+    mappings = get_json_schema_resource_mappings()
+
+    mapping = next(m for m in mappings if uri in m)
+    assert mapping is not None
+
+    assert uri.encode("utf-8") in mapping[uri]
+
+
+def test_proxy_is_mapping():
+    assert isinstance(ResourceMappingProxy({}), Mapping)
+
+
+def test_proxy_maybe_wrap():
+    mapping = {
+        "http://somewhere.org/resources/foo": "foo",
+        "http://somewhere.org/resources/bar": "bar",
+    }
+
+    proxy = ResourceMappingProxy.maybe_wrap(mapping)
+    assert proxy.delegate is mapping
+    assert ResourceMappingProxy.maybe_wrap(proxy) is proxy
+
+    with pytest.raises(TypeError):
+        ResourceMappingProxy.maybe_wrap([])
+
+
+def test_proxy_properties():
+    mapping = {
+        "http://somewhere.org/resources/foo": "foo",
+        "http://somewhere.org/resources/bar": "bar",
+    }
+
+    proxy = ResourceMappingProxy(mapping, package_name="foo", package_version="1.2.3")
+
+    assert len(proxy) == len(mapping)
+    assert set(proxy.keys()) == set(mapping.keys())
+    for uri in mapping:
+        assert proxy[uri] is mapping[uri]
+
+    assert proxy.package_name == "foo"
+    assert proxy.package_version == "1.2.3"
+    assert proxy.class_name.endswith("dict")
+
+
+def test_proxy_hash_and_eq():
+    mapping = {
+        "http://somewhere.org/resources/foo": "foo",
+        "http://somewhere.org/resources/bar": "bar",
+    }
+    proxy1 = ResourceMappingProxy(mapping)
+    proxy2 = ResourceMappingProxy(mapping, package_name="foo", package_version="1.2.3")
+
+    assert proxy1 == proxy2
+    assert hash(proxy1) == hash(proxy2)
+    assert proxy1 != mapping
+    assert proxy2 != mapping
+
+
+def test_proxy_repr():
+    mapping = {
+        "http://somewhere.org/resources/foo": "foo",
+        "http://somewhere.org/resources/bar": "bar",
+    }
+
+    proxy = ResourceMappingProxy(mapping, package_name="foo", package_version="1.2.3")
+
+    assert ".dict" in repr(proxy)
+    assert "package: foo==1.2.3" in repr(proxy)
+    assert "len: 2" in repr(proxy)
+
+    empty_proxy = ResourceMappingProxy({})
+
+    assert ".dict" in repr(empty_proxy)
+    assert "package: (none)" in repr(empty_proxy)
+    assert "len: 0" in repr(empty_proxy)
