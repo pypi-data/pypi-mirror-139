b89cf0f1c0b334bb09f30f61e43da65518e4ba2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:00 +0000

    Add package hspylib/0.11.33/hspylib-0.11.33-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.33-py3-none-any.whl/hspylib/core/metaclass/singleton.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib
+   @package: main.metaclass
+      @file: singleton.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2021, HSPyLib team
+"""
+
+import logging as log
+from typing import Type
+
+
+# pylint: disable=bad-mcs-classmethod-argument
+class Singleton(type):
+    """TODO"""
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """TODO"""
+        if not cls.has_instance(cls):
+            instance = super(Singleton, cls).__call__(*args, **kwargs)
+            assert instance is not None, f'Unable to create Singleton instance: {cls}'
+            cls.INSTANCE = instance
+            cls._instances[cls] = instance
+            log.debug(f'Created a new Singleton instance: {cls.__module__}.{cls.__name__}')
+
+        return cls._instances[cls]
+
+    @classmethod
+    def instances(mcs) -> dict:
+        """TODO"""
+        return Singleton._instances
+
+    @classmethod
+    def has_instance(mcs, cls) -> bool:
+        """TODO"""
+        return cls in Singleton._instances
+
+    @classmethod
+    def del_instance(mcs, clazz: Type) -> bool:
+        """TODO"""
+        singleton_instances = Singleton.__getattribute__(mcs, '_instances')
+        if clazz in singleton_instances:
+            log.warning(f'Deleted an existing Singleton instance: {mcs.__module__}.{mcs.__name__}')
+            del mcs._instances[clazz]
+            delattr(clazz, 'INSTANCE')
+            del clazz
+            return True
+
+        return False
