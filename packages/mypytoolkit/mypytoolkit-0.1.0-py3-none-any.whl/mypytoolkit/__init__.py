3e73bc638ed0024ac461a5f197ec7ccfb3805253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:07 +0000

    Add package mypytoolkit/0.1.0/mypytoolkit-0.1.0-py3-none-any.whl

---
+++ packages/mypytoolkit/mypytoolkit-0.1.0-py3-none-any.whl/mypytoolkit/__init__.py
@@ -0,0 +1,9 @@
+# Document checker
+from mypytoolkit.samedocs import *
+
+# Date and time
+from mypytoolkit.dates import *
+
+def tprint(arg):
+    """Prints the contents of a variable/object along with its type."""
+    print(arg, type(arg))
