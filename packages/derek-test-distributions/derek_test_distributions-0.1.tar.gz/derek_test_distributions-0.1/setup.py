e03ca253471dbb0d85925e9c1f1ef465056884b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:41 +0000

    Add package derek-test-distributions/0.1/derek_test_distributions-0.1.tar.gz

---
+++ packages/derek-test-distributions/derek_test_distributions-0.1.tar.gz/derek_test_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='derek_test_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['derek_test_distributions'],
+      author = 'Derek Chen',
+      zip_safe=False)
