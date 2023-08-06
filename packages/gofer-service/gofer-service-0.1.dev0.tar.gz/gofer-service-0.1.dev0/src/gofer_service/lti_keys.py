8372ad559e1ae6821c7c2994af0fc67486ebf693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:14 +0000

    Add package gofer-service/0.1.dev0/gofer-service-0.1.dev0.tar.gz

---
+++ packages/gofer-service/gofer-service-0.1.dev0.tar.gz/gofer-service-0.1.dev0/src/gofer_service/lti_keys.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import yaml
+
+def get(key_name) -> str:
+    """
+    This method first tries to decrypt sops file and then tries the
+    environment. it will return an Exception if the key is not found
+
+    :param key_name: the name of the key to retrieve from the Secrets or the environment
+    :return: the secret
+    """
+    try:
+        secret = get_via_env(key_name)
+        if secret is None:
+            secret = get_via_sops(key_name)
+        return secret
+    except Exception as ex:
+        raise Exception(f"LTI key not configured: {key_name}; Grade NOT posted to LTI server; please configure: Error: {ex}") from ex
+
+
+
+def get_via_sops(key):
+    """
+    This function attempts to use sops to decrpyt the secrets/gke_key.yaml
+
+    :param key: the key to find in Google Secrets Manager
+    :return: the value of the key or None
+    """
+    try:
+        secrets_path = os.path.join(os.path.dirname(__file__), "secrets/gke_key.yaml")
+        sops_output = subprocess.check_output(["/root/go/bin/sops", "-d", secrets_path], stderr=subprocess.STDOUT)
+        dct = yaml.safe_load(sops_output)
+        return dct[key]
+    except Exception as ex:
+        raise ex
+
+def get_via_env(key):
+    """
+    This checks the environment for the key.
+
+    :param key: the key to find in the environment
+    :return: the value of the key or None
+    """
+    if key in os.environ:
+        return os.environ[key]
+    return None
