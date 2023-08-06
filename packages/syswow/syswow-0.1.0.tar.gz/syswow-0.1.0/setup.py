b63ddaa993552a2ce15627fb5073f085b7db81f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package syswow/0.1.0/syswow-0.1.0.tar.gz

---
+++ packages/syswow/syswow-0.1.0.tar.gz/syswow-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'syswow',
+    packages = ['.'],
+    version = '0.1.0',
+    description = 'DLL Injection',
+    author = 'syswow',
+    author_email = '64syswow64@protonmail.com',
+    keywords = ['DLLinjector', 'DLL'],
+    classifiers = [],
+)
