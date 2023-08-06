276d7d1714fecbe5ed8d21d15a982e0a1ede7a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:39 +0000

    Add package odoo10-addon-shopinvader/10.0.2.22.0.99.dev3/odoo10_addon_shopinvader-10.0.2.22.0.99.dev3-py2-none-any.whl

---
+++ packages/odoo10-addon-shopinvader/odoo10_addon_shopinvader-10.0.2.22.0.99.dev3-py2-none-any.whl/odoo/addons/shopinvader/wizards/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from . import shopinvader_category_binding_wizard
+from . import shopinvader_category_unbinding_wizard
+from . import shopinvader_variant_binding_wizard
+from . import shopinvader_variant_unbinding_wizard
+from . import shopinvader_partner_binding
+from . import shopinvader_partner_binding_line
+from . import shopinvader_redirection_wizard
