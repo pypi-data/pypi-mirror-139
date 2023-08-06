c93aaeb8c83cc8125b2177c1c8850bdc387ccfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:14 +0000

    Add package manim-tools/0.1/manim-tools-0.1.tar.gz

---
+++ packages/manim-tools/manim-tools-0.1.tar.gz/manim-tools-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='manim-tools',
+      version='0.1',
+      description='An extending command line tool for the manim package',
+      url='http://github.com/r14r/manim+',
+      author='Ralph Göstenmeier',
+      author_email='r14r@via-internet.de',
+      license='MIT',
+      packages=['manim-tools'],
+      zip_safe=False)
+    
