081d7475ba845a9aaa1b0c90d91869e060be1612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:51 +0000

    Add package TESS-Localize/0.2.6/TESS_Localize-0.2.6.tar.gz

---
+++ packages/TESS-Localize/TESS_Localize-0.2.6.tar.gz/TESS_Localize-0.2.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+    
+
+setup(name='TESS_Localize',
+version='0.2.6',
+description='Package for localizing variable stars in TESS Photometry',
+url='https://github.com/Higgins00/TESS-Localize',
+author='Michael Higgins',
+author_email='michael.higgins@duke.edu',
+license='GNU General Public License v3.0',
+install_requires=requirements,
+packages=['TESS_Localize'],
+zip_safe=False)
+
