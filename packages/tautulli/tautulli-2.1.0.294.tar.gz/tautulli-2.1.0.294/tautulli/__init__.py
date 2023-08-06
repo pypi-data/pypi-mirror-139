df6e7fe6b3f07ab9685723f89a44c09824a22b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:23 +0000

    Add package tautulli/2.1.0.294/tautulli-2.1.0.294.tar.gz

---
+++ packages/tautulli/tautulli-2.1.0.294.tar.gz/tautulli-2.1.0.294/tautulli/__init__.py
@@ -0,0 +1,2 @@
+from tautulli.jsonapi import RawAPI
+from tautulli.objectapi import ObjectAPI
---
+++ packages/tautulli/tautulli-2.1.0.294.tar.gz/tautulli-2.1.0.294/tautulli/_info.py
@@ -0,0 +1,13 @@
+__version__ = '2.1.0.294'
+
+__title__ = "tautulli"
+__author__ = 'Nate Harris'
+__author_email__ = 'n8gr8gbln@gmail.com'
+__github_username__ = "nwithan8"
+__github_repo__ = "pytulli"
+__copyright__ = "Copyright © 2022 - Nate Harris"
+__license__ = 'GNU General Public License v3 (GPLv3)'
+__description__ = "A complete Python client for Tautulli's API"
+__keywords__ = ["Tautulli", "API", "client", "Plex", "PMS", "Plex Media Server", "media", "server", "JSON"]
+
+__min_api_version__ = "2.9.4"
