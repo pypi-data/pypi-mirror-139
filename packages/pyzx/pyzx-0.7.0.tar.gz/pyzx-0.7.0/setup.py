16053b297d3730bb45856113f1bc05fd27ae2ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:34 +0000

    Add package pyzx/0.7.0/pyzx-0.7.0.tar.gz

---
+++ packages/pyzx/pyzx-0.7.0.tar.gz/pyzx-0.7.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "readme.md").read_text()
+
+setup(
+    name="pyzx",
+    author="Quantomatic",
+    author_email="john@vdwetering.name",
+    version="0.7.0",
+    url="https://github.com/Quantomatic/pyzx.git",
+    description="Library for quantum circuit rewriting and optimisation using the ZX-calculus",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache2",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 4 - Beta",
+    ],
+    packages=[
+        "pyzx",
+        "pyzx.circuit",
+        "pyzx.graph",
+        "pyzx.routing",
+        "pyzx.local_search",
+        "pyzx.scripts",
+    ],
+    python_requires='>=3.6',
+    install_requires=["typing_extensions>=3.7.4",
+                      "numpy>=1.14",
+                      "pyperclip>=1.8.1",
+                      "tqdm>=4.56.0"],
+    include_package_data=True,
+)
