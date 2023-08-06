6f67d91cb5d5a0f358306023a9d85c33c2b8b5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:37 +0000

    Add package piotr1000200030004455/0.0.1/piotr1000200030004455-0.0.1.tar.gz

---
+++ packages/piotr1000200030004455/piotr1000200030004455-0.0.1.tar.gz/piotr1000200030004455-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name="piotr1000200030004455",
+    version="0.0.1",
+    author="Uczeń SDA",
+    author_email="young@termal.com",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Testing library"
+)
+
