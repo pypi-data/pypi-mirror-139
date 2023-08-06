cb2619b5aec90c430def4ba7a8f161e1d9bdcd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:19 +0000

    Add package artesian-sdk/3.0.0.dev1/artesian_sdk-3.0.0.dev1-py3-none-any.whl

---
+++ packages/artesian-sdk/artesian_sdk-3.0.0.dev1-py3-none-any.whl/test/helpers.py
@@ -0,0 +1,12 @@
+from unittest.mock import *
+import Artesian.Query._Query as _Query
+
+def TrackRequests(func):
+    @patch.object(_Query._Query, '_exec')
+    def wrapper(self, mock):
+        class Qs:
+            def getQs(_self):
+                return dict(map(lambda x:x.split('='), mock.call_args.args[0][0].split('?')[1].split('&')))
+
+        func(self, Qs())
+    return wrapper
