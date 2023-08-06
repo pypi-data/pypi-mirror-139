86df87d19edca480e57b7ff416e4cf3a6567f72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:40 +0000

    Add package heart-disease-classification-model/0.0.11/heart%20disease%20classification%20model-0.0.11.tar.gz

---
+++ packages/heart-disease-classification-model/heart%20disease%20classification%20model-0.0.11.tar.gz/heart disease classification model-0.0.11/src/test/test_load_from_database.py
@@ -0,0 +1,40 @@
+"""Tests the load from database function."""
+import pandas as pd
+
+from src.config.settings import DATABASE_PATH, SAMPLE_DATA_PATH, config
+from src.preprocessing.datamanager import load_from_database, preprocess_data
+
+sample_data = pd.read_csv(SAMPLE_DATA_PATH)
+sample_data = sample_data.iloc[:,1:]
+processed_sample_data = preprocess_data(sample_data)
+
+sample_input = [
+    "Male",
+    "Yes",
+    "Normal",
+    21,
+    "Low",
+    120,
+    0.2,
+    266000,
+    100,
+    105,
+    16.3,
+    185,
+    80,
+    23.374726077428782,
+]
+
+
+def test_load_from_database():
+    """
+    Checks if the columns retrieved are all present.
+
+    :return: None
+    """
+    sample_database = load_from_database(DATABASE_PATH)
+    print(f"sample_database.columns.to_list() is \n{sample_database.columns.to_list()}")
+    assert (
+        sample_database.columns.to_list()
+        == config.modelConfig.total_features_with_target
+    )
