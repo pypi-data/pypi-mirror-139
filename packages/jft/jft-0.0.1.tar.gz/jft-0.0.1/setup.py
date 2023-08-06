88b06d73bf93dc49fddecfd33c79e9f8b95a5aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:00 +0000

    Add package jft/0.0.1/jft-0.0.1.tar.gz

---
+++ packages/jft/jft-0.0.1.tar.gz/jft-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='jft',
+  version='0.0.1',
+  license='MIT',
+  author="John Forbes",
+  author_email='john.robert.forbes@gmail.com',
+  packages=find_packages('jft'),
+  package_dir={'': 'jft'},
+  url='https://gitlab.com/zereiji/jft',
+  keywords='jft',
+  install_requires=[],
+)
