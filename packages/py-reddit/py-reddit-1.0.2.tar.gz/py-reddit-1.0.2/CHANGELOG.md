8bab532fbec38c9fd04eb53dca201e49b2a5b03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:11 +0000

    Add package py-reddit/1.0.2/py-reddit-1.0.2.tar.gz

---
+++ packages/py-reddit/py-reddit-1.0.2.tar.gz/py-reddit-1.0.2/CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## February 18, 2022
+
+Minor changes to the description of the project - 1.0.2
+
+## February 18, 2022
+
+Initial release - 1.0.1
---
+++ packages/py-reddit/py-reddit-1.0.2.tar.gz/py-reddit-1.0.2/src/py-reddit/__init__.py
@@ -0,0 +1,17 @@
+"""
+Python reddit wrapper 
+~~~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for rendering reddit submissions, usable for both asynchronous and synchronous programming.
+
+:copyright: (c) 2022-present by KING7077.
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'py-reddit'
+__author__ = 'KING7077'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present KING7077'
+__version__ = '1.0.1'
+
+from .reddit import *
