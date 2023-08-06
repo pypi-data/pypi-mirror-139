c980d48f2d1d5ee3cd2a4877c9d65829431bdf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:52 +0000

    Add package lob/4.5.4/lob-4.5.4.tar.gz

---
+++ packages/lob/lob-4.5.4.tar.gz/lob-4.5.4/lob/__init__.py
@@ -0,0 +1,24 @@
+# Resources
+from lob.resource import (
+    Address,
+    BankAccount,
+    BillingGroup,
+    BulkUSVerification,
+    BulkIntlVerification,
+    Card,
+    CardOrder,
+    Check,
+    Letter,
+    Postcard,
+    SelfMailer,
+    USAutocompletion,
+    USReverseGeocodeLookup,
+    USVerification,
+    USZipLookup,
+    IntlVerification
+)
+
+from lob.version import VERSION
+
+api_key = None
+api_base = 'https://api.lob.com/v1'
