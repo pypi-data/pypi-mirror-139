d2a0bdd86d568820e6e7f792348e7540267b3eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:16 +0000

    Add package testerino/0.1.0/testerino-0.1.0.tar.gz

---
+++ packages/testerino/testerino-0.1.0.tar.gz/testerino-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['testerino']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'testerino',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Michael Watson',
+    'author_email': 'test@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
