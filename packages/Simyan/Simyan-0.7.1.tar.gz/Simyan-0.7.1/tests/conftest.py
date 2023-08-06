69553da39ccfc1b8c056bd6a9fc266f5d6ad896b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:33 +0000

    Add package Simyan/0.7.1/Simyan-0.7.1.tar.gz

---
+++ packages/Simyan/Simyan-0.7.1.tar.gz/Simyan-0.7.1/tests/conftest.py
@@ -0,0 +1,25 @@
+"""
+The Conftest module.
+
+This module contains pytest fixtures.
+"""
+import os
+
+import pytest
+
+from simyan import create_session
+from simyan.sqlite_cache import SQLiteCache
+
+
+@pytest.fixture(scope="session")
+def comicvine_api_key():
+    """Set the ComicVine API key fixture."""
+    return os.getenv("COMICVINE_API_KEY", "INVALID")
+
+
+@pytest.fixture(scope="session")
+def comicvine(comicvine_api_key):
+    """Set the Simyan session fixture."""
+    return create_session(
+        api_key=comicvine_api_key, cache=SQLiteCache("tests/Simyan-Cache.sqlite", expiry=None)
+    )
