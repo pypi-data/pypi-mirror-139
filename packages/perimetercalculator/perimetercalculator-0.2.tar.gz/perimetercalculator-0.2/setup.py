57ca034774515bdfe2d2e2d236d0e00af9990204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:31:07 +0000

    Add package perimetercalculator/0.2/perimetercalculator-0.2.tar.gz

---
+++ packages/perimetercalculator/perimetercalculator-0.2.tar.gz/perimetercalculator-0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name = "perimetercalculator",
+    version = "0.2",
+    author = "Sayyam Sabadra",
+    author_email = "sayyam9210@gmail.com",
+    description = ("This is a perimeter finder which can finde the perimeter of any number given by user"),
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)                         
