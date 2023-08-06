ff90d278164597c1a19d780e502f5d9607df0dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package pytorch-ident/0.2/pytorch-ident-0.2.tar.gz

---
+++ packages/pytorch-ident/pytorch-ident-0.2.tar.gz/pytorch-ident-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-ident',
+    version='0.2',
+    url='https://github.com/forgi86/pytorch-ident.git',
+    author='Marco Forgione',
+    author_email='marco.forgione1986@gmail.com',
+    description='System identification with pytorch package',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'matplotlib', 'torch'],  # to be checked
+    extras_require={
+        'continuous-time integration': ["nodepy"],
+        'download datasets': ["requests"],
+        'open datasets': ["pandas"],
+        'generate documentation': ["sphinx"]
+    }
+)
