9c40e0f0feb454f7333383cbe84e1314bd00871e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:26 +0000

    Add package py-reddit/1.1.1/py_reddit-1.1.1-py3-none-any.whl

---
+++ packages/py-reddit/py_reddit-1.1.1-py3-none-any.whl/reddit/__init__.py
@@ -0,0 +1,10 @@
+"""
+py-reddut: Reddit and python, made simple.
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for managing reddit submissions.
+:copyright: (c) 2022-present KING7077
+:license: MIT, see LICENSE for more details.
+"""
+
+
+from .reddit import *
