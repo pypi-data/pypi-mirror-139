fa6a1faf0d59f689d98c97360a2d59c65a52089f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:30 +0000

    Add package lzy-MyRmb/0.1/lzy-MyRmb-0.1.tar.gz

---
+++ packages/lzy-MyRmb/lzy-MyRmb-0.1.tar.gz/lzy-MyRmb-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='lzy-MyRmb',
+      version='0.1',
+      author_email='mvli@qq.com',
+      py_modules=['MyRmb']
+      )
+
+# python setup.py sdist
+# twine upload dist/*
