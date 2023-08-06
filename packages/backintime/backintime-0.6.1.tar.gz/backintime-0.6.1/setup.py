d9ce9b148b31517b5436f512f5e04a5251bd2bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:05 +0000

    Add package backintime/0.6.1/backintime-0.6.1.tar.gz

---
+++ packages/backintime/backintime-0.6.1.tar.gz/backintime-0.6.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'backintime',
+    packages = ['backintime'],
+    version = '0.6.1',
+    description = 'Trading strategies backtesting',
+    author='Akim Mukhtarov',
+    author_email = 'akim.int80h@gmail.com',
+    url = 'https://github.com/akim-mukhtarov/backintime',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+)
