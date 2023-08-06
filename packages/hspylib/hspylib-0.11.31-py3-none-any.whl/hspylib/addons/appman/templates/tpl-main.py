75dee6a3f87f4ae4289b0f1ed8765e84f4e005cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:53 +0000

    Add package hspylib/0.11.31/hspylib-0.11.31-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.31-py3-none-any.whl/hspylib/addons/appman/templates/tpl-main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   TODO Purpose of the file
+   @project: HSPyLib
+   main.addons.appman.templates
+      @file: tpl-main.py
+   @created: Tue, 1 Jun 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2021, HSPyLib team
+"""
+
+import sys
+
+from hspylib.core.tools.commons import dirname, get_path, read_version, sysout
+from hspylib.modules.cli.application.application import Application
+
+HERE = get_path(__file__)
+
+
+class Main(Application):
+    """TODO"""
+
+    # The application version
+    VERSION = read_version(f"{HERE}/.version")
+
+    # Usage message
+    USAGE = (HERE / "usage.txt").read_text().format('.'.join(map(str, VERSION)))
+
+    def __init__(self, app_name: str):
+        # Invoke the super constructor without source_dir parameter to skip creation of log and properties
+        super().__init__(app_name, self.VERSION, self.USAGE, source_dir=dirname(__file__))
+
+    def _setup_parameters(self, *params, **kwargs) -> None:
+        """Initialize application parameters and options"""
+
+    def _main(self, *params, **kwargs) -> None:
+        """Run the application with the command line arguments"""
+        sysout(f'Hello {self._app_name}')
+
+    def _cleanup(self) -> None:
+        """Execute http_code cleanup before exiting"""
+
+
+if __name__ == "__main__":
+    # Application entry point
+    Main('Application name').INSTANCE.run(sys.argv[1:])
