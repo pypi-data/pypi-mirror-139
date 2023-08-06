0ef2c41a5363d694002236f92e570a0aefb33c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:26 +0000

    Add package pytest-bootstrap/0.2.0/pytest-bootstrap-0.2.0.tar.gz

---
+++ packages/pytest-bootstrap/pytest-bootstrap-0.2.0.tar.gz/pytest-bootstrap-0.2.0/setup.py
@@ -0,0 +1,50 @@
+import re
+from setuptools import find_packages, setup
+
+
+# Load the README and make modifications for unsupported sphinx commands.
+with open('README.rst') as fp:
+    long_description = fp.read()
+long_description = long_description \
+    .replace('.. doctest::', '.. code-block:: python') \
+    .replace(':math:', ':code:')
+long_description = re.sub(
+    r'(\.\. automodule:: .*?$)|(\.\. toctree::)|(\.\. plot:: .*?$)', r':code:`\1`',
+    long_description, flags=re.MULTILINE
+)
+
+
+# Load the version number
+try:
+    with open('VERSION') as fp:
+        version = fp.read().strip()
+except FileNotFoundError:
+    version = 'dev'
+
+
+setup(
+    name='pytest-bootstrap',
+    packages=find_packages(),
+    version=version,
+    install_requires=[
+        'numpy',
+    ],
+    extras_require={
+        'tests': [
+            'flake8',
+            'pytest',
+            'pytest-cov',
+            'scipy',
+            'twine',
+        ],
+        'docs': [
+            'matplotlib',
+            'sphinx',
+        ]
+    },
+    classifiers=[
+        'Framework :: Pytest',
+    ],
+    long_description_content_type="text/x-rst",
+    long_description=long_description,
+)
