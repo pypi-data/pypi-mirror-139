f4b838264ab5f7816b49e80fbd119084e8fac9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:35 +0000

    Add package CatCut-Api/1.0/CatCut%20Api-1.0.tar.gz

---
+++ packages/CatCut-Api/CatCut%20Api-1.0.tar.gz/CatCut Api-1.0/setup.py
@@ -0,0 +1,26 @@
+import sys
+from setuptools import setup, find_packages
+with open('README.md','r') as f:
+    long_description = f.read()
+    
+version = '1.0'
+
+deps = [
+    'requests'
+]
+
+setup(
+    name='CatCut Api',
+    version=version,
+    url='https://github.com/Halone228/CatCut-Python',
+    author='Kirill Savchuk',
+    author_email='randomnik192@gmail.com',
+    description=('Api for site https://catcut.net'),
+    license='MIT',
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=deps,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
