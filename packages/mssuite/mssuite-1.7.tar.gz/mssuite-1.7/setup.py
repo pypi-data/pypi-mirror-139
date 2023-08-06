8605a9742bc274e4741aa0d76b767ca6291956f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:04 +0000

    Add package mssuite/1.7/mssuite-1.7.tar.gz

---
+++ packages/mssuite/mssuite-1.7.tar.gz/mssuite-1.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="mssuite", 
+    version="1.7",
+    author="Kevin Klann",
+    author_email="klann@em.uni-frankfurt.de",
+    description="Collection of data analysis tools",
+    
+    packages=setuptools.find_packages(),
+    package_data={'':['../data/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires =['statsmodels','pandas','numpy','DynaTMT-py','seaborn','scipy','matplotlib'],
+    python_requires='>=3.6',
+)
