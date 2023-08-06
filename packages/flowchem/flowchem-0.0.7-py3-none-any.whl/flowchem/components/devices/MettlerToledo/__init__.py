bd44301cbd0fbe4875cb76547e4502bae6d924d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:49 +0000

    Add package flowchem/0.0.7/flowchem-0.0.7-py3-none-any.whl

---
+++ packages/flowchem/flowchem-0.0.7-py3-none-any.whl/flowchem/components/devices/MettlerToledo/__init__.py
@@ -0,0 +1,1 @@
+from .FlowIR import FlowIR
---
+++ packages/flowchem/flowchem-0.0.7-py3-none-any.whl/flowchem/exceptions.py
@@ -0,0 +1,23 @@
+""" Exceptions used in the flowchem module. """
+
+
+class DeviceError(BaseException):
+    """Generic DeviceError"""
+
+    pass
+
+
+class InvalidConfiguration(DeviceError):
+    """The configuration provided is not valid, e.g. no connection w/ device obtained"""
+
+    pass
+
+
+class ActuationError(DeviceError):
+    """The attepted move did not succeed"""
+
+    pass
+
+
+class ProtocolCancelled(Exception):
+    pass
