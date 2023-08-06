e1d5c7727f730bafeed959cbefcbc0a1b0a5d108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:31:03 +0000

    Add package objexplore/1.6.2/objexplore-1.6.2.tar.gz

---
+++ packages/objexplore/objexplore-1.6.2.tar.gz/objexplore-1.6.2/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from setuptools import setup
+
+from objexplore import version as VERSION
+
+# The directory containing this file
+cur_dir = Path(__file__).parent
+
+# The text of the README file
+README = (cur_dir / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="objexplore",
+    version=VERSION,
+    description="Interactive Python Object Explorer",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kylepollina/objexplore",
+    author="Kyle Pollina",
+    author_email="kylepollina@pm.me",
+    license="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["objexplore"],
+    include_package_data=True,
+    install_requires=["blessed==1.17.12", "rich>=10.9.0"],
+)
