1b6438a666f9873f17b0c78bef6dbd30224965c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:28 +0000

    Add package netaudio/0.0.1/netaudio-0.0.1-py3-none-any.whl

---
+++ packages/netaudio/netaudio-0.0.1-py3-none-any.whl/netaudio/__init__.py
@@ -0,0 +1,15 @@
+import sys
+
+from .dante.browser import DanteBrowser
+from .dante.channel import DanteChannel
+from .dante.control import DanteControl
+from .dante.device import DanteDevice
+from .dante.multicast import DanteMulticast
+from .dante.subscription import DanteSubscription
+
+__author__ = "Chris Ritsen"
+__maintainer__ = "Chris Ritsen <chris.ritsen@gmail.com>"
+__version__ = "0.0.0"
+
+if sys.version_info <= (3, 9):
+    raise ImportError("Python version > 3.9 required.")
---
+++ packages/netaudio/netaudio-0.0.1-py3-none-any.whl/netaudio/console/application.py
@@ -0,0 +1,22 @@
+from cleo.application import Application
+
+from netaudio.console.commands import (
+    ChannelCommand,
+    ConfigCommand,
+    DeviceCommand,
+    SubscriptionCommand,
+)
+
+
+def main() -> int:
+    application = Application("netaudio", "0.0.1", complete=True)
+    application.add(ChannelCommand())
+    application.add(ConfigCommand())
+    application.add(DeviceCommand())
+    application.add(SubscriptionCommand())
+
+    return application.run()
+
+
+if __name__ == "__main__":
+    main()
