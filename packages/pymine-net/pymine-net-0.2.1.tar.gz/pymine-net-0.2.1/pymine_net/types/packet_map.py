c543d4969751b28489649fa72a8642043ab3b2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:32 +0000

    Add package pymine-net/0.2.1/pymine-net-0.2.1.tar.gz

---
+++ packages/pymine-net/pymine-net-0.2.1.tar.gz/pymine-net-0.2.1/pymine_net/types/packet_map.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import Dict, List, Tuple, Type, Union
+
+from pymine_net.enums import GameState, PacketDirection
+from pymine_net.errors import DuplicatePacketIdError
+from pymine_net.types.packet import ClientBoundPacket, Packet, ServerBoundPacket
+
+
+class StatePacketMap:
+    """Stores a game state's packets"""
+
+    def __init__(
+        self,
+        state: GameState,
+        server_bound: Dict[int, Type[ServerBoundPacket]],
+        client_bound: Dict[int, Type[ClientBoundPacket]],
+    ):
+        self.state = state
+        self.server_bound = server_bound
+        self.client_bound = client_bound
+
+    @classmethod
+    def from_list(
+        cls, state: GameState, packets: List[Type[Packet]], *, check_duplicates: bool = False
+    ) -> StatePacketMap:
+        self = cls(
+            state,
+            {p.id: p for p in packets if issubclass(p, ServerBoundPacket)},
+            {p.id: p for p in packets if issubclass(p, ClientBoundPacket)},
+        )
+
+        if check_duplicates:
+            for packet_id in self.server_bound.keys():
+                found = [
+                    p for p in packets if p.id == packet_id and issubclass(p, ServerBoundPacket)
+                ]
+
+                if len(found) > 1:
+                    raise DuplicatePacketIdError(
+                        "unknown", state, packet_id, PacketDirection.SERVERBOUND
+                    )
+
+            for packet_id in self.client_bound.keys():
+                found = [
+                    p for p in packets if p.id == packet_id and issubclass(p, ClientBoundPacket)
+                ]
+
+                if len(found) > 1:
+                    raise DuplicatePacketIdError(
+                        "unknown", state, packet_id, PacketDirection.CLIENTBOUND
+                    )
+
+        return self
+
+
+class PacketMap:
+    """Stores a Minecraft protocol's packets"""
+
+    def __init__(self, protocol: Union[str, int], packets: Dict[GameState, StatePacketMap]):
+        self.protocol = protocol
+        self.packets = packets
+
+    def __getitem__(self, key: Tuple[PacketDirection, int, int]) -> Packet:
+        direction, state, packet_id = key
+
+        if direction == PacketDirection.CLIENTBOUND:
+            return self.packets[state].client_bound[packet_id]
+
+        return self.packets[state].server_bound[packet_id]
---
+++ packages/pymine-net/pymine-net-0.2.1.tar.gz/pymine-net-0.2.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymine_net',
+ 'pymine_net.net',
+ 'pymine_net.net.asyncio',
+ 'pymine_net.net.socket',
+ 'pymine_net.packets',
+ 'pymine_net.packets.v_1_18_1.handshaking',
+ 'pymine_net.packets.v_1_18_1.login',
+ 'pymine_net.packets.v_1_18_1.play',
+ 'pymine_net.packets.v_1_18_1.status',
+ 'pymine_net.types']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cryptography>=36.0.1,<37.0.0', 'mutf8>=1.0.6,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pymine-net',
+    'version': '0.2.1',
+    'description': 'Networking library for Minecraft in Python',
+    'long_description': "# PyMine-Net\n[![CodeFactor](https://www.codefactor.io/repository/github/py-mine/pymine-net/badge)](https://www.codefactor.io/repository/github/py-mine/pymine-net)\n![code size](https://img.shields.io/github/languages/code-size/py-mine/PyMine-Net?color=0FAE6E)\n![code style](https://img.shields.io/badge/code%20style-black-000000.svg)\n\n*PyMine-Net - an extensible and modular Minecraft networking library in Python*\n\n## Features\n- Buffer class with methods for dealing with various types and data formats used by Minecraft\n- High level abstractions for packets with a system to seperate different protocol's packets\n- Miscellaneous classes for dealing with different Minecraft data structures relevant to networking\n",
+    'author': 'PyMine-Net Developers & Contributors',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/py-mine/pymine-net',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
