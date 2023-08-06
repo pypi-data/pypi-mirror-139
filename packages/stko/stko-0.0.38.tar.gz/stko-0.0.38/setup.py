ff80a0b2cb0a41cf0c78815fcf5380a9b6a8b390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:01 +0000

    Add package stko/0.0.38/stko-0.0.38.tar.gz

---
+++ packages/stko/stko-0.0.38.tar.gz/stko-0.0.38/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="stko",
+    version="0.0.38",
+    author="Steven Bennett, Andrew Tarzia",
+    author_email="s.bennett18@imperial.ac.uk",
+    description=(
+        "Contains molecular optimizers and property calculators for "
+        "use with stk."
+    ),
+    url="https://github.com/JelfsMaterialsGroup/stko",
+    packages=setuptools.find_packages(),
+    install_requires=(
+        'scipy',
+        'numpy',
+        'networkx',
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
