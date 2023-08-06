28d9a3f37f407096fdff30efc4c0e03a44739307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:16 +0000

    Add package torchnorms/1.4.0/torchnorms-1.4.0-py3-none-any.whl

---
+++ packages/torchnorms/torchnorms-1.4.0-py3-none-any.whl/torchnorms/tnorms/ss.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch import nn, Tensor
+
+from torchnorms.tnorms.base import BaseTNorm
+
+from typing import Optional
+
+
+class SchweizerSklarTNorm(BaseTNorm):
+    def __init__(self,
+                 p: Optional[Tensor] = None,
+                 default_p: float = 0.1) -> None:
+        super().__init__()
+        self.p = p
+        if self.p is None:
+            self.p = nn.Parameter(torch.tensor(default_p))
+        assert len(self.p.shape) == 0
+
+        self.relu = torch.nn.ReLU()
+        self.eps = 0.001
+
+    def __call__(self,
+                 a: Tensor,
+                 b: Tensor) -> Tensor:
+        res: Optional[Tensor] = None
+        # Careful that the value of parametr p can be 10e-8, thus maybe add eps.
+        self.adjust_nan_param()
+
+        self.p.data = self.relu(self.p.data)
+        if self.p == 0:
+            self.p.data += self.eps
+
+        # self.p.data = torch.clamp(self.p.data, min=-4)
+        if torch.isnan(self.p):
+            print("NAN a BRAT")
+            self.p.data = torch.tensor(0.1)
+
+
+        res = (a ** self.p + b ** self.p) - 1.0
+        res = torch.maximum(res, torch.tensor(0.0))
+        res = res ** (1.0 / self.p)
+
+
+        if torch.sum(torch.isnan(res)):
+            print(self.p)
+            print(res)
+            print("GANDON NAN")
+
+        return res
