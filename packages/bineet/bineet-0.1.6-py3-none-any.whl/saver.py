18fa9f9eaef9f5a4ee91860c340a90d2b6cfb401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:17 +0000

    Add package bineet/0.1.6/bineet-0.1.6-py3-none-any.whl

---
+++ packages/bineet/bineet-0.1.6-py3-none-any.whl/saver.py
@@ -0,0 +1,28 @@
+def Save(data,name= "data"):
+    if type(data) == str:
+        with open("{}.txt".format(name),"w") as file:
+            file.write(data)
+    elif type(data) == list:
+        with open("{}.txt".format(name),"w") as file:
+            for i in data:
+                file.write("{}\n".format(i))
+    elif type(data) == dict:
+        with open("{}.txt".format(name),"w") as file:
+            for key, value in data.items(): 
+               file.write('%s,%s\n' % (key, value))
+    else:
+
+        print("Plsease check the data type !!")
+
+
+def SuperSaver(data,name="data.bineet"):
+    import pickle
+    with open(name,'wb') as f:
+        pickle.dump(data,f)
+
+def SuperRead(data):
+    import pickle
+    with open(data,'rb') as file:
+        read_data = pickle.load(file)
+
+    return read_data
