9bd431668fe23dffb244688e0967a62fad9344d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:01 +0000

    Add package serverless-builder/1.4.1/serverless_builder-1.4.1-py3-none-any.whl

---
+++ packages/serverless-builder/serverless_builder-1.4.1-py3-none-any.whl/serverless/service/plugins/composed_vars.py
@@ -0,0 +1,8 @@
+from serverless.service.plugins.generic import Generic
+
+
+class ComposedVars(Generic):
+    yaml_tag = u"!ComposedVarsPlugin"
+
+    def __init__(self):
+        super().__init__("serverless-plugin-composed-vars")
