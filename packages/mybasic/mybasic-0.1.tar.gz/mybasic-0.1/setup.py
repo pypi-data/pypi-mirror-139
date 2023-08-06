c3685a59bd50564b4a38286da2f889f5afb87084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:36 +0000

    Add package mybasic/0.1/mybasic-0.1.tar.gz

---
+++ packages/mybasic/mybasic-0.1.tar.gz/mybasic-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mybasic',
+      version='0.1',
+      description='Testing installation of Package',
+      url='https://github.com/JonahYeoh',
+      author='auth',
+      author_email='auth@gmail.com',
+      license='MIT',
+      packages=['mybasic'],
+      zip_safe=False)
