757f5ebbba56cb54bf7a181409c14f0194bc19e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:20 +0000

    Add package ipl-stats/0.1.2.2/ipl-stats-0.1.2.2.tar.gz

---
+++ packages/ipl-stats/ipl-stats-0.1.2.2.tar.gz/ipl-stats-0.1.2.2/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ipl_stats']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['altair>=4.2.0,<5.0.0',
+ 'chart-studio>=1.1.0,<2.0.0',
+ 'cufflinks>=0.17.3,<0.18.0',
+ 'importlib-resources>=5.4.0,<6.0.0',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'pandas>=1.4.1,<2.0.0',
+ 'plotly>=5.6.0,<6.0.0',
+ 'seaborn>=0.11.2,<0.12.0',
+ 'streamlit>=1.5.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['start = src.ipl_stats.runserver:main']}
+
+setup_kwargs = {
+    'name': 'ipl-stats',
+    'version': '0.1.2.2',
+    'description': '',
+    'long_description': None,
+    'author': 'naveengct',
+    'author_email': 'naveenrajesh2222@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
