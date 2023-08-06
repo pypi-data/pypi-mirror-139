4a2405f480eae43e352e418980ee1c76ad4d83c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package bobcat-miner/0.15.0/bobcat-miner-0.15.0.tar.gz

---
+++ packages/bobcat-miner/bobcat-miner-0.15.0.tar.gz/bobcat-miner-0.15.0/README.md
@@ -0,0 +1,115 @@
+[![PyPI](https://img.shields.io/pypi/v/bobcat_miner.svg)](https://pypi.org/project/bobcat-miner/)
+[![Dockerhub](https://img.shields.io/docker/v/aidanmelen/bobcat?color=blue&label=docker%20build)](https://hub.docker.com/r/aidanmelen/bobcat)
+[![Release](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/release.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/release.yaml)
+[![Tests](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/tests.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/tests.yaml)
+[![Lint](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/lint.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/lint.yaml)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+
+# bobcat miner python
+
+A command line tool used to automate the Bobcat miner. This project also offers a robust python SDK's for interacting with the Bobcat miner.
+
+## Install
+
+```bash
+pipx install bobcat-miner
+```
+
+Please see this [guide](https://packaging.python.org/en/latest/guides/installing-stand-alone-command-line-tools/) for more information about installing stand alone command line tools with [pipx](https://pypa.github.io/pipx/).
+
+## Quick Start
+
+Automatically diagnose and repair the Bobcat miner!
+
+```bash
+bobcat autopilot
+✅ Online Status: Online ⭐
+✅ Sync Status: Synced (gap:-1) 💫
+✅ Relay Status: Not Relayed ✨
+✅ Network Status: Good 📶
+✅ Temperature Status: Good (38°C) ☀️
+```
+
+or run with the official Docker image
+
+```bash
+docker run --rm -it aidanmelen/bobcat autopilot
+```
+
+Run `bobcat --help` to learn about the available sub-commands and options.
+
+## Finding your Bobcat
+
+By default, the Bobcat Autopilot will search the common `192.168.0.0/24` and `10.0.0.0/24` local networks to find the Bobcat miner.
+
+### Find Bobcat by Animal Name
+
+This will connect to the Bobcat on your network that matches the animal name.
+
+```bash
+bobcat --animal "Fancy Awesome Bobcat" -C DEBUG autopilot
+🐛 Connected to Bobcat: 192.168.0.10
+🐛 Refresh: Miner Data
+🐛 Verified Bobcat Animal: fancy-awesome-bobcat
+🐛 The Bobcat Autopilot is starting 🚀 🚀 🚀
+```
+
+### Specify the Hostname / IP Address
+
+Otherwise, follow these [instructions](https://bobcatminer.zendesk.com/hc/en-us/articles/4412905935131-How-to-Access-the-Diagnoser) to find your Bobcats's ip address and specify it with:
+
+```bash
+bobcat --ip-address 192.168.0.10 -C DEBUG autopilot
+🐛 Connected to Bobcat: 192.168.0.10
+🐛 The Bobcat Autopilot is starting 🚀 🚀 🚀
+```
+
+## Monitoring with Discord
+
+Monitor your Bobcat remotely by sending events to a Discord channel by specifying a [webhook URL](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks). No need for VPN or SSH agent setup!
+
+```bash
+bobcat --discord-webhook-url https://discord.com/api/webhooks/xxx autopilot
+✅ Online Status: Online ⭐
+✅ Sync Status: Synced (gap:0) 💫
+⚠️ Relay Status: Relayed
+✅ Network Status: Good 📶
+❌ Temperature Status: Hot (78°C) 🌋
+```
+
+By default, all events `WARNING` or higher (i.e. `ERROR` and `CRITICAL`) will be sent to the Discord channel. This can be configured to include `DEBUG` and `INFO` events as well.
+
+<!-- <img src="https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/bobcat-autopilot-discord-app.png" alt="drawing" style="width:500px;"/> -->
+<img src="https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/bobcat-autopilot-discord-app.png" alt="drawing" width="300"/>
+
+### Dry Run
+
+This example is admittedly contrived, but it demonstrates how the `--dry-run` option can be used show what actions would normally be performed against the bobcat without actually running them.
+
+```bash
+bobcat --dry-run reboot
+Are you sure you want to restart your hotspot? [y/N]: y
+⚠️ Dry run is enabled: Reboot Skipped
+```
+
+## Bobcat SDK
+
+Please see the [Bobcat SDK Docs](https://github.com/aidanmelen/bobcat-miner-python/blob/main/docs/bobcat_sdk.md) for more information.
+
+
+## Contributions
+
+Please see the [Contributions Docs](https://github.com/aidanmelen/bobcat-miner-python/blob/main/docs/contributions.md) for more information. This document includes sections for Development, Test and Release.
+
+## DIY Troubleshooting
+
+Please see [No Witness's Troubleshooting Guide](https://www.nowitness.org/troubleshooting/) for more information.
+
+## Donations
+
+Donations are welcome and appreciated! :gift:
+
+[![HNT: 14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR](https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/wallet.jpg)](https://explorer-v1.helium.com/accounts/14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR)
+
+HNT: [14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR](https://explorer-v1.helium.com/accounts/14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR)
---
+++ packages/bobcat-miner/bobcat-miner-0.15.0.tar.gz/bobcat-miner-0.15.0/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['bobcat_miner']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0',
+ 'backoff>=1.11.1,<2.0.0',
+ 'beautifulsoup4>=4.10.0,<5.0.0',
+ 'click>=8.0.3,<9.0.0',
+ 'discord-lumberjack>=1.0.4,<2.0.0',
+ 'filelock>=3.4.2,<4.0.0',
+ 'requests>=2.27.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['bobcat = bobcat_miner.cli:cli']}
+
+setup_kwargs = {
+    'name': 'bobcat-miner',
+    'version': '0.15.0',
+    'description': "A command line tool used to automate the Bobcat miner. This project also offers a robust python SDK's for interacting with the Bobcat miner.",
+    'long_description': '[![PyPI](https://img.shields.io/pypi/v/bobcat_miner.svg)](https://pypi.org/project/bobcat-miner/)\n[![Dockerhub](https://img.shields.io/docker/v/aidanmelen/bobcat?color=blue&label=docker%20build)](https://hub.docker.com/r/aidanmelen/bobcat)\n[![Release](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/release.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/release.yaml)\n[![Tests](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/tests.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/tests.yaml)\n[![Lint](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/lint.yaml/badge.svg)](https://github.com/aidanmelen/bobcat-miner-python/actions/workflows/lint.yaml)\n[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n\n\n# bobcat miner python\n\nA command line tool used to automate the Bobcat miner. This project also offers a robust python SDK\'s for interacting with the Bobcat miner.\n\n## Install\n\n```bash\npipx install bobcat-miner\n```\n\nPlease see this [guide](https://packaging.python.org/en/latest/guides/installing-stand-alone-command-line-tools/) for more information about installing stand alone command line tools with [pipx](https://pypa.github.io/pipx/).\n\n## Quick Start\n\nAutomatically diagnose and repair the Bobcat miner!\n\n```bash\nbobcat autopilot\n✅ Online Status: Online ⭐\n✅ Sync Status: Synced (gap:-1) 💫\n✅ Relay Status: Not Relayed ✨\n✅ Network Status: Good 📶\n✅ Temperature Status: Good (38°C) ☀️\n```\n\nor run with the official Docker image\n\n```bash\ndocker run --rm -it aidanmelen/bobcat autopilot\n```\n\nRun `bobcat --help` to learn about the available sub-commands and options.\n\n## Finding your Bobcat\n\nBy default, the Bobcat Autopilot will search the common `192.168.0.0/24` and `10.0.0.0/24` local networks to find the Bobcat miner.\n\n### Find Bobcat by Animal Name\n\nThis will connect to the Bobcat on your network that matches the animal name.\n\n```bash\nbobcat --animal "Fancy Awesome Bobcat" -C DEBUG autopilot\n🐛 Connected to Bobcat: 192.168.0.10\n🐛 Refresh: Miner Data\n🐛 Verified Bobcat Animal: fancy-awesome-bobcat\n🐛 The Bobcat Autopilot is starting 🚀 🚀 🚀\n```\n\n### Specify the Hostname / IP Address\n\nOtherwise, follow these [instructions](https://bobcatminer.zendesk.com/hc/en-us/articles/4412905935131-How-to-Access-the-Diagnoser) to find your Bobcats\'s ip address and specify it with:\n\n```bash\nbobcat --ip-address 192.168.0.10 -C DEBUG autopilot\n🐛 Connected to Bobcat: 192.168.0.10\n🐛 The Bobcat Autopilot is starting 🚀 🚀 🚀\n```\n\n## Monitoring with Discord\n\nMonitor your Bobcat remotely by sending events to a Discord channel by specifying a [webhook URL](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks). No need for VPN or SSH agent setup!\n\n```bash\nbobcat --discord-webhook-url https://discord.com/api/webhooks/xxx autopilot\n✅ Online Status: Online ⭐\n✅ Sync Status: Synced (gap:0) 💫\n⚠️ Relay Status: Relayed\n✅ Network Status: Good 📶\n❌ Temperature Status: Hot (78°C) 🌋\n```\n\nBy default, all events `WARNING` or higher (i.e. `ERROR` and `CRITICAL`) will be sent to the Discord channel. This can be configured to include `DEBUG` and `INFO` events as well.\n\n<!-- <img src="https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/bobcat-autopilot-discord-app.png" alt="drawing" style="width:500px;"/> -->\n<img src="https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/bobcat-autopilot-discord-app.png" alt="drawing" width="300"/>\n\n### Dry Run\n\nThis example is admittedly contrived, but it demonstrates how the `--dry-run` option can be used show what actions would normally be performed against the bobcat without actually running them.\n\n```bash\nbobcat --dry-run reboot\nAre you sure you want to restart your hotspot? [y/N]: y\n⚠️ Dry run is enabled: Reboot Skipped\n```\n\n## Bobcat SDK\n\nPlease see the [Bobcat SDK Docs](https://github.com/aidanmelen/bobcat-miner-python/blob/main/docs/bobcat_sdk.md) for more information.\n\n\n## Contributions\n\nPlease see the [Contributions Docs](https://github.com/aidanmelen/bobcat-miner-python/blob/main/docs/contributions.md) for more information. This document includes sections for Development, Test and Release.\n\n## DIY Troubleshooting\n\nPlease see [No Witness\'s Troubleshooting Guide](https://www.nowitness.org/troubleshooting/) for more information.\n\n## Donations\n\nDonations are welcome and appreciated! :gift:\n\n[![HNT: 14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR](https://raw.githubusercontent.com/aidanmelen/bobcat-miner-python/main/assets/wallet.jpg)](https://explorer-v1.helium.com/accounts/14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR)\n\nHNT: [14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR](https://explorer-v1.helium.com/accounts/14HmckNU4WHDDtGH29FMqVENzZAYh5a9XRiLfY2AN6ghfHMvAuR)\n',
+    'author': 'Aidan Melen',
+    'author_email': 'aidanmelen@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/aidanmelen/bobcat-miner-python',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
