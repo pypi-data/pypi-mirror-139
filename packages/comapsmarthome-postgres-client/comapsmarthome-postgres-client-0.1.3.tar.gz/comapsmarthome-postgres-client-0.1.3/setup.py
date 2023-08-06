5b299b06ce0b9f55ef54a20a7e74ed444be4b4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:31 +0000

    Add package comapsmarthome-postgres-client/0.1.3/comapsmarthome-postgres-client-0.1.3.tar.gz

---
+++ packages/comapsmarthome-postgres-client/comapsmarthome-postgres-client-0.1.3.tar.gz/comapsmarthome-postgres-client-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+      name='comapsmarthome-postgres-client',
+     version="0.1.3",
+     author="Aurélien Sylvan",
+     author_email="aurelien.sylvan@comap.eu",
+     description='this library allow to simplify the basic operation in postgres databases like select, insert, update, delete and upsert',
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="",
+     package=['postgres_client'],
+    install_requires=[],
+    classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+    ]
+)
