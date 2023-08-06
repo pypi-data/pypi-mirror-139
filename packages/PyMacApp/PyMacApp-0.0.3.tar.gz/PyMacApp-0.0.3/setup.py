ee68fbe42bf44a4f113d90944bc8186817175c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:33 +0000

    Add package PyMacApp/0.0.3/PyMacApp-0.0.3.tar.gz

---
+++ packages/PyMacApp/PyMacApp-0.0.3.tar.gz/PyMacApp-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyMacApp",
+    version="0.0.3",
+    author="The Nicholas R. Barrow Company, LLC",
+    author_email="me@nicholasrbarrow.com",
+    description="Tools for creating MacOS desktop apps and installers, including signing and notorizing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp",
+    project_urls={
+        "Bug Tracker": "https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "pymacapp"},
+    packages=setuptools.find_packages(where="pymacapp"),
+    python_requires=">=3.9",
+    install_requires=["PyInstaller"]
+)
