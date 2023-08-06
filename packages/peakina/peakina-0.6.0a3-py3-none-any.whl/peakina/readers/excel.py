fb8b404b1a5c7f928b2f5c4e69a9095d424d9007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:44 +0000

    Add package peakina/0.6.0a3/peakina-0.6.0a3-py3-none-any.whl

---
+++ packages/peakina/peakina-0.6.0a3-py3-none-any.whl/peakina/readers/excel.py
@@ -0,0 +1,55 @@
+"""
+Module to add excel files support
+"""
+import logging
+from typing import Any, Optional, Union
+
+import pandas as pd
+
+LOGGER = logging.getLogger(__name__)
+
+
+def read_excel(
+    filepath: str,
+    *,
+    preview_offset: Optional[int] = None,
+    preview_nrows: Optional[int] = None,
+    sheet_name: Optional[Union[str, int]] = 0,
+    na_values: Any = None,
+    keep_default_na: bool = False,
+    skiprows: Optional[int] = None,
+    nrows: Optional[int] = None,
+) -> pd.DataFrame:
+    df = pd.read_excel(
+        filepath,
+        sheet_name=sheet_name,
+        na_values=na_values,
+        keep_default_na=keep_default_na,
+        skiprows=skiprows,
+        nrows=nrows,
+    )
+    # if there are several sheets, pf.read_excel returns a dict {sheet_name: df}
+    if isinstance(df, dict):
+        for sheet_name, _df in df.items():
+            _df["__sheet__"] = sheet_name
+        df = pd.concat(df.values(), sort=False)
+
+    if preview_offset is not None and preview_nrows is not None:
+        return df[preview_offset : preview_offset + preview_nrows]
+    return df
+
+
+def excel_meta(filepath: str, datasource: "DataSource") -> dict:  # noqa: F821
+    """
+    Returns a dictionary with the meta information of the excel file.
+    """
+    kwargs = datasource.reader_kwargs
+    kwargs.pop("preview_offset", None)
+    kwargs.pop("preview_nrows", None)
+
+    excel_file = pd.ExcelFile(filepath)
+    df = pd.read_excel(excel_file, **datasource.reader_kwargs)
+    return {
+        "sheetnames": excel_file.sheet_names,
+        "nrows": df.shape[0],
+    }
