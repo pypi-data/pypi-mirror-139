028c7dc17954542d49b4327feeee2a3f707975ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:59 +0000

    Add package paxplot/0.1.dev0/paxplot-0.1.dev0.tar.gz

---
+++ packages/paxplot/paxplot-0.1.dev0.tar.gz/paxplot-0.1.dev0/src/paxplot/test_datasets.py
@@ -0,0 +1,21 @@
+"""Tests for paxplot dataset"""
+
+import io
+import unittest
+import datasets
+
+
+class PaxplotDatasets(unittest.TestCase):
+    def test_tradeoff(self):
+        """
+        Test for tradeoff dataset
+        """
+        stream = datasets.tradeoff()
+        self.assertIsInstance(
+            stream,
+            io.BufferedReader
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
