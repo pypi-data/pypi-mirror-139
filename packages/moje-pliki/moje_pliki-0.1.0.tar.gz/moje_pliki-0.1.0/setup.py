7dc34e89e83a9faa0e9e7b5eedd75a9edf6f2b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:17 +0000

    Add package moje-pliki/0.1.0/moje_pliki-0.1.0.tar.gz

---
+++ packages/moje-pliki/moje_pliki-0.1.0.tar.gz/moje_pliki-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='moje_pliki',
+    version='0.1.0',
+    author='Me',
+    author_email='damiandubaj@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='my library'
+)
