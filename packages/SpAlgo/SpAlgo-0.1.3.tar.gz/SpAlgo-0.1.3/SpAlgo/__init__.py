9c300744f16e95eb9951ecdcf398520233937c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:58 +0000

    Add package SpAlgo/0.1.3/SpAlgo-0.1.3.tar.gz

---
+++ packages/SpAlgo/SpAlgo-0.1.3.tar.gz/SpAlgo-0.1.3/SpAlgo/__init__.py
@@ -0,0 +1,10 @@
+"""
+The python source code.
+"""
+
+from SpAlgo._array import Array
+from SpAlgo._matrix import Matrix
+from SpAlgo._subseq import LongestMax
+
+__version__ = "0.1.1"
+__all__ = ['Matrix', 'LongestMax', 'Array']
