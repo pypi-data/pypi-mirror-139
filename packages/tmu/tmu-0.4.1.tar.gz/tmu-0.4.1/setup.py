5349a93dc42204dc9701395b7150af0ea246d29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:52 +0000

    Add package tmu/0.4.1/tmu-0.4.1.tar.gz

---
+++ packages/tmu/tmu-0.4.1.tar.gz/tmu-0.4.1/setup.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+os.chdir(os.path.dirname(sys.argv[0]) or ".")
+
+setup(
+    name='tmu',
+    version='0.4.1',
+    url='https://github.com/cair/tmu/',
+    author='Ole-Christoffer Granmo',
+    author_email='ole.granmo@uia.no',
+    license='MIT',
+    description='Implements the Tsetlin Machine, Coalesced Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and drop clause. TMU is written in Python with wrappers for C and CUDA-based clause evaluation and updating.',
+    long_description='Implements the Tsetlin Machine, Coalesced Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and drop clause. TMU is written in Python with wrappers for C and CUDA-based clause evaluation and updating.',
+
+    setup_requires=["cffi>=1.0.0"],
+    packages=find_packages(),
+    cffi_modules=["./tmu/clause_bank_extension_build.py:ffibuilder", "./tmu/tools_extension_build.py:ffibuilder", "./tmu/weight_bank_extension_build.py:ffibuilder", ],
+    install_requires=["cffi>=1.0.0"],
+)
