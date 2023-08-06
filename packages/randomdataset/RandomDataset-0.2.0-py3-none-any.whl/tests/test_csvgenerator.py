0af1e67a9b79c8b349b7d6952f262319a4a8a05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:37 +0000

    Add package randomdataset/0.2.0/RandomDataset-0.2.0-py3-none-any.whl

---
+++ packages/randomdataset/RandomDataset-0.2.0-py3-none-any.whl/tests/test_csvgenerator.py
@@ -0,0 +1,24 @@
+# RandomDataset
+# Copyright (c) 2021 Eric Kerfoot, KCL, see LICENSE file
+
+import unittest
+import tempfile
+from randomdataset import StrFieldGen, IntFieldGen, FloatFieldGen, Dataset, CSVGenerator
+
+
+class TestCSVGenerator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.fields = [
+            StrFieldGen("Name", 6, 12),
+            IntFieldGen("Age", 1, 90),
+            IntFieldGen("Height", 50, 250),
+            FloatFieldGen("BMI", 16, 32)
+        ]
+
+        self.ds = Dataset("Humans", self.fields)
+
+    def test_write(self):
+        gen = CSVGenerator(self.ds, 10)
+
+        with tempfile.TemporaryFile("w") as o:
+            gen.write_stream(o)
