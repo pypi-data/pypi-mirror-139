88518b021bba778b94f9b4495d44d7253a6c5376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:59 +0000

    Add package nautobot-device-lifecycle-mgmt/0.4.1/nautobot_device_lifecycle_mgmt-0.4.1-py3-none-any.whl

---
+++ packages/nautobot-device-lifecycle-mgmt/nautobot_device_lifecycle_mgmt-0.4.1-py3-none-any.whl/nautobot_device_lifecycle_mgmt/api/nested_serializers.py
@@ -0,0 +1,89 @@
+"""Nested/brief alternate REST API serializers for nautobot_device_lifecycle_mgmt models."""
+
+from rest_framework import serializers
+
+from nautobot.core.api import WritableNestedSerializer
+
+from nautobot_device_lifecycle_mgmt import models
+
+
+class NestedSoftwareLCMSerializer(WritableNestedSerializer):
+    """Nested/brief serializer for SoftwareLCM."""
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:nautobot_device_lifecycle_mgmt-api:softwarelcm-detail"
+    )
+
+    class Meta:
+        """Meta attributes."""
+
+        model = models.SoftwareLCM
+        fields = ["id", "url", "device_platform", "version", "end_of_support"]
+        read_only_fields = ["device_platform"]
+
+
+class NestedProviderLCMSerializer(WritableNestedSerializer):
+    """Nested serializer for the provider class."""
+
+    class Meta:
+        """Meta magic method for the Provider nested serializer."""
+
+        model = models.ProviderLCM
+        fields = [
+            "id",
+            "name",
+            "description",
+            "physical_address",
+            "phone",
+            "email",
+            "comments",
+        ]
+
+
+class NestedContractLCMSerializer(WritableNestedSerializer):
+    """API serializer."""
+
+    provider = NestedProviderLCMSerializer(many=False, read_only=False, required=True, help_text="Contract Provider")
+
+    class Meta:
+        """Meta attributes."""
+
+        model = models.ContractLCM
+        fields = [
+            "id",
+            "provider",
+            "name",
+            "start",
+            "end",
+            "cost",
+            "support_level",
+            "contract_type",
+            "expired",
+        ]
+
+
+class NestedCVELCMSerializer(WritableNestedSerializer):
+    """Nested serializer for the CVE class."""
+
+    url = serializers.HyperlinkedIdentityField(view_name="plugins-api:nautobot_device_lifecycle_mgmt-api:cvelcm-detail")
+
+    class Meta:
+        """Meta magic method for the CVE nested serializer."""
+
+        model = models.CVELCM
+        fields = [
+            "id",
+            "url",
+            "display",
+            "name",
+            "published_date",
+            "link",
+            "status",
+            "description",
+            "severity",
+            "cvss",
+            "cvss_v2",
+            "cvss_v3",
+            "fix",
+            "comments",
+        ]
---
+++ packages/nautobot-device-lifecycle-mgmt/nautobot_device_lifecycle_mgmt-0.4.1-py3-none-any.whl/nautobot_device_lifecycle_mgmt/api/urls.py
@@ -0,0 +1,28 @@
+"""API URLs for the Lifecycle Management plugin."""
+
+from rest_framework import routers
+from nautobot_device_lifecycle_mgmt.api.views import (
+    HardwareLCMView,
+    ContractLCMView,
+    ProviderLCMView,
+    ContactLCMView,
+    SoftwareLCMViewSet,
+    ValidatedSoftwareLCMViewSet,
+    CVELCMViewSet,
+    VulnerabilityLCMViewSet,
+)
+
+router = routers.DefaultRouter()
+
+router.register(r"hardware", HardwareLCMView)
+router.register(r"contract", ContractLCMView)
+router.register(r"provider", ProviderLCMView)
+router.register(r"contact", ContactLCMView)
+router.register(r"software", SoftwareLCMViewSet)
+router.register(r"validated-software", ValidatedSoftwareLCMViewSet)
+router.register(r"cve", CVELCMViewSet)
+router.register(r"vulnerability", VulnerabilityLCMViewSet)
+
+app_name = "nautobot_device_lifecycle_mgmt"
+
+urlpatterns = router.urls
