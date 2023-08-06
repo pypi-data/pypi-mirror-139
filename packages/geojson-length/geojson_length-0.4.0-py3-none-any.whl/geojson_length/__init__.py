6aa71f3101f811249f4ea29fe566a701193c87b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package geojson-length/0.4.0/geojson_length-0.4.0-py3-none-any.whl

---
+++ packages/geojson-length/geojson_length-0.4.0-py3-none-any.whl/geojson_length/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for geojson-length."""
+
+__author__ = """Zaitra"""
+__email__ = "info@zaitra.io"
+__version__ = "v1.0.0"
+
+from geojson_length.geojson_length import calculate_distance, Unit
+
+__all__ = [calculate_distance, Unit]
