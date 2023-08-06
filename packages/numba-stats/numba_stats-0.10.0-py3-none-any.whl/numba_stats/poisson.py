df146d72b9ebcb19e5b8a6d8a3a7848fb77f0820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:21:44 +0000

    Add package numba-stats/0.10.0/numba_stats-0.10.0-py3-none-any.whl

---
+++ packages/numba-stats/numba_stats-0.10.0-py3-none-any.whl/numba_stats/poisson.py
@@ -0,0 +1,35 @@
+import numba as nb
+import numpy as np
+from ._special import gammaincc as _gammaincc
+from math import lgamma as _lgamma
+
+_signatures = [
+    nb.float32(nb.int32, nb.float32),
+    nb.float64(nb.intp, nb.float64),
+]
+
+
+@nb.vectorize(_signatures)
+def logpmf(k, mu):
+    """
+    Return log of probability mass for Poisson distribution.
+    """
+    if mu == 0:
+        return 0.0 if k == 0 else -np.inf
+    return k * np.log(mu) - _lgamma(k + 1.0) - mu
+
+
+@nb.vectorize(_signatures)
+def pmf(k, mu):
+    """
+    Return probability mass for Poisson distribution.
+    """
+    return np.exp(logpmf(k, mu))
+
+
+@nb.vectorize(_signatures)
+def cdf(k, mu):
+    """
+    Evaluate cumulative distribution function of Poisson distribution.
+    """
+    return _gammaincc(k + 1, mu)
