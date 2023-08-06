ee9f935aae2b84da310031abd06a0f5e16b4cdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:30 +0000

    Add package PyCanOpenDevice/1.1/PyCanOpenDevice-1.1-py3-none-any.whl

---
+++ packages/PyCanOpenDevice/PyCanOpenDevice-1.1-py3-none-any.whl/PyCanOpenDevice/CanOpen_BusActions.py
@@ -0,0 +1,24 @@
+import can
+from PyCanOpenDevice import CanOpen_Messages
+
+
+class CanOpen_BusActions:
+    def __init__(self, bus: can.Bus):
+        self.bus = bus
+
+    def send_one_with_message_object(self, message: can.Message):
+        try:
+            self.bus.send(message)
+            print("Message sent on {}".format(self.bus.channel_info))
+        except can.CanError:
+            print("Message NOT sent")
+
+    def send_one(self, id: int, data: bytearray):
+        msg = can.Message(arbitration_id=id,
+                          data=data,
+                          is_extended_id=False)
+
+        self.send_one_with_message_object(msg)
+
+    def bootup_device(self, id):
+        self.send_one_with_message_object(CanOpen_Messages.get_bootupMessage(id))
