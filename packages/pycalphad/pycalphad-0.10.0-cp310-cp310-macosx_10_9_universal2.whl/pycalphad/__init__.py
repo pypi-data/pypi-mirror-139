ed8a3b494fdc3c7f2d6921bfb83617144fc48d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:03 +0000

    Add package pycalphad/0.10.0/pycalphad-0.10.0-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/pycalphad/pycalphad-0.10.0-cp310-cp310-macosx_10_9_universal2.whl/pycalphad/__init__.py
@@ -0,0 +1,34 @@
+import warnings
+warnings.filterwarnings('ignore', message='divide by zero encountered in log')
+warnings.filterwarnings('ignore', message='invalid value encountered in true_divide')
+
+from pycalphad.core.errors import *
+import pycalphad.variables as v
+from pycalphad.model import Model, ReferenceState
+from pycalphad.io.database import Database
+
+# Trigger format extension hooks
+import pycalphad.io.tdb
+
+from pycalphad.core.calculate import calculate
+from pycalphad.core.equilibrium import equilibrium
+from pycalphad.plot.binary import binplot
+from pycalphad.plot.ternary import ternplot
+from pycalphad.plot.eqplot import eqplot
+
+# Set the version of pycalphad
+try:
+    from ._dev import get_version
+    # We have a local (editable) installation and can get the version based on the
+    # source control management system at the project root.
+    __version__ = get_version(root='..', relative_to=__file__)
+    del get_version
+except ImportError:
+    # Fall back on the metadata of the installed package
+    try:
+        from importlib.metadata import version
+    except ImportError:
+        # backport for Python<3.8
+        from importlib_metadata import version
+    __version__ = version("pycalphad")
+    del version
