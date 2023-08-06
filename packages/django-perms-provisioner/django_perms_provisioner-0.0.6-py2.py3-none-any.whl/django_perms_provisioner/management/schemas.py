c873176f827c569b6241b20fb4b42acc63218f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:58 +0000

    Add package django-perms-provisioner/0.0.6/django_perms_provisioner-0.0.6-py2.py3-none-any.whl

---
+++ packages/django-perms-provisioner/django_perms_provisioner-0.0.6-py2.py3-none-any.whl/django_perms_provisioner/management/schemas.py
@@ -0,0 +1,25 @@
+PERMISSIONS_SCHEMA = {
+    "groups": {
+        "required": True,
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "name": {"required": True, "type": "string"},
+                "permissions": {
+                    "required": False,
+                    "type": "dict",
+                    "keysrules": {"required": True, "type": "string"},
+                    "valuesrules": {
+                        "type": "list",
+                        "schema": {
+                            "required": True,
+                            "type": "string",
+                            "regex": "^[\w\_]+\.[\w\_]+$",
+                        },
+                    },
+                },
+            },
+        },
+    }
+}
