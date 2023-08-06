2853ee812a4b3a3d932b49f1883d87b1469985e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package langtoxicscore/0.0.3/LangToxicScore-0.0.3.tar.gz

---
+++ packages/langtoxicscore/LangToxicScore-0.0.3.tar.gz/LangToxicScore-0.0.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['langtoxicscore']
+setup_kwargs = {
+    'name': 'langtoxicscore',
+    'version': '0.0.3',
+    'description': 'GetToxicity(string)',
+    'long_description': None,
+    'author': 'Dmitry Filinov',
+    'author_email': 'dm.filinov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
