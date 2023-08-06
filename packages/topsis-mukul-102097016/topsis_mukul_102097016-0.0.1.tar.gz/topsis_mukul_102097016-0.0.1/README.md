b38dbd12af218d268ce2991fc41c5b87446aca59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:38 +0000

    Add package topsis-mukul-102097016/0.0.1/topsis_mukul_102097016-0.0.1.tar.gz

---
+++ packages/topsis-mukul-102097016/topsis_mukul_102097016-0.0.1.tar.gz/topsis_mukul_102097016-0.0.1/README.md
@@ -0,0 +1,4 @@
+#topsis
+
+#how to install 
+pip install topsis_mukul_102097016
---
+++ packages/topsis-mukul-102097016/topsis_mukul_102097016-0.0.1.tar.gz/topsis_mukul_102097016-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+   long_description = fh.read()
+
+setup(
+    name='topsis_mukul_102097016',
+    version='0.0.1',
+    description='Topsis package by Mukul Tayal-102097016-CSE4',
+    author= 'Bhuvnesh Jindal',
+    #url = '',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['mukul tayal', 'tiet', 'topsis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['topsis_mukul_102097016'],
+    package_dir={'':'src'},
+    install_requires = [
+        'pandas'
+    ]
+)
