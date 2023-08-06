50cefb64df723ee4ea72308931495a9a51084052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:17 +0000

    Add package my-pieniodzis-lib/0.0.1/my_pieniodzis_lib-0.0.1.tar.gz

---
+++ packages/my-pieniodzis-lib/my_pieniodzis_lib-0.0.1.tar.gz/my_pieniodzis_lib-0.0.1/SETUP.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+	name='my_pieniodzis_lib',
+	version='0.0.1',
+	author='Maciej',
+	author_mail='maciej.pieniek@gmail.com',
+	packages=find_packages(),
+	include_package_data=True,
+	description='Super test libary'
+)
