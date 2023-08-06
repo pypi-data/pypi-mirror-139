666f546db2c666360b965dc18640684d18788bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:18 +0000

    Add package rpgtk/0.0.2.3a0/rpgtk-0.0.2.3a0.tar.gz

---
+++ packages/rpgtk/rpgtk-0.0.2.3a0.tar.gz/rpgtk-0.0.2.3a0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rpgtk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rpgtk',
+    'version': '0.0.2.3a0',
+    'description': 'the python rpg toolkit',
+    'long_description': '# RPGTK :dice:',
+    'author': 'Gabriel Sarmento',
+    'author_email': 'gabrielfs.bot@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/is-gabs/rpgtk',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
