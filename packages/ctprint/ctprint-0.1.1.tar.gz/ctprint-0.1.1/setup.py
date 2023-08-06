7e2a5c1ae1ceb6f2313458ee3bac0565c6a48ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:24 +0000

    Add package ctprint/0.1.1/ctprint-0.1.1.tar.gz

---
+++ packages/ctprint/ctprint-0.1.1.tar.gz/ctprint-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ctprint']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ctprint',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'saner99',
+    'author_email': 'it@saner99.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
