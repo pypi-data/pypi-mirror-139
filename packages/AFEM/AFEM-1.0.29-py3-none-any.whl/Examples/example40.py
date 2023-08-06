1cfa5f76a81cb633fe4fa07f2386cd9c3f1a7732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:32 +0000

    Add package AFEM/1.0.29/AFEM-1.0.29-py3-none-any.whl

---
+++ packages/AFEM/AFEM-1.0.29-py3-none-any.whl/Examples/example40.py
@@ -0,0 +1,25 @@
+if __name__ == '__main__':
+    from FEM.EulerBernoulliBeam import EulerBernoulliBeam
+    from FEM.Geometry import Lineal
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    L = 100
+    geometria = Lineal(L, 80, 1, 2)
+    geometria.cbe = [[0, 0.0], [1, 0.0]]
+
+    W = 10.0
+    Fx = 0.0
+    I = 1/12
+    A = 1
+    E = 30000000.0
+    EI = E*I
+    EA = E*A
+
+    O = EulerBernoulliBeam(geometria, EI, f=W)
+    O.solve()
+
+    # analitica = W*L**4/EI/8
+    print(O.U[-2][0], W*L**4/EI/8)
+    plt.show()
+    a = 0
---
+++ packages/AFEM/AFEM-1.0.29-py3-none-any.whl/FEM/Elements/E2D/Serendipity.py
@@ -0,0 +1,92 @@
+"""Define a o2 serendipity element"""
+
+
+from .Element2D import Element2D, np
+from .RectangularScheme import RectangularScheme
+from ..E1D.QuadraticElement import QuadraticElement
+
+
+class Serendipity(Element2D, RectangularScheme):
+    """Creates a Serendipity element
+
+    Args:
+        coords (np.ndarray): Coordinate matrix of element
+        gdl (np.ndarray): Coordinate matrix of element GDL's
+        n (int, optional): Number of gauss points. Defaults to 3.
+    """
+
+    def __init__(self, coords: np.ndarray, gdl: np.ndarray, n: int = 3, **kargs) -> None:
+        """Creates a Serendipity element
+
+        Args:
+            coords (np.ndarray): Coordinate matrix of element
+            gdl (np.ndarray): Coordinate matrix of element GDL's
+            n (int, optional): Number of gauss points. Defaults to 3.
+        """
+
+        _coords = np.array([coords[i] for i in range(4)])
+        coords = np.array(coords)
+
+        he1 = np.linalg.norm(coords[1]-coords[0])
+        e1 = QuadraticElement(np.array([[0], [he1*0.5], [he1]]),
+                              np.array([[-1, -1, -1]]), border=True)
+
+        he2 = np.linalg.norm(coords[2]-coords[1])
+        e2 = QuadraticElement(np.array([[0], [he2*0.5], [he2]]),
+                              np.array([[-1, -1, -1]]), border=True)
+
+        he3 = np.linalg.norm(coords[3]-coords[2])
+        e3 = QuadraticElement(np.array([[0], [he3*0.5], [he3]]),
+                              np.array([[-1, -1, -1]]), border=True)
+
+        he4 = np.linalg.norm(coords[0]-coords[3])
+        e4 = QuadraticElement(np.array([[0], [he4*0.5], [he4]]),
+                              np.array([[-1, -1, -1]]), border=True)
+
+        self.borders = [e1, e2, e3, e4]
+        Element2D.__init__(self, coords, _coords, gdl, **kargs)
+        RectangularScheme.__init__(self, n, **kargs)
+
+    def psis(self, z: np.ndarray) -> np.ndarray:
+        """Calculates the shape functions of a given natural coordinates
+
+        Args:
+            z (np.ndarray): Natural coordinates matrix
+
+        Returns:
+            np.ndarray: Shape function evaluated in Z points
+        """
+
+        return np.array([
+            0.25*(1.0-z[0])*(1.0-z[1])*(-1.0-z[0]-z[1]),
+            0.25*(1.0+z[0])*(1.0-z[1])*(-1.0+z[0]-z[1]),
+            0.25*(1.0+z[0])*(1.0+z[1])*(-1.0+z[0]+z[1]),
+            0.25*(1.0-z[0])*(1.0+z[1])*(-1.0-z[0]+z[1]),
+            0.5*(1.0-z[0]**2.0)*(1.0-z[1]),
+            0.5*(1.0+z[0])*(1.0-z[1]**2.0),
+            0.5*(1.0-z[0]**2.0)*(1.0+z[1]),
+            0.5*(1.0-z[0])*(1.0-z[1]**2.0)
+        ]).T
+
+    def dpsis(self, z: np.ndarray) -> np.ndarray:
+        """Calculates the shape functions derivatives of a given natural coordinates
+
+        Args:
+            z (np.ndarray): Natural coordinates matrix
+
+        Returns:
+            np.ndarray: Shape function derivatives evaluated in Z points
+        """
+
+        return np.array(
+            [[-0.25*(z[1]-1.0)*(2.0*z[0]+z[1]), -0.25*(z[0]-1.0)*(2.0*z[1]+z[0])],
+             [-0.25*(z[1]-1.0)*(2.0*z[0]-z[1]),
+              0.25*(z[0]+1.0)*(2.0*z[1]-z[0])],
+             [0.25*(z[1]+1.0)*(2.0*z[0]+z[1]),
+              0.25*(z[0]+1.0)*(2.0*z[1]+z[0])],
+             [0.25*(z[1]+1.0)*(2.0*z[0]-z[1]), -
+              0.25*(z[0]-1.0)*(2.0*z[1]-z[0])],
+             [(z[1]-1.0)*z[0], 0.5*(z[0]**2.0-1.0)],
+             [-0.5*(z[1]**2.0-1.0), -z[1]*(z[0]+1.0)],
+             [-(z[1]+1.0)*z[0], -0.5*(z[0]**2.0-1.0)],
+             [0.5*(z[1]**2.0-1.0), z[1]*(z[0]-1.0)]])
