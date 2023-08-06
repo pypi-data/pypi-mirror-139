eaf8a0f1521cbe9c4107d69d45c4500f9591f0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:52 +0000

    Add package blackfire/1.11.0/blackfire-1.11.0-cp36-cp36m-musllinux_1_1_x86_64.whl

---
+++ packages/blackfire/blackfire-1.11.0-cp36-cp36m-musllinux_1_1_x86_64.whl/blackfire/hooks/odoo/patch.py
@@ -0,0 +1,42 @@
+from blackfire.utils import wrap, import_module, get_logger, get_executable_path
+
+log = get_logger(__name__)
+
+
+def patch():
+
+    # Since Odoo is not importable by default, try to add
+    # its directory in python path.
+    # Assumes that odoo-bin is in the same directory than odoo module.
+    import sys
+    executable_path = get_executable_path(sys.argv[0])
+    if executable_path is not None and executable_path.endswith('/odoo-bin'):
+        import os
+        odoo_path = os.path.dirname(executable_path)
+        log.debug("Detected Odoo path: %s", odoo_path)
+        sys.path.append(odoo_path)
+
+    module = import_module('odoo')
+    if not module:
+        return False
+
+    # already patched?
+    if getattr(module, '_blackfire_patch', False):
+        return
+
+    try:
+        from blackfire.hooks.odoo.middleware import OdooMiddleware
+
+        module.service.wsgi_server.application = OdooMiddleware(
+            module.service.wsgi_server.application
+        )
+
+        log.debug('Odoo version %s patched.', (module.release.version))
+
+        setattr(module, '_blackfire_patch', True)
+
+        return True
+    except Exception as e:
+        log.exception(e)
+
+    return False
