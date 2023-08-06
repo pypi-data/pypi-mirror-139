f26dc717fe09e3a98444f6f5e976f8aa64fd3862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:57 +0000

    Add package skomplikowana-nazwa/0.1.0/skomplikowana_nazwa-0.1.0.tar.gz

---
+++ packages/skomplikowana-nazwa/skomplikowana_nazwa-0.1.0.tar.gz/skomplikowana_nazwa-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='skomplikowana_nazwa',
+    version='0.1.0',
+    author='Tomek',
+    author_email='tbroniak@gmail.com',
+    include_package_data=True,
+    description='Super przydatna biblioteka'
+)
