3deb2e25add7d3f4facf67dd02e4dcb37c1d501a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:03 +0000

    Add package depthai-gui/2.0.6/depthai-gui-2.0.6.tar.gz

---
+++ packages/depthai-gui/depthai-gui-2.0.6.tar.gz/depthai-gui-2.0.6/demo.py
@@ -0,0 +1,51 @@
+import sys
+import cv2
+import argparse
+import depthai as dai
+
+from pathlib import Path
+from DAIPipelineGraph import DAIPipelineGraph
+
+def main():
+    default_json_path = (Path(__file__).parent / "ExampleGraph.json").resolve().absolute()
+    if not default_json_path.exists():
+        default_json_path = None
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "-p", "--path", type=Path, default=default_json_path, help="Path to pipeline graph (Default: %(default)s)")
+    args = parser.parse_args()
+
+    if args.path is None:
+        raise RuntimeError("Path to pipeline graph file is required.")
+
+    # Create the pipeline
+    pipeline_graph = DAIPipelineGraph( path=str(args.path) )
+
+    # Display all XLinkOut data as CV frames
+    with dai.Device( pipeline_graph.pipeline ) as device:
+        queues = {}
+        frames = {}
+
+        for stream_id in pipeline_graph.xout_streams:
+            queues[ stream_id ] = device.getOutputQueue( stream_id )
+            frames[ stream_id ] = None
+
+        while True:
+            for stream_id in pipeline_graph.xout_streams:
+                get_result = queues[ stream_id ].tryGet()
+
+                # RGB Frame
+                if get_result is not None:
+                    frames[ stream_id ] = get_result.getCvFrame()
+
+                # SHOW IMAGE
+                if frames[ stream_id ] is not None:
+                    cv2.imshow( stream_id, frames[ stream_id ] )
+
+            # HANDLE QUIT
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/depthai-gui/depthai-gui-2.0.6.tar.gz/depthai-gui-2.0.6/pipeline_editor.py
@@ -0,0 +1,30 @@
+def main():
+    import sys
+    import argparse
+    from Qt import QtWidgets
+    from NodeGraphQt import setup_context_menu
+    from pathlib import Path
+    from DAINodes import DAINodeGraph
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "-p", "--path", type=Path, default=Path(__file__).parent, help="Path to save/load default directory")
+    parser.add_argument( "-o", "--open", type=Path, default=None, help="Path to project file to open")
+    args = parser.parse_args()
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    # create node graph controller.
+    graph = DAINodeGraph()
+
+    # set up default menu and commands.
+    setup_context_menu( graph, set_default_file_path=str(args.path), open_file=args.open)
+
+    # show the node graph widget.
+    graph_widget = graph.widget
+    graph_widget.show()
+
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
