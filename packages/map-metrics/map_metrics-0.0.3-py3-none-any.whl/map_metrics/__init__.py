45566db3123cce07aa23a428b5f35f0665fccf2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:18 +0000

    Add package map-metrics/0.0.3/map_metrics-0.0.3-py3-none-any.whl

---
+++ packages/map-metrics/map_metrics-0.0.3-py3-none-any.whl/map_metrics/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Map Metrics."""
+
+__author__ = """Anastasiia Kornilova"""
+__email__ = "anastasiia.kornilova@skoltech.ru"
+__version__ = "0.0.1"
+
+from map_metrics.metrics import *
