6dadfd4797bdf739fc6e8a273090d00850a72b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:27 +0000

    Add package test-lib989765/1.0/test_lib989765-1.0.tar.gz

---
+++ packages/test-lib989765/test_lib989765-1.0.tar.gz/test_lib989765-1.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup (
+    name='test_lib989765',
+    version='1.0',
+    author='kochan_a',
+    author_email='alak229@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='my first test library'
+)
