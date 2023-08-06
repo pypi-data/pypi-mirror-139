673ec01fdf86216dec58e829d6092a55a46e27e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:42 +0000

    Add package generic-api-wrapper/0.1.2/generic_api_wrapper-0.1.2-py3-none-any.whl

---
+++ packages/generic-api-wrapper/generic_api_wrapper-0.1.2-py3-none-any.whl/generic_api_wrapper/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Generic API wrapper."""
+
+__author__ = """Sergii Gryshkevych"""
+__email__ = ""
+__version__ = "0.1.2"
+
+
+from generic_api_wrapper.aws_iam_auth import AwsIamAuth
+from generic_api_wrapper.generic_api_wrapper import Api
