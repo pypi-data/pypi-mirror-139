e2091c86795916a6683f5c826899fc2d9341e771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:01 +0000

    Add package deal/4.19.2/deal-4.19.2-py3-none-any.whl

---
+++ packages/deal/deal-4.19.2-py3-none-any.whl/deal/_colors.py
@@ -0,0 +1,44 @@
+
+from typing import Dict
+
+import pygments
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import PythonLexer
+
+from ._state import state
+
+
+COLORS = dict(
+    red='\033[91m',
+    green='\033[92m',
+    yellow='\033[93m',
+    blue='\033[94m',
+    magenta='\033[95m',
+    end='\033[0m',
+)
+NOCOLORS = dict(
+    red='',
+    green='',
+    yellow='',
+    blue='',
+    magenta='',
+    end='',
+)
+
+
+def highlight(source: str) -> str:
+    source = pygments.highlight(
+        code=source,
+        lexer=PythonLexer(),
+        formatter=TerminalFormatter(),
+    )
+    return source.rstrip()
+
+
+def get_colors(args) -> Dict[str, str]:
+    if not state.color:
+        return NOCOLORS
+    if args.nocolor:
+        state.color = False
+        return NOCOLORS
+    return COLORS
