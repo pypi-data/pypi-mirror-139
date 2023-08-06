2b6a0da83b049b23053623a0e8e5a47cbfeed618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:31:00 +0000

    Add package better-partial/1.0.2/better_partial-1.0.2-py3-none-any.whl

---
+++ packages/better-partial/better_partial-1.0.2-py3-none-any.whl/better_partial/better_partial.py
@@ -0,0 +1,45 @@
+class Unsupplied:
+  pass
+
+class AllPositionalsUnsupplied:
+  pass 
+
+_ = Unsupplied()
+
+___ = AllPositionalsUnsupplied()
+
+def partial(fn):
+  def g(*partial_args, **partial_kwargs):
+    all_unsupplied = False
+    if any(arg is ___ for arg in partial_args):
+      assert len(partial_args) == 1
+      all_unsupplied = True
+    
+    unsupplied_arg_positions = [i for i, arg in enumerate(partial_args) if arg is _]
+    unsupplied_kwargs = {key: value for key, value in partial_kwargs.items() if value is _}
+    if len(unsupplied_arg_positions) == 0 and len(unsupplied_kwargs) == 0 and (not all_unsupplied):
+      return fn(*partial_args, **partial_kwargs)
+    @partial
+    def h(*args, **kwargs):
+      nonlocal partial_args, all_unsupplied
+      partial_args = list(args) if all_unsupplied else list(partial_args)
+      for i, arg in zip(unsupplied_arg_positions, args):
+        partial_args[i] = arg
+      for key, value in kwargs.items():
+        partial_kwargs[key] = value
+      partial_args = tuple(partial_args)
+      return fn(*partial_args, **partial_kwargs)
+    return h
+  return g
+
+if __name__ == '__main__':
+  @partial
+  def f(a, b, c):
+    return (a, b, c)
+  
+  print('f(1,2,3)', f(1,2,3))
+  g = f(1, _, 3)
+  print('g(2)', g(2))
+  g = f(1, _, _)
+  h = g(_, 3)
+  print('h(2)', h(2))
