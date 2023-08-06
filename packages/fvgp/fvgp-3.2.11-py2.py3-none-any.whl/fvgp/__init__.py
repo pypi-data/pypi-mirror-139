52799d5efef717ad6235288dc63ef4d914159fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:06 +0000

    Add package fvgp/3.2.11/fvgp-3.2.11-py2.py3-none-any.whl

---
+++ packages/fvgp/fvgp-3.2.11-py2.py3-none-any.whl/fvgp/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for fvGP."""
+
+__author__ = """Marcus Michael Noack"""
+__email__ = 'MarcusNoack@lbl.gov'
+
+from . import _version
+__version__ = _version.get_versions()['version']
