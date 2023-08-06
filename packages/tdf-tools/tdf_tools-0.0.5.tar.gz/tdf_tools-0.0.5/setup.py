fd8a90a0c57151fd0822b668b2ec0bd8cd76a71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:07 +0000

    Add package tdf-tools/0.0.5/tdf_tools-0.0.5.tar.gz

---
+++ packages/tdf-tools/tdf_tools-0.0.5.tar.gz/tdf_tools-0.0.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tdf_tools",
+    version="0.0.5",
+    author="xujian",
+    author_email="17826875951@163.com",
+    description="flutter开发流程工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.2dfire.net/app/flutter/tools/package_tools",
+    # packages=setuptools.find_packages(),
+    packages=['tdf_tools'],
+    install_requires=[
+        'ruamel.yaml',
+        'gitlab'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'tdf_init=tdf_tools:create',
+            'tdf_tools=tdf_tools:process'
+        ]
+    }
+)
