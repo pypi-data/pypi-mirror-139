9adb4c8d1b962bbe0ce23d783d0b5435d695a4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package ez/9.4.6/ez-9.4.6.tar.gz

---
+++ packages/ez/ez-9.4.6.tar.gz/ez-9.4.6/ez/__init__.py
@@ -0,0 +1,13 @@
+# insert path for pysecretes local and heroku
+import sys, os
+exec("try:HERE=os.path.dirname(os.path.abspath(__file__));_=sys.path.remove(HERE) if HERE in sys.path else 0;sys.path.insert(0,HERE)\nexcept:HERE=os.getcwd()") # HERE for interactive run
+sys.path.insert(1,'/') if '/' not in sys.path else None
+
+from . ez import *
+from . firefox import *
+
+__all__=[
+'Firefox', 'BY', 'KEYS',
+'JDict', 'TimeStamp', 'Moment', 'GSheet', 
+'fire', 'requests', 'sqlite3', 'json', 'io', 'os', 'sys', 'platform', 'string', 'random', 're', 'datetime', 'tzlocal', 'pd', 'np', 'urlparse', 'BeautifulSoup', 
+]
