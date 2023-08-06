d17f07cef96c31189faf6497f576db31cbe1a319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:25 +0000

    Add package vital/1.1.9/vital-1.1.9-py3-none-any.whl

---
+++ packages/vital/vital-1.1.9-py3-none-any.whl/vital/api/body.py
@@ -0,0 +1,53 @@
+from typing import List, Mapping, Optional
+
+from vital.api.api import API
+
+
+class Body(API):
+    """Endpoints for geetting body data."""
+
+    def get(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get Body data
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data strava etc.
+        """
+        return self.client.get(
+            f"/summary/body/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
+
+    def get_raw(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get Body data
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data strava etc.
+        """
+        return self.client.get(
+            f"/summary/body/{user_key}/raw",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
