730318e3dcbe783eac467cf02309bcf689868ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:21 +0000

    Add package Daug/2022.2.20.2/Daug-2022.2.20.2.tar.gz

---
+++ packages/Daug/Daug-2022.2.20.2.tar.gz/Daug-2022.2.20.2/Daug/__init__.py
@@ -0,0 +1,7 @@
+def setup(bot):
+    bot.load_extension('Daug.extensions.channels')
+    bot.load_extension('Daug.extensions.favorite')
+    bot.load_extension('Daug.extensions.join')
+    bot.load_extension('Daug.extensions.leave')
+    bot.load_extension('Daug.extensions.thread')
+    bot.load_extension('Daug.extensions.utils')
