5c01b93ecba4721f53f1a6ad6f08d74f953b1aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package pypipublishtest/0.1.9/pypipublishtest-0.1.9.tar.gz

---
+++ packages/pypipublishtest/pypipublishtest-0.1.9.tar.gz/pypipublishtest-0.1.9/README.md
@@ -0,0 +1,8 @@
+# pypipublishtest
+
+
+
+## Testing things that shouldn't work:
+- correct tag x
+- missing tag
+- incorrect tag
---
+++ packages/pypipublishtest/pypipublishtest-0.1.9.tar.gz/pypipublishtest-0.1.9/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypipublishtest']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pypipublishtest',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': "# pypipublishtest\n\n\n\n## Testing things that shouldn't work:\n- correct tag x\n- missing tag\n- incorrect tag",
+    'author': 'Nicholas',
+    'author_email': 'duzilin@hotmail.co.za',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
