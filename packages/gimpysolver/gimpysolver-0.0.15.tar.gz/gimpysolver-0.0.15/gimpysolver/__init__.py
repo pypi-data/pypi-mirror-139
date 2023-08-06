7a2d79263b8cd4da9a6ef95b2f6dffcddf6fe435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:12 +0000

    Add package gimpysolver/0.0.15/gimpysolver-0.0.15.tar.gz

---
+++ packages/gimpysolver/gimpysolver-0.0.15.tar.gz/gimpysolver-0.0.15/gimpysolver/__init__.py
@@ -0,0 +1,9 @@
+"""
+Captcha solver to SEACE Portal Scraper
+The model have a 92% Accuracy.
+"""
+#from gimpysolver import captcha_solver
+
+__version__ = "0.0.8"
+__author__ = 'César Cortez'
+__credits__ = 'Digitalia Tec'
