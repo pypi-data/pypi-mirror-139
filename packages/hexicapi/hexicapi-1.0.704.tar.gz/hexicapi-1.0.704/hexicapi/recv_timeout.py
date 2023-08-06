dd0bd9b5c3dd03ad19159574b51b26f7ab8a691a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:13 +0000

    Add package hexicapi/1.0.704/hexicapi-1.0.704.tar.gz

---
+++ packages/hexicapi/hexicapi-1.0.704.tar.gz/hexicapi-1.0.704/hexicapi/recv_timeout.py
@@ -0,0 +1,35 @@
+import socket, time
+def recv_timeout(the_socket, timeout=0.05, packet_size=1024):
+    # make socket non blocking
+    the_socket.setblocking(0)
+
+    # total data partwise in an array
+    total_data = []
+    data = ''
+
+    # beginning time
+    begin = time.time()
+    while 1:
+        # if you got some data, then break after timeout
+        if total_data and time.time() - begin > timeout:
+            break
+
+        # if you got no data at all, wait a little longer, twice the timeout
+        elif time.time() - begin > timeout * 2:
+            break
+
+        # recv something
+        try:
+            data = the_socket.recv(packet_size)
+            if data:
+                total_data.append(data)
+                # change the beginning time for measurement
+                begin = time.time()
+            else:
+                # sleep for sometime to indicate a gap
+                time.sleep(0.1)
+        except:
+            pass
+    the_socket.setblocking(1)
+    # join all parts to make final string
+    return ''.join(total_data)
