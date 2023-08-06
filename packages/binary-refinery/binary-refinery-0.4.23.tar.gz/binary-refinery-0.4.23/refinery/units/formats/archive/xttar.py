48abc8bad74f07268766b647e7ca582017d67e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:24:56 +0000

    Add package binary-refinery/0.4.23/binary-refinery-0.4.23.tar.gz

---
+++ packages/binary-refinery/binary-refinery-0.4.23.tar.gz/binary-refinery-0.4.23/refinery/units/formats/archive/xttar.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import tarfile
+import datetime
+
+from refinery.lib.structures import MemoryFile
+from refinery.units.formats.archive import ArchiveUnit
+
+
+class xttar(ArchiveUnit):
+    """
+    Extract files from a Tar archive.
+    """
+    def __init__(self, *paths, list=False, join_path=False, drop_path=False, path=b'path', date=b'date'):
+        super().__init__(*paths, list=list, join_path=join_path, drop_path=drop_path, path=path, date=date)
+
+    def unpack(self, data):
+        archive = tarfile.open(fileobj=MemoryFile(data))
+        for info in archive.getmembers():
+            if not info.isfile():
+                continue
+            extractor = archive.extractfile(info)
+            if extractor is None:
+                continue
+            date = datetime.datetime.fromtimestamp(info.mtime)
+            yield self._pack(info.name, date, lambda e=extractor: e.read())
