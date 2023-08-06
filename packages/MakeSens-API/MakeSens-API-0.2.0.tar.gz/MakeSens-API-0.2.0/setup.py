91c9e1044321f8eb3c52f4533156c15b125e9326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:21 +0000

    Add package MakeSens-API/0.2.0/MakeSens-API-0.2.0.tar.gz

---
+++ packages/MakeSens-API/MakeSens-API-0.2.0.tar.gz/MakeSens-API-0.2.0/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup,find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="MakeSens-API",
+    version="0.2.0",
+    description="MakeSense API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ciencia-de-datos/API_Python/tree/main/MakeSensCloud-base",
+    author="MakeSens Maximiliano G.Ch. DAPL",
+    author_email="jose.salamancacoy@gmail.com",
+    license="GNU General Public License v3.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["MakeSens"],
+    include_package_data=True,
+    install_requires=["pandas==1.3.0", "requests==2.22.0","json==2.0.9","typing","datetime"]
+    ,
+    entry_points={
+        "console_scripts": [
+            "MakeSens-API=MakeSens.__main__:main",
+        ]
+    },
+)
