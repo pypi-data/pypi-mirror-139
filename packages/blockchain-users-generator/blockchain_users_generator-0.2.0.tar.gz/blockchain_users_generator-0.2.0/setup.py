92685c2aa83da97d9e6a459a5a64199103da9b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package blockchain-users-generator/0.2.0/blockchain_users_generator-0.2.0.tar.gz

---
+++ packages/blockchain-users-generator/blockchain_users_generator-0.2.0.tar.gz/blockchain_users_generator-0.2.0/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+  name='blockchain_users_generator',
+  version='0.2.0',
+  license='MIT',
+  author="Alexandro Tapia",
+  author_email='alexandro@zpace.link',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  url='https://github.com/alexandrotapiaflores/blockchain_users_generator',
+  keywords='users generator blockchain',
+  install_requires=[
+    "asn1crypto==1.4.0",
+    "cffi==1.15.0",
+    "coincurve==17.0.0",
+    "names==0.3.0",
+    "pycparser==2.21",
+    "pysha3==1.0.2",
+  ],
+)
---
+++ packages/blockchain-users-generator/blockchain_users_generator-0.2.0.tar.gz/blockchain_users_generator-0.2.0/src/blockchain_users_generator/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .country import CountryInfo, get_random_country
+from .blockchain import BlockchainInfo, generate_blockchain_info
