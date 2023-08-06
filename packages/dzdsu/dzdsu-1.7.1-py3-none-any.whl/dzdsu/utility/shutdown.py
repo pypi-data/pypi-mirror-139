1defdd87fd9c6086905add3f765240a76eca5dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:00 +0000

    Add package dzdsu/1.7.1/dzdsu-1.7.1-py3-none-any.whl

---
+++ packages/dzdsu/dzdsu-1.7.1-py3-none-any.whl/dzdsu/utility/shutdown.py
@@ -0,0 +1,45 @@
+"""Server shutdown."""
+
+from time import sleep
+
+from dzdsu.server import Server
+from dzdsu.utility.logger import LOGGER
+
+
+__all__ = ['shutdown']
+
+
+def shutdown(server: Server, message: str, countdown: int) -> bool:
+    """Shut down the server iff it needs a restart."""
+
+    if not server.is_running:
+        return True
+
+    try:
+        server.countdown(message, countdown=countdown)
+    except (ConnectionRefusedError, TimeoutError, ConnectionResetError):
+        LOGGER.error('Could not notify users about shutdown.')
+        return False
+
+    LOGGER.info('Kicking remaining players.')
+
+    try:
+        server.kick_all('Server restart.')
+    except (ConnectionRefusedError, TimeoutError, ConnectionResetError):
+        LOGGER.warning('Could not kick all remaining players.')
+
+    LOGGER.info('Stopping server.')
+
+    try:
+        server.shutdown()
+    except (ConnectionRefusedError, TimeoutError, ConnectionResetError):
+        return False
+
+    LOGGER.info('Waiting for server to shut down.')
+
+    while server.is_running:
+        print('.', end='')
+        sleep(1)
+
+    print()
+    return True
