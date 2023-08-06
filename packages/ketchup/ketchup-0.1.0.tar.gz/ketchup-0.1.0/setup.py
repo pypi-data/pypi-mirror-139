2367332ce3ac33ed797cfc2c5b7ce926b933d29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:51 +0000

    Add package ketchup/0.1.0/ketchup-0.1.0.tar.gz

---
+++ packages/ketchup/ketchup-0.1.0.tar.gz/ketchup-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ketchup']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'colorama>=0.4.4,<0.5.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'typer>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['ketchup = ketchup.cli:init']}
+
+setup_kwargs = {
+    'name': 'ketchup',
+    'version': '0.1.0',
+    'description': 'Ketchup is a tool for launching your tests in parallel',
+    'long_description': None,
+    'author': 'Romain Commandé',
+    'author_email': 'rcommande@meilleursagents.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
