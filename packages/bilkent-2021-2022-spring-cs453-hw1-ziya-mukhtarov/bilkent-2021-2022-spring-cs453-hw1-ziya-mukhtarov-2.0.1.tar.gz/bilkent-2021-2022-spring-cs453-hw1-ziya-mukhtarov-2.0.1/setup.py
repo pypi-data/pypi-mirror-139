c6196d7d4d9f742f110bcfcd8cbce9f88c8ecb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:48 +0000

    Add package bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov/2.0.1/bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov-2.0.1.tar.gz

---
+++ packages/bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov/bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov-2.0.1.tar.gz/bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov-2.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="bilkent-2021-2022-spring-cs453-hw1-ziya-mukhtarov",
+    author="Ziya Mukhtarov",
+    version="2.0.1",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
