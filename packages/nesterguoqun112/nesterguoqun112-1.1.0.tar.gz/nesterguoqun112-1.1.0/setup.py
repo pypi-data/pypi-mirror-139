60684b79858dc1cc147ac82050f7e2f4af2df6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:26 +0000

    Add package nesterguoqun112/1.1.0/nesterguoqun112-1.1.0.tar.gz

---
+++ packages/nesterguoqun112/nesterguoqun112-1.1.0.tar.gz/nesterguoqun112-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterguoqun112',
+    version = '1.1.0',
+    py_modules = ['nesterguoqun112'],
+    author = 'guoqun',
+    author_email = 'guoqun2014dev@163.com',
+    url = 'https://pypi.org',
+    description = 'A simple printer of nested lists',
+)
