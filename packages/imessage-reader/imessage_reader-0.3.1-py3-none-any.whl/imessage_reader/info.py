be653b88880befb2e7a24fa2953c974b99e6dcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:55 +0000

    Add package imessage-reader/0.3.1/imessage_reader-0.3.1-py3-none-any.whl

---
+++ packages/imessage-reader/imessage_reader-0.3.1-py3-none-any.whl/imessage_reader/info.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+App info, license, version
+Python 3.8+
+Date created: June 14th, 2020
+Date modified: September 17th, 2021
+"""
+
+VERSION = '0.3.1'
+
+
+def app_info():
+    """
+    Show app infos (Version & License)
+    """
+    print('\n\n    ##### A Python script to read iMessage data #####')
+    print('    #              Created by niftycode             #')
+    print(f'    #                 Version {VERSION}                 #')
+    print('    #                  MIT License                  #')
+    print('    #################################################\n\n')
+    print()
