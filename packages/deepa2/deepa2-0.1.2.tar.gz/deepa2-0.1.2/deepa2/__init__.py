9ac0cf582079a691bbcbf79e5d85f2b6921cb796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:41 +0000

    Add package deepa2/0.1.2/deepa2-0.1.2.tar.gz

---
+++ packages/deepa2/deepa2-0.1.2.tar.gz/deepa2-0.1.2/deepa2/__init__.py
@@ -0,0 +1,18 @@
+"""Creating, managing, using DeepA2 datasets"""
+# flake8: noqa
+# pylint: skip-file
+
+from deepa2.datastructures import (
+    ArgdownStatement,
+    BaseExample,
+    DA2_ANGLES_MAP,
+    DeepA2Item,
+    DeepA2BaseItem,
+    Formalization,
+    GenerativeMode,
+    QuotedStatement,
+)
+
+from deepa2.parsers import (
+    DeepA2Layouter,
+)
