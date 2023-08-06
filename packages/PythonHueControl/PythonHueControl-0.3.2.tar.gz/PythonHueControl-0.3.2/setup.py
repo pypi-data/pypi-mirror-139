a3c1d126dc5e7b3eaab1b16c156b6bb2d4f138a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package PythonHueControl/0.3.2/PythonHueControl-0.3.2.tar.gz

---
+++ packages/PythonHueControl/PythonHueControl-0.3.2.tar.gz/PythonHueControl-0.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='PythonHueControl',
+    version='0.3.2',
+    packages=['pythonhuecontrol', 'pythonhuecontrol.v1', 'pythonhuecontrol.v1.rule', 'pythonhuecontrol.v1.group',
+              'pythonhuecontrol.v1.light', 'pythonhuecontrol.v1.scene', 'pythonhuecontrol.v1.bridge',
+              'pythonhuecontrol.v1.sensor', 'pythonhuecontrol.v1.schedule', 'pythonhuecontrol.v2'],
+    package_dir={'': '.'},
+    url='https://github.com/elnkosc/PythonHueControl',
+    license='GPL',
+    author='Koen',
+    author_email='koen@schilders.org',
+    description='Feature Rich Python API for Hue V1 and V2',
+    long_description=readme(),
+    long_description_content_type='text/markdown'
+)
