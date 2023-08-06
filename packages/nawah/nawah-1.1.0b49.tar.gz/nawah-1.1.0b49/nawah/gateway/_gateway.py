044f20882a232cd98b808785a647f4087041684b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package nawah/1.1.0b49/nawah-1.1.0b49.tar.gz

---
+++ packages/nawah/nawah-1.1.0b49.tar.gz/nawah-1.1.0b49/nawah/gateway/_gateway.py
@@ -0,0 +1,27 @@
+from nawah.config import Config
+from nawah.classes import UnexpectedGatewayException, InvalidGatewayException
+
+import logging, traceback
+
+logger = logging.getLogger('nawah')
+
+
+class Gateway:
+    @staticmethod
+    def send(*, gateway: str, **kwargs):
+        if Config.test:
+            logger.debug('Skipping \'Gateway.send\' action due to test mode.')
+            return
+
+        if gateway not in Config.gateways.keys():
+            raise InvalidGatewayException(gateway=gateway)
+
+        try:
+            Config.gateways[gateway](**kwargs)
+        except Exception as e:
+            logger.error('Gateway call with following \'kwargs\' failed:')
+            logger.error(kwargs)
+            logger.error(traceback.format_exc())
+            raise UnexpectedGatewayException(gateway=gateway)
+
+        return
