a1b8e0a7ca5c6ec9f20af3be70656e8dc4f48f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:36 +0000

    Add package nebullvm/0.1.1/nebullvm-0.1.1.tar.gz

---
+++ packages/nebullvm/nebullvm-0.1.1.tar.gz/nebullvm-0.1.1/nebullvm/installers/install_tvm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set non interactive mode for apt-get
+export DEBIAN_FRONTEND=noninteractive
+
+if [ ! -d "tvm" ]
+then
+  git clone --recursive https://github.com/apache/tvm tvm
+fi
+
+cd tvm
+mkdir -p build
+cp $CONFIG_PATH build/
+cd build
+cmake ..
+make -j8
+cd ../python
+python3 setup.py install --user
+cd ../..
+if [[ $OSTYPE == "darwin"* ]]
+then
+  brew install openblas gfortran
+  pip install pybind11 cython pythran
+  conda install -y scipy
+  pip install xgboost decorator
+else
+  pip3 install decorator attrs tornado psutil xgboost cloudpickle
+fi
