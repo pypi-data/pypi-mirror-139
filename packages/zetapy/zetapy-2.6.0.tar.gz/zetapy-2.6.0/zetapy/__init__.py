507df2251e487466348e3de6ba746d39904c0548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:04 +0000

    Add package zetapy/2.6.0/zetapy-2.6.0.tar.gz

---
+++ packages/zetapy/zetapy-2.6.0.tar.gz/zetapy-2.6.0/zetapy/__init__.py
@@ -0,0 +1,6 @@
+from zetapy.main import getZeta, getIFR
+from zetapy.msd import getMultiScaleDeriv
+from zetapy.dependencies import getPeak, getGumbel, getOnset, getTempOffset, flatten
+
+__version__ = '2.6.0'
+__author__ = 'Jorrit Montijn, Guido Meijer & Alexander Heimel'
