1fe4cb778a0a2cf4294bb78f8ebc606e79e89749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:45 +0000

    Add package samap/0.3.5/samap-0.3.5.tar.gz

---
+++ packages/samap/samap-0.3.5.tar.gz/samap-0.3.5/samap/__init__.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import scipy as sp
+import numpy as np
+import samalg.utilities as ut
+import warnings
+import scanpy as sc
+__version__ = "0.3.5"
+
+
+def q(x):
+    return np.array(list(x))
