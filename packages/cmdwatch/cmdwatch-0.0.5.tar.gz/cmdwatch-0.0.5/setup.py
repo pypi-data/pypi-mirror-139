415d3eda44513c26be5cac7cc4f1817d101c211d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:37 +0000

    Add package cmdwatch/0.0.5/cmdwatch-0.0.5.tar.gz

---
+++ packages/cmdwatch/cmdwatch-0.0.5.tar.gz/cmdwatch-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cmdwatch",
+    version="0.0.5",
+    author="Karthik E C",
+    author_email="eckarthik39@gmail.com",
+    description="A CLI tool to watch for command outputs and store them to a file for further analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points='''
+       [console_scripts]
+       cmdwatch=cmdwatch.__main__:main
+   ''',
+    url="https://github.com/eckarthik/cmdwatcher",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+)
