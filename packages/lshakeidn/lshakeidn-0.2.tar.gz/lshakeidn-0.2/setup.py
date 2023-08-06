98195bfcb220c1d793841e30feb7b386a3b8c894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:34 +0000

    Add package lshakeidn/0.2/lshakeidn-0.2.tar.gz

---
+++ packages/lshakeidn/lshakeidn-0.2.tar.gz/lshakeidn-0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lshakeidn",
+    version="0.2",
+    author="NaranggiSoko",
+    author_email="ranggi_me@yahoo.com",
+    description="This package gets the latest earthquake in Indonesia from the official government agency",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/giesoko/live_indonesia_earthquake",
+    project_urls={
+        "Website": "https://pramudyasoko.blogspot.com/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
