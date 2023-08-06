688485d240fc8e1cf81a1b8ebc819b6e65ab1b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:08 +0000

    Add package make-us-rich/0.1.0.dev2/make-us-rich-0.1.0.dev2.tar.gz

---
+++ packages/make-us-rich/make-us-rich-0.1.0.dev2.tar.gz/make-us-rich-0.1.0.dev2/make_us_rich/cli/__init__.py
@@ -0,0 +1,18 @@
+ALL = [
+    "conf",
+]
+
+SERVING = [
+    "api",
+]
+
+INTERFACE = [
+    "interface",
+    "database",
+]
+
+TRAINING = [
+    "data",
+]
+
+COMPONENTS = ["serving", "interface", "training"]
