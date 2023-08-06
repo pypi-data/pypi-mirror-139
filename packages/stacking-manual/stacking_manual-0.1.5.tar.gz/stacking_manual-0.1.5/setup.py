5f1f6ffb296d2401dd2bac9f17dcdaef1c16a72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:08 +0000

    Add package stacking-manual/0.1.5/stacking_manual-0.1.5.tar.gz

---
+++ packages/stacking-manual/stacking_manual-0.1.5.tar.gz/stacking_manual-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# this_directory = Path(__file__).parent
+# with open("README.md", "rb") as f:
+#     long_description = f.read()
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='stacking_manual',
+    version='0.1.5',
+#     license='MIT',
+#     author="Giorgos Myrianthous",
+#     author_email='email@example.com',
+#     packages=find_packages('src'),
+#     package_dir={'': 'src'},
+#     url='https://github.com/gmyrianthous/example-publish-pypi',
+#     keywords='example project',
+    install_requires=[
+        'scikit-learn',
+        'pandas'
+      ],
+    long_description = long_description
+
+)
