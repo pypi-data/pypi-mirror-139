548c578e3b1fdb067c524693d809147eb42660bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:29:45 +0000

    Add package ciscotmg/0.2.6/ciscotmg-0.2.6.tar.gz

---
+++ packages/ciscotmg/ciscotmg-0.2.6.tar.gz/ciscotmg-0.2.6/setup.py
@@ -0,0 +1,32 @@
+""" pypi package """
+from os import path
+from setuptools import setup, find_packages
+from ciscotmg.meta import Meta
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open(
+    path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
+) as f:
+    long_description = f.read()
+
+setup(
+    name="ciscotmg",
+    author=Meta.__author__,
+    author_email=Meta.__email__,
+    description=Meta.__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=Meta.__url__,
+    version=Meta.__version__,
+    packages=find_packages(),
+    py_modules=["ciscotmg"],
+    install_requires=requirements,
+    entry_points="""
+        [console_scripts]
+        ciscotmg=ciscotmg.cli:cli
+    """,
+    python_requires=">=3.9",
+    license=Meta.__license__ + "; " + Meta.__copyright__,
+)
