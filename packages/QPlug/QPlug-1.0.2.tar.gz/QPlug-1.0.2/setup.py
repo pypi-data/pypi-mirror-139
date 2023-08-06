36811b2d3e076904cf73576113d33b8a13025da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:47 +0000

    Add package QPlug/1.0.2/QPlug-1.0.2.tar.gz

---
+++ packages/QPlug/QPlug-1.0.2.tar.gz/QPlug-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+long_description = "Qt dependency for D-Analyst."
+
+setuptools.setup(
+    name = "QPlug",
+    version = "1.0.2",
+    author = "Agbakosi Adeoluwa(180 Memes), Diachronic Technologies",
+    author_email = "adeoluwaagbakosi@gmail.com",
+    description = "Qt dependency for D-Analyst ",
+    long_description = long_description,
+    url = "https://github.com/Diachronic-Technologies/Qtop",
+    project_urls = {
+        "Bug Tracker":"https://github.com/Diachronic-Technologies/Qtop/issues"
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"":"main"},
+    packages=setuptools.find_packages(where="main"),
+    python_requires=">=3.6",
+    install_requires = []
+)
