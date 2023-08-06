49761ce689b5dbb0ed807579a79ac9a6a101feff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:13 +0000

    Add package pytest-splunk-addon/3.0.0b1/pytest-splunk-addon-3.0.0b1.tar.gz

---
+++ packages/pytest-splunk-addon/pytest-splunk-addon-3.0.0b1.tar.gz/pytest-splunk-addon-3.0.0b1/pytest_splunk_addon/helmut/exceptions/wait.py
@@ -0,0 +1,28 @@
+#
+# Copyright 2021 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+class WaitTimedOut(RuntimeError):
+    """
+    This exception is raised when a designated wait period times out.
+    """
+
+    def __init__(self, seconds_waited):
+        self.seconds_waited = seconds_waited
+        super(WaitTimedOut, self).__init__(self._error_message)
+
+    @property
+    def _error_message(self):
+        message = "Search was not done after {0} seconds"
+        return message.format(self.seconds_waited)
