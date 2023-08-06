8b9cf600300b2f64fa07ae4a4148ebbf5c5f4bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:01:07 +0000

    Add package etpyfitest1/0.0.7/etpyfitest1-0.0.7-py3-none-any.whl

---
+++ packages/etpyfitest1/etpyfitest1-0.0.7-py3-none-any.whl/package/__init__.py
@@ -0,0 +1,4 @@
+print("Ich importiere die Package")
+
+from .modul2 import fun2
+from . import modul1
---
+++ packages/etpyfitest1/etpyfitest1-0.0.7-py3-none-any.whl/package/modul2.py
@@ -0,0 +1,7 @@
+print("Ich bin Modul2")
+
+def fun2():
+    print("Ich bin eine Funktion in Modul2")
+
+if __name__ == "__main__":
+    fun2()
