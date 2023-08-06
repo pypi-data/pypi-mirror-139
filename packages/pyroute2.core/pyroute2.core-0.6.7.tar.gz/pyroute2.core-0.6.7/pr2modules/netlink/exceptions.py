1e7199180104ced36634106de27522e0f5366816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:30 +0000

    Add package pyroute2.core/0.6.7/pyroute2.core-0.6.7.tar.gz

---
+++ packages/pyroute2.core/pyroute2.core-0.6.7.tar.gz/pyroute2.core-0.6.7/pr2modules/netlink/exceptions.py
@@ -0,0 +1,71 @@
+import os
+
+
+class NetlinkError(Exception):
+    '''
+    Base netlink error
+    '''
+
+    def __init__(self, code, msg=None):
+        msg = msg or os.strerror(code)
+        super(NetlinkError, self).__init__(code, msg)
+        self.code = code
+        self.extra_code = 0
+
+
+class NetlinkDecodeError(Exception):
+    '''
+    Base decoding error class.
+
+    Incapsulates underlying error for the following analysis
+    '''
+
+    def __init__(self, exception):
+        self.exception = exception
+
+
+class NetlinkHeaderDecodeError(NetlinkDecodeError):
+    '''
+    The error occured while decoding a header
+    '''
+
+    pass
+
+
+class NetlinkDataDecodeError(NetlinkDecodeError):
+    '''
+    The error occured while decoding the message fields
+    '''
+
+    pass
+
+
+class NetlinkNLADecodeError(NetlinkDecodeError):
+    '''
+    The error occured while decoding NLA chain
+    '''
+
+    pass
+
+
+class IPSetError(NetlinkError):
+    '''
+    Netlink error with IPSet special error codes.
+
+    Messages are imported from errcode.c
+    '''
+
+    pass
+
+
+class NetlinkDumpInterrupted(NetlinkError):
+    '''
+    Raised when NLM_F_DUMP_INTR is set in the flags.
+    '''
+
+    def __init__(self):
+        super(NetlinkDumpInterrupted, self).__init__(-1, 'dump interrupted')
+
+
+class SkipInode(Exception):
+    pass
