fb55fcc08786584c25e20009b7e7c6eb84f4ae5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:31 +0000

    Add package tuf/1.0.0/tuf-1.0.0.tar.gz

---
+++ packages/tuf/tuf-1.0.0.tar.gz/tuf-1.0.0/tuf/__init__.py
@@ -0,0 +1,11 @@
+# Copyright New York University and the TUF contributors
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+"""TUF
+"""
+
+# This value is used in the requests user agent.
+# setup.cfg has it hard-coded separately.
+# Currently, when the version is changed, it must be set in both locations.
+# TODO: Single-source the version number.
+__version__ = "1.0.0"
