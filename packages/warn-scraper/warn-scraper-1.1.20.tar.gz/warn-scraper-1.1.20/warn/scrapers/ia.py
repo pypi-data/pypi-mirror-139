f382116ec7698a5d1cf333345c80b12de069ee86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:47 +0000

    Add package warn-scraper/1.1.20/warn-scraper-1.1.20.tar.gz

---
+++ packages/warn-scraper/warn-scraper-1.1.20.tar.gz/warn-scraper-1.1.20/warn/scrapers/ia.py
@@ -0,0 +1,90 @@
+import logging
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from openpyxl import load_workbook
+
+from .. import utils
+from ..cache import Cache
+
+__authors__ = ["zstumgoren", "Dilcia19", "shallotly", "palewire"]
+__tags__ = ["html", "excel"]
+
+logger = logging.getLogger(__name__)
+
+
+def scrape(
+    data_dir: Path = utils.WARN_DATA_DIR,
+    cache_dir: Path = utils.WARN_CACHE_DIR,
+) -> Path:
+    """
+    Scrape data from Iowa.
+
+    Keyword arguments:
+    data_dir -- the Path were the result will be saved (default WARN_DATA_DIR)
+    cache_dir -- the Path where results can be cached (default WARN_CACHE_DIR)
+
+    Returns: the Path where the file is written
+    """
+    # Go to the page
+    url = "https://www.iowaworkforcedevelopment.gov/worker-adjustment-and-retraining-notification-act"
+    r = utils.get_url(url)
+    html = r.text
+
+    # Save it to the cache
+    cache = Cache(cache_dir)
+    cache.write("ia/source.html", html)
+
+    # Parse out the Excel link
+    soup = BeautifulSoup(html, "html.parser")
+    excel_url = soup.find("a", {"title": "WARN Log Excel File"})["href"]
+
+    # Download the Excel file
+    excel_path = cache.download("ia/source.xlsx", excel_url)
+
+    # Parse it
+    row_list = _parse_xlsx(excel_path)
+
+    # Get historic file
+    historic_url = "https://www.iowaworkforcedevelopment.gov/sites/search.iowaworkforcedevelopment.gov/files/documents/2018/WARN_20180503.xlsx"
+    historic_excel_path = cache.download("ia/historic.xlsx", historic_url)
+
+    # Parse it, minus the header
+    row_list += _parse_xlsx(historic_excel_path)[1:]
+
+    # Set the export path
+    data_path = data_dir / "ia.csv"
+
+    # Write out the file
+    utils.write_rows_to_csv(data_path, row_list)
+
+    # Return the path to the file
+    return data_path
+
+
+def _parse_xlsx(excel_path: Path) -> list:
+    """Parse the XLSX file at the provided path.
+
+    Args:
+    excel_path (Path): The path to an XLSX file
+
+    Returns a list of values ready to write.
+    """
+    # Open it up
+    workbook = load_workbook(filename=excel_path)
+
+    # Get the first sheet
+    worksheet = workbook.worksheets[0]
+
+    # Convert the sheet to a list of lists
+    row_list = []
+    for r in worksheet.rows:
+        column = [cell.value for cell in r]
+        row_list.append(column)
+
+    # Pass it back
+    return row_list
+
+
+if __name__ == "__main__":
+    scrape()
