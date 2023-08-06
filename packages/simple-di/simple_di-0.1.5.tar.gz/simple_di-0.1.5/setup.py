2cd9fc46e1abfd54be3e09db85754f20e4418f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:45 +0000

    Add package simple-di/0.1.5/simple_di-0.1.5.tar.gz

---
+++ packages/simple-di/simple_di-0.1.5.tar.gz/simple_di-0.1.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="simple_di",
+    version="0.1.5",
+    author="bojiang",
+    author_email="bojiang_@outlook.com",
+    description="simple dependency injection library",
+    long_description=long_description,
+    license="Apache License 2.0",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bentoml/simple_di",
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    package_data={"simple_di": ["py.typed"]},
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    python_requires=">=3.6.1",
+    install_requires=[
+        'dataclasses; python_version < "3.7.0"',
+        'types-dataclasses; python_version < "3.7.0"',
+    ],
+    extras_require={"test": ["pytest", "mypy"]},
+)
