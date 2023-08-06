c7db9d16d6d8bc3d44f74e46c04da03136059a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:04:36 +0000

    Add package solidclient/0.0.9/solidclient-0.0.9.tar.gz

---
+++ packages/solidclient/solidclient-0.0.9.tar.gz/solidclient-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='solidclient',
+      version='0.0.9',
+      description='A Solid client in Python',
+      long_description='file: INSTALL.md',
+      long_description_content_type='text/markdown',
+      url='https://gitlab.com/arbetsformedlingen/individdata/oak/python-solid-client',
+      author='Max Dzyuba',
+      author_email='max.dzyuba@arbetsformedlingen.se',
+      license='MIT',
+      keywords='Solid Client OpenID Connect OIDC DPoP',
+      packages=find_packages(exclude=["tests"]),
+      install_requires=[
+          'requests==2.25.1',
+          'jwcrypto==1.0',
+          'rdflib==5.0.0',
+          'solid-file==0.2.0',
+          'oic==1.3.0',
+          'httpx==0.18.2',
+      ],
+      include_package_data=True,
+      python_requires='>= 3.6',
+      test_suite='tests',
+      )
