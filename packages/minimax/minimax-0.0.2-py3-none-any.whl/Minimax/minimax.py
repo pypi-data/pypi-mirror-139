2ec7f9cd061c51ceefa75815ed52b1442e0f267b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package minimax/0.0.2/minimax-0.0.2-py3-none-any.whl

---
+++ packages/minimax/minimax-0.0.2-py3-none-any.whl/Minimax/minimax.py
@@ -0,0 +1,8 @@
+class Minimax:
+    def __init__(self) -> None:
+        self.environment = None
+        self.heuristics = None
+        self.successors = None
+    
+    def keyword(self) -> str:
+        return "Bergur Tareq"
