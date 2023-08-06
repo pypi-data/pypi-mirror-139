e2bc0086924fb07826f4226de6138cf275a48a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package python-stopwatch2/1.1.0/python_stopwatch2-1.1.0-py3-none-any.whl

---
+++ packages/python-stopwatch2/python_stopwatch2-1.1.0-py3-none-any.whl/stopwatch/__init__.py
@@ -0,0 +1,6 @@
+from .profile import profile
+from .statistics import Statistics
+from .stopwatch import Stopwatch
+from .utils import format_elapsed_time
+
+__all__ = ['Statistics', 'Stopwatch', 'format_elapsed_time', 'profile']
