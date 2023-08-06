abedca4b69e8e36e017382dc043a9bb9478f871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:29 +0000

    Add package torchlm/0.1.5/torchlm-0.1.5-py3-none-any.whl

---
+++ packages/torchlm/torchlm-0.1.5-py3-none-any.whl/torchlm/__init__.py
@@ -0,0 +1,8 @@
+# Versions
+__version__ = '0.1.5'
+# Transforms Module: 100+ transforms available, can bind torchvision and
+# albumentations into torchlm pipeline with autodtype wrapper.
+from .transforms import *
+# Utils Module: some utils methods.
+from .utils import *
+# Other Modules: TODO
