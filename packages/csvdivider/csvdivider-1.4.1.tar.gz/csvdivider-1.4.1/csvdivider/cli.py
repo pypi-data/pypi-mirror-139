d99a2306dde04e24f2e9e1d44bebf4df8a59caf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:11 +0000

    Add package csvdivider/1.4.1/csvdivider-1.4.1.tar.gz

---
+++ packages/csvdivider/csvdivider-1.4.1.tar.gz/csvdivider-1.4.1/csvdivider/cli.py
@@ -0,0 +1,26 @@
+import argparse
+from csvdivider.csv_divider import csv_divider
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Dividing CSV into separated CSVs.')
+
+    parser.add_argument('lines', type=int,
+                        help='Insert a column of line numbers at the front of the output.')
+
+    parser.add_argument('-i', '--input_file', dest='input_file', required=True,
+                        help='The CSV file to operate on. ')
+
+    parser.add_argument('-o', '--output_path', dest='output_path', nargs='?',
+                        help='output folder')
+
+    parser.add_argument('--head', dest='head_row', action=argparse.BooleanOptionalAction,
+                        help='with or without column names, default value: True')
+
+    args = parser.parse_args()
+
+    csv_divider(**vars(args))
+
+
+if __name__ == '__main__':
+    main()
