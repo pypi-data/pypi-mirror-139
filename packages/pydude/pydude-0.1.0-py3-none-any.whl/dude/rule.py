1ed1306064ce3eaab4200cc505dc4f93d4c2ee36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:53 +0000

    Add package pydude/0.1.0/pydude-0.1.0-py3-none-any.whl

---
+++ packages/pydude/pydude-0.1.0-py3-none-any.whl/dude/rule.py
@@ -0,0 +1,26 @@
+from typing import Any, Callable, NamedTuple, Tuple
+
+
+class Rule(NamedTuple):
+    group: str
+    selector: str
+    url_pattern: str
+    handler: Any  # function or coroutine
+    setup: bool
+    navigate: bool
+    priority: int
+
+
+def rule_sorter(rule: Rule) -> Tuple[str, str, str]:
+    return rule.url_pattern, rule.group, rule.selector
+
+
+def rule_grouper(rule: Rule) -> Tuple[str, str]:
+    return rule.url_pattern, rule.group
+
+
+def rule_filter(setup: bool = False, navigate: bool = False) -> Callable:
+    def wrapper(rule: Rule) -> bool:
+        return rule.setup is setup and rule.navigate is navigate
+
+    return wrapper
