6318d9ab934d06fc6071f2d226b31dace3fd866a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package aliby-post/0.1.20/aliby-post-0.1.20.tar.gz

---
+++ packages/aliby-post/aliby-post-0.1.20.tar.gz/aliby-post-0.1.20/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['postprocessor',
+ 'postprocessor.core',
+ 'postprocessor.core.functions',
+ 'postprocessor.core.old',
+ 'postprocessor.core.processes',
+ 'postprocessor.examples']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'aliby-agora>=0.2.22,<0.3.0',
+ 'catch22>=0.2.0,<0.3.0',
+ 'leidenalg>=0.8.8,<0.9.0',
+ 'more-itertools>=8.12.0,<9.0.0',
+ 'numpy>=1.17.3',
+ 'pandas>=1.1.4,<2.0.0',
+ 'pathos>=0.2.8,<0.3.0',
+ 'scikit-learn>=1.0.2,<2.0.0',
+ 'scipy>=1.4.1',
+ 'seaborn>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'aliby-post',
+    'version': '0.1.20',
+    'description': 'Post-processing tools for aliby pipeline.',
+    'long_description': None,
+    'author': 'Alán Muñoz',
+    'author_email': 'amuoz@ed.ac.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<3.11',
+}
+
+
+setup(**setup_kwargs)
