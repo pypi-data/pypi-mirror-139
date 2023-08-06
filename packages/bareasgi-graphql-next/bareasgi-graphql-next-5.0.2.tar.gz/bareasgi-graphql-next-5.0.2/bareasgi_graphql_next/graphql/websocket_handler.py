4dcc486f80bfda0f888f239f0e9dff4ba930e53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:34 +0000

    Add package bareasgi-graphql-next/5.0.2/bareasgi-graphql-next-5.0.2.tar.gz

---
+++ packages/bareasgi-graphql-next/bareasgi-graphql-next-5.0.2.tar.gz/bareasgi-graphql-next-5.0.2/bareasgi_graphql_next/graphql/websocket_handler.py
@@ -0,0 +1,24 @@
+"""
+WebSocket handler
+"""
+
+from bareasgi import WebSocketRequest
+import graphql
+
+from .websocket_instance import GraphQLWebSocketHandlerInstance
+
+
+class GraphQLWebSocketHandler:
+    """GraphQL WebSocket handler"""
+
+    def __init__(self, schema: graphql.GraphQLSchema):
+        """GraphQL WebSocket handler
+
+        Args:
+            schema (graphql.GraphQLSchema): The schema
+        """
+        self.schema = schema
+
+    async def __call__(self, request: WebSocketRequest) -> None:
+        instance = GraphQLWebSocketHandlerInstance(self.schema, request)
+        await instance.start(request.scope['subprotocols'])
