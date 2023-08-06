9a487209c0b5e892fc41e7d1969cfe5521472790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:15 +0000

    Add package flitton-fib-py1/0.0.1/flitton_fib_py1-0.0.1.tar.gz

---
+++ packages/flitton-fib-py1/flitton_fib_py1-0.0.1.tar.gz/flitton_fib_py1-0.0.1/flitton_fib_py/__init__.py
@@ -0,0 +1,7 @@
+from curses import A_ALTCHARSET
+from multiprocessing.context import assert_spawning
+from re import A
+
+
+def say_hello() -> None:
+    print("the Flitton Fobonacci module is saying hello")
