8ec1831ec3c4ad7051e18e2d4597984650ab3096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:34 +0000

    Add package jaye/0.0.7/jaye-0.0.7-py3-none-any.whl

---
+++ packages/jaye/jaye-0.0.7-py3-none-any.whl/jaye/hello_jaye.py
@@ -0,0 +1,3 @@
+def hello_jaye():
+    print('jaye입니다. 만나서 반갑습니다.')
+    return None
---
+++ packages/jaye/jaye-0.0.7-py3-none-any.whl/jaye/settings.py
@@ -0,0 +1,8 @@
+from rich.console import Console
+
+CONSOLE = Console()
+JAYE_ACCOUNT = ""
+PASSWORD = ""
+BASE_URL_ACCOUNT = "https://account.jaye.world"
+BASE_URL_PUBLIC = "https://qaas.jaye.world"
+TOKEN = ""
