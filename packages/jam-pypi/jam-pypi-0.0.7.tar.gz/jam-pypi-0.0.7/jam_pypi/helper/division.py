75dd1a4c4334d4b55ea50cbd772c373be6b62c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package jam-pypi/0.0.7/jam-pypi-0.0.7.tar.gz

---
+++ packages/jam-pypi/jam-pypi-0.0.7.tar.gz/jam-pypi-0.0.7/jam_pypi/helper/division.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+Array = np.array
+
+
+def div(x: Array, y: Array) -> Array:
+    """div two Array
+
+    :param x: comments for x
+    :type x: Array
+    :param y: comments for y
+    :type y: Array
+    :return: summation results
+    :rtype: Array
+    """
+    return x / y
