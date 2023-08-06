233e4d6e076d27384807af2e347e8f455e1fcca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:44 +0000

    Add package sysflow-tools/0.4.0rc3/sysflow_tools-0.4.0rc3-py3-none-any.whl

---
+++ packages/sysflow-tools/sysflow_tools-0.4.0rc3-py3-none-any.whl/sysflow/sysflow/__init__.py
@@ -0,0 +1,3 @@
+from ..schema_classes import SchemaClasses
+
+SysFlow = SchemaClasses.sysflow.SysFlowClass
---
+++ packages/sysflow-tools/sysflow_tools-0.4.0rc3-py3-none-any.whl/sysflow/sysflow/flow/__init__.py
@@ -0,0 +1,5 @@
+from ...schema_classes import SchemaClasses
+
+FileFlow = SchemaClasses.sysflow.flow.FileFlowClass
+NetworkFlow = SchemaClasses.sysflow.flow.NetworkFlowClass
+ProcessFlow = SchemaClasses.sysflow.flow.ProcessFlowClass
