1a6f7ec84492705108f5a94c2a9dcc4a07c2af57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:15 +0000

    Add package zbrojiMiGa-fsucic/1.0.0/zbrojiMiGa_fsucic-1.0.0.tar.gz

---
+++ packages/zbrojiMiGa-fsucic/zbrojiMiGa_fsucic-1.0.0.tar.gz/zbrojiMiGa_fsucic-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='zbrojiMiGa_fsucic',
+	version='1.0.0',
+	py_modules=['zbroj_do_n'],
+	author='fsucic_python',
+	author_email='fransucic46@gmail.com',
+	description='Sums numbers from 1 to n',
+     )
---
+++ packages/zbrojiMiGa-fsucic/zbrojiMiGa_fsucic-1.0.0.tar.gz/zbrojiMiGa_fsucic-1.0.0/zbroj_do_n.py
@@ -0,0 +1,7 @@
+
+
+def zbrojiGa(n):
+    k=0
+    for i in range(n+1):
+        k=k+i
+    return k
