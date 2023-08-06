30be57a0001a44c3d01bf0624b6c83b32a98d128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:17 +0000

    Add package django-pfx/0.2.dev128/django_pfx-0.2.dev128-py3-none-any.whl

---
+++ packages/django-pfx/django_pfx-0.2.dev128-py3-none-any.whl/pfx/pfxcore/views/__init__.py
@@ -0,0 +1,23 @@
+from .authentication_views import (
+    AuthenticationView,
+    ForgotenPasswordView,
+    SignupView,
+)
+from .filters_views import Filter, FilterGroup, ModelFilter
+from .locale_views import LocaleRestView
+from .rest_views import (
+    BaseRestView,
+    BodyMixin,
+    CreateRestViewMixin,
+    DeleteRestViewMixin,
+    DetailRestViewMixin,
+    ListRestViewMixin,
+    MediaRestViewMixin,
+    ModelBodyMixin,
+    ModelMixin,
+    ModelResponseMixin,
+    RestView,
+    SecuredRestViewMixin,
+    UpdateRestViewMixin,
+    resource_not_found,
+)
