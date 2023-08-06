4ee1575ea3df4afee81c50e265afac9b835dfcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:46 +0000

    Add package mrmr-selection/0.2.2/mrmr_selection-0.2.2.tar.gz

---
+++ packages/mrmr-selection/mrmr_selection-0.2.2.tar.gz/mrmr_selection-0.2.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf8") as f:
+    long_description = f.read()
+
+setup(
+    name='mrmr_selection',
+    version='0.2.2',
+    description='minimum-Redundancy-Maximum-Relevance algorithm for feature selection',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/smazzanti/mrmr',
+    author='Samuele Mazzanti',
+    author_email='mazzanti.sam@gmail.com',
+    license='MIT',
+    packages=['mrmr'],
+    install_requires=[
+        'jinja2',
+        'tqdm',
+        'joblib',
+        'pandas>=1.0.3',
+        'numpy>=1.18.1',
+        'sklearn',
+        'category_encoders'
+    ],
+    zip_safe=False
+)
