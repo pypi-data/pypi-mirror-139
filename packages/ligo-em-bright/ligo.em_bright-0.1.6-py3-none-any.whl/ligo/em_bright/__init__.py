855dbf4a74f96ca31200b419fc1820187cb3bd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:53 +0000

    Add package ligo-em-bright/0.1.6/ligo.em_bright-0.1.6-py3-none-any.whl

---
+++ packages/ligo-em-bright/ligo.em_bright-0.1.6-py3-none-any.whl/ligo/em_bright/__init__.py
@@ -0,0 +1,15 @@
+from astropy.utils.data import download_file
+
+__version__ = '0.1.6'
+
+PACKAGE_DATA_LINKS = {
+    'knn_em_classifier.pkl': 'https://git.ligo.org/deep.chatterjee/em-bright/-/raw/v0.1.6/ligo/em_bright/data/knn_em_classifier.pkl',  # noqa: E501
+    'knn_ns_classifier.pkl': 'https://git.ligo.org/deep.chatterjee/em-bright/-/raw/v0.1.6/ligo/em_bright/data/knn_ns_classifier.pkl',  # noqa: E501
+    'equil_2H.dat': 'https://git.ligo.org/deep.chatterjee/em-bright/-/raw/v0.1.6/ligo/em_bright/data/equil_2H.dat'  # noqa: E501
+}
+PACKAGE_FILENAMES = dict.fromkeys(PACKAGE_DATA_LINKS)
+
+for name, url in PACKAGE_DATA_LINKS.items():
+    PACKAGE_FILENAMES[name] = download_file(
+        url, cache=True, pkgname='ligo.em_bright'
+    )
