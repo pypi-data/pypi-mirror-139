f1ef9a304baf1f5b1e1d94ed0934c4a26a31bc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:15 +0000

    Add package x509generator/0.1/x509generator-0.1.tar.gz

---
+++ packages/x509generator/x509generator-0.1.tar.gz/x509generator-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='x509generator',
+    version='0.1',
+    packages=['x509generator'],
+    url='https://github.com/codinlikewilly/py-certgenerator',
+    license='MIT ',
+    author='will',
+    author_email='will@theapiguys.com',
+    description='Generates SSL x509 certificates',
+    download_url="https://github.com/codinlikewilly/x509generator/archive/refs/tags/v_01.tar.gz",
+    keywords=['x509', 'SSL', 'OpenSSL'],
+    install_requires=[
+        'pyopenssl',
+    ],
+)
