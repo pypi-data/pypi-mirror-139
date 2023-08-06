6ebaccb5269e691afa19f0f76ef282c2ffead8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:43 +0000

    Add package skutterbug/0.0.1/skutterbug-0.0.1.tar.gz

---
+++ packages/skutterbug/skutterbug-0.0.1.tar.gz/skutterbug-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['skutterbug', 'skutterbug.uri']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'skutterbug',
+    'version': '0.0.1',
+    'description': 'Analyze URLs',
+    'long_description': None,
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@ragt.ag',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
