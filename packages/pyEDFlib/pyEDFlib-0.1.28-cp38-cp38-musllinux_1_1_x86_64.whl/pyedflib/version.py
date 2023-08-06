dace4e0e448590d0842c59176b7e186966b0f925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:26 +0000

    Add package pyEDFlib/0.1.28/pyEDFlib-0.1.28-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.28-cp38-cp38-musllinux_1_1_x86_64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.28'
+version = '0.1.28'
+full_version = '0.1.28'
+git_revision = '62515d52c7ac63313519b395b56bd3039555fecd'
+release = True
+
+if not release:
+    version = full_version
