b555683cd4afb7ef3c498c4065e5a4125202a8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:33 +0000

    Add package ling2me/0.0.9/ling2me-0.0.9.tar.gz

---
+++ packages/ling2me/ling2me-0.0.9.tar.gz/ling2me-0.0.9/ling2me/ling2me.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class Ling2MeRequest():
+
+    def translate(apikey, source_text, from_language, to_language):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+apikey
+        }
+        data = {
+                'text': source_text,  # text
+                'from': from_language,  # from language
+                'to': to_language,  # to language
+            }
+        response = requests.post('https://app.ling2me.com/api/translate', headers=headers, data=data)
+        
+        json_response = json.loads(response.text)
+
+        return json_response['data']['translated_text']
