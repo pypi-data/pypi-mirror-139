2c29a5a9d7cd935e144f59bca6cbf31050cb04fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:29 +0000

    Add package Choam/0.1.21/Choam-0.1.21-py3-none-any.whl

---
+++ packages/Choam/Choam-0.1.21-py3-none-any.whl/choam/constants.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import platform
+from pathlib import Path
+
+# system details
+OPERATING_SYSTEM = platform.system().lower()
+PYTHON_INTERPRETER: str = sys.executable
+
+# configurations
+FOLDER_SEPERATOR: str
+SETUP_FILE_NAME: str
+
+# Processing platform specific configurations
+if OPERATING_SYSTEM == "windows":
+    FOLDER_SEPERATOR = "\\"
+    SETUP_FILE_NAME = "setup"
+
+    PYTHON_INTERPRETER = os.popen("where python").read().split("\n")[0]
+
+else:
+    FOLDER_SEPERATOR = "/"
+    SETUP_FILE_NAME = "setup.py"
+
+    PYTHON_INTERPRETER = os.popen("which python3").read().split("\n")[0]
