44cb22fb8e80053df9fb738f64552546806f9be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:33 +0000

    Add package oopnet/0.2.2/oopnet-0.2.2-py3-none-any.whl

---
+++ packages/oopnet/oopnet-0.2.2-py3-none-any.whl/oopnet/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.2"
+
+from .elements import *
+from .graph import *
+from .plotter import *
+from .report import *
+from .utils.adders import *
+from .utils.getters import *
+from .utils.removers import *
+from .utils import *
