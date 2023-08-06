faf4730de40f7a0666e76ed46306e1f105b1b395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:32 +0000

    Add package pearchimageserviceclient/0.1/pearchimageserviceclient-0.1.tar.gz

---
+++ packages/pearchimageserviceclient/pearchimageserviceclient-0.1.tar.gz/pearchimageserviceclient-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pearchimageserviceclient",
+    version="0.1",
+    author="Alejandro",
+    author_email="alejandrog@pearchdigital.com",
+    description="This is a package for handle image service call.",
+    packages=['images_service_client_pearch_digital'],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
