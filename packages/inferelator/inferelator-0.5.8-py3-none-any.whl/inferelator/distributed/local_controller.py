366cea473c39685ce576d51504cf94f5513af620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:33 +0000

    Add package inferelator/0.5.8/inferelator-0.5.8-py3-none-any.whl

---
+++ packages/inferelator/inferelator-0.5.8-py3-none-any.whl/inferelator/distributed/local_controller.py
@@ -0,0 +1,48 @@
+"""
+LocalController just runs everything in a single process
+"""
+
+import collections.abc
+
+from inferelator.distributed import AbstractController
+from inferelator import utils
+from inferelator.utils import Validator as check
+
+
+class LocalController(AbstractController):
+
+    _controller_name = "local"
+
+    client = None
+    chunk = None
+
+    @classmethod
+    def connect(cls, *args, **kwargs):
+        return True
+
+    @classmethod
+    def map(cls, func, *arg, **kwargs):
+        """
+        Map a function across iterable(s) and return a list of results
+
+        :param func: function
+            Mappable function
+        :param args: iterable
+            Iterator(s)
+        """
+        assert check.argument_callable(func)
+        assert check.argument_list_type(arg, collections.abc.Iterable)
+        return list(map(func, *arg))
+
+    @classmethod
+    def set_processes(cls, process_count):
+        """
+        Set the number of dask workers to use
+        :param process_count: int
+        :return:
+        """
+        utils.Debug.vprint("Local does not support multiple cores", level=0)
+
+    @classmethod
+    def shutdown(cls):
+        return True
