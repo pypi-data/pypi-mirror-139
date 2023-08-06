9c6bb4d2cec8db357294ef11cd2a9d26fbf6e6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:33 +0000

    Add package narrow-down/0.8.0/narrow_down-0.8.0-cp37-abi3-win_amd64.whl

---
+++ packages/narrow-down/narrow_down-0.8.0-cp37-abi3-win_amd64.whl/narrow_down/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for narrow-down."""
+
+__author__ = """Christian Krudewig"""
+__email__ = "chr1st1ank@krudewig-online.de"
+__version__ = "0.8.0"
+
+from . import data_types, hash, similarity_store, storage  # noqa
