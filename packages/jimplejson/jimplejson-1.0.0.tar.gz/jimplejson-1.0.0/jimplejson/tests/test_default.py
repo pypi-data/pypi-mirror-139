293b9c5870578322a463de626fc84fc0b8c812d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:39 +0000

    Add package jimplejson/1.0.0/jimplejson-1.0.0.tar.gz

---
+++ packages/jimplejson/jimplejson-1.0.0.tar.gz/jimplejson-1.0.0/jimplejson/tests/test_default.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+import jimplejson as json
+
+class TestDefault(TestCase):
+    def test_default(self):
+        self.assertEqual(
+            json.dumps(type, default=repr),
+            json.dumps(repr(type)))
