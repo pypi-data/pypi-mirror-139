f457d93cfbc7ded283d094d611bfb2dad912e56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:37 +0000

    Add package randomdataset/0.2.0/RandomDataset-0.2.0.tar.gz

---
+++ packages/randomdataset/RandomDataset-0.2.0.tar.gz/RandomDataset-0.2.0/tests/test_fieldgen.py
@@ -0,0 +1,48 @@
+# RandomDataset
+# Copyright (c) 2021 Eric Kerfoot, KCL, see LICENSE file
+
+
+import unittest
+
+from randomdataset import StrFieldGen, IntFieldGen, DateTimeFieldGen, UIDFieldGen
+
+
+class TestStrFieldGen(unittest.TestCase):
+    def test_str_basic(self):
+        gen = StrFieldGen("test", 8, 9)
+
+        s = gen()
+
+        self.assertIsInstance(s, str)
+        self.assertEqual(len(s), 8)
+
+    def test_int_basic(self):
+        gen = IntFieldGen("test", 0, 10)
+
+        s = gen()
+
+        self.assertIsInstance(s, int)
+        self.assertTrue(0 <= s < 10)
+
+    def test_uid_basic(self):
+        gen = UIDFieldGen("test", 5)
+
+        s = gen()
+
+        self.assertEqual(s, 5)
+
+        s = gen()
+
+        self.assertEqual(s, 6)
+
+    def test_datetime_basic(self):
+        gen = DateTimeFieldGen("test")
+
+        s = gen()
+        self.assertIsInstance(s, float)
+
+    def test_datetime_str(self):
+        gen = DateTimeFieldGen("test", as_string=True)
+
+        s = gen()
+        self.assertIsInstance(s, str)
