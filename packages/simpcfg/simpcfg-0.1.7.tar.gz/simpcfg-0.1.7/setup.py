0601848a292b15a557b9a350609c9f0b33c6d565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:08 +0000

    Add package simpcfg/0.1.7/simpcfg-0.1.7.tar.gz

---
+++ packages/simpcfg/simpcfg-0.1.7.tar.gz/simpcfg-0.1.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpcfg',
+    version='0.1.7',
+    description='Simple Configuration Manager',
+    url='https://github.com/StephenMal/simpcfg',
+    author='Stephen Maldonado',
+    author_email='simpcfg@stephenmal.com',
+    packages=find_packages(),
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+    ]
+)
