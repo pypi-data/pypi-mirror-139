82ea6ca8196c5a0a3a1b0e85a015f5f52becb9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:20 +0000

    Add package aliby/0.1.21/aliby-0.1.21.tar.gz

---
+++ packages/aliby/aliby-0.1.21.tar.gz/aliby-0.1.21/extraction/core/functions/trap.py
@@ -0,0 +1,16 @@
+## Trap-wise calculations
+
+import numpy as np
+
+
+def imBackground(cell_masks, trap_image):
+    '''
+    :param cell_masks: (numpy 3d array) cells' segmentation mask
+    :param trap_image: the image for the trap in which the cell is (all
+    channels)
+    '''
+    if not len(cell_masks):
+        cell_masks = np.zeros_like(trap_image)
+
+    background = ~cell_masks.sum(axis=2).astype(bool)
+    return (np.median(trap_image[np.where(background)]))
