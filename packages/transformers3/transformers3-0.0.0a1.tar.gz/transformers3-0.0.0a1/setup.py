31f49ba51f0d364def4a10d4ef97278b803c1ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:42 +0000

    Add package transformers3/0.0.0a1/transformers3-0.0.0a1.tar.gz

---
+++ packages/transformers3/transformers3-0.0.0a1.tar.gz/transformers3-0.0.0a1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='transformers3',
+    description="coming soon",
+    version="0.0.0a1",
+    author='John Reese',
+    author_email='john@noswap.com',
+)
