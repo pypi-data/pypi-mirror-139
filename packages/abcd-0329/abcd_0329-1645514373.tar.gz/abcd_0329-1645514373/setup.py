86d994089f8e533b66ad649482955c36005233a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:22 +0000

    Add package abcd-0329/1645514373/abcd_0329-1645514373.tar.gz

---
+++ packages/abcd-0329/abcd_0329-1645514373.tar.gz/abcd_0329-1645514373/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+import datetime
+import sys
+
+
+def GetVersion():
+    try:
+        with open('./version', 'r') as f:
+            version = f.read()
+    except:
+        now_time = datetime.datetime.now()
+        version = "%s.%s.%s" % (
+            now_time.strftime('%y') + now_time.strftime('%m'),
+            now_time.strftime('%d') + now_time.strftime('%H'),
+            now_time.strftime('%M') + now_time.strftime('%S'))
+
+        now_time = int(datetime.datetime.now().timestamp())
+        version = str(now_time)
+    return version
+
+
+setup(name='abcd_0329',
+      version=GetVersion(),
+      author='abc',
+      zip_safe=False,
+      url="https://pypi.org/",
+      include_package_data=True,
+      author_email='',
+      python_requires=">=3.3",
+      packages=['uif_server'],
+      license="MIT",
+      install_requires=['requests', 'aiohttp', 'caddy-runtime'])
