43088d276cddfca66c750e209dda792dc35008b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:32 +0000

    Add package baas-web/1.0.8/baas_web-1.0.8.tar.gz

---
+++ packages/baas-web/baas_web-1.0.8.tar.gz/baas_web-1.0.8/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import setuptools
+
+setup(
+    name='baas_web',  # 包的名字
+    author='perilong',  # 作者
+    version='1.0.8',  # 版本号
+    #license='MIT',
+
+    description='baas web project',  # 描述
+    author_email='yeqianlong@mrray.cn',  # 你的邮箱**
+    packages=["baasWeb"],
+
+    # 依赖包
+    install_requires=[
+        'seleniumwebui >= 1.0.5',
+    ],
+
+)
