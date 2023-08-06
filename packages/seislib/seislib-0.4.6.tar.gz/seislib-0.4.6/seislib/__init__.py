e847ac444a24f691019c9703e6745a376ea47325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:50 +0000

    Add package seislib/0.4.6/seislib-0.4.6.tar.gz

---
+++ packages/seislib/seislib-0.4.6.tar.gz/seislib-0.4.6/seislib/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Fabrizio Magrini
+@email: fabrizio.magrini@uniroma3.it
+@email2: fabrizio.magrini90@gmail.com
+
+seispy
+=============================================================================
+"""
+
+
+from __future__ import print_function
+import sys
+import os
+from .__version__ import __version__
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'clib'))
+from .tomography.grid import EqualAreaGrid
+from . import an
+from . import colormaps
+from . import eq
+from . import exceptions
+from . import plotting
+from . import tomography
+from . import utils
+#from .tomography.tomography import SeismicTomography
+
+
+
