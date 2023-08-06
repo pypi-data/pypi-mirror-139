7db8c0d5fb2d196d823f123c1363f8c39d84b688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:15 +0000

    Add package sublib/1.1.1/sublib-1.1.1.tar.gz

---
+++ packages/sublib/sublib-1.1.1.tar.gz/sublib-1.1.1/README.md
@@ -0,0 +1,124 @@
+<h1 align="center">Sublib</h1>
+
+<p align="center">Python library for easier management and processing of subtitle files.</p>
+
+<p  align="center">
+	<a style="text-decoration:none" href="https://github.com/TheFifthLeaf/sublib/releases">
+		<img src="https://img.shields.io/github/v/release/TheFifthLeaf/sublib?color=3C7DD9" alt="Releases">
+	</a>
+	<a style="text-decoration:none" href="https://www.python.org/downloads/">
+		<img src="https://img.shields.io/badge/python-3.6%2B-3C7DD9" alt="Python Version">
+	</a>
+	<a style="text-decoration:none" href="https://choosealicense.com/licenses/gpl-3.0/">
+		<img src="https://img.shields.io/badge/license-GPL%20V3-3C7DD9" alt="License GPLv3">
+	</a>
+	<a href="https://www.codefactor.io/repository/github/thefifthleaf/sublib">
+		<img src="https://img.shields.io/codefactor/grade/github/TheFifthLeaf/sublib/main?color=3C7DD9" alt="CodeFactor" />
+	</a>
+</p>
+
+## Installation
+
+Currently, Sublib supports releases of Python 3.6 onwards. To install the current release:
+```bash
+pip install sublib
+```
+..or you can just download package files via [GitHub](https://github.com/TheFifthLeaf/sublib/archive/refs/tags/v1.1.1.zip).
+
+## Testing
+
+Sublib uses unittest tests. Tests can be run with:
+```bash
+python -m unittest discover -s tests
+```
+
+## Getting Started
+
+To use the module you need to import it first:
+```python
+import sublib
+```
+
+Detection of the subtitle format:
+```python
+# If the format is unknown it will return 'undefined'
+sub_format = sublib.detect("subtitle.srt", "utf-8")
+```
+
+Creation of the subtitle object:
+```python
+# You can choose from: MPlayer2, SubRip, MicroDVD, TMPlayer
+subtitle = sublib.SubRip("subtitle.srt", "utf-8")
+```
+
+Each subtitle object has two methods:
+```python
+# Returns a list of lines in a universal format
+# [[datetime.timedelta(...), datetime.timedelta(...), 'Line 01|Line 02], ...]
+general = subtitle.get_general_format()
+```
+```python
+# Formats lines and adds them to an existing object
+empty_subtitle = sublib.MPlayer2()
+empty_subtitle.set_from_general_format(general)
+```
+
+..and several attributes:
+```python
+subtitle.path		# The file path you used to create the object
+subtitle.encoding 	# The encoding you used to create the object
+subtitle.content 	# The contents of the file as a list of lines
+subtitle.format 	# The regex format of a specific type of subtitle
+```
+
+Boolean conversion:
+```python
+# Empty object will return False
+print(bool(subtitle))
+# Object with content will return True
+subtitle.set_from_general_format(general)
+if subtitle:
+	print(subtitle.content)
+```
+
+Object content comparison:
+```python
+# The contents of the 'content' attributes of each object are compared
+if subtitle_1 != subtitle_2:
+	subtitle_2.content = subtitle_1.content
+```
+
+Return the number of lines in the file:
+```python
+# In all formats except SubRip, this is the number of lines that will be displayed
+print(len(subtitle))
+```
+
+Presence of a string in the subtitles may be check with 'in' statement:
+```python
+# The individual lines are searched sequentially
+if "some text" in subtitle:
+	return "Yes"
+```
+
+Iterating over the subtitle lines:
+```python
+# The individual lines are searched sequentially
+for line in subtitle:
+	print(line)
+```
+
+## Supported formats
+
+- as **mpl** - MPlayer2 (.txt)
+- as **srt** - SubRip (.srt)
+- as **sub** - MicroDVD (.sub)
+- as **tmp** - TMPlayer (.txt)
+
+## Contributing
+
+Pull requests are welcome.
+
+## License
+
+[GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)
---
+++ packages/sublib/sublib-1.1.1.tar.gz/sublib-1.1.1/sublib/sublib.py
@@ -0,0 +1,342 @@
+import re
+import sys
+import datetime as dt
+
+
+# Functions
+
+
+def detect(path_file, encoding):
+    """A function to detect subtitle format
+    Parameters:
+        path_file (str): path to a file that may contain subtitles
+        encoding (str): The type of encoding to use when opening the file
+    Returns:
+        str: detected format or 'undefined'
+    """
+    mpl_reg = "\\[[0-9]+\\]\\[[0-9]+\\] .*\n"                                                              # [START][STOP] TEXT\n
+    srt_reg = "[0-9]+\n[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}\n*\n"   # NUM\nSTART --> STOP\n
+    sub_reg = "{[0-9]+}{[0-9]+}.*\n"                                                                       # {START}{STOP}TEXT\n
+    tmp_reg = "[0-9]+:[0-9]+:[0-9]+:.*\n"                                                                  # START:TEXT\n
+    with open(path_file, "rt", encoding=encoding, errors="ignore") as file:
+        content = file.read()
+    if len(re.findall(mpl_reg, content)) > 0:
+        result = "mpl"
+    elif len(re.findall(sub_reg, content)) > 0:
+        result = "sub"
+    elif len(re.findall(srt_reg, content)) > 0:
+        result = "srt"
+    elif len(re.findall(tmp_reg, content)) > 0:
+        result = "tmp"
+    else:
+        result = "undefined"
+    return result
+
+
+# Classes
+
+
+class Subtitle:
+    """A class to represent subtitle file in general. The specific format classes inherit from it.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+        content (list): The lines of the subtitle file
+    """
+
+    def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
+        self.path = path
+        self.encoding = encoding
+        if self.path is not None and \
+           self.encoding is not None:
+            try:
+                with open(path, "rt", encoding=encoding, errors="ignore") as file:
+                    self.content = [line.rstrip("\n") if line != "\n" else line
+                                    for line in file.readlines()]
+            except Exception:
+                self.content = None
+                print(sys.exc_info())
+        else:
+            self.content = None
+
+    def __str__(self):
+        """Specifies how str() is displayed: class_name("path", "encoding")"""
+        return f'{self.__class__.__name__}("{self.path}", "{self.encoding}")'
+
+    def __repr__(self):
+        """Specifies how repr() is displayed: class_name(path="path", encoding="encoding")"""
+        return f'{self.__class__.__name__}(path="{self.path}", encoding="{self.encoding}")'
+
+    def __bool__(self):
+        """Specifies what bool() should return: True if self.content is not None"""
+        return False if self.content is None else True
+
+    def __eq__(self, other):
+        """Specifies when subtitle calsses are equal: True if self.content value of both is equal"""
+        return True if self.content == other.content else False
+
+    def __len__(self):
+        """Specifies what len() should return: Number of lines in file"""
+        return len(self.content)
+
+    def __contains__(self, item):
+        """Specifies what "in" should do: Search for match in every line"""
+        for line in self.content:
+            if line.count(item) > 0:
+                return True
+
+    def __iter__(self):
+        """Specifies what iter() should do: Set line index to 0"""
+        self._i = 0
+        return self
+
+    def __next__(self):
+        """Specifies what next() should return: Line of file at a specific index"""
+        line = self.content[self._i]
+        self._i += 1
+        return line
+
+
+class MPlayer2(Subtitle):
+    """A class to represent MPlayer2 subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+        content (list): The lines of the subtitle file
+        format (str): RegEx of specified format
+    """
+
+    def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
+        super().__init__(path, encoding)
+        self.format = r"\\[[0-9]+\\]\\[[0-9]+\\] .*\n"
+
+    def get_general_format(self):
+        """Convert MPlayer2 formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
+        lines = [line.split("]", 2) for line in (line.rstrip("\n") for line in self.content)]
+        for line in lines:
+            line[0] = dt.timedelta(
+                seconds=round(float(line[0].replace("[", "")) / 10.0, 1)    # MPL use as time: sec * 10
+            )
+            line[1] = dt.timedelta(
+                seconds=round(float(line[1].replace("[", "")) / 10.0, 1)
+            )
+            line[2] = line[2].lstrip()
+        return lines
+
+    def set_from_general_format(self, lines):
+        """Convert general list to MPlayer2 foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
+        for line in lines:
+            line[0] = round(line[0].total_seconds() * 10)    # MPL use as time: sec * 10
+            line[1] = round(line[1].total_seconds() * 10)
+        self.content = [f"[{line[0]}][{line[1]}] {line[2]}" for line in lines]
+
+
+class SubRip(Subtitle):
+    """A class to represent SubRip subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+        content (list): The lines of the subtitle file
+        format (str): RegEx of specified format
+    """
+
+    def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
+        super().__init__(path, encoding)
+        self.format = r"[0-9]+\n[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}\n*\n"
+
+    def get_general_format(self):
+        """Convert SubRip formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
+        lines, temp = [], []
+        for line in self.content:
+            if line != "\n":
+                temp.append(line)
+            else:
+                lines.append(temp)
+                temp = []
+        lines = [[*line[1].split(" --> "), "|".join(line[2:])] for line in lines]
+        for line in lines:
+            line[0] = dt.datetime.strptime(line[0], "%H:%M:%S,%f")
+            line[1] = dt.datetime.strptime(line[1], "%H:%M:%S,%f")
+            line[0] = dt.timedelta(
+                hours=line[0].hour,
+                minutes=line[0].minute,
+                seconds=line[0].second,
+                microseconds=line[0].microsecond
+            )
+            line[1] = dt.timedelta(
+                hours=line[1].hour,
+                minutes=line[1].minute,
+                seconds=line[1].second,
+                microseconds=line[1].microsecond
+            )
+            for style in re.findall(r"</.*>", line[2]):
+                line[2] = line[2].replace(style, "")
+            for style in re.findall(r"<.*>", line[2]):
+                line[2] = line[2].replace(style, "")
+        return lines
+
+    def set_from_general_format(self, lines):
+        """Convert general list to SubRip foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
+        for line in lines:
+            line[0] = str(line[0])
+            line[1] = str(line[1])
+            if len(line[0]) == 7:
+                line[0] = line[0] + "." + "".zfill(6)    # If no microsecons, fill with zeros
+            if len(line[1]) == 7:
+                line[1] = line[1] + "." + "".zfill(6)
+            line[0] = dt.datetime.strptime(line[0], "%H:%M:%S.%f")
+            line[1] = dt.datetime.strptime(line[1], "%H:%M:%S.%f")
+            line[0] = line[0].strftime("%H:%M:%S.%f").replace(".", ",")
+            line[1] = line[1].strftime("%H:%M:%S.%f").replace(".", ",")
+            line[0] = line[0][:len(line[0]) - 3]
+            line[1] = line[1][:len(line[1]) - 3]
+            line[2] = line[2].replace("|", "\n")
+        self.content = [f"{num}\n{line[0]} --> {line[1]}\n{line[2]}\n\n"
+                        for num, line in enumerate(lines, 1)]
+
+
+class MicroDVD(Subtitle):
+    """A class to represent MicroDVD subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+        content (list): The lines of the subtitle file
+        format (str): RegEx of specified format
+    """
+
+    def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
+        super().__init__(path, encoding)
+        self.format = r"{[0-9]+}{[0-9]+}.*\n"
+
+    def get_general_format(self):
+        """Convert MicroDVD formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
+        lines = [line.split("}", 2) for line in (line.rstrip("\n") for line in self.content)]
+        for line in lines:
+            line[0] = dt.timedelta(
+                seconds=round(float(line[0].replace("{", "")) / 23.976, 3)    # SUB use frames as time
+            )
+            line[1] = dt.timedelta(
+                seconds=round(float(line[1].replace("{", "")) / 23.976, 3)
+            )
+            for style in re.findall(r"{.*}", line[2]):
+                line[2] = line[2].replace(style, "")
+        return lines
+
+    def set_from_general_format(self, lines):
+        """Convert general list to MicroDVD foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
+        for line in lines:
+            line[0] = round(line[0].total_seconds() * 23.976)    # SUB use frames as time
+            line[1] = round(line[1].total_seconds() * 23.976)
+        self.content = [f"{{{line[0]}}}{{{line[1]}}}{line[2]}" for line in lines]
+
+
+class TMPlayer(Subtitle):
+    """A class to represent TMPlayer subtitle file.
+    Attributes:
+        path (str): Absolute or relative path to subtitle file that will be read
+        encoding (str): Valid encoding that will be use to open a subtitle file
+        content (list): The lines of the subtitle file
+        format (str): RegEx of specified format
+    """
+
+    def __init__(self, path=None, encoding=None):
+        """Construct classes attributes.
+        Parameters:
+            path (str): Absolute or relative path to subtitle file that will be read
+            encoding (str): Valid encoding that will be use to open a subtitle file
+        """
+        super().__init__(path, encoding)
+        self.format = r"[0-9]+:[0-9]+:[0-9]+:.*\n"
+
+    def get_general_format(self):
+        """Convert TMPlayer formatted lines to general list
+        Parameters:
+            None
+        Returns:
+            list: lines one by one in general format
+        """
+        lines = [line.split(":", 3) for line in (line.rstrip("\n") for line in self.content)]
+        lines = [[f"{line[0]}:{line[1]}:{line[2]}", f"{line[0]}:{line[1]}:{line[2]}", line[3]]
+                 for line in lines]
+        for line in lines:
+            line[0] = dt.datetime.strptime(line[0], "%H:%M:%S")
+            line[1] = dt.datetime.strptime(line[1], "%H:%M:%S")
+            line[0] = dt.timedelta(
+                hours=line[0].hour,
+                minutes=line[0].minute,
+                seconds=line[0].second,
+                microseconds=line[0].microsecond
+            )
+            line[1] = dt.timedelta(
+                hours=line[1].hour,
+                minutes=line[1].minute,
+                seconds=line[1].second + 1,
+                microseconds=line[1].microsecond
+            )
+        return lines
+
+    def set_from_general_format(self, lines):
+        """Convert general list to TMPlayer foramt
+        Parameters:
+            lines (list): Lines in general formatt that will be converted
+        Action:
+            It sets self.content from provided list of lines.
+        """
+        for line in lines:
+            line[0] = str(line[0])
+            if len(line[0]) == 7:
+                line[0] = line[0] + "." + "".zfill(6)    # If no microsecons, fill with zeros
+            line[0] = dt.datetime.strptime(line[0], "%H:%M:%S.%f")
+            line[0] = line[0].strftime("%H:%M:%S.%f")
+            line[0] = line[0][:len(line[0]) - 7]
+        self.content = [f"{line[0]}:{line[2]}" for line in lines]
