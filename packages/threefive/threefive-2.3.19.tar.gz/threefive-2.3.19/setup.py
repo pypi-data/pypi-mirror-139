159d34bfc763f4070a5ac992a200a9a1645fcea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:05 +0000

    Add package threefive/2.3.19/threefive-2.3.19.tar.gz

---
+++ packages/threefive/threefive-2.3.19.tar.gz/threefive-2.3.19/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import setuptools
+import threefive
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="threefive",
+    version=threefive.version(),
+    author="Adrian",
+    author_email="spam@so.slo.me",
+    description="The World Wide Leader in SCTE35",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/futzu/threefive",
+    extra_requires=[
+        "crcmod",
+        "pyaes",
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    python_requires=">=3.6",
+)
