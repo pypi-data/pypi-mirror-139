d72ab83edf99a2872c8f2a35b0096ff236a91519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:09 +0000

    Add package pybricks/2.0.0.post2/pybricks-2.0.0.post2.tar.gz

---
+++ packages/pybricks/pybricks-2.0.0.post2.tar.gz/pybricks-2.0.0.post2/pybricks/hubs.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2018-2020 The Pybricks Authors
+
+"""LEGO® Programmable Hubs."""
+from ._common import (Speaker as _Speaker, Battery as _Battery,
+                      ColorLight as _ColorLight, KeyPad as _KeyPad)
+from .media.ev3dev import Image as _Image
+
+
+class EV3Brick:
+    """LEGO® MINDSTORMS® EV3 Brick."""
+    screen = _Image('_screen_')
+    speaker = _Speaker()
+    battery = _Battery()
+    light = _ColorLight()
+    buttons = _KeyPad()
