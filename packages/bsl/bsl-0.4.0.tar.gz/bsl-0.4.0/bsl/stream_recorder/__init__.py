1df220535efb20d7cdf1268e97df84b6ce30f72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:27 +0000

    Add package bsl/0.4.0/bsl-0.4.0.tar.gz

---
+++ packages/bsl/bsl-0.4.0.tar.gz/bsl-0.4.0/bsl/stream_recorder/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module for signal recording.
+
+Record signals into fif format, a standard format mainly used in MNE EEG
+analysis library.
+"""
+
+from .stream_recorder import StreamRecorder  # noqa: F401
+
+
+__all__ = ['StreamRecorder']
---
+++ packages/bsl/bsl-0.4.0.tar.gz/bsl-0.4.0/bsl/triggers/tests/test_mock.py
@@ -0,0 +1,23 @@
+import pytest
+
+from bsl import logger, set_log_level
+from bsl.triggers import MockTrigger
+
+
+set_log_level('INFO')
+logger.propagate = True
+
+
+def test_trigger_mock(caplog):
+    """Testing for Mock triggers."""
+    trigger = MockTrigger(verbose=True)
+    assert trigger.verbose
+    trigger.verbose = False
+    assert trigger.signal(1)
+    assert 'MOCK trigger set to 1' in caplog.text
+    assert trigger.signal(2)
+    assert 'MOCK trigger set to 2' in caplog.text
+
+    with pytest.raises(TypeError, match="'value' must be an instance"):
+        trigger.signal(3.0)
+    assert 'MOCK trigger set to 3.0' not in caplog.text
