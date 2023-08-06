f6bfbe61e862e92a93ceda0ce72617619d7eef18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:14 +0000

    Add package django-connexion/0.0.1/django_connexion-0.0.1.tar.gz

---
+++ packages/django-connexion/django_connexion-0.0.1.tar.gz/django_connexion-0.0.1/django_connexion/tests/test_basic.py
@@ -0,0 +1,18 @@
+def test_settings(settings):
+    assert settings.INSTALLED_APPS == ['django_connexion.tests.testapp']
+
+
+def test_json_spec(client):
+    response = client.get('/helloworld/openapi.json')
+    content = response.json()
+    assert content['info']['title'] == 'Hello World'
+
+
+def test_yaml_spec(client):
+    response = client.get('/helloworld/openapi.yaml')
+    assert b'title: Hello World' in response.content
+
+
+def test_endpoint(client):
+    response = client.post('/helloworld/greeting/ze')
+    assert b'Hello ze' in response.content
