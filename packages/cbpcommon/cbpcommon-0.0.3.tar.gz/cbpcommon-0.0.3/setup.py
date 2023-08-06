0264fd56fab0ebf5e2b7005ef50b27a0e3b84dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:11 +0000

    Add package cbpcommon/0.0.3/cbpcommon-0.0.3.tar.gz

---
+++ packages/cbpcommon/cbpcommon-0.0.3.tar.gz/cbpcommon-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='cbpcommon',
+    version='0.0.3',
+    url='https://github.com/Crypto-Bot-Platform/cbpcommon',
+    license='MIT',
+    author='Boris Tsekinovsky',
+    author_email='t.boris@gmail.com',
+    description='Common library for Crypto Bot Platform',
+    packages=['.'],
+    include_package_data=True,
+    install_requires=["avro==1.11.0", "confluent_kafka==1.8.2", "setuptools~=60.5.0", "elasticsearch~=7.16.3", "jsonpickle"],
+)
