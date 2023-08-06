836330d65bacc9897aa6ca9e98731f1128423ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:37 +0000

    Add package imio.smartweb.core/1.0a42/imio.smartweb.core-1.0a42.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.0a42.tar.gz/imio.smartweb.core-1.0a42/src/imio/smartweb/core/tests/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+
+def get_sections_types():
+    return [
+        "imio.smartweb.SectionContact",
+        # "imio.smartweb.SectionEvents",
+        "imio.smartweb.SectionFiles",
+        "imio.smartweb.SectionGallery",
+        "imio.smartweb.SectionHTML",
+        "imio.smartweb.SectionLinks",
+        "imio.smartweb.SectionMap",
+        # "imio.smartweb.SectionNews",
+        "imio.smartweb.SectionSelections",
+        # "imio.smartweb.SectionSendinblue",
+        "imio.smartweb.SectionText",
+        "imio.smartweb.SectionVideo",
+    ]
+
+
+def get_json(json_filename):
+    with open(
+        os.path.join(
+            os.path.dirname(__file__),
+            json_filename,
+        ),
+    ) as json_file:
+        json_procedures_raw_mock = json.load(json_file)
+        return json_procedures_raw_mock
+
+
+def get_leadimage_filename():
+    leadimage = os.path.join(
+        os.path.dirname(__file__),
+        "resources/plone.png",
+    )
+    return leadimage
