ea0dd91be1c8b131c10cc6d39a02d46487c881ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package templateco/0.1.5/templateco-0.1.5.tar.gz

---
+++ packages/templateco/templateco-0.1.5.tar.gz/templateco-0.1.5/templateco.py
@@ -0,0 +1,141 @@
+import logging
+from pathlib import Path
+from typing import Any, Dict, List
+
+from colorama import Fore, Style, init
+from questionary import Choice, checkbox, unsafe_prompt
+
+from templateco.exceptions import NoPluginsFoundException, PluginOperationException
+from templateco.output import generate_output
+from templateco.plugin import TemplatecoPlugin, list_compatible_plugins
+
+logger = logging.getLogger(__name__)
+
+
+def _execute_plugin(
+    language: str, folder: Path, plugin: TemplatecoPlugin, config: Dict[str, Any]
+) -> None:
+    """
+    Execute a single Templateco plugin.
+
+    This function prompts for the plugin's questions, runs the hooks and generates
+    the template files.
+
+    :param language:
+    :param folder: Folder to output to.
+    :param plugin: Plugin to execute.
+    :param config: Extra configuration passed to Templateco.
+    :return: None.
+    """
+    logger.debug(f"[{plugin.friendly_name}] Running plugin.")
+    try:
+        top_level_folder_name = folder.name
+        logger.debug(f"[{plugin.friendly_name}] Prompting for interaction.")
+
+        data = unsafe_prompt(
+            plugin.get_questions(
+                language=language, folder=top_level_folder_name, config=config
+            )
+        )
+
+        if plugin.pre_template_hook:
+            logger.debug(f"[{plugin.friendly_name}] Running pre-template hook.")
+            plugin.pre_template_hook(
+                language=language,
+                folder=top_level_folder_name,
+                config=config,
+                data=data,
+            )
+
+        logger.debug(f"[{plugin.friendly_name}] Determining template from answers.")
+        folders = plugin.folders_to_template(
+            language=language, folder=top_level_folder_name, config=config, data=data
+        )
+
+        logger.debug(f"[{plugin.friendly_name}] Templating folder structure.")
+        generate_output(plugin.path_to_module, folder, folders, data)
+        logger.debug(f"[{plugin.friendly_name}] Successfully templated.")
+
+        if plugin.post_template_hook:
+            logger.debug(f"[{plugin.friendly_name}] Running post-template hook.")
+            plugin.post_template_hook(
+                language=language,
+                folder=top_level_folder_name,
+                output_folder=folder,
+                config=config,
+                data=data,
+            )
+    except PluginOperationException:
+        logger.error(f"[{plugin.friendly_name}] Plugin raised an operation exception:")
+        logger.debug(
+            f"[{plugin.friendly_name}] These exceptions should be raised to the user."
+        )
+        raise
+    except Exception as err:
+        logger.error(f"[{plugin.friendly_name}] Plugin raised an unhandled exception.")
+        logger.error(err)
+
+
+def templateco(
+    *,
+    language: str,
+    folder: Path,
+    config: Dict[str, Any] = {},
+    namespaces: List[str] = ["templateco"],
+) -> None:
+    """
+    Execute Templateco, and produce a templated folder.
+
+    Supports only kwargs, so invoke with language="foo" rather than positional
+    arguments.
+
+    :param language: Name of template to load plugins for.
+    :param folder: Folder to generate e.g. $(PWD)/output_folder.
+    :param config: Extra configuration passed to Templateco.
+    :param namespaces: List of Templateco plugin namespaces to accept.
+    :return: None.
+    """
+    # Initialise Colorama for terminal output.
+    init(autoreset=True)
+    print(
+        Fore.GREEN
+        + Style.BRIGHT
+        + "[ Templateco ]"
+        + Style.RESET_ALL
+        + " - A Templating Ecosystem"
+    )
+
+    logger.debug("Instantiating Templateco.")
+
+    # Get a complete list of all compatible Templateco plugins.
+    plugins = [
+        plugin
+        for plugin in list_compatible_plugins(namespaces)
+        if language in plugin.compatible_languages
+    ]
+
+    logger.debug(f"Found {len(plugins)} plugin(s).")
+
+    if len(plugins) == 0:
+        logger.debug("Query resulted in zero plugins, raising NoPlugins exception.")
+        raise NoPluginsFoundException()
+    elif len(plugins) > 1:
+        logger.debug("Multiple plugins found, asking user for choices.")
+        # Ask the user which plugins should be executed.
+        plugins = checkbox(
+            "Select plugins to use",
+            choices=[
+                Choice(
+                    plugin.friendly_name,
+                    checked=plugin.selected_by_default,
+                    value=plugin,
+                )
+                for plugin in plugins
+            ],
+        ).unsafe_ask()
+
+    for plugin in plugins:
+        print("\nConfiguring " + Fore.GREEN + Style.BRIGHT + f"{plugin.friendly_name}")
+        _execute_plugin(language, folder, plugin, config)
+
+    print("\nSuccessfully templated " + Style.BRIGHT + Fore.GREEN + str(folder))
---
+++ packages/templateco/templateco-0.1.5.tar.gz/templateco-0.1.5/typing.py
@@ -0,0 +1,142 @@
+from pathlib import Path
+from typing import Any, Dict, Iterable, List, Mapping
+
+from typing_extensions import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class GetQuestions(Protocol):
+    """
+    The get_questions typing definition.
+    """
+
+    def __call__(
+        self, *, language: str, folder: str, config: Dict[str, Any]
+    ) -> Iterable[Mapping[str, Any]]:
+        """
+        Return a Questionary dictionary-type iterable.
+
+        :param language: Name of the requested template.
+        :param folder: Name of the top-level folder.
+        :param config: Extra configuration passed to Templateco.
+        :return: Dictionary of data passed to the template.
+        """
+        pass
+
+
+@runtime_checkable
+class FoldersToTemplate(Protocol):
+    """
+    The folders_to_template typing definition.
+    """
+
+    def __call__(
+        self,
+        *,
+        language: str,
+        folder: str,
+        config: Dict[str, Any],
+        data: Dict[str, Any]
+    ) -> List[str]:
+        """
+        Take all input and returns a list of folders within the plugin.
+
+        These folders are used to specify which should be templated, and intended to
+        support optional configuration.
+
+        :param language: Name of the requested template.
+        :param folder: Name of the top-level folder.
+        :param config: Extra configuration passed to Templateco.
+        :param data: Dictionary of data passed to the template.
+        :return: List of folders to template.
+        """
+        pass
+
+
+@runtime_checkable
+class PreTemplateHook(Protocol):
+    """
+    The pre_template_hook typing definition.
+    """
+
+    def __call__(
+        self,
+        *,
+        language: str,
+        folder: str,
+        config: Dict[str, Any],
+        data: Dict[str, Any]
+    ) -> None:
+        """
+        Run before a plugin creates a template.
+
+        This optional function is intended to be used to modify the data dictionary
+        that's passed to Jinja2 for the templating.
+
+        :param language: Name of the requested template.
+        :param folder: Name of the top-level folder.
+        :param config: Extra configuration passed to Templateco.
+        :param data: Dictionary of data passed to the template.
+        :return: Nothing.
+        """
+        pass
+
+
+@runtime_checkable
+class PostTemplateHook(Protocol):
+    """
+    The post_template_hook typing definition.
+    """
+
+    def __call__(
+        self,
+        *,
+        language: str,
+        folder: str,
+        output_folder: Path,
+        config: Dict[str, Any],
+        data: Dict[str, Any]
+    ) -> None:
+        """
+        Run after a plugin has successfully templated.
+
+        This optional function is intended to be used to run steps after the templating
+        has been run, and passes in the path to the final directory. This can be used
+        to run CLI commands, or alter the folder data if wished.
+
+        :param language: Name of the requested template.
+        :param folder: Name of the top-level folder.
+        :param output_folder: Path to the generated folder.
+        :param config: Extra configuration passed to Templateco.
+        :param data: Dictionary of data passed to the template.
+        :return: Nothing.
+        """
+        pass
+
+
+def get_questions_decorator(func: GetQuestions) -> GetQuestions:
+    """
+    Decorate the get_questions plugin function for type checking.
+    """
+    return func
+
+
+def folders_to_template_decorator(func: FoldersToTemplate) -> FoldersToTemplate:
+    """
+    Decorate the folders_to_template plugin function for type checking.
+    """
+    return func
+
+
+def pre_template_hook_decorator(func: PreTemplateHook) -> PreTemplateHook:
+    """
+    Decorate the pre_template_hook plugin function for type checking.
+    """
+    return func
+
+
+def post_template_hook_decorator(func: PostTemplateHook) -> PostTemplateHook:
+    """
+    Decorate the post_template_hook plugin function for type checking.
+    """
+    return func
