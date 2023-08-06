4744c7c5932442157a18cf3428c0514da2cbddce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:25 +0000

    Add package Choam/0.1.20/Choam-0.1.20.tar.gz

---
+++ packages/Choam/Choam-0.1.20.tar.gz/Choam-0.1.20/setup.py
@@ -0,0 +1,26 @@
+from importlib_metadata import entry_points
+from setuptools import setup, find_packages
+import pathlib
+
+directory = pathlib.Path(__file__).parent
+long_description = (directory / "README.md").read_text()
+
+setup(
+    name="Choam",
+	version="0.1.20",
+    description="Python project scaffolder/manager",
+    packages=["choam"],
+    keywords=['package', 'manager'],
+	install_requires=['fire', 'toml', 'twine', 'wheel', 'choam', 'typing', 'autoapi', 'findimports'],
+    project_urls={
+        'Source': 'https://github.com/cowboycodr/choam'
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "choam = choam:choam"
+        ]
+    }
+)
