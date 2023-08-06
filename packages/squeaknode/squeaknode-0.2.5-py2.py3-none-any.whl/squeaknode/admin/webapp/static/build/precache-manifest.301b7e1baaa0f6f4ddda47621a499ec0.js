732f5d985ff8c889ce005fe8b75a54d33631c30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:36 +0000

    Add package squeaknode/0.2.5/squeaknode-0.2.5-py2.py3-none-any.whl

---
+++ packages/squeaknode/squeaknode-0.2.5-py2.py3-none-any.whl/squeaknode/admin/webapp/static/build/precache-manifest.301b7e1baaa0f6f4ddda47621a499ec0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2a91224ab7baba390f73d018030594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5f6a7e6d0590628b0ce",
+    "url": "/static/css/3.1f6bb621.chunk.css"
+  },
+  {
+    "revision": "f11a5bdfebea5d14e77a",
+    "url": "/static/css/4.6512f3a3.chunk.css"
+  },
+  {
+    "revision": "e6c0845bcb9aa532caa6",
+    "url": "/static/css/5.3aaab79d.chunk.css"
+  },
+  {
+    "revision": "525fd53240f918e2a1c3",
+    "url": "/static/css/main.f60a9de3.chunk.css"
+  },
+  {
+    "revision": "30a64ed0c4f769678322",
+    "url": "/static/js/2.b3688b8c.chunk.js"
+  },
+  {
+    "revision": "a569361d31625f49e7e30aea21628ed5",
+    "url": "/static/js/2.b3688b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f6a7e6d0590628b0ce",
+    "url": "/static/js/3.686cdeb7.chunk.js"
+  },
+  {
+    "revision": "f11a5bdfebea5d14e77a",
+    "url": "/static/js/4.04dd649a.chunk.js"
+  },
+  {
+    "revision": "e6c0845bcb9aa532caa6",
+    "url": "/static/js/5.6b67b257.chunk.js"
+  },
+  {
+    "revision": "525fd53240f918e2a1c3",
+    "url": "/static/js/main.919826e6.chunk.js"
+  },
+  {
+    "revision": "0e3f27bd22910e4b49e8",
+    "url": "/static/js/runtime-main.9240de99.js"
+  },
+  {
+    "revision": "a0c2d343127a93b025409da6fd970700",
+    "url": "/static/media/icon.a0c2d343.svg"
+  }
+]);
