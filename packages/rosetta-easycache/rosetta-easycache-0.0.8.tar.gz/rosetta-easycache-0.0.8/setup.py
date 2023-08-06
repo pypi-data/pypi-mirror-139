f7965cb4481ae1b8fef55d3efa250c3a6c85f500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:21 +0000

    Add package rosetta-easycache/0.0.8/rosetta-easycache-0.0.8.tar.gz

---
+++ packages/rosetta-easycache/rosetta-easycache-0.0.8.tar.gz/rosetta-easycache-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rosetta-easycache",# Replace with your own username
+    version="0.0.8",
+    author="Zhiyuan Zhang",
+    author_email="zhangzhiyuan1@joyy.com",
+    description="一个快速使用异步redis优化数据库查询项目",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.duowan.com/ai/nlp/easy-cache",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['ujson', 'bson', 'loguru'],
+    python_requires='>=3.7',
+)
