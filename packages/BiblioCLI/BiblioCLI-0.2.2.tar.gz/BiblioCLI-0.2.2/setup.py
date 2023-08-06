8859928bfe01fa519dc85845cecfbb38297c5403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package BiblioCLI/0.2.2/BiblioCLI-0.2.2.tar.gz

---
+++ packages/BiblioCLI/BiblioCLI-0.2.2.tar.gz/BiblioCLI-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from importlib.metadata import entry_points
+
+from setuptools import setup
+
+setup(
+    name="BiblioCLI",
+    version="0.2.2",
+    description="A command line interface for capturing and storing information about books you want to read",
+    url="https://github.com/KeaganStokoe/biblioCLI",
+    author="Keagan Stokoe",
+    author_email="keagan.stokoe@gmail.com",
+    py_modules=["bibliocli"],
+    install_requires=["Click", "requests", "rich"],
+    entry_points={"console_scripts": "bib=bibliocli:bibliocli"},
+)
