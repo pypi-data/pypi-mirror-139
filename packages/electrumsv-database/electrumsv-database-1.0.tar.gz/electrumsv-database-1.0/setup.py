ea4706ed2ab01e3ff28f53d6cf38439d284ef613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:05 +0000

    Add package electrumsv-database/1.0/electrumsv-database-1.0.tar.gz

---
+++ packages/electrumsv-database/electrumsv-database-1.0.tar.gz/electrumsv-database-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='electrumsv-database',
+      version='1.00',
+      description='Database support for use of SQLite (possibly other databases later).',
+      author='Roger Taylor',
+      author_email='roger.taylor.email@gmail.com',
+      url='https://github.com/electrumsv/electrumsv-database',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      license='MIT license',
+      package_data={ "electrumsv_database": ["py.typed"] },
+      install_requires=[
+          'pysqlite3; platform_system!="Windows"'
+      ])
