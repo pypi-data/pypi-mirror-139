f5abe581e5476139df99e7837b7c3a361d0fa853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:59 +0000

    Add package stout/0.0.3/stout-0.0.3.tar.gz

---
+++ packages/stout/stout-0.0.3.tar.gz/stout-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setup(
+    name='stout',
+    version='0.0.3',
+    author='suckerpunched',
+    author_email='',
+    description='',
+    long_description='',#long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/suckerpunched/stout',
+    project_urls = {
+        "Bug Tracker": "https://github.com/suckerpunched/stout/issues"
+    },
+    license='MIT',
+    packages=['stout'],
+    install_requires=[],
+)
+
