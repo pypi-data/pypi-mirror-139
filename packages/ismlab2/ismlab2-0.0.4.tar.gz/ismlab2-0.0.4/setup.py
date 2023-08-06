f955218eb25bee569cdd9693c7b088baca6795f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:51 +0000

    Add package ismlab2/0.0.4/ismlab2-0.0.4.tar.gz

---
+++ packages/ismlab2/ismlab2-0.0.4.tar.gz/ismlab2-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='ismlab2',
+    version='0.0.4',    
+    description='A example Python package',
+    url='https://github.com/isba06',
+    author='Ismail Bayramov',
+    author_email='example@mail.ru',
+    license='BSD 2-clause',
+    packages=['ismlab2'],
+    install_requires=['scipy',
+                      'matplotlib <=3.0',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
