35949bd0cb4dc1552020f7f7f24a6d410d456e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:10 +0000

    Add package DirTreeGenerator/1.0/DirTreeGenerator-1.0.tar.gz

---
+++ packages/DirTreeGenerator/DirTreeGenerator-1.0.tar.gz/DirTreeGenerator-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='DirTreeGenerator',
+      version='1.0',
+      description='This Module can generate tree of files in directory.',
+      packages=['treeGen'],
+      author_email='max.taran.210@mail.ru',
+      zip_safe=False)
