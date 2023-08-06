5db3bd446b10143a802d836708a41796c55b279e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:05 +0000

    Add package nesterdeeas/0.0.1/nesterdeeas-0.0.1.tar.gz

---
+++ packages/nesterdeeas/nesterdeeas-0.0.1.tar.gz/nesterdeeas-0.0.1/src/nesterdeeas/nesterdeeas.py
@@ -0,0 +1,10 @@
+'''This is the "nesterdeeas.py" module and it provides one function called print_lol() which prints lists that may or may not include lists'''
+
+
+def print_lol(the_list):
+    '''This function takes a positional argument called "thi_list", which is any Python list(of, possibly, nested lists). Each data item in the provided list is (recursively) printed on the screen on its own line'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
