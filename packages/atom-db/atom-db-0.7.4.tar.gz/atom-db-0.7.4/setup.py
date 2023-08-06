cd6a6b94ce19ba2057eb86a65521c276bc67b34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:37 +0000

    Add package atom-db/0.7.4/atom-db-0.7.4.tar.gz

---
+++ packages/atom-db/atom-db-0.7.4.tar.gz/atom-db-0.7.4/setup.py
@@ -0,0 +1,35 @@
+"""
+Copyright (c) 2019-2021, Jairus Martin.
+
+Distributed under the terms of the MIT License.
+
+The full license is in the file LICENSE.text, distributed with this software.
+
+Created on Feb 21, 2019
+
+@author: jrm
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="atom-db",
+    version="0.7.4",
+    author="CodeLV",
+    author_email="frmdstryr@gmail.com",
+    url="https://github.com/codelv/atom-db",
+    description="Database abstraction layer for atom objects",
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    requires=["atom"],
+    python_requires=">=3.7",
+    install_requires=["atom>=0.7.0", "bytecode"],
+    optional_requires=[
+        "sqlalchemy",
+        "aiomysql",
+        "aiopg",
+        "aiosqlite",  # sql database support
+        "motor",  # nosql database support
+    ],
+    packages=find_packages(),
+)
