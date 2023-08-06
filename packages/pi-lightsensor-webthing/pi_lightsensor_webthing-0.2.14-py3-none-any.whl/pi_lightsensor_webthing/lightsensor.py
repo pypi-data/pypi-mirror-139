6fe43b87a9925fd9c3b1ab5a0ba1077871547358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package pi-lightsensor-webthing/0.2.14/pi_lightsensor_webthing-0.2.14-py3-none-any.whl

---
+++ packages/pi-lightsensor-webthing/pi_lightsensor_webthing-0.2.14-py3-none-any.whl/pi_lightsensor_webthing/lightsensor.py
@@ -0,0 +1,34 @@
+import logging
+import board
+import adafruit_bh1750
+from threading import Thread
+from time import sleep
+
+
+class LightSensor:
+
+    def __init__(self):
+        self.measures = list()
+        i2c = board.I2C()
+        self.sensor = adafruit_bh1750.BH1750(i2c)
+        logging.info("light sensor connected")
+
+    def listen(self, listener):
+        Thread(target=self.__listen, args=(listener,), daemon=True).start()
+
+    def __listen(self, listener):
+        loop = 0
+        while True:
+            try:
+                self.measures.append(self.sensor.lux)
+                while len(self.measures) > 7:
+                    self.measures.pop(0)
+                loop +=1
+                if loop > 3:
+                    loop = 0
+                    sorted_measures = sorted(self.measures)
+                    median = sorted_measures[int(len(sorted_measures) * 0.5)]
+                    listener(int(median))
+            except Exception as e:
+                print("error occurred", e)
+            sleep(1)
