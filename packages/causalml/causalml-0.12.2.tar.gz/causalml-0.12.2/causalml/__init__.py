9ca15bb2b32b7ed4bbbdbcc11cc9bf4da8d1b018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:31 +0000

    Add package causalml/0.12.2/causalml-0.12.2.tar.gz

---
+++ packages/causalml/causalml-0.12.2.tar.gz/causalml-0.12.2/causalml/__init__.py
@@ -0,0 +1,12 @@
+name = "causalml"
+__version__ = "0.12.2"
+__all__ = [
+    "dataset",
+    "features",
+    "feature_selection",
+    "inference",
+    "match",
+    "metrics",
+    "optimize",
+    "propensity",
+]
