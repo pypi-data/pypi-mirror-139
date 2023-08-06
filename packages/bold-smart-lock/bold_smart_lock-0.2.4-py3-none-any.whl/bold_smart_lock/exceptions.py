b44b1818dcebb23cb84ed61d90ed32bc758b9dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:52 +0000

    Add package bold-smart-lock/0.2.4/bold_smart_lock-0.2.4-py3-none-any.whl

---
+++ packages/bold-smart-lock/bold_smart_lock-0.2.4-py3-none-any.whl/bold_smart_lock/exceptions.py
@@ -0,0 +1,35 @@
+class AuthenticateFailed(Exception):
+    """AuthenticateFailed exception for Bold."""
+
+class InvalidEmail(Exception):
+    """InvalidEmail exception for Bold."""
+
+class InvalidPhone(Exception):
+    """InvalidPhone exception for Bold."""
+
+class EmailOrPhoneNotSpecified(Exception):
+    """EmailOrPhoneNotSpecified exception for Bold."""
+
+class EmailOrPhoneNotSpecified(Exception):
+    """EmailOrPhoneNotSpecified exception for Bold."""
+
+class MissingValidationId(Exception):
+    """MissingValidationId exception for Bold."""
+
+class InvalidValidationId(Exception):
+    """InvalidValidationId exception for Bold."""
+
+class InvalidValidationCode(Exception):
+    """InvalidValidationCode exception for Bold."""
+
+class InvalidValidationResponse(Exception):
+    """InvalidValidationResponse exception for Bold."""
+
+class VerificationNotFound(Exception):
+    """VerificationNotFound exception for Bold."""
+
+class Forbidden(Exception):
+    """Forbidden exception for Bold."""
+
+class TokenMissing(Exception):
+    """TokenMissing exception for Bold."""
