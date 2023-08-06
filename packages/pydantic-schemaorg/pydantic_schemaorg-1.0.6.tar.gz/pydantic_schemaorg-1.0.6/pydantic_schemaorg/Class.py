d4ee0f28b85c28626b8105805abddd6fbc1e909d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:34 +0000

    Add package pydantic-schemaorg/1.0.6/pydantic_schemaorg-1.0.6.tar.gz

---
+++ packages/pydantic-schemaorg/pydantic_schemaorg-1.0.6.tar.gz/pydantic_schemaorg-1.0.6/pydantic_schemaorg/Class.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from typing import List, Optional, Union
+
+
+from pydantic import Field
+from pydantic_schemaorg.Intangible import Intangible
+
+
+class Class(Intangible):
+    """A class, also often called a 'Type'; equivalent to rdfs:Class.
+
+    See: https://schema.org/Class
+    Model depth: 3
+    """
+    type_: str = Field(default="Class", alias='@type', constant=True)
+    supersededBy: Optional[Union[List[Union['Class', 'Enumeration', 'Property', str]], 'Class', 'Enumeration', 'Property', str]] = Field(
+        default=None,
+        description="Relates a term (i.e. a property, class or enumeration) to one that supersedes it.",
+    )
+    
+
+if TYPE_CHECKING:
+    from pydantic_schemaorg.Enumeration import Enumeration
+    from pydantic_schemaorg.Property import Property
