c10ea99daffafd9ed1729224b0776daf86085f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:28 +0000

    Add package cg-api/1.0.3/cg_api-1.0.3.tar.gz

---
+++ packages/cg-api/cg_api-1.0.3.tar.gz/cg_api-1.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='cg_api',
+    version='1.0.3',
+    packages=['cg_api', 'cg_api.api','cg_api.config', 'cg_api.util'],
+    license='MIT',
+    description = 'Python project for CoingeckoAPI',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author = 'Emre Bekar',
+    author_email = 'emre.bekar@hotmail.com.tr',
+    install_requires=['requests'],
+    url = 'https://github.com/emrebekar/cg_api',
+    download_url = 'https://github.com/emrebekar/coingeckoapi/archive/cg_api-1.0.3.tar.gz',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    )
