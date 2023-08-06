96770dfe6c432de0394bc6a30eee67420d3924d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:32 +0000

    Add package stout/0.0.4/stout-0.0.4.tar.gz

---
+++ packages/stout/stout-0.0.4.tar.gz/stout-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setup(
+    name='stout',
+    version='0.0.4',
+    author='suckerpunched',
+    author_email='',
+    description='',
+    long_description='',#long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/suckerpunched/stout',
+    project_urls = {
+        "Bug Tracker": "https://github.com/suckerpunched/stout/issues"
+    },
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+)
+
---
+++ packages/stout/stout-0.0.4.tar.gz/stout-0.0.4/stout/util/__init__.py
@@ -0,0 +1,6 @@
+from . import error
+from . import group
+from . import iter_chunks
+from . import obj
+from . import path_import
+from . import time_diff
